1f43cbcd5f3ad69a296280c530e2c3537fc0a9f5
sh1mj1 <7wlgns@gmail.com>
2023-10-19 18:49:24 +0900

    feat: 사용자의 수를 입력받는다

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,6 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Console.readLine
 import camp.nextstep.edu.missionutils.Randoms.pickNumberInRange
 
 fun main() {
@@ -13,5 +14,23 @@ fun main() {
             answer.add(randomNum)
         }
     }
+
+    // B: 사용자의 수를 입력받는다.
+    print("숫자를 입력해주세요 : ")
+    val input = readLine()
+
+    // B-2 유효성 검증
+    if (input.length != 3) {
+        throw IllegalArgumentException("입력한 수가 3자리가 아닙니다.")
+    }
+    if (input.contains("""[^123456789]""".toRegex())) {
+        throw IllegalArgumentException("유효하지 않은 형식입니다.")
+    }
+
+    val numSet = input.toCharArray().toSet()
+    if (numSet.size < 3) {
+        throw IllegalArgumentException("중복된 수를 가집니다.")
+    }
+
 
 }
