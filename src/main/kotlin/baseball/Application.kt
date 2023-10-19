9fd1ed2ea459aeadd12484cac7bf1ba59c67b1e0
Nohsein <si2238@naver.com>
2023-10-20 01:05:42 +0900

    feat: Add valid number input
    숫자 입력 기능 추가
    입력의 유효성 판단 추가

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,6 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Console
 import camp.nextstep.edu.missionutils.Randoms
 import kotlin.collections.mutableListOf
 
@@ -15,5 +16,21 @@ fun main() {
         }
     }
 
-   
+    println("숫자 야구 게임을 시작합니다.")
+    print("숫자를 입력해주세요 : ")
+    val num = Console.readLine()
+    readNumber(num)
+}
+
+// 입력 유효성 확인
+fun readNumber(num: String): Int {
+    try {
+        val validNum = num.toInt()
+        if (num.length != 3) {
+            throw IllegalArgumentException("숫자는 3자리여야 합니다. ")
+        }
+        return validNum
+    } catch (e: NumberFormatException) {
+        throw IllegalArgumentException("숫자를 입력해 주세요")
+    }
 }
