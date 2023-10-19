89d73d535678d2f25d54506653d76446de6f277f
Seannnn55 <sally99h@gmail.com>
2023-10-20 03:56:15 +0900

    난수 생성 기능 추가

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,17 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
-    TODO("프로그램 구현")
+
+    var ran1: Int = Randoms.pickNumberInRange(1, 9)
+    var ran2: Int = Randoms.pickNumberInRange(1, 9)
+    var ran3: Int = Randoms.pickNumberInRange(1, 9)
+
+    var ans: Int = 100*ran1+10*ran2+ran3
+
+    //컴퓨터가 3자리 수를 생성한다.
+
+
+
 }
