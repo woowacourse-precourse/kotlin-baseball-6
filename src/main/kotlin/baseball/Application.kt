5dade1894f147864378b84e509a85334480f3f2e
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2023-10-19 21:21:28 +0900

    feat: 컴퓨터 수와 입력 값을 비교하여 결과 출력 기능 구현

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -4,18 +4,52 @@ import camp.nextstep.edu.missionutils.*
 
 const val GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다."
 const val NUMBER_INPUT_MESSAGE = "숫자를 입력해주세요 : "
+const val MATCHING_ZERO = "낫싱"
+
 fun main() {
 
     println(GAME_START_MESSAGE)
 
+    var targetNumber = makeNumber()
+    println(targetNumber)
     while(true){
-        val targetNumber = makeNumber()
         val inputNumber = inputNumber()
         if(inputNumber.isEmpty()){
             throw IllegalArgumentException()
         }
+        getInputResult(targetNumber, inputNumber)
     }
 
+}
+
+fun getInputResult(targetNumber: MutableList<Int>, inputNumber: MutableList<Int>):Boolean{
+
+    var strike = 0
+    var ball = 0
+
+    for(i in 0 .. 2){
+        if(targetNumber[i] == inputNumber[i]) strike++
+    }
+    if(targetNumber[0] == inputNumber[1]) ball++
+    if(targetNumber[0] == inputNumber[2]) ball++
+    if(targetNumber[1] == inputNumber[0]) ball++
+    if(targetNumber[1] == inputNumber[2]) ball++
+    if(targetNumber[2] == inputNumber[0]) ball++
+    if(targetNumber[2] == inputNumber[1]) ball++
+
+    if(strike == 0 && ball == 0) {
+        println(MATCHING_ZERO)
+        return false
+    }
+    if(strike == 3){
+        println("${strike}스트라이크")
+        return true
+    }
+
+    if(ball != 0) print("${ball}볼 ")
+    if(strike != 0) print("${strike}스트라이크")
+    println()
+    return false
 }
 
 fun makeNumber(): MutableList<Int>{
@@ -48,6 +82,7 @@ fun inputNumber(): MutableList<Int>{
 fun inputNumberCheck(inputNumber: String): Boolean{
 
     val duplicateCheckArray = BooleanArray(9) // 중복 숫자 확인 배열
+
     for(num in inputNumber){
         if(num !in '1' .. '9'){ // 유효 형식 확인
             return false
