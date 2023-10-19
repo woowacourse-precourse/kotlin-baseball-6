e5ffa175a96dc7b5132db56d664dd7bc99eb251d
hoogje <ajyong316@gmail.com>
2023-10-20 00:21:12 +0900

    feat(GameProgram):컴퓨터의 3자리 숫자를 생성하는 기능 구현

--- src/main/kotlin/baseball/GameProgram.kt
+++ src/main/kotlin/baseball/GameProgram.kt
@@ -1,8 +1,24 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+
 class GameProgram {
 
     fun startGame() {
         println("숫자 야구 게임을 시작합니다.")
+
+        val computerNumbers = createRandomNumbers()
+    }
+
+    private fun createRandomNumbers():List<Int> {
+        val randomNumbers = mutableListOf<Int>()
+
+        while (randomNumbers.size < 3) {
+            val number = Randoms.pickNumberInRange(1,9)
+            if (!randomNumbers.contains(number)) {
+                randomNumbers.add(number)
+            }
+        }
+        return randomNumbers
     }
 }
