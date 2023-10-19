698c41b1b552dc928946ddd3b6d95bb68e57f496
somin <salth0601@gmail.com>
2023-10-20 00:26:35 +0900

    feat: 사용자 숫자 입력 받기 구현
    - 입력 문자열을 MutableList<Int>로 변환
    - 유효성 검사 기능 추후 구현 예정

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,6 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Console
 import camp.nextstep.edu.missionutils.Randoms
 
 fun main() {
@@ -9,6 +10,7 @@ fun main() {
 
 fun startBaseballGame() {
     val answer = creatRandomNumberList()
+    val user = getUserInput()
 }
 
 fun creatRandomNumberList(): MutableList<Int> {
@@ -19,4 +21,15 @@ fun creatRandomNumberList(): MutableList<Int> {
             numbers.add(randomNumber)
     }
     return numbers
+}
+
+fun getUserInput(): MutableList<Int> {
+    print("숫자를 입력해주세요 : ")
+    var userNumber = Console.readLine().toInt()
+    val userNumberList = mutableListOf<Int>()
+    repeat(3) {
+        userNumberList.add(0, userNumber % 10)
+        userNumber /= 10
+    }
+    return userNumberList
 }
