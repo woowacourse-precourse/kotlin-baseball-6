b74cf378b6624b3b60b139bfbf73517c6f4ebbd8
dpcks0509 <dpcks0509@gmail.com>
2023-10-19 22:14:36 +0900

    기능3. 정답 확인

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -10,6 +10,8 @@ fun main() {
     val computer: List<Int> = generateRandomThreeNumber()
 
     val user: List<Int> = inputThreeAnswerNumber()
+
+    checkAnswer(computer, user)
 }
 
 /*
@@ -59,4 +61,46 @@ fun inputThreeAnswerNumber(): List<Int> {
     }
 
     return answerList
+}
+
+/*
+    기능3. 정답 확인
+    : 컴퓨터는 입력한 숫자에 대한 결과를 출력한다.
+    같은 수가 같은 자리에 있으면 스트라이크, 다른 자리에 있으면 볼, 같은 수가 전혀 없으면 낫싱이란 힌트를 얻는다.
+    이 같은 과정을 반복해 컴퓨터가 선택한 3개의 숫자를 모두 맞히면 게임이 종료된다.
+ */
+fun checkAnswer(computer: List<Int>, user: List<Int>) {
+    var ball = 0
+    var strike = 0
+
+    for (i in 0..2) {
+        if (computer[i] == user[i]) {
+            strike += 1
+        }
+
+        if (user.contains(computer[i])) {
+            ball += 1
+        }
+    }
+
+    if (strike == 3) {
+        println("3스트라이크")
+        println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
+    }
+
+    if (ball == 0 && strike == 0) {
+        println("낫싱")
+    }
+
+    if (ball != 0 && strike != 0) {
+        println("${ball}볼 ${strike}스트라이크")
+    }
+
+    if (ball != 0) {
+        println("${ball}볼")
+    }
+
+    if (strike != 0) {
+        println("${strike}스트라이크")
+    }
 }
