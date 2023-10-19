e0165f5b6aec7b1abd56757332be6d10309798ac
dpcks0509 <dpcks0509@gmail.com>
2023-10-19 18:36:49 +0900

    기능1. 랜덤숫자 3개 생성

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,27 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
-    TODO("프로그램 구현")
+    println("숫자 야구 게임을 시작합니다.")
+
+    val computer: List<Int> = generateRandomThreeNumber()
+}
+
+/*
+    기능1. 랜덤숫자 3개 생성
+    : 컴퓨터는 1에서 9까지 서로 다른 임의의 수 3개를 선택한다.
+ */
+fun generateRandomThreeNumber(): List<Int> {
+    val randomList = mutableListOf<Int>()
+
+    while (randomList.size < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (!randomList.contains(randomNumber)) {
+            randomList.add(randomNumber)
+        }
+    }
+
+    return randomList
 }
