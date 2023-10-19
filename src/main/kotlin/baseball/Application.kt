8b019fa2f7c931b08eb15b3710c6fa54e757de27
Nohsein <si2238@naver.com>
2023-10-20 00:34:58 +0900

    feat: Add computer random number
    컴퓨터가 생각한  서로 다른 3자리 수 생성

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,19 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+import kotlin.collections.mutableListOf
+
+
 fun main() {
-    TODO("프로그램 구현")
+    // 3자리 랜덤 숫자 생성
+    // computer[0]은 첫번째 자리 [2]는 세번째 자리
+    val computer = mutableListOf<Int>()
+    while (computer.size < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (!computer.contains(randomNumber)) {
+            computer.add(randomNumber)
+        }
+    }
+
+   
 }
