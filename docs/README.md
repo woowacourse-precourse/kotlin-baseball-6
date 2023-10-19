14ac6dbaa3228c22164abcb15c0c3bda77d61ef2
Dongsu Lee <dongsu0717@nate.com>
2023-10-20 06:24:06 +0900

    ..

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,9 @@
--컴퓨터 랜덤수 정하기 #RandomNumber
--숫자 맞추기 3개(이상한 값 입력시 -IllegalArgumentException 발생시킨후 앱종료)  #GuessNumber()        
+-컴퓨터 랜덤수 정하기 #ComputerNumber      
+-숫자 맞추기 3개(이상한 값 입력시 -IllegalArgumentException 발생시킨후 앱종료) #GuessNumber()        
 -입력 값에 대한 결과출력(1스,1볼 /다 맞히면 종료)	 #Result()  
--게임 끝난후 재시작, 종료를 구분하는 1,2 		#GameRestart()   
+-게임 끝난후 재시작, 종료를 구분하는 1,2 		#GameRestart()  
+
+- 컴퓨터 랜덤 수 정하기 pickNumberInRange() 사용, 함수 #randomNumber
+- 입력받기 readline()사용, 1~9숫자 and 중복x and 3자리, 아니면 IllegalArgumentException 발생, 클래스 #InputNumber
+- 비교하기 같은자리 스트라이크,  다른자리볼, 없으면 낫싱, 3스트라이크시 축하말 #CompareNumber  
+- 다맞추면 재시작 or 종료 #reOrStop
--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,13 +1,29 @@
 package baseball
-
 import camp.nextstep.edu.missionutils.Randoms
 
-
+/*
 fun main() {
     println("숫자 야구 게임을 시작합니다.")
     var computerNumber = RandomNumber()
+    print("숫자를 입력해주세요 :")
+    var guessNumber = readLine()
+    var result = Result(guessNumber)
+    if(check(guessNumber)){
+        println("$guessNumber")
+    }
+    else{
+        throw IllegalArgumentException("종료 되었습니다.")
+    }
+
 
 }
+fun check(guessNumber: String?): Boolean {
+    if (guessNumber == null || guessNumber.length != 3) return false
+    if (guessNumber.contains('0')) return false
+
+    return guessNumber[0] != guessNumber[1] && guessNumber[0] != guessNumber[2] && guessNumber[1] != guessNumber[2]
+}
+
 class RandomNumber(){       // 랜덤으로 1~9까지중 3개 뽑기
     val computer = mutableListOf<Int>()
     init {
@@ -19,17 +35,96 @@ class RandomNumber(){       // 랜덤으로 1~9까지중 3개 뽑기
         }
     }
 }
-class GuessNumber(){
-    fun choice(){
+
+
+class Result(var message : Int) {
+    fun resultStrike() {
+        while (true) {
+            if(){
+
+            }
+
+        }
+    }
+}
+*/
+
+var strikeCount : Int = 0
+var ballCount : Int = 0
+fun main(){
+    println("숫자 야구 게임을 시작합니다.")
+    val pick = randNumber()
+    println(pick)
+    do{
         print("숫자를 입력해주세요 :")
-        val choiceNumber = readLine()
+        var number = inputNumber()
+
+        var cN = compareNumber()
+        cN
+    } while(strikeCount != 3)
+
+}
+fun randNumber(): List<Int> {
+    var computer = mutableListOf<Int>()
+    while (computer.size < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (!computer.contains(randomNumber)) {
+            computer.add(randomNumber)
+        }
+    }
+    return computer
+}
+fun inputNumber(): String {            // 반환값 정수다
+    var input = readLine()!!.toString() // 이변수는 null이 아니다
+    if(input.contains('0')
+        || input.length !=3
+        || (input[0] == input[1] && input[1] == input[2] && input[2] == input[0])) {
+        throw IllegalArgumentException("종료 되었습니다.")
+    }
+    return input
+}
+fun compareNumber(){
+    var aa = randNumber().toString()
+    var bb = inputNumber()
+    for(i in 0..2){
+        for(j in 0..2){
+            if (bb[j] == aa[i] && bb[j] == aa[j]){
+                strikeCount++
+                continue
+            }else if(bb[j] == aa[i]){
+                ballCount++
+                continue
+            }else{
+
+            }
+        }
+
+        if(strikeCount !=0 && ballCount !=0){
+            println("${ballCount}볼 ${strikeCount}스트라이크")
+        }else if(strikeCount != 0 && ballCount == 0){
+            println("${strikeCount}스트라이크")
+        }else if(strikeCount == 0 && ballCount != 0){
+            println("${ballCount}볼")
+        }else if(strikeCount ==0 && ballCount ==0){
+            println("낫싱")
+        }else{
+
+        }
     }
 }
 
-class Result(){
+fun reOrStop(){
+    println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+    var choice = readLine().toString()
+    if(choice == "1"){
+        //프로그램 종료
+    }else if(choice == "2"){
+        //프로그램
+    }else{
 
+    }
 }
 
-class GameRestart(){
 
-}
+
+
