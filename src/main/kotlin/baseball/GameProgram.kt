a6513f50086668d806e5100f9cb2b9b460846f82
hoogje <ajyong316@gmail.com>
2023-10-20 02:46:11 +0900

    feat(GameProgram):사용자로부터 답을 입력 받을 때 문구 출력 기능 구현

--- src/main/kotlin/baseball/GameProgram.kt
+++ src/main/kotlin/baseball/GameProgram.kt
@@ -19,6 +19,7 @@ class GameProgram {
         } while (hintMessage != "3스트라이크")
 
         println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
+        println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
     }
 
     private fun createRandomNumbers():List<Int> {
