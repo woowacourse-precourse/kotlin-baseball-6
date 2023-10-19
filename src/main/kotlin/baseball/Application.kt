650de721b5fc7f5bfe0c2c3ed113c093b05ee8ae
dpcks0509 <dpcks0509@gmail.com>
2023-10-19 23:22:43 +0900

    기능4. 게임 재시작 및 종료, 기능1 리팩토링 - return방식 수정

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -2,23 +2,31 @@ package baseball
 
 import camp.nextstep.edu.missionutils.Randoms
 import camp.nextstep.edu.missionutils.Console
-import java.lang.Exception
+
+var computer: List<Int> = emptyList()
 
 fun main() {
     println("숫자 야구 게임을 시작합니다.")
 
-    val computer: List<Int> = generateRandomThreeNumber()
+    generateRandomThreeNumber()
 
-    val user: List<Int> = inputThreeAnswerNumber()
+    var start = true
+    while (start) {
+        val user: List<Int> = inputThreeAnswerNumber()
 
-    checkAnswer(computer, user)
+        val result = checkAnswer(computer, user)
+
+        if (result) {
+            start = gameRestartOrOver()
+        }
+    }
 }
 
 /*
     기능1. 랜덤숫자 3개 생성
     : 컴퓨터는 1에서 9까지 서로 다른 임의의 수 3개를 선택한다.
  */
-fun generateRandomThreeNumber(): List<Int> {
+fun generateRandomThreeNumber() {
     val randomList = mutableListOf<Int>()
 
     while (randomList.size < 3) {
@@ -28,7 +36,7 @@ fun generateRandomThreeNumber(): List<Int> {
         }
     }
 
-    return randomList
+    computer = randomList
 }
 
 /*
@@ -48,6 +56,10 @@ fun inputThreeAnswerNumber(): List<Int> {
     for (input in inputList) {
         try {
             val number = input.toString().toInt()
+
+            if (number == 0) {
+                throw IllegalArgumentException("1~9사이의 숫자만 입력해주세요.")
+            }
 
             if (answerList.contains(number)) {
                 throw IllegalArgumentException("중복된 숫자는 입력할 수 없습니다.")
@@ -69,13 +81,14 @@ fun inputThreeAnswerNumber(): List<Int> {
     같은 수가 같은 자리에 있으면 스트라이크, 다른 자리에 있으면 볼, 같은 수가 전혀 없으면 낫싱이란 힌트를 얻는다.
     이 같은 과정을 반복해 컴퓨터가 선택한 3개의 숫자를 모두 맞히면 게임이 종료된다.
  */
-fun checkAnswer(computer: List<Int>, user: List<Int>) {
+fun checkAnswer(computer: List<Int>, user: List<Int>): Boolean {
     var ball = 0
     var strike = 0
 
     for (i in 0..2) {
         if (computer[i] == user[i]) {
             strike += 1
+            continue
         }
 
         if (user.contains(computer[i])) {
@@ -86,21 +99,50 @@ fun checkAnswer(computer: List<Int>, user: List<Int>) {
     if (strike == 3) {
         println("3스트라이크")
         println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
+        return true
     }
 
     if (ball == 0 && strike == 0) {
         println("낫싱")
+        return false
     }
 
     if (ball != 0 && strike != 0) {
         println("${ball}볼 ${strike}스트라이크")
+        return false
     }
 
     if (ball != 0) {
         println("${ball}볼")
+        return false
     }
 
     if (strike != 0) {
         println("${strike}스트라이크")
+        return false
     }
+
+    return false
+}
+
+/*
+    기능4. 게임 재시작 및 종료
+    : 게임을 종료한 후 게임을 다시 시작하거나 완전히 종료할 수 있다.
+    게임이 끝난 경우 재시작/종료를 구분하는 1과 2 중 하나의 수
+ */
+fun gameRestartOrOver(): Boolean {
+    println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+
+    val decision = Console.readLine()
+
+    if (decision == "1") {
+        generateRandomThreeNumber()
+        return true
+    }
+
+    if (decision == "2") {
+        return false
+    }
+
+    throw IllegalArgumentException("숫자 1과 2 중 하나의 수만 입력해주세요.")
 }
