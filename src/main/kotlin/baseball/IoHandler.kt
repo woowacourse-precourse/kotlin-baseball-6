561ca4096b570f5ce6c4628e04e63dee6e9b6858
DoDoBest <send1684@gmail.com>
2023-10-19 20:29:43 +0900

    fix: 0을 입력 받는 경우에 예외처리

--- src/main/kotlin/baseball/IoHandler.kt
+++ src/main/kotlin/baseball/IoHandler.kt
@@ -6,6 +6,7 @@ import camp.nextstep.edu.missionutils.Console
 class IoHandler {
 
     private val validInputAfterGame = listOf("1", "2")
+    private val validInputWhileGame = (1..10).map { (it + ASCII_0_CODE).toChar() }
 
     fun getInput(inputType: InputType): String {
         val input = Console.readLine()
@@ -26,7 +27,7 @@ class IoHandler {
         val inputNum = mutableSetOf<Char>()
 
         input.forEach {
-            if (it.isDigit().not() || inputNum.contains(it) || inputNum.size > 2) {
+            if (it !in validInputWhileGame || inputNum.contains(it) || inputNum.size > 2) {
                 throw IllegalArgumentException()
             }
             inputNum.add(it)
@@ -38,5 +39,9 @@ class IoHandler {
             return
         }
         throw IllegalArgumentException()
+    }
+
+    companion object {
+        private const val ASCII_0_CODE = 48
     }
 }
