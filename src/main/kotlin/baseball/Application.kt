6d2b03777a0d4d9a02dc114ab81fe63c0aa59dd1
sh1mj1 <7wlgns@gmail.com>
2023-10-19 18:17:05 +0900

    feat: 컴퓨터가 맞혀야 할 정답을 생성한다

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,17 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms.pickNumberInRange
+
 fun main() {
-    TODO("프로그램 구현")
+    println("숫자 야구 게임을 시작합니다.")
+
+    // A. 컴퓨터는 사용자가 맞힐 정답을 생성한다.
+    val answer = mutableListOf<Int>()
+    while (answer.size < 3) {
+        val randomNum = pickNumberInRange(1, 9)
+        if (!answer.contains(randomNum)) {
+            answer.add(randomNum)
+        }
+    }
+
 }
