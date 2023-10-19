3365f8bc5e1fdba59180affbe2cdb753bb448ebb
Joontae <chlwnsxo0@gmail.com>
2023-10-20 03:26:42 +0900

    resultString()

--- src/main/kotlin/baseball/Computer.kt
+++ src/main/kotlin/baseball/Computer.kt
@@ -30,4 +30,17 @@ class Computer {
             }
         }
     }
+    private fun resultString(strike: Int, ball: Int) : String {
+        if(strike == 0 && ball == 0)
+            return "낫싱"
+        else if(strike == 3){
+            isEnd = true
+            return "3스트라이크"
+        }
+        else if(strike == 0)
+            return "${ball}볼"
+        else if(ball == 0)
+            return "${strike}스트라이크"
+        return "${ball}볼 ${strike}스트라이크"
+    }
 }
