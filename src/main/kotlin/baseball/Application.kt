7ef1a7379e20728b8392e62e8c0b92c491bcd6a5
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2023-10-19 23:10:06 +0900

    docs: README.md 내 예외상황 추가

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -12,100 +12,101 @@ fun main() {
 
     println(GAME_START_MESSAGE)
     var targetNumber = makeNumber()
-    while(true){
+
+    while (true) {
         val inputNumber = inputNumber()
-        if(inputNumber.isEmpty()){
+        if (inputNumber.isEmpty()) {
             throw IllegalArgumentException()
         }
-        if(getInputResult(targetNumber, inputNumber)){
-            if(gameRestartCheck()){
+        if (getInputResult(targetNumber, inputNumber)) {
+            if (gameRestartCheck()) {
                 targetNumber = makeNumber()
                 continue
             }
             break
         }
     }
-
+    Console.close()
 }
 
-fun gameRestartCheck(): Boolean{
+fun gameRestartCheck(): Boolean {
 
     println(GAME_RESTART_MESSAGE)
-    var restart = Console.readLine()
-    if(restart != "1" && restart != "2") throw IllegalArgumentException()
+    val restart = Console.readLine()
+    if (restart != "1" && restart != "2") throw IllegalArgumentException()
     return restart == "1"
 }
 
-fun getInputResult(targetNumber: MutableList<Int>, inputNumber: MutableList<Int>):Boolean{
+fun getInputResult(targetNumber: MutableList<Int>, inputNumber: MutableList<Int>): Boolean {
 
     var strike = 0
     var ball = 0
 
-    for(i in 0 .. 2){
-        if(targetNumber[i] == inputNumber[i]) strike++
+    for (i in 0..2) {
+        if (targetNumber[i] == inputNumber[i]) strike++
     }
-    if(targetNumber[0] == inputNumber[1]) ball++
-    if(targetNumber[0] == inputNumber[2]) ball++
-    if(targetNumber[1] == inputNumber[0]) ball++
-    if(targetNumber[1] == inputNumber[2]) ball++
-    if(targetNumber[2] == inputNumber[0]) ball++
-    if(targetNumber[2] == inputNumber[1]) ball++
+    if (targetNumber[0] == inputNumber[1]) ball++
+    if (targetNumber[0] == inputNumber[2]) ball++
+    if (targetNumber[1] == inputNumber[0]) ball++
+    if (targetNumber[1] == inputNumber[2]) ball++
+    if (targetNumber[2] == inputNumber[0]) ball++
+    if (targetNumber[2] == inputNumber[1]) ball++
 
-    if(strike == 0 && ball == 0) {
+    if (strike == 0 && ball == 0) {
         println(MATCHING_ZERO)
         return false
     }
-    if(strike == 3){
+    if (strike == 3) {
         println(MATCHING_ALL)
         return true
     }
 
-    if(ball != 0) print("${ball}볼 ")
-    if(strike != 0) print("${strike}스트라이크")
+    if (ball != 0) print("${ball}볼 ")
+    if (strike != 0) print("${strike}스트라이크")
     println()
     return false
 }
 
-fun makeNumber(): MutableList<Int>{
+fun makeNumber(): MutableList<Int> {
 
     val computerNumber = mutableListOf<Int>()
 
-    while(computerNumber.size < 3){
+    while (computerNumber.size < 3) {
         val randomNumber = Randoms.pickNumberInRange(1, 9)
-        if(!computerNumber.contains(randomNumber)){
+        if (!computerNumber.contains(randomNumber)) {
             computerNumber.add(randomNumber)
         }
     }
     return computerNumber
 }
 
-fun inputNumber(): MutableList<Int>{
+fun inputNumber(): MutableList<Int> {
 
     val playNumber = mutableListOf<Int>()
     print(NUMBER_INPUT_MESSAGE)
     val inputNumber = Console.readLine()
-    if(inputNumberCheck(inputNumber)){
+    if (inputNumberCheck(inputNumber)) {
         inputNumber.forEach {
-            playNumber.add(it-'0')
+            playNumber.add(it - '0')
         }
         return playNumber
     }
     throw IllegalArgumentException()
 }
 
-fun inputNumberCheck(inputNumber: String): Boolean{
+fun inputNumberCheck(inputNumber: String): Boolean {
 
-    if(inputNumber.length != 3) return false
+    if (inputNumber.length != 3) return false
     val duplicateCheckArray = BooleanArray(9) // 중복 숫자 확인 배열
 
-    for(num in inputNumber){
-        if(num !in '1' .. '9'){ // 유효 형식 확인
+    for (num in inputNumber) {
+        if (num !in '1'..'9') { // 유효 형식 확인
             return false
         }
-        if(duplicateCheckArray[num-'1']){ // 중복 숫자 확인
+        if (duplicateCheckArray[num - '1']) { // 중복 숫자 확인
             return false
         }
-        duplicateCheckArray[num-'1'] = true
+        duplicateCheckArray[num - '1'] = true
     }
     return true
 }
