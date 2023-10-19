aaea8b92f1d48e4818df0633a92fe428139f1f01
hoogje <ajyong316@gmail.com>
2023-10-20 00:56:12 +0900

    feat(GameProgram):사용자가 잘못된 값을 입력할 경우 IIllegalArgumentException을 발생 시키는 기능 구현

--- src/main/kotlin/baseball/GameProgram.kt
+++ src/main/kotlin/baseball/GameProgram.kt
@@ -8,6 +8,7 @@ class GameProgram {
     fun startGame() {
         println("숫자 야구 게임을 시작합니다.")
         val computerNumbers = createRandomNumbers()
+
         print("숫자를 입력해주세요 : ")
     }
 
@@ -26,5 +27,13 @@ class GameProgram {
     private fun readUserInput():String {
         val userInput = Console.readLine()
         return userInput.trim()
+    }
+
+    private fun validateUserInput(userInput : String) {
+        for (c in userInput.toCharArray()) {
+            if (!c.isDigit()) throw IllegalArgumentException("숫자를 입력 해야 합니다.")
+        }
+        if (userInput.length != 3) throw IllegalArgumentException("3개의 숫자를 입력 해야 합니다.")
+        if (userInput[0] == userInput[1] || userInput[0] == userInput[2] || userInput[1] == userInput[2]) throw IllegalArgumentException("서로 중복되지 않는 숫자를 입력 해야 합니다.")
     }
 }
