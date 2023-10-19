47e11c03b2c3f30c748943af339c39a96e99dc2b
somin <salth0601@gmail.com>
2023-10-20 00:11:04 +0900

    feat: 랜덤 숫자 생성 함수 구현
    - 중복되지 않는 숫자들로 이뤄짐
    - 범위는 1부터 9, 길이는 3
    - MutableList<Int>로 반환

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,9 +1,22 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
     println("숫자 야구 게임을 시작합니다.")
     startBaseballGame()
 }
 
 fun startBaseballGame() {
+    val answer = creatRandomNumberList()
+}
+
+fun creatRandomNumberList(): MutableList<Int> {
+    val numbers = mutableListOf<Int>()
+    while (numbers.size < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (!numbers.contains(randomNumber))
+            numbers.add(randomNumber)
+    }
+    return numbers
 }
