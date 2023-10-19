a28b8c988f65eaa0742d04fde1eed0433c9e9955
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2023-10-19 17:34:11 +0900

    feat: 1 ~ 9 까지의 서로 다른 임의의 수를 가진 3자리수 생성기능 구현

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,23 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
-    TODO("프로그램 구현")
+
+    println("숫자 야구 게임을 시작합니다.")
+
+    while(true){
+        val targetNumber = makeNumber()
+    }
+}
+
+fun makeNumber():MutableList<Int>{
+    val computerNumber = mutableListOf<Int>()
+    while(computerNumber.size < 3){
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if(!computerNumber.contains(randomNumber)){
+            computerNumber.add(randomNumber)
+        }
+    }
+    return computerNumber
 }
