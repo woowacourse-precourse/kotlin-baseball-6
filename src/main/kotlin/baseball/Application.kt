fc258a76542d1297a9fb4920709b7191141b225e
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2023-10-19 22:25:44 +0900

    feat: 게임 종료 및 재시작 기능 구현

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -5,21 +5,35 @@ import camp.nextstep.edu.missionutils.*
 const val GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다."
 const val NUMBER_INPUT_MESSAGE = "숫자를 입력해주세요 : "
 const val MATCHING_ZERO = "낫싱"
+const val MATCHING_ALL = "3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료"
+const val GAME_RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
 
 fun main() {
 
     println(GAME_START_MESSAGE)
-
     var targetNumber = makeNumber()
-    println(targetNumber)
     while(true){
         val inputNumber = inputNumber()
         if(inputNumber.isEmpty()){
             throw IllegalArgumentException()
         }
-        getInputResult(targetNumber, inputNumber)
+        if(getInputResult(targetNumber, inputNumber)){
+            if(gameRestartCheck()){
+                targetNumber = makeNumber()
+                continue
+            }
+            break
+        }
     }
 
+}
+
+fun gameRestartCheck(): Boolean{
+
+    println(GAME_RESTART_MESSAGE)
+    var restart = Console.readLine()
+    if(restart != "1" && restart != "2") throw IllegalArgumentException()
+    return restart == "1"
 }
 
 fun getInputResult(targetNumber: MutableList<Int>, inputNumber: MutableList<Int>):Boolean{
@@ -42,7 +56,7 @@ fun getInputResult(targetNumber: MutableList<Int>, inputNumber: MutableList<Int>
         return false
     }
     if(strike == 3){
-        println("${strike}스트라이크")
+        println(MATCHING_ALL)
         return true
     }
 
@@ -69,18 +83,19 @@ fun inputNumber(): MutableList<Int>{
 
     val playNumber = mutableListOf<Int>()
     print(NUMBER_INPUT_MESSAGE)
-
     val inputNumber = Console.readLine()
     if(inputNumberCheck(inputNumber)){
         inputNumber.forEach {
             playNumber.add(it-'0')
         }
+        return playNumber
     }
-    return playNumber
+    throw IllegalArgumentException()
 }
 
 fun inputNumberCheck(inputNumber: String): Boolean{
 
+    if(inputNumber.length != 3) return false
     val duplicateCheckArray = BooleanArray(9) // 중복 숫자 확인 배열
 
     for(num in inputNumber){
