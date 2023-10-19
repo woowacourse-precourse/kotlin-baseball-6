eb5142ae5454173212ccd751903b6e4d93df128b
Hong <bayyyy@naver.com>
2023-10-19 19:12:23 +0900

    [Feat]: 숫자생성 함수 추가

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,20 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
-    TODO("프로그램 구현")
+
+}
+
+val BASE_BALL_SIZE = 3
+fun createBaseBallNumber(): List<Int>{
+    val computer = mutableListOf<Int>()
+    while (computer.size < BASE_BALL_SIZE) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (!computer.contains(randomNumber)) {
+            computer.add(randomNumber)
+        }
+    }
+    return computer
 }
