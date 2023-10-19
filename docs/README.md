a30403d3c103cbb364fd168d1f05596d393e4540
Dongsu Lee <dongsu0717@nate.com>
2023-10-19 21:41:28 +0900

    while init 오류해결

---
+++ docs/README.md
@@ -0,0 +1,4 @@
+-컴퓨터 랜덤수 정하기 #RandomNumber
+-숫자 맞추기 3개(이상한 값 입력시 -IllegalArgumentException 발생시킨후 앱종료)  #GuessNumber()        
+-입력 값에 대한 결과출력(1스,1볼 /다 맞히면 종료)	 #Result()  
+-게임 끝난후 재시작, 종료를 구분하는 1,2 		#GameRestart()   
--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,6 +1,35 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+
+
 fun main() {
-    var a =2
-    print(a)
+    println("숫자 야구 게임을 시작합니다.")
+    var computerNumber = RandomNumber()
+
+}
+class RandomNumber(){       // 랜덤으로 1~9까지중 3개 뽑기
+    val computer = mutableListOf<Int>()
+    init {
+        while (computer.size < 3) {
+            val randomNumber = Randoms.pickNumberInRange(1, 9)
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber)
+            }
+        }
+    }
+}
+class GuessNumber(){
+    fun choice(){
+        print("숫자를 입력해주세요 :")
+        val choiceNumber = readLine()
+    }
+}
+
+class Result(){
+
+}
+
+class GameRestart(){
+
 }
