82fd1b5a825c155b00407eeeb89541bc16828b0a
lh99j <grand2181@gmail.com>
2023-10-20 13:27:40 +0900

    feat: 사용자의 게임 재시작/종료 입력

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -96,6 +96,12 @@ fun printGameEndMessage() {
     println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
 }
 
+fun decideGame() {
+    println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+
+    val userInput = Console.readLine()
+}
+
 var gameFlag = true
 
 fun main() {
