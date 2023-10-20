fb748e7333298bdc3c883b53e0ad7d08f69ad5db
dohun_ <35682247+parkdohuni@users.noreply.github.com>
2023-10-20 16:37:51 +0900

    Feat (inputNumber()) : 입력 함수 구현

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,7 +1,14 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
     startGame()
+    inputNumber()
 }
 
 fun startGame() = println("숫자 야구 게임을 시작합니다.")
+fun inputNumber() {
+    print("숫자를 입력해주세요 : ")
+    val inputNumber = Console.readLine()
+}
