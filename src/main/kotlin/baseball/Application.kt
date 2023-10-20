d10a4f888ae017b5b730ba7eafc0bc30b6e99425
gretea5 <gretea4489@gmail.com>
2023-10-20 22:25:08 +0900

    feat(Application.kt): 컴퓨터의 수 생성 기능

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,26 @@
 package baseball
+import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
 
 fun main() {
-    TODO("프로그램 구현")
+    gamePlay()
 }
+//야구 게임 진행
+fun gamePlay() {
+    println("숫자 야구 게임을 시작합니다.")
+    val computer = makeComputerNumber()
+    println("${computer[0]} ${computer[1]} ${computer[2]}")
+}
+
+//컴퓨터의 수를 생성하는 함수
+fun makeComputerNumber() : MutableList<Int> {
+    val computer = mutableListOf<Int>()
+    while (computer.size < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (!computer.contains(randomNumber)) {
+            computer.add(randomNumber)
+        }
+    }
+    return computer
+}
+
