9fdbd8999627a27eaf09ad17f3f61ed3889d43a4
fheodaos <yun090405@gmail.com>
2023-10-21 02:41:42 +0900

    숫자야구(1차 구현)
    게임 시작 시 서로 겹치지 않는 세자리 수 랜덤 생성을 위해 Randoms.pickUniqueNumbersInRange() 사용
    
    입력 값이 세 자리의 수가 아닌 경우와 겹치는 수가 존재할 시 IllegalArgumentException 오류 출력
    
    입력 값이 정답이 아닐 시 입력 값과 정답 값의 차이를 찾아 스트라이크와 볼 측정 후 출력
    
    입력 값이 정답일 시 flag 변수를 이용해 while문 탈출
    
    게임 종료 후 입력이 1 혹은 2가 아닐 시 IllegalArgumentException 오류 출력

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -4,47 +4,90 @@ import camp.nextstep.edu.missionutils.Console
 import camp.nextstep.edu.missionutils.Randoms
 import kotlin.random.Random
 
+fun check(str_num : String): Boolean{
+    for(item in str_num) {
+        var cnt = 0
+        for(item_2 in str_num){
+            if(item == item_2)
+                cnt++;
+        }
+        if(cnt > 1)
+            return true
+    }
+    return false
+}
+
 fun main() {
 
     println("숫자 야구 게임을 시작합니다.")
     while(true){
-        //var computer_number = Randoms.pickUniqueNumbersInRange(1,9,3)
-        var computer_number = arrayOf(1,2,3)
+        var rand = Randoms.pickUniqueNumbersInRange(1,9,3)
 
         var sum = 0;
-        for(item in computer_number){
+        for(item in rand){
             sum += item
             sum *= 10
         }
         sum /= 10
 
+        var rand_num = sum.toString()
+
         while(true){
+            var flag = false
             print("숫자를 입력해주세요 : ")
-            var scan : Int = Console.readLine().toInt()
+            var str_num = Console.readLine()
+            if(str_num.toInt() > 999 || str_num.toInt() < 100 || check(str_num)){
+                throw IllegalArgumentException("입력 오류")
+            }
+            while(true) {
 
-            if(scan > 999 || scan < 100){
-                if(scan < 0){
-                    println("양수를 입력해주세요")
-                    continue
+                var strike = 0
+                var ball = 0
+
+                if (!str_num.equals(rand_num)) {
+                    for(item in str_num) {
+                        if (rand_num.indexOf(item) == str_num.indexOf(item)) { //인덱스 값이 같을 때 스트라이크 1 증가
+                            strike++
+                        } else {
+                            if (rand_num.contains(item)) {//인덱스 값이 다르나 랜덤 수에 값이 존재 시 볼 1 증가
+                                ball++
+                            }
+                        }
+                    }
+                    if(strike == 0 && ball == 0){
+                        println("낫싱")
+                    }else{
+                        if(ball > 0){
+                            print("${ball}볼 ")
+                        }
+                        if(strike > 0){
+                            print("${strike}스트라이크")
+                        }
+                        println()
+                    }
+
+                    print("숫자를 입력해주세요 : ")
+                    str_num = Console.readLine()
+                    if(str_num.toInt() > 999 || str_num.toInt() < 100 || check(str_num)){
+                        throw IllegalArgumentException("입력 오류")
+                    }
+
+                } else {
+                    println("정답입니다.")
+                    flag = true
+                    break
                 }
-                println("세자리의 숫자만 입력해주세요")
-                continue
             }
-
-            if(scan != sum){
-                println("틀렸습니다.")
-            }else{
-                println("정답입니다.")
-                break;
-            }
+            if(flag)
+                break
         }
-
-        println("3개의 숫자를 모두 맞히셨습니다! 게임종료 \n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+        println("3개의 숫자를 모두 맞히셨습니다! 게임 종료 \n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
         var scan : Int = Console.readLine().toInt()
-
-        if(scan != 1){
-            break;
-        }
+        if(scan == 1)
+        else if(scan == 2)
+            break
+        else
+            throw IllegalArgumentException("입력 오류")
     }
 
 
