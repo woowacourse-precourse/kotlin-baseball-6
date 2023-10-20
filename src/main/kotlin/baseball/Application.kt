fc81506081a47e1c3fbae1a264ac8439eb266692
jihyeonbaem <jihyeonbae424@gmail.com>
2023-10-20 18:12:48 +0900

    feat(computerNumber): 컴퓨터 숫자 생성하기
    구현할 기능 목록에 컴퓨터 숫자 생성을 추가해 추가적인 변경사항들이 생김
    1. startGame()에서 return computerNumber()로 수정함
    2. userInputNumber에서 userNumber로 함수명을 수정함

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,4 +1,6 @@
 package baseball
+import camp.nextstep.edu.missionutils.Randoms
+import kotlin.collections.mutableListOf as mutableListOf
 
 fun main() {
     startGame()
@@ -6,11 +8,23 @@ fun main() {
 
 fun startGame() {
     println("숫자 야구 게임을 시작합니다.")
-    return userInputNumber()
+    return computerNumber()
 }
 
-fun userInputNumber() {
-//    println("숫자를 입력해주세요 : ")
+fun computerNumber() {
+    val computer = mutableListOf<Int>()
+    while (computer.size < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (!computer.contains(randomNumber)) {
+            computer.add(randomNumber)
+        }
+    }
+    println(computer)
+    return userNumber()
+}
+
+fun userNumber() {
+
 }
 
 
