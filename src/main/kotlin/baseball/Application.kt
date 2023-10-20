9f398a53b8e5b9e3fd1148f203cd752c5c0c4922
ekeonn <52497657+ekeonn@users.noreply.github.com>
2023-10-21 06:38:23 +0900

    Refactor(change variable name)
    변수명이 어색한거같아 변경해주었습니다

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -31,8 +31,8 @@ fun main() {
                 println("3스트라이크\n" + "3개의 숫자를 모두 맞히셨습니다! 게임 종료")
                 println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
 
-                val inputOneIsPlayTwoIsDone = Console.readLine()
-                when (inputOneIsPlayTwoIsDone) {
+                val inputOneIsPlayTwoIsEnd = Console.readLine()
+                when (inputOneIsPlayTwoIsEnd) {
                     "1" -> computerRandomNumber = getRandomNumber()
                     "2" -> {
                         println("게임 종료")
