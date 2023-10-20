62d799921c5a6a3a4809b013f476ae22706f5341
V. hs Sin <cbrnt1210@naver.com>
2023-10-20 16:24:35 +0900

    User_response 유저에게 입력을 받아서 반환함 길이가 3이 아니면 에러 Answer이라는 데이터 클래스로 값을 전달하고 비교

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,25 @@
 package baseball
 
+//필요한 함수 목록
+//1. 문제를 난수로 생성하기, seed값이 인자로
+//2. 사용자로 할지 ai로 할지 결정
+//3. 사용자의 경우 숫자를 3개 입력 받음
+//4. 그 입력과 문제를 비교 후에 정보 출력
+//5. 그렇게 끝나면 다시 할지 종료할 지 결정 (무한 루프로 처리하고 종료를 break 처리하자)
+//+a 강화학습을 통해서 맞추는 로직 찾기
+
+import kotlin.random.Random
+data class Answer(val a: Int, val b: Int, val c: Int)
+
+fun User_response():Answer {
+    val base = readln()
+    if (base.length != 3) {
+        throw IllegalArgumentException()
+    }
+    return Answer(base[0].toString().toInt(), base[1].toString().toInt(), base[2].toString().toInt()) //toInt로 하면 아스키 코드 값으로 변환됨
+}
+
 fun main() {
-    TODO("프로그램 구현")
+    println(problem(0))
+    println(User_response())
 }
