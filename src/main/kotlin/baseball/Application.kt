6b8f3d6091fc38ba49c5d6e3d54aa8a43d7ee6cf
rlaxodud214 <rlaxodud214@naver.com>
2023-10-20 11:45:43 +0900

    Feat: 게임 시작을 위한 세팅

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,41 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+import camp.nextstep.edu.missionutils.Console
+
+/*  명령어 학습(C : Ctrl, A: Alt, S:Shift)
+    1. CA + l : 자동 정렬
+    2. CA + o : import 정리
+    3. S + F6 : 이름 한 번에 수정
+    4. C + F12 : 파일 구조 확인
+    5. C + E : 최근 작업 파일 목록(CA L or R도 가능)
+    6. CS + BackSpace : 최근 수정 위치
+    7. S 2번 : 모든 프로젝트 파일에서 찾기
+    8. // TODO키워드 : 에러처리 해야할 내용 -> 작업하다가 중간중간에 나중에 수정해야할 부분 체크 가능!!!
+    9. Git에 업로드 안된 내용이라도 Local History를 통해서 파일 단위로 코드 복구가 가능하다.
+*/
+
 fun main() {
-    TODO("프로그램 구현")
+    // 1. 게임 시작을 위한 세팅
+    val answer = answerSelect()
+    println("숫자 야구 게임을 시작합니다.")
+    // println("answer : ${answer.contentToString()}")
+
+    // 2. 사용자 입력
+
 }
+
+fun answerSelect(): IntArray {
+    val computer = mutableListOf<Int>()
+
+    while (computer.size < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (!computer.contains(randomNumber)) {
+            computer.add(randomNumber)
+        }
+    }
+
+    return computer.toIntArray()
+}
+
+
