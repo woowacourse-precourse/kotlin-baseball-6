701843e3c3fb84f24d56510d840ff019bd14c41f
lh99j <grand2181@gmail.com>
2023-10-20 12:36:42 +0900

    feat: 컴퓨터 고유의 숫자를 정하는 기능

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,9 +1,25 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+
 fun printGameStartMessage() {
     println("숫자 야구 게임을 시작합니다.")
 }
 
+fun setComputerNumber(): MutableList<Int> {
+    val computer = mutableListOf<Int>()
+
+    while (computer.size < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (!computer.contains(randomNumber)) {
+            computer.add(randomNumber)
+        }
+    }
+
+    return computer
+}
+
 fun main() {
     printGameStartMessage()
+    val computerNumber = setComputerNumber()
 }
