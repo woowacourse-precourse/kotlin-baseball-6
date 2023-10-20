ea5f586f4b0cd0602d21bd29d9d54f8f0d3c3016
dohun_ <35682247+parkdohuni@users.noreply.github.com>
2023-10-20 21:17:30 +0900

    Feat (createRandomNumber()) : 서로 다른 숫자 3자리의 수로 구성된 랜덤값을 생성하는 기능 구현

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,11 +1,12 @@
 package baseball
 
 import camp.nextstep.edu.missionutils.Console
-import kotlin.reflect.typeOf
-
+import camp.nextstep.edu.missionutils.Randoms
 fun main() {
     startGame()
     inputNumber()
+    val randomNumber = createRandomNumber()
+    println(randomNumber)
 }
 
 fun startGame() = println("숫자 야구 게임을 시작합니다.")
@@ -35,4 +36,15 @@ fun checkIsDigit(inputNumber: String) {
     inputNumber.forEach {
         if (49 > it.code || 57 < it.code) callException()
     }
+}
+
+fun createRandomNumber(): String {
+    val computer = mutableListOf<Int>()
+    while (computer.size < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (!computer.contains(randomNumber)) {
+            computer.add(randomNumber)
+        }
+    }
+    return computer.joinToString("")
 }
