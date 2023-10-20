f5209b952abe4bcf3f888bac1b632f720c4f0cc7
JunJangE <wnswkd486@gmail.com>
2023-10-20 10:33:25 +0900

    feat: 입력한 수에 대한 결과를 볼, 스트라이크 개수로 표시하는 기능

--- src/main/kotlin/baseball/view/OutputView.kt
+++ src/main/kotlin/baseball/view/OutputView.kt
@@ -1,4 +1,6 @@
 package baseball.view
+
+import baseball.model.Score
 import utils.Constant.GAME_START_MESSAGE
 import utils.Constant.NUMBER_INPUT_MESSAGE
 
@@ -7,5 +9,11 @@ class OutputView {
     fun printGameStartMessage() = println(GAME_START_MESSAGE)
 
     fun printNumberInputMessage() = print(NUMBER_INPUT_MESSAGE)
+
+    fun printScoreMessage(score: Score) = println(buildString {
+        if (score.balls > 0) append("${score.balls}볼 ")
+        if (score.strikes > 0) append("${score.strikes}스트라이크")
+        if (score.outs == 3) append("낫싱")
+    }.trim())
 
 }
