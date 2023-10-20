4b057c7b972d2df7d11bed4d1a533107dd53f4bc
somin <salth0601@gmail.com>
2023-10-20 16:57:51 +0900

    feat: 정답 시 재시작 기능 구현
    - 한 라운드가 끝날 때마다 do-while 문으로 재시작 여부 검사
    - 랜덤 생성 숫자와 입력받은 숫자가 같으면 정답처리

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -5,12 +5,17 @@ import camp.nextstep.edu.missionutils.Randoms
 
 fun main() {
     println("숫자 야구 게임을 시작합니다.")
-    startBaseballGame()
+    do {
+        startBaseballGame()
+    } while (isRestartable())
 }
 
 fun startBaseballGame() {
     val answer = creatRandomNumberList()
-    val user = getUserInput()
+    var user = getUserInput()
+    if (user == answer) {
+        println("3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료")
+    }
 }
 
 fun creatRandomNumberList(): MutableList<Int> {
@@ -32,4 +37,10 @@ fun getUserInput(): MutableList<Int> {
         userNumber /= 10
     }
     return userNumberList
+}
+
+fun isRestartable(): Boolean {
+    println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+    val restartChar = Console.readLine()
+    return ("1" == restartChar)
 }
