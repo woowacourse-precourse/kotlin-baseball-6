673c7e84c4c32815c601e9f7cca245bdef7c2c44
jihyeonbaem <jihyeonbae424@gmail.com>
2023-10-21 01:14:33 +0900

    chore: 리셋

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,31 +1,6 @@
 package baseball
-import camp.nextstep.edu.missionutils.Randoms
-import kotlin.collections.mutableListOf as mutableListOf
 
+// 프로그램의 시작점
 fun main() {
-    startGame()
-}
-
-fun startGame() {
-    println("숫자 야구 게임을 시작합니다.")
-    return computerNumber()
-}
-
-fun computerNumber() {
-    val computer = mutableListOf<Int>()
-    while (computer.size < 3) {
-        val randomNumber = Randoms.pickNumberInRange(1, 9)
-        if (!computer.contains(randomNumber)) {
-            computer.add(randomNumber)
-        }
-    }
-    println(computer)
-    return userNumber()
-}
-
-fun userNumber() {
 
 }
-
-
-
