b323a4f6d5f991dc4893f5cb2df9241f50b51ba4
HahyunKang <ganghahyeon06@gmail.com>
2023-10-21 00:52:28 +0900

    게임 결과값 기능 구현 완료

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -5,26 +5,50 @@ fun main() {
 
     //랜덤 정수 생성
     val numProducer = RandomNumProducer()
+    val gameCalculator = GameCalculator()
     var answerList : List<Int> = emptyList()
     var inputList : List<Int> = emptyList()
+    var gameResult : GameResult
 
     answerList = numProducer.makeRandomNum()
 
+    answerList.forEach { println(it) }
+
     //게임 시작
     print("숫자 야구 게임을 시작합니다.\n")
-    //문자열 입력
-    try {
-        print("숫자를 입력해주세요 : ")
-        val inputNum = readlnOrNull()
-        //입력 받지 않았거나 숫자 3자리로 구성되어 있지 않은 경우  에러 처리
-        if (inputNum != null && inputNum.length==3) {
-            inputList= checkInput(inputNum)
-        }else throw IllegalArgumentException("압력 값은 숫자 3자리로 구성되어야 합니다.")
 
-    }catch (e : IllegalArgumentException){
-        println("오류 발생 :${e.message}")
+    while(true) {
+        //문자열 입력
+        try {
+            print("숫자를 입력해주세요 : ")
+            val inputNum = readlnOrNull()
+            //입력 받지 않았거나 숫자 3자리로 구성되어 있지 않은 경우  에러 처리
+            if (inputNum != null && inputNum.length == 3) {
+                inputList = checkInput(inputNum)
+
+            } else throw IllegalArgumentException("압력 값은 숫자 3자리로 구성되어야 합니다.")
+
+            //게임 시작
+           gameResult = gameCalculator.calculate(answerList, inputList)
+
+            if(gameResult.strike==0 && gameResult.ball==0){
+                println("낫싱")
+            }else{
+                println(gameResult.ball.toString()+ "볼" + " " +gameResult.strike.toString() + "스트라이크" )
+
+                    if(gameResult.strike==3) {
+                        //게임종료
+                        println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
+                        println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+
+                    }
+            }
+
+
+        } catch (e: IllegalArgumentException) {
+            println("오류 발생 :${e.message}")
+        }
     }
-
 }
 
 
---
+++ src/main/kotlin/baseball/GameCalculator.kt
@@ -0,0 +1,32 @@
+package baseball
+
+class GameCalculator {
+
+   private var gameResult = GameResult()
+    fun calculate(ansNum : List<Int>, inputNum : List<Int>) : GameResult {
+
+        //초기화
+        gameResult = GameResult(0,0)
+
+        inputNum.forEachIndexed {
+             inputIndex ,input->
+
+            if(ansNum.contains(input)){
+
+                ansNum.forEachIndexed { ansIndex, ans ->
+                    if(ans == input && inputIndex==ansIndex) gameResult.strike +=1
+                    else if(ans == input && inputIndex != ansIndex) gameResult.ball +=1
+                }
+
+
+        }else{
+            gameResult = GameResult(0,0)
+        }
+
+        }
+
+
+        return gameResult
+
+    }
+}
