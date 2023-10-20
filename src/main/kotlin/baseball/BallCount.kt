ac45f42580e1cf29b9ce5724dd542ee0603edec4
민대인 <bamin0422@gmail.com>
2023-10-20 17:05:39 +0900

    refactor: Kotlin Style Guide 적용 (1) - BallCount 데이터 클래스

--- src/main/kotlin/baseball/BallCount.kt
+++ src/main/kotlin/baseball/BallCount.kt
@@ -4,8 +4,13 @@ data class BallCount(val strike: Int, val ball: Int) {
     fun isNothing() = (strike + ball) == 0
     fun isStrikeOut() = strike == 3
     override fun toString(): String =
-        if (isNothing()) "낫싱"
-        else if (ball == 0) "${strike}스트라이크"
-        else if (strike == 0) "${ball}볼"
-        else "${ball}볼 ${strike}스트라이크"
+        if (isNothing()) {
+            "낫싱"
+        } else if (ball == 0) {
+            "${strike}스트라이크"
+        } else if (strike == 0) {
+            "${ball}볼"
+        } else {
+            "${ball}볼 ${strike}스트라이크"
+        }
 }
