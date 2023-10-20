2770a1e937be2d70218cc7167a6b9a86d4cd5a61
Dongsu Lee <dongsu0717@nate.com>
2023-10-20 23:52:55 +0900

    진짜 최종

--- docs/README.md
+++ docs/README.md
@@ -6,9 +6,11 @@
     - readline()사용
     - 1~9숫자 and 중복x and 3자리 != IllegalArgumentException
     - #inputNumber
+
 3.비교하기 
     - 같은숫자,같은자리 = 스트라이크
     - 같은숫자,다른자리 = 볼 
     - 숫자가 안겹친다 = 낫싱
     - 3스트라이크시 축하말 후 재실행or 종료 선택  
     - #CompareNumber
+
--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,82 +1,6 @@
 package baseball
 import camp.nextstep.edu.missionutils.Randoms
-/*
-var strikeCount : Int = 0
-var ballCount : Int = 0
-fun main(){
-    println("숫자 야구 게임을 시작합니다.")
-    val pick = randNumber()
-    println(pick)
-    do{
-        print("숫자를 입력해주세요 :")
-        var number = inputNumber()
 
-        var cN = compareNumber()
-        cN
-    } while(strikeCount != 3)
-
-}
-fun randNumber(): List<Int> {
-    var computer = mutableListOf<Int>()
-    while (computer.size < 3) {
-        val randomNumber = Randoms.pickNumberInRange(1, 9)
-        if (!computer.contains(randomNumber)) {
-            computer.add(randomNumber)
-        }
-    }
-    return computer
-}
-fun inputNumber(): String {            // 반환값 정수다
-    var input = readLine()!!.toString() // 이변수는 null이 아니다
-    if(input.contains('0')
-        || input.length !=3
-        || (input[0] == input[1] && input[1] == input[2] && input[2] == input[0])) {
-        throw IllegalArgumentException("종료 되었습니다.")
-    }
-    return input
-}
-fun compareNumber(){
-    var aa = randNumber().toString()
-    var bb = inputNumber()
-    for(i in 0..2){
-        for(j in 0..2){
-            if (bb[j] == aa[i] && bb[j] == aa[j]){
-                strikeCount++
-                continue
-            }else if(bb[j] == aa[i]){
-                ballCount++
-                continue
-            }else{
-
-            }
-        }
-
-        if(strikeCount !=0 && ballCount !=0){
-            println("${ballCount}볼 ${strikeCount}스트라이크")
-        }else if(strikeCount != 0 && ballCount == 0){
-            println("${strikeCount}스트라이크")
-        }else if(strikeCount == 0 && ballCount != 0){
-            println("${ballCount}볼")
-        }else if(strikeCount ==0 && ballCount ==0){
-            println("낫싱")
-        }else{
-
-        }
-    }
-}
-
-fun reOrStop(){
-    println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
-    var choice = readLine().toString()
-    if(choice == "1"){
-        //프로그램 종료
-    }else if(choice == "2"){
-        //프로그램
-    }else{
-
-    }
-}
-*/
 fun main(){
     var keepPlaying: Boolean
     do {
@@ -85,17 +9,16 @@ fun main(){
         var strikeCount = 0
 
         while (strikeCount != 3) {
-            //println(cN) // 나중에 바꿀꺼
             strikeCount = compareNumber(cN)
         }
 
         println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
-        print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+        println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
         keepPlaying = readLine()?.toLowerCase() == "1"
 
     } while (keepPlaying)
 
-    println("게임을 종료합니다. 감사합니다!")
+    println("게임을 종료합니다.")
 }
 fun randNumber(): List<Int> {
     var computer = mutableListOf<Int>()
@@ -107,19 +30,18 @@ fun randNumber(): List<Int> {
     }
     return computer
 }
-fun inputNumber(): List<Int> {            // 반환값 정수다
-    var input = readLine()!!.toString() // 이변수는 null이 아니다
+fun inputNumber(): List<Int> {
+    var input = readLine()!!.toString()
     if(input.contains('0')
         || input.length !=3
         || ((input[0] == input[1]) || (input[1] == input[2]) || (input[2] == input[0]))) {
-        throw IllegalArgumentException("")
+        throw IllegalArgumentException()
     }
-    return input.map { it.toString().toInt() } // 문자열에서 숫자 리스트로 변환
+    return input.map { it.toString().toInt() }
 }
 
-fun compareNumber(cN : List<Int>): Int {        //비교, 출력
-    // var aa = cN //이게 아니라 randNumber의 computer을 받아와야한느데, 이렇게하면 새로운 숫자가생성됨
-    print("숫자를 입력해주세요 :")
+fun compareNumber(cN : List<Int>): Int {
+    print("숫자를 입력해주세요 : ")
     val userNumbers = inputNumber()
     var strikeCount: Int = 0
     var ballCount: Int = 0
@@ -138,21 +60,6 @@ fun compareNumber(cN : List<Int>): Int {        //비교, 출력
     }
     return strikeCount
 }
-
-
-//when이용해 간략하게 바꾸기
-    /*
-    if(strikeCount !=0 && ballCount !=0){
-        println("${ballCount}볼 ${strikeCount}스트라이크")
-    }else if(strikeCount != 0 && ballCount == 0){
-        println("${strikeCount}스트라이크")
-    }else if(strikeCount == 0 && ballCount != 0){
-        println("${ballCount}볼")
-    }else if(strikeCount ==0 && ballCount ==0){
-        println("낫싱")
-    }else{
-    }
-     */
 
 
 
