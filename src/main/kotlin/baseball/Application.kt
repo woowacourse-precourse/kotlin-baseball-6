1087ebe6be31688e3bb2a96cadba8db1bb9ef486
getupminaaa <getupmina@gmail.com>
2023-10-20 19:00:48 +0900

    feat: 플레이어가 입력한 값 검사 기능 구현
    regex를 사용하여 플레이어의 입력값이 1-9 사이의 숫자로 구성된 세 자릿수인지 Boolean으로 리턴받는다.
    또한 Set 자료구조의 원소 중복이 불가한 특징을 이용해 서로 다른 숫자로 구성되어있는지를 검사하여 Boolean으로 리턴받는다.
    리턴받은 두 개의 Boolean 값을 AND 하여 true가 되어야만 해당 값을 사용할 수 있도록 한다.

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,7 +1,18 @@
 package baseball
 
 import camp.nextstep.edu.missionutils.Console
+
 fun main() {
     print("숫자를 입력해주세요 : ")
     val userInput = Console.readLine() //플레이어 입력
+
+    val regex = "[1-9]{3}".toRegex() //1-9사이의 숫자로 구성된 세 자릿수 정규표현식
+
+    //regex와 일치하면서 set을 이용해 중복된 수가 있는지 확인
+    if (regex.matches(userInput) && (userInput.length == userInput.toSet().size)) {
+        val userInputList:List<String> = userInput.chunked(1)
+
+    } else {
+        
+    }
 }
