919a4eabcc563f2b6f3c361591a8e4e1fe827932
Jinhwan Noh <xsest55@ajou.ac.kr>
2023-10-20 20:38:50 +0900

    Feat: getInput, throw exception when illegal inputs

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,6 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Console
 import camp.nextstep.edu.missionutils.Randoms
 
 fun isInstanceOfList(list: MutableList<Int>, rand: Int): Boolean {
@@ -24,6 +25,29 @@ fun newGame(): Int {
         index++
     }
     return target.toInt()
+}
+
+fun getInput(): Int {
+    val str: String = Console.readLine()
+    try {
+        val num: Int = str.toInt()
+        if ((num > 999) or (num < 100))
+            throw IllegalArgumentException("illegal length was input")
+        val list = mutableListOf<Int>(num / 100, num % 100 / 10, num % 10)
+        for (i in 0..2) {
+            for (j in 0..2) {
+                if (i == j)
+                    continue
+                else if (list[i] == list[j])
+                    throw IllegalArgumentException("duplicate-digit was input")
+            }
+        }
+        return num
+    } catch (e: IllegalArgumentException) {
+        throw IllegalArgumentException(e)
+    } catch (e: Exception) {
+        throw IllegalArgumentException("non-digit was input")
+    }
 }
 
 fun main() {
