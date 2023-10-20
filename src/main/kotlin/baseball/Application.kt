585c698ac2704a3dd87badb807eefe9666f16e03
lh99j <grand2181@gmail.com>
2023-10-20 12:46:57 +0900

    feat: 사용자의 입력이 유효한지 확인 기능

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -2,6 +2,8 @@ package baseball
 
 import camp.nextstep.edu.missionutils.Randoms
 import camp.nextstep.edu.missionutils.Console
+import java.lang.IllegalArgumentException
+
 
 fun printGameStartMessage() {
     println("숫자 야구 게임을 시작합니다.")
@@ -22,10 +24,41 @@ fun setComputerNumber(): MutableList<Int> {
 
 fun inputUserNumber(): MutableList<Int> {
     print("숫자를 입력해주세요 : ")
+    val userNumber = Console.readLine()
+    checkValidNumber(userNumber)
 
-    return Console.readLine().map { it.digitToInt() }.toMutableList()
+    return userNumber.map { it.digitToInt() }.toMutableList()
 }
 
+fun checkValidNumber(number: String) {
+    //사용자의 입력이 3자리인지 확인
+    if (number.length != 3) {
+        throw java.lang.IllegalArgumentException("3자리의 입력이 아닙니다.")
+    }
+
+    //사용자의 입력이 숫자인지 확인
+    for (i in 0..<number.length) {
+        val asciiCode = number[i].code
+
+        if (asciiCode !in 48..57) {
+            throw IllegalArgumentException("사용자의 입력이 숫자가 아닙니다.")
+        }
+    }
+
+    //사용자의 입력된 숫자에 중복된 숫자가 있는지 확인
+    if (number.toList().distinct().size != 3) {
+        throw IllegalArgumentException("사용자의 입력 중 중복된 숫자가 존재합니다.")
+    }
+
+    //사용자의 입력 숫자의 각 자리수가 1부터 9까지인지 확인
+    number.forEach { num ->
+        val iNum = num.digitToInt()
+
+        if (iNum !in 1..9) {
+            throw IllegalArgumentException("사용자의 입력이 1부터 9까지의 숫자가 아닙니다.")
+        }
+    }
+}
 
 fun main() {
     printGameStartMessage()
