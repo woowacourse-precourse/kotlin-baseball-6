02c6b1bdbb58e6e21789deb78591d4d338eb9b01
민대인 <bamin0422@gmail.com>
2023-10-20 15:13:28 +0900

    fix: 게임 종료 메시지 여러번 출력되는 현상 해결

--- src/main/kotlin/baseball/Computer.kt
+++ src/main/kotlin/baseball/Computer.kt
@@ -31,7 +31,10 @@ class Computer {
                 println(ballCounts)
                 if (ballCounts.isStrikeOut()) {
                     println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
-                    if (checkRematch(Console.readLine()!!.toInt())) startGame()
+                    if (checkRematch(Console.readLine()!!.toInt())) {
+                        startGame()
+                        return
+                    }
                     println("게임 종료")
                     return
                 }
