99467c2ffb69223156dc930b2a77d240af268778
JunJangE <wnswkd486@gmail.com>
2023-10-20 10:26:09 +0900

    feat: 숫자를 입력해주세요 출력 기능

--- src/main/kotlin/baseball/view/OutputView.kt
+++ src/main/kotlin/baseball/view/OutputView.kt
@@ -1,7 +1,11 @@
 package baseball.view
 import utils.Constant.GAME_START_MESSAGE
+import utils.Constant.NUMBER_INPUT_MESSAGE
+
 class OutputView {
 
     fun printGameStartMessage() = println(GAME_START_MESSAGE)
+
+    fun printNumberInputMessage() = print(NUMBER_INPUT_MESSAGE)
 
 }
