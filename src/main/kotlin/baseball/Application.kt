f6b4a525f6c2cfdc9c2942ea32137476336bc13c
dohun_ <35682247+parkdohuni@users.noreply.github.com>
2023-10-20 20:30:33 +0900

    Feat (checkIsDigit()) : 입력한 값이 1~9까지의 숫자인지 확인하는 함수 구현

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,6 +1,7 @@
 package baseball
 
 import camp.nextstep.edu.missionutils.Console
+import kotlin.reflect.typeOf
 
 fun main() {
     startGame()
@@ -12,6 +13,7 @@ fun inputNumber() {
     print("숫자를 입력해주세요 : ")
     val inputNumber = Console.readLine()
     checkLength(inputNumber)
+    checkIsDigit(inputNumber)
     checkDifferentNumber(inputNumber)
 }
 
@@ -26,5 +28,11 @@ fun checkLength(inputNumber: String) {
 fun checkDifferentNumber(inputNumber: String) {
     if (inputNumber[0] == inputNumber[1] || inputNumber[0] == inputNumber[2] || inputNumber[1] == inputNumber[2]) {
         callException()
+    }
+}
+
+fun checkIsDigit(inputNumber: String) {
+    inputNumber.forEach {
+        if (49 > it.code || 57 < it.code) callException()
     }
 }
