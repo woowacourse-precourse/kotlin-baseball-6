9851dc0f697b77e246a6e3227a96d1eb9db881b3
pipeline <sorkwhwo@naver.com>
2023-10-20 21:13:06 +0900

    feat: 컴퓨터가 1~9까지의 무작위 숫자 3개 생성

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,15 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
     println("숫자 야구 게임을 시작합니다.")
+}
+
+fun generateComputerNumbers(): List<Int> {
+    val computerNumbers = mutableSetOf<Int>()
+    while (computerNumbers.size < 3) {
+        computerNumbers.add(Randoms.pickNumberInRange(1, 9))
+    }
+    return computerNumbers.toList()
 }
