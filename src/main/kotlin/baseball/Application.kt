e42e9f784b6720991e3b176ea777a2a3ef74a28f
jaejunlee11 <sktks11@naver.com>
2023-10-20 14:58:16 +0900

    랜덤 숫자 생성 구현

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,27 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+import camp.nextstep.edu.missionutils.Console
+
+
 fun main() {
-    TODO("프로그램 구현")
+    // 랜덤한 3개의 숫자 생성
+    val computer = createNumber()
+    
 }
+
+//컴퓨터가 랜덤한 3개의 숫자를 생성하는 함수
+// 입력 : X
+// 출력 : 랜덤한 숫자 3개가 담긴 MutableList<Int>
+fun createNumber () :MutableList<Int> {
+    val computer :MutableList<Int>  = mutableListOf()
+    while (computer.size < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (!computer.contains(randomNumber)) {
+            computer.add(randomNumber)
+        }
+    }
+    return computer
+}
+
+
