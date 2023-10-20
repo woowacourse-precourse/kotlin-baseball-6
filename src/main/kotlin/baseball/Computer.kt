5ad9d5b9c57e371dbba8b8c4ca56d98655265882
민대인 <bamin0422@gmail.com>
2023-10-20 13:39:47 +0900

    feat: startGame 게임시작 메서드 추가 및 setBaseBallNumberList라는 Random으로 숫자 3개 컴퓨터에 가져오는 메서드 추가

--- src/main/kotlin/baseball/Computer.kt
+++ src/main/kotlin/baseball/Computer.kt
@@ -1,4 +1,21 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+
 class Computer {
+    private lateinit var baseballNumberList: MutableList<Int>
+
+    fun startGame() {
+        setBaseBallNumberList()
+    }
+
+    private fun setBaseBallNumberList() {
+        baseballNumberList = mutableListOf()
+        with(baseballNumberList) {
+            while (size < 3) {
+                val randomNumber = Randoms.pickNumberInRange(1, 9)
+                if (!contains(randomNumber)) add(randomNumber)
+            }
+        }
+    }
 }
