cf4e8bac7d9ab9d043147cb426f695dd4c6928c3
gaeun <geun5744@gmail.com>
2023-10-20 17:50:56 +0900

    [FEAT] 입력한 숫자의 예외상황 처리 구현

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -29,6 +29,17 @@ fun createRandomNum(): List<Int> {
 fun getUserInputNum(): List<Int> {
     val userInputChar = Console.readLine().toCharArray()
     val inputNum = userInputChar.map { num -> num.toString().toInt() }
+    checkIsValidNum(inputNum)
     return inputNum
+}
+
+fun checkIsValidNum(inputNum: List<Int>) {
+    if (inputNum.size != 3) throw IllegalArgumentException("3자리의 수를 입력해주세요")
+    for (num in inputNum) {
+        if (inputNum.count { (it == num) } > 1)
+            throw IllegalArgumentException("서로 다른 숫자를 입력해주세요")
+        else if (num < 1 || num > 9)
+            throw IllegalArgumentException("각각 1~9 사이의 숫자를 입력해주세요")
+    }
 }
 
