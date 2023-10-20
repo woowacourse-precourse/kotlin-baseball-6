29d777afadc27b07004ad6042b5c5be50dbf00d6
jaehan4707 <jahan4707@naver.com>
2023-10-20 21:01:07 +0900

    feat : InputView,InputValidator 추가

---
+++ src/main/kotlin/baseball/view/InputValidator.kt
@@ -0,0 +1,22 @@
+package baseball.view
+
+import baseball.util.GameMessage
+import java.lang.IllegalArgumentException
+
+object InputValidator {
+
+    fun validateInputLength(input: String) {
+        if (input.length != 3)
+            throw IllegalArgumentException(GameMessage.GAME_ERROR_INPUT_LENGTH)
+    }
+
+    fun validateInputNumber(input: String) {
+        if (input.toIntOrNull() == null)
+            throw IllegalArgumentException(GameMessage.GAME_ERROR_INPUT_NUMBER)
+    }
+
+    fun validateInputDuplicate(input: String){
+        if (input.toSet().size != 3)
+            throw IllegalArgumentException(GameMessage.GAME_ERROR_INPUT_DUPLICATE)
+    }
+}
---
+++ src/main/kotlin/baseball/view/InputView.kt
@@ -0,0 +1,13 @@
+package baseball.view
+
+import camp.nextstep.edu.missionutils.Console
+class InputView {
+
+    fun getInputNumber() : String{
+        val input = Console.readLine()
+        InputValidator.validateInputLength(input)
+        InputValidator.validateInputNumber(input)
+        InputValidator.validateInputDuplicate(input)
+        return input
+    }
+}
