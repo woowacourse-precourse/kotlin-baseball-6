453297d03cbd2b3f46288929b632fdced4757ec2
jaehan4707 <jahan4707@naver.com>
2023-10-20 20:02:54 +0900

    feat : GameMessage 추가

---
+++ src/main/kotlin/baseball/util/GameMessage.kt
@@ -0,0 +1,8 @@
+package baseball.util
+
+object GameMessage {
+    const val GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다."
+    const val GAME_INPUT_NUMBER_MESSAGE = "숫자를 입력해주세요 : "
+    const val GAME_SUCCESS_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료"
+    const val GAME_END_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
+}
