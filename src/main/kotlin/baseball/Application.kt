08bed92b4b8f719e0b59076d1112e7fc83987391
Jinhwan Noh <xsest55@ajou.ac.kr>
2023-10-20 20:10:58 +0900

    Feat: Game start - Random number generation

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,31 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+
+fun isInstanceOfList(list: MutableList<Int>, rand: Int): Boolean {
+    for (i in list) {
+        if (i == rand)
+            return true
+    }
+    return false
+}
+
+fun newGame(): Int {
+    val list = mutableListOf<Int>(0, 0, 0)
+    var target: String = ""
+    var index = 0
+    while (index < 3) {
+        val rand = Randoms.pickNumberInRange(1, 9)
+        if (isInstanceOfList(list, rand)) {
+            continue
+        }
+        list[index] = rand
+        target += rand.toString()
+        index++
+    }
+    return target.toInt()
+}
+
 fun main() {
-    TODO("프로그램 구현")
+
 }
