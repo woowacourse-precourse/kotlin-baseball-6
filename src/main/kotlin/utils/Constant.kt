4b19c8db60d1b15c2f83cbfbe999e38a97d88ba1
JunJangE <wnswkd486@gmail.com>
2023-10-20 09:46:38 +0900

    feat: 상수 값 정의

---
+++ src/main/kotlin/utils/Constant.kt
@@ -0,0 +1,9 @@
+package utils
+
+object Constant {
+    const val GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다."
+    const val NUMBER_INPUT_MESSAGE = "숫자를 입력해주세요 : "
+    const val GAME_SUCCESS_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료"
+    const val GAME_RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
+    const val GAME_OVER_MESSAGE = "게임 종료"
+}
