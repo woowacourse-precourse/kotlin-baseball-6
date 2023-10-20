e6b1c1efcf8380ebc920564f353b4820b6cafa07
민대인 <bamin0422@gmail.com>
2023-10-20 14:59:38 +0900

    fix: 게임 종료 후, 재시작 테스트케이스 통과하도록 3자리 수가 아닌 수 입력될 시 게임 종료

--- src/main/kotlin/baseball/Computer.kt
+++ src/main/kotlin/baseball/Computer.kt
@@ -19,20 +19,24 @@ class Computer {
                 if (!contains(randomNumber)) add(randomNumber)
             }
         }
+        println(baseballNumberList)
     }
 
     private fun playGame() {
         println("숫자 야구 게임을 시작합니다.\n숫자를 입력해주세요 : ")
         while (true) {
             val userInput = getUserInput()
-            val ballCounts = checkUserInput(userInput)
-            println(ballCounts)
-            if (ballCounts.isStrikeOut()) {
-                println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
-                break
-            }
+            if (userInput.size == 3) {
+                val ballCounts = checkUserInput(userInput)
+                println(ballCounts)
+                if (ballCounts.isStrikeOut()) {
+                    println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+                    if (checkRematch(Console.readLine()!!.toInt())) startGame()
+                    println("게임 종료")
+                    return
+                }
+            } else break
         }
-        if(checkRematch(Console.readLine()!!.toInt())) startGame()
     }
 
     private fun checkUserInput(userInput: List<Int>): BallCount {
