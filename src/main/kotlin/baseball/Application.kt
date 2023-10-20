9556aedf845a0a30361e6577b0686ff625ffce44
Kim Gimun <kmkim7575@gmail.com>
2023-10-20 18:01:16 +0900

    feat: add infinite loop to play games until termination

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,7 +1,15 @@
 package baseball
 
+import baseball.Constants.CONTINUE_OR_NOT
 import baseball.Constants.START
+import camp.nextstep.edu.missionutils.Console
 
 fun main() {
     println(START)
+    while (true) {
+        Player.startGame()
+        println(CONTINUE_OR_NOT)
+        val input = Console.readLine().toInt()
+        if (input == 2) return else if (input == 1) continue
+    }
 }
