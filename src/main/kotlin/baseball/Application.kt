5986ee528cf4e70c910b9f0e4c465701df62e33f
ErroredPasta <ForeignServices3132@pm.me>
2023-10-20 20:26:08 +0900

    feat: 전반적인 흐름 구현

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,27 @@
 package baseball
 
 fun main() {
-    TODO("프로그램 구현")
+    println("숫자 야구 게임을 시작합니다.")
+
+    do {
+        val playAgain = playGame()
+    } while (playAgain.again)
+
+    println("게임 종료")
 }
+
+
+fun playGame(): PlayAgain {
+    // TODO: target number 생성
+
+    // TODO: 유저 입력 가져오기 + validation 
+
+    // TODO: 입력과 target number 비교 + 출력
+    
+    // TODO: 1 입력하면 again을 true, 2 입력하면 again false
+    return PlayAgain(again = false)
+}
+
+
+@JvmInline
+value class PlayAgain(val again: Boolean)
