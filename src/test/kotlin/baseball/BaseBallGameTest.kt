0073b4a5c563fc4ecf6a2a12d33987005ff1a7c9
hoogje <ajyong316@gmail.com>
2023-10-21 02:11:13 +0900

    refactor&style(BaseBallGameTest):테스트 기능 별로 내부 클래스로 분리 및 테스트 함수 이름 수정

--- src/test/kotlin/baseball/BaseBallGameTest.kt
+++ src/test/kotlin/baseball/BaseBallGameTest.kt
@@ -1,110 +1,124 @@
 package baseball
 
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
 class BaseBallGameTest {
     private val game = BaseBallGame()
     private var userInput = ""
-    @Test
-    fun `컴퓨터 숫자 크기 테스트`() {
-        val computerNumbers = game.generateRandomNumbers()
-        assertThat(computerNumbers).hasSize(3)
+
+    @Nested
+    inner class NumbersTest {
+        @Test
+        fun `컴퓨터의 랜덤 값이 3자리인지 테스트`() {
+            val computerNumbers = game.generateRandomNumbers()
+            assertThat(computerNumbers).hasSize(3)
+        }
+
+        @Test
+        fun `컴퓨터의 랜덤 값이 중복되었는지 테스트`() {
+            val computerNumbers = game.generateRandomNumbers()
+            computerNumbers.toHashSet()
+            assertThat(computerNumbers).hasSize(3)
+        }
+
+        @Test
+        fun `사용자의 야구 숫자 입력 값이 숫자 아닌 다른 형식일 때 예외 테스트`() {
+            userInput = "12Q"
+            assertThrows<IllegalArgumentException> { (game.validateUserNumbers(userInput)) }
+        }
+
+        @Test
+        fun `사용자의 야구 숫자 입력 값이 범위를 초과하였을 때 예외 테스트`() {
+            userInput = "1234"
+            assertThrows<IllegalArgumentException> { (game.validateUserNumbers(userInput)) }
+        }
+
+        @Test
+        fun `사용자의 야구 숫자 입력 값이 범위보다 작을 때 예외 테스트`() {
+            userInput = "12"
+            assertThrows<IllegalArgumentException> { (game.validateUserNumbers(userInput)) }
+        }
+
+        @Test
+        fun `사용자의 야구 숫자 입력 값이 중복되었을 때 예외 테스트`() {
+            userInput = "122"
+            assertThrows<IllegalArgumentException> { (game.validateUserNumbers(userInput)) }
+        }
     }
 
-    @Test
-    fun `컴퓨터 숫자 중복 테스트`() {
-        val computerNumbers = game.generateRandomNumbers()
-        computerNumbers.toHashSet()
-        assertThat(computerNumbers).hasSize(3)
+    @Nested
+    inner class CompareNumbersTest() {
+        @Test
+        fun `컴퓨터와 사용자의 숫자를 비교하여 결과 확인 - 낫싱`() {
+            val computerNumbers = listOf(1,2,3)
+            val userNumbers = listOf(4,5,6)
+            val result = listOf(0,0)
+            assertThat(result).isEqualTo(game.compareNumbers(computerNumbers,userNumbers))
+        }
+
+        @Test
+        fun `컴퓨터와 사용자의 숫자를 비교하여 결과 확인 - 3스트라이크`() {
+            val computerNumbers = listOf(1,2,3)
+            val userNumbers = listOf(1,2,3)
+            val result = listOf(0,3)
+            assertThat(result).isEqualTo(game.compareNumbers(computerNumbers,userNumbers))
+        }
+
+        @Test
+        fun `컴퓨터와 사용자의 숫자를 비교하여 결과 확인 - 2볼`() {
+            val computerNumbers = listOf(1,2,3)
+            val userNumbers = listOf(2,3,4)
+            val result = listOf(2,0)
+            assertThat(result).isEqualTo(game.compareNumbers(computerNumbers,userNumbers))
+        }
     }
 
-    @Test
-    fun `사용자의 야구 숫자가 숫자가 아닌 다른 형식일 때 예외 테스트`() {
-        userInput = "12Q"
-        assertThrows<IllegalArgumentException> { (game.validateUserNumbers(userInput)) }
+    @Nested
+    inner class HintMessageTest {
+        @Test
+        fun `낫싱 테스트`() {
+            val result = listOf(0,0)
+            val hintMessage = "낫싱"
+            assertThat(hintMessage).isEqualTo(game.provideHintMessage(result))
+        }
+
+        @Test
+        fun `볼 테스트`() {
+            val result = listOf(2,0)
+            val hintMessage = "2볼"
+            assertThat(hintMessage).isEqualTo(game.provideHintMessage(result))
+        }
+
+        @Test
+        fun `스트라이크 테스트`() {
+            val result = listOf(0,2)
+            val hintMessage = "2스트라이크"
+            assertThat(hintMessage).isEqualTo(game.provideHintMessage(result))
+        }
+
+        @Test
+        fun `볼과 스트라이크 테스트`() {
+            val result = listOf(2,1)
+            val hintMessage = "2볼 1스트라이크"
+            assertThat(hintMessage).isEqualTo(game.provideHintMessage(result))
+        }
     }
 
-    @Test
-    fun `사용자의 야구 숫자가 범위를 초과하였을 때 예외 테스트`() {
-        userInput = "1234"
-        assertThrows<IllegalArgumentException> { (game.validateUserNumbers(userInput)) }
-    }
+    @Nested
+    inner class UserAnswerTest() {
+        @Test
+        fun `사용자의 답변 입력 값이 숫자가 아닐 때 예외 테스트`() {
+            userInput = "T"
+            assertThrows<IllegalArgumentException> { (game.validateUserAnswer(userInput)) }
+        }
 
-    @Test
-    fun `사용자의 야구 숫자가 범위보다 작을 때 예외 테스트`() {
-        userInput = "12"
-        assertThrows<IllegalArgumentException> { (game.validateUserNumbers(userInput)) }
-    }
-
-    @Test
-    fun `사용자의 야구 숫자가 중복되었을 때 예외 테스트`() {
-        userInput = "122"
-        assertThrows<IllegalArgumentException> { (game.validateUserNumbers(userInput)) }
-    }
-
-    @Test
-    fun `컴퓨터와 사용자의 숫자 비교 확인 - 1`() {
-        val computerNumbers = listOf(1,2,3)
-        val userNumbers = listOf(4,5,6)
-        val result = listOf(0,0)
-        assertThat(result).isEqualTo(game.compareNumbers(computerNumbers,userNumbers))
-    }
-
-    @Test
-    fun `컴퓨터와 사용자의 숫자 비교 확인 - 2`() {
-        val computerNumbers = listOf(1,2,3)
-        val userNumbers = listOf(1,2,3)
-        val result = listOf(0,3)
-        assertThat(result).isEqualTo(game.compareNumbers(computerNumbers,userNumbers))
-    }
-
-    @Test
-    fun `컴퓨터와 사용자의 숫자 비교 확인 - 3`() {
-        val computerNumbers = listOf(1,2,3)
-        val userNumbers = listOf(2,3,4)
-        val result = listOf(2,0)
-        assertThat(result).isEqualTo(game.compareNumbers(computerNumbers,userNumbers))
-    }
-
-    @Test
-    fun `낫싱 테스트`() {
-        val result = listOf(0,0)
-        val hintMessage = "낫싱"
-        assertThat(hintMessage).isEqualTo(game.provideHintMessage(result))
-    }
-
-    @Test
-    fun `볼 테스트`() {
-        val result = listOf(2,0)
-        val hintMessage = "2볼"
-        assertThat(hintMessage).isEqualTo(game.provideHintMessage(result))
-    }
-
-    @Test
-    fun `스트라이크 테스트`() {
-        val result = listOf(0,2)
-        val hintMessage = "2스트라이크"
-        assertThat(hintMessage).isEqualTo(game.provideHintMessage(result))
-    }
-
-    @Test
-    fun `볼과 스트라이크 테스트`() {
-        val result = listOf(2,1)
-        val hintMessage = "2볼 1스트라이크"
-        assertThat(hintMessage).isEqualTo(game.provideHintMessage(result))
-    }
-
-    @Test
-    fun `사용자의 답이 숫자가 아닐 때 예외 테스트`() {
-        userInput = "T"
-        assertThrows<IllegalArgumentException> { (game.validateUserAnswer(userInput)) }
-    }
-
-    @Test
-    fun `사용자의 답이 범위를 벗어났을 때 예외 테스트`() {
-        userInput = "0"
-        assertThrows<IllegalArgumentException> { (game.validateUserAnswer(userInput)) }
+        @Test
+        fun `사용자의 답변 입력 값이 범위를 벗어났을 때 예외 테스트`() {
+            userInput = "0"
+            assertThrows<IllegalArgumentException> { (game.validateUserAnswer(userInput)) }
+        }
     }
 }
