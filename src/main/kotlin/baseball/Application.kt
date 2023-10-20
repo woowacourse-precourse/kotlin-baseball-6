955b2a3c0f723a133b1119208491810e596e5cbd
jihyeonbaem <jihyeonbae424@gmail.com>
2023-10-20 17:37:44 +0900

    feat(startGame): 1-1. 게임 시작 문구 출력 후 2.로 이동하기

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,17 @@
 package baseball
 
 fun main() {
-    TODO("프로그램 구현")
+    startGame()
 }
+
+fun startGame() {
+    println("숫자 야구 게임을 시작합니다.")
+    return userInputNumber()
+}
+
+fun userInputNumber() {
+//    println("숫자를 입력해주세요 : ")
+}
+
+
+
