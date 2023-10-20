086eaf276bf29c67439f00bb43edfee9d2d20ab0
hoogje <ajyong316@gmail.com>
2023-10-21 01:55:14 +0900

    test(BaseBallGameTest):숫자를 비교하여 결과를 생성하는 메소드 및 힌트 메세지를 생성하는 메소드 테스트 코드 작성

--- src/test/kotlin/baseball/BaseBallGameTest.kt
+++ src/test/kotlin/baseball/BaseBallGameTest.kt
@@ -43,4 +43,56 @@ class BaseBallGameTest {
         userInput = "122"
         assertThrows<IllegalArgumentException> { (game.validateUserNumbers(userInput)) }
     }
+
+    @Test
+    fun `컴퓨터와 사용자의 숫자 비교 확인 - 1`() {
+        val computerNumbers = listOf(1,2,3)
+        val userNumbers = listOf(4,5,6)
+        val result = listOf(0,0)
+        assertThat(result).isEqualTo(game.compareNumbers(computerNumbers,userNumbers))
+    }
+
+    @Test
+    fun `컴퓨터와 사용자의 숫자 비교 확인 - 2`() {
+        val computerNumbers = listOf(1,2,3)
+        val userNumbers = listOf(1,2,3)
+        val result = listOf(0,3)
+        assertThat(result).isEqualTo(game.compareNumbers(computerNumbers,userNumbers))
+    }
+
+    @Test
+    fun `컴퓨터와 사용자의 숫자 비교 확인 - 3`() {
+        val computerNumbers = listOf(1,2,3)
+        val userNumbers = listOf(2,3,4)
+        val result = listOf(2,0)
+        assertThat(result).isEqualTo(game.compareNumbers(computerNumbers,userNumbers))
+    }
+
+    @Test
+    fun `낫싱 테스트`() {
+        val result = listOf(0,0)
+        val hintMessage = "낫싱"
+        assertThat(hintMessage).isEqualTo(game.provideHintMessage(result))
+    }
+
+    @Test
+    fun `볼 테스트`() {
+        val result = listOf(2,0)
+        val hintMessage = "2볼"
+        assertThat(hintMessage).isEqualTo(game.provideHintMessage(result))
+    }
+
+    @Test
+    fun `스트라이크 테스트`() {
+        val result = listOf(0,2)
+        val hintMessage = "2스트라이크"
+        assertThat(hintMessage).isEqualTo(game.provideHintMessage(result))
+    }
+
+    @Test
+    fun `볼과 스트라이크 테스트`() {
+        val result = listOf(2,1)
+        val hintMessage = "2볼 1스트라이크"
+        assertThat(hintMessage).isEqualTo(game.provideHintMessage(result))
+    }
 }
