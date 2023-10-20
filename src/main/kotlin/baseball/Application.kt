2a22e38d18a421ab6fd8362ffe3099cb27ef866c
dohun_ <35682247+parkdohuni@users.noreply.github.com>
2023-10-20 17:45:08 +0900

    Feat (checkLength:()) : 입력값 길이가 3이 아닌지 확인하는 함수 구현

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -11,5 +11,13 @@ fun startGame() = println("숫자 야구 게임을 시작합니다.")
 fun inputNumber() {
     print("숫자를 입력해주세요 : ")
     val inputNumber = Console.readLine()
+    checkLength(inputNumber)
 }
-fun callException(): Nothing = throw IllegalArgumentException("")
+
+fun callException(): Nothing = throw IllegalArgumentException("잘못")
+
+fun checkLength(inputNumber: String) {
+    if (inputNumber.length != 3) {
+        callException()
+    }
+}
