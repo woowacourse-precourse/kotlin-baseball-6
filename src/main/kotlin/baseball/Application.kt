5982e61cb21d928f0f520da7603afbea7d11254a
getupminaaa <getupmina@gmail.com>
2023-10-20 19:13:01 +0900

    feat: 난수를 생성하는 기능 구현

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,8 +1,19 @@
 package baseball
 
 import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
 
 fun main() {
+    //난수 생성
+    var temp = ""
+    val randomNumList = mutableListOf<String>()
+    while (randomNumList.size < 3) {
+        temp = Randoms.pickNumberInRange(1, 9).toString()
+        if (!randomNumList.contains(temp)) {
+            randomNumList.add(temp)
+        }
+    }
+
     print("숫자를 입력해주세요 : ")
     val userInput = Console.readLine() //플레이어 입력
 
@@ -11,8 +22,12 @@ fun main() {
     //regex와 일치하면서 set을 이용해 중복된 수가 있는지 확인
     if (regex.matches(userInput) && (userInput.length == userInput.toSet().size)) {
         val userInputList:List<String> = userInput.chunked(1)
-
+        //난수와 사용자 입력 비교
     } else {
         throw IllegalArgumentException("It is not a triple digit composed of different numbers between 1 and 9 :$userInput")
     }
+
+
+
+
 }
