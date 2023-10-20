abbf985cabea49cad03957c1018233d27aacddfc
민대인 <bamin0422@gmail.com>
2023-10-20 14:06:20 +0900

    feat: userInput에서 스트라이크와 볼 갯수를 세 BallCount를 반환하는 checkUserInput 메서드 구현 및 볼 갯수 세는 checkBallCount 메서드 구현 후 playGame 메서드에 추가

--- src/main/kotlin/baseball/Computer.kt
+++ src/main/kotlin/baseball/Computer.kt
@@ -23,12 +23,33 @@ class Computer {
 
     private fun playGame() {
         println("숫자 야구 게임을 시작합니다.\n숫자를 입력해주세요 : ")
-        // TODO : userInput과 baseballNumberList 비교 후, break하는 로직 필요
         while (true) {
             val userInput = getUserInput()
-            println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
-            break;
+            val ballCounts = checkUserInput(userInput)
+            println(ballCounts)
+            if (ballCounts.isStrikeOut()) {
+                println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+                break
+            }
         }
+    }
+
+    private fun checkUserInput(userInput: List<Int>): BallCount {
+        var strikeCount = 0
+        var ballCount = 0
+        userInput.forEachIndexed { _index, _ball ->
+            if (_ball == baseballNumberList[_index]) strikeCount++
+            else ballCount += checkBallCount(_ball)
+        }
+        return BallCount(strikeCount, ballCount)
+    }
+
+    private fun checkBallCount(userBall: Int): Int {
+        var ballCount = 0
+        baseballNumberList.forEach { _computerBall ->
+            if (userBall == _computerBall) ballCount++
+        }
+        return ballCount
     }
 
     private fun getUserInput(): List<Int> =
