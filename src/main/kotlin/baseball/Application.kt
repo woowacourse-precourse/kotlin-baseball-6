8d8b943402cdaf1967d50416aab4d6b948327fc3
pipeline <sorkwhwo@naver.com>
2023-10-20 21:16:51 +0900

    feat: 플레이어로부터 숫자를 입력 받아, 해당 입력값이 유효한지 검증

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,6 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Console
 import camp.nextstep.edu.missionutils.Randoms
 
 fun main() {
@@ -12,4 +13,21 @@ fun generateComputerNumbers(): List<Int> {
         computerNumbers.add(Randoms.pickNumberInRange(1, 9))
     }
     return computerNumbers.toList()
+}
+
+fun getValidPlayerNumbers(): List<Int> {
+    val playerInputNumbers = getPlayerInputNumbers()
+    validatePlayerInputNumbers(playerInputNumbers)
+    return playerInputNumbers.map { it.toString().toInt() }
+}
+
+fun getPlayerInputNumbers(): List<Char> {
+    print("숫자를 입력해주세요 : ")
+    return Console.readLine().toList()
+}
+
+fun validatePlayerInputNumbers(playerInputNumbers: List<Char>) {
+    if (playerInputNumbers.size != 3 || playerInputNumbers.toSet().size != 3 || !playerInputNumbers.all { it.isDigit() }) {
+        throw IllegalArgumentException("ERROR")
+    }
 }
