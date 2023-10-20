518082a190d8795752955bfb7b7c3372e9fdf73b
seonm9119 <seonm9119@gmail.com>
2023-10-21 02:22:39 +0900

    feat: baseballGame_v2.0.0

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -19,12 +19,6 @@ fun generateRandomNumber(): List<Int> {
 
 fun processInputWithException(input: String): List<Int> {
 
-    /*
-     * 1. 예외 처리
-     * - 숫자 이외의 값을 입력 받을 경우 ex) a12, dog, *&^ ...
-     * - 서로 다른 숫자가 3개가 아닐 경우 ex) 112, 233, 242 ...
-     * */
-
     try {
 
         val number = input.map { it.toString().toInt() }.distinct()
@@ -41,21 +35,7 @@ fun processInputWithException(input: String): List<Int> {
 
 }
 
-
 fun baseballGame(user: List<Int>, computer: List<Int>): Pair<Int, Int> {
-
-    /*
-    * 2. 게임 설계
-    * - 서로다른 3개의 수로 이루어진 랜덤 숫자 생성
-    * - 컴퓨터와 사용자의 숫자를 비교하여 결과 출력
-    *   - 스트라이크: 같은 자리에 숫자가 동일할 경우
-    *       - 스트라이크의 개수가 3일 경우,
-    *       - 게임을 새로시작 1, 종료 2 입력 받음.
-    *   - 볼: 동일한 숫자의 개수 - 스트라이크 개수
-    *   - 낫싱: 3개의 숫자가 모두 다를 경우
-    * */
-
-
 
     println("computer: $computer")
 
@@ -67,8 +47,7 @@ fun baseballGame(user: List<Int>, computer: List<Int>): Pair<Int, Int> {
 
 }
 
-
-fun displyResult(strike: Int, ball: Int): MutableList<String> {
+fun displayResult(strike: Int, ball: Int): MutableList<String> {
 
     val result = mutableListOf<String>()
 
@@ -90,53 +69,39 @@ fun main() {
 
     loop@ do {
 
-        // 1. 랜덤수 생성
+        // TODO("1. computer 랜덤수 생성")
         val computer = generateRandomNumber()
 
         do {
 
-            //2. 숫자 입력받기
+            // TODO("2. 숫자 입력 받기 - 예외처리")
             print("숫자를 입력해주세요 : ")
             val userInput = readLine()
-            val user: List<Int>
+            val user = processInputWithException(userInput)
 
-            try {
-                user = processInputWithException(userInput)
-            } catch (_: IllegalArgumentException) {
-                println("ERROR: 잘못된 값을 입력하여, 프로그램을 종료 합니다.")
-                break@loop
-            }
-
-
-            //3. 숫자 맞추기
+            // TODO("3. 숫자 비교 하기")
             val (strike, ball) = baseballGame(user, computer)
 
-            //3.1 출력형식
-            val result = displyResult(strike, ball)
+
+            // TODO("4. 결과 출력 하기")
+            val result = displayResult(strike, ball)
             println(result.joinToString(separator = " "))
 
 
         } while (strike < 3)
 
-        //4. 종료 여부 확인
 
+        // TODO("5. 종료 여부 확인 - 예외처리")
         println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
         println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
 
         val isExit: Boolean
         val flag = readLine()
 
-        isExit = try {
-
-            when (flag) {
-                "1" -> true
-                "2" -> false
-                else -> throw IllegalArgumentException()
-            }
-
-        } catch (_: IllegalArgumentException) {
-            println("ERROR: 잘못된 값을 입력하여, 프로그램을 종료 합니다.")
-            break@loop
+        isExit = when (flag) {
+            "1" -> true
+            "2" -> false
+            else -> throw IllegalArgumentException()
         }
 
     } while (isExit)
--- src/test/kotlin/baseball/ApplicationTest.kt
+++ src/test/kotlin/baseball/ApplicationTest.kt
@@ -4,39 +4,30 @@ import camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeT
 import camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest
 import camp.nextstep.edu.missionutils.test.NsTest
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
-
-class ApplicationTest {
-
-    @Nested
-    inner class Exceptions {
-
-        @Test
-        fun `숫자 이외의 값을 입력 받을 경우`() {
-            assertSimpleTest {
-                assertThrows<IllegalArgumentException> { processInputWithException("cat") }
-                assertThrows<IllegalArgumentException> { processInputWithException("") }
-                assertThrows<IllegalArgumentException> { processInputWithException("dog&cat") }
-                assertThrows<IllegalArgumentException> { processInputWithException("abc123") }
-            }
-        }
-
-        @Test
-        fun `서로 다른 3개의 숫자가 아닐 경우`() {
-            assertSimpleTest {
-                assertThrows<IllegalArgumentException> { processInputWithException("131") }
-                assertThrows<IllegalArgumentException> { processInputWithException("12345") }
-                assertThrows<IllegalArgumentException> { processInputWithException("5675") }
-                assertThrows<IllegalArgumentException> { processInputWithException("12") }
-            }
-
-        }
-
-
+class ApplicationTest : NsTest() {
+    @Test
+    fun `게임종료 후 재시작`() {
+        assertRandomNumberInRangeTest(
+            {
+                run("246", "135", "1", "597", "589", "2")
+                assertThat(output())
+                    .contains("낫싱", "3스트라이크", "1볼 1스트라이크", "3스트라이크", "게임 종료")
+            },
+            1, 3, 5, 5, 8, 9
+        )
     }
 
+    @Test
+    fun `예외 테스트`() {
+        assertSimpleTest {
+            assertThrows<IllegalArgumentException> { runException("1234") }
+        }
+    }
 
+    override fun runMain() {
+        main()
+    }
 }
