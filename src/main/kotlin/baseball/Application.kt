371011dcde7c352f9f25414354873c6c1a5696dc
gaeun <geun5744@gmail.com>
2023-10-20 16:53:10 +0900

    [FEAT] 상대방의 랜덤수 리스트 생성 함수 구현

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,9 +1,24 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
     gameStartMessage()
+    val computerNum = createRandomNum()
 }
 
 fun gameStartMessage() {
     println("숫자 야구 게임을 시작합니다.")
 }
+
+fun createRandomNum(): List<Int> {
+    val computer = mutableListOf<Int>()
+    while (computer.size < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (!computer.contains(randomNumber)) {
+            computer.add(randomNumber)
+        }
+    }
+    return computer
+}
+
