94c27c2e0b587184ca572cc67cd73579e3b374b8
JunJangE <wnswkd486@gmail.com>
2023-10-20 09:48:54 +0900

    fix: 상수 값 적용

--- src/main/kotlin/utils/Exception.kt
+++ src/main/kotlin/utils/Exception.kt
@@ -1,11 +1,13 @@
 package utils
 
+import utils.Constant.GAME_OVER_MESSAGE
+
 object Exception {
 
-    fun String.userWrongTypeAnswer() = require(this.toIntOrNull() != null) { "게임 종료" }
+    fun String.userWrongTypeAnswer() = require(this.toIntOrNull() != null) { GAME_OVER_MESSAGE }
 
-    fun String.userWrongSizeAnswer() = require(this.length == 3) { "게임 종료" }
+    fun String.userWrongSizeAnswer() = require(this.length == 3) { GAME_OVER_MESSAGE }
 
-    fun String.userWrongDuplicationAnswer() = require(this.toSet().size == this.length) { "게임 종료" }
+    fun String.userWrongDuplicationAnswer() = require(this.toSet().size == this.length) { GAME_OVER_MESSAGE }
 
 }
