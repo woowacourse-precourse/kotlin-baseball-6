47a98867830e90c9bdb09cdcd0d8345d38e1f419
rlaxodud214 <rlaxodud214@naver.com>
2023-10-21 00:54:43 +0900

    Refactor: ball, strike 검증 코드 calculate.kt로 옮김

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -32,8 +32,7 @@ fun main() {
         )
         if (inputData.size == BASEBALL_DIGITS) {
             val isAnswer = calculateBallAndStrike(inputData, answer)
-            if (isAnswer == false)
-                continue
+            if (isAnswer == false) continue
         }
 
         // 4. 메뉴 입력 받기 : 1 or 2
@@ -49,41 +48,4 @@ fun main() {
             isStay = false
         }
     } while (isStay)
-}
-
-/** [3]. 1) 매개변수 2개를 이용하여 "S, B" 형태로 변환 */
-fun calculateBallAndStrike(inputData: IntArray, answer: IntArray): Boolean {
-    var calculateResult = "" + inputData.filterIndexed { index, i ->
-        i in answer.filter { it != answer[index] } // Ball
-    }.size
-
-    calculateResult += ", " + inputData.filterIndexed { index, i ->
-        i == answer[index] // Strike
-    }.size
-
-    // TODO For Debuging
-    // println(calculateResult)
-
-    calculateResultPrint(calculateResult)
-    if(calculateResult == "0, ${BASEBALL_DIGITS}")
-        return true
-    return false
-}
-
-/** [3]. 2) "S, B" 형태의 값에 따라 문구 출력, 정답을 맞췄는지 반환 */
-fun calculateResultPrint(calculateResult: String) {
-    val (ball, strike) = calculateResult.split(", ")
-
-    if (strike == "${BASEBALL_DIGITS}") {
-        println("${BASEBALL_DIGITS}스트라이크")
-        println("${BASEBALL_DIGITS}개의 숫자를 모두 맞히셨습니다! 게임 종료")
-    } else if (strike != "0" && ball != "0") {
-        println("${ball}볼 ${strike}스트라이크")
-    } else if (strike == "0" && ball == "0") {
-        println("낫싱")
-    } else if (ball == "0") {
-        println("${strike}스트라이크")
-    } else if (strike == "0") {
-        println("${ball}볼")
-    }
 }
