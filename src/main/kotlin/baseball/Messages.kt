9aa62cb1e821be7c44411642c14768efef9166ac
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2023-10-20 23:16:50 +0900

    refactor: move hard coded messages

--- src/main/kotlin/baseball/Messages.kt
+++ src/main/kotlin/baseball/Messages.kt
@@ -4,4 +4,12 @@ object Messages {
 
     const val DuplicatedNumber = "중복된 숫자가 존재합니다."
     const val ContainsNonDigitChar = "숫자가 아닌 값이 존재합니다."
+    const val Welcome = "숫자 야구 게임을 시작합니다."
+    const val Bye = "게임 종료"
+    const val TypeNumber = "숫자를 입력해주세요 : "
+    const val FinishInGame = "3개의 숫자를 모두 맞히셨습니다! 게임 종료"
+    const val GameOption = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
+    const val Nothing = "낫싱"
+    const val Ball = "볼"
+    const val Strike = "스트라이크"
 }
--- src/main/kotlin/baseball/Printer.kt
+++ src/main/kotlin/baseball/Printer.kt
@@ -3,23 +3,23 @@ package baseball
 class Printer {
 
     fun printWelcome() {
-        println("숫자 야구 게임을 시작합니다.")
+        println(Messages.Welcome)
     }
 
     fun printBye() {
-        println("게임 종료")
+        println(Messages.Bye)
     }
 
     fun printTypeNumber() {
-        print("숫자를 입력해주세요 : ")
+        print(Messages.TypeNumber)
     }
 
     fun printFinishInGame() {
-        println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
+        println(Messages.FinishInGame)
     }
 
     fun printGameOption() {
-        println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+        println(Messages.GameOption)
     }
 
     fun printJudgeResult(judgeResult: JudgeResult) {
@@ -29,17 +29,17 @@ class Printer {
 
     private fun JudgeResult.toMessage(): String {
         if (balls == 0 && strikes == 0) {
-            return "낫싱"
+            return Messages.Nothing
         }
         var message = ""
         if (balls > 0) {
-            message += "${balls}볼"
+            message += "${balls}${Messages.Ball}"
         }
         if (strikes > 0) {
             if (message.isNotEmpty()) {
                 message += " "
             }
-            message += "${strikes}스트라이크"
+            message += "${strikes}${Messages.Strike}"
         }
         return message
     }
