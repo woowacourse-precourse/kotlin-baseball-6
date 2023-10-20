1f19c1b18b4f63ee01b0766f22d9bf1e33ee4fd8
JunJangE <wnswkd486@gmail.com>
2023-10-20 10:27:23 +0900

    feat: 랜덤 야구 숫자 생성 기능

--- src/main/kotlin/baseball/domain/BaseballGame.kt
+++ src/main/kotlin/baseball/domain/BaseballGame.kt
@@ -1,3 +1,14 @@
 package baseball.domain
+
+import camp.nextstep.edu.missionutils.Randoms
+
 class BaseballGame {
+    fun generateRandomNumber(): String {
+        val uniqueNumbers = mutableSetOf<Int>()
+        while (uniqueNumbers.size < 3) {
+            val randomNumber = Randoms.pickNumberInRange(1, 9)
+            uniqueNumbers.add(randomNumber)
+        }
+        return uniqueNumbers.joinToString("")
+    }
 }
