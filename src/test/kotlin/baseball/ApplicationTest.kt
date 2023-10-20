490043ee3c83af8539b2aa2086dbf450508afa85
pipeline <sorkwhwo@naver.com>
2023-10-20 21:29:24 +0900

    test: 테스트 케이스 추가

--- src/test/kotlin/baseball/ApplicationTest.kt
+++ src/test/kotlin/baseball/ApplicationTest.kt
@@ -21,9 +21,27 @@ class ApplicationTest : NsTest() {
     }
 
     @Test
+    fun `게임종료 후 재시작2`() {
+        assertRandomNumberInRangeTest(
+            {
+                run("246", "1", "958", "123", "189", "985", "579", "487", "589", "2")
+                assertThat(output())
+                    .contains("3스트라이크", "3볼", "낫싱", "2스트라이크", "2볼 1스트라이크", "2스트라이크", "1스트라이크", "3스트라이크", "게임 종료")
+            },
+            2, 4, 6, 5, 8, 9
+        )
+    }
+
+    @Test
     fun `예외 테스트`() {
         assertSimpleTest {
             assertThrows<IllegalArgumentException> { runException("1234") }
+            assertThrows<IllegalArgumentException> { runException("abc") }
+            assertThrows<IllegalArgumentException> { runException("12") }
+            assertThrows<IllegalArgumentException> { runException("122") }
+            assertThrows<IllegalArgumentException> { runException("1223") }
+            assertThrows<IllegalArgumentException> { runException("1 3") }
+            assertThrows<IllegalArgumentException> { runException("1 3 5") }
         }
     }
 
