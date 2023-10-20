ecbd40d5e124562c57ccdb84839f9d7ae8e70738
pipeline <sorkwhwo@naver.com>
2023-10-20 21:23:05 +0900

    feat: 메인 함수에서 게임 시작

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -5,6 +5,9 @@ import camp.nextstep.edu.missionutils.Randoms
 
 fun main() {
     println("숫자 야구 게임을 시작합니다.")
+    do {
+        playGame()
+    } while (askReplay())
 }
 
 fun generateComputerNumbers(): List<Int> {
