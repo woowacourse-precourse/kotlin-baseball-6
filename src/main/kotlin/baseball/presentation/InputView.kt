0f43627de2b065e6507b7ddeb07d619366fa242e
JunJangE <wnswkd486@gmail.com>
2023-10-20 09:34:21 +0900

    feat: 숫자를 입력하는 기능

--- src/main/kotlin/baseball/presentation/InputView.kt
+++ src/main/kotlin/baseball/presentation/InputView.kt
@@ -1,3 +1,19 @@
 package baseball.presentation
+
+import camp.nextstep.edu.missionutils.Console
+import utils.Exception.userWrongDuplicationAnswer
+import utils.Exception.userWrongSizeAnswer
+import utils.Exception.userWrongTypeAnswer
+
 class InputView {
+
+    fun getInputNumber(): String {
+        val number: String = Console.readLine()
+
+        number.userWrongTypeAnswer()
+        number.userWrongSizeAnswer()
+        number.userWrongDuplicationAnswer()
+
+        return number
+    }
 }
