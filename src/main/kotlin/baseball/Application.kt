b0b7aa7ad7d3e1dd05900bab0e6553ac8bf30ea1
Hogu59 <dbstjdguswkd@naver.com>
2023-10-21 17:31:20 +0900

    출력 - 게임 시작 문구 출력

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -4,12 +4,20 @@ import camp.nextstep.edu.missionutils.Randoms
 import camp.nextstep.edu.missionutils.Console
 
 fun main() {
-    val computer = mutableListOf<Int>()
-    while (computer.size < 3) {
-        val randomNumber = Randoms.pickNumberInRange(1, 9)
-        if (!computer.contains(randomNumber)) {
-            computer.add(randomNumber)
+
+    while(true) {
+        val computer = mutableListOf<Int>()
+        while (computer.size < 3) {
+            val randomNumber = Randoms.pickNumberInRange(1, 9)
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber)
+            }
         }
+        //computer.forEach { print(it) }
+
+
+        println("숫자 야구 게임을 시작합니다.")
+력
     }
-    computer.forEach { print(it) }
+
 }
