b8963da4facdbfc724c54f27dc7a6eb4f57dc3f8
윤동현 <donghyunyoon81@gmail.com>
2023-10-21 15:01:06 +0900

    feat: 게임 승리시 안내를 출력하는 기능 추가

--- src/main/kotlin/baseball/view/Guide.kt
+++ src/main/kotlin/baseball/view/Guide.kt
@@ -9,8 +9,12 @@ class Guide {
     fun inputUserNumber(){
         System.out.println(INPUT_NUMBER)
     }
+    fun succeed(){
+        System.out.println(SUCCEED)
+    }
     companion object {
         private const val GAME_START = "숫자 야구 게임을 시작합니다."
         private const val INPUT_NUMBER = "숫자를 입력해주세요 : "
+        private const val SUCCEED = "3개의 숫자를 모두 맞히셨습니다! 게임 종료"
     }
 }
