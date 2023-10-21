1ee220adf29e67a74e018d50356a268569e036fc
Jun <ayz1070@gmail.com>
2023-10-21 22:33:38 +0900

    3. 사용자 입력과 랜덤 생성값 비교하여 스트라이크, 볼 개수 세기

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -19,12 +19,23 @@ fun main() {
             answers.add(randomNumber)
         }
     }
-    print("숫자를 입력해주세요 : ")
-    numString = Console.readLine()
-    nums = numString.map{it.toString().toInt()}.toMutableList()
+    while(strike != 3){
+        print("숫자를 입력해주세요 : ")
+        numString = Console.readLine()
+        nums = numString.map{it.toString().toInt()}.toMutableList()
 
+        strike=0
+        ball=0
 
-    print(answers)
+        for(i in 0..2){
+            if(nums[i] in answers && nums[i]==answers[i]){
+                strike += 1
+            }else if (nums[i] in answers && nums[i]!=answers[i]){
+                ball += 1
+            }
+        }
+
+    }
 
 
 }
