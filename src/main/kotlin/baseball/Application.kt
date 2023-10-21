ad32494bd9fcbb572c9ee1350020e1f3cc5e2c55
junyong <junyong008@gmail.com>
2023-10-21 17:34:35 +0900

    feat: Add random number draw feature

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,7 +1,18 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
 
     // 시작 안내 문구 출력
     println("숫자 야구 게임을 시작합니다.")
+
+    // 1부터 9까지의 서로 다른 랜덤 숫자 3개 뽑기
+    val computer = mutableListOf<Int>()
+    while (computer.size < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (!computer.contains(randomNumber)) {
+            computer.add(randomNumber)
+        }
+    }
 }
