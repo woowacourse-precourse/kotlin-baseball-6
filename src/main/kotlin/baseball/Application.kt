2c92c04e276f4b38109462b4a6393ef37d8a9dc3
Sihwan Kim <vkn123@naver.com>
2023-10-22 02:48:09 +0900

    feat: 점수 출력기능 구현

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,13 +1,39 @@
 package baseball
 
-fun createRandom(): Int {
-    return (1..9).random()
+import camp.nextstep.edu.missionutils.Randoms
+import camp.nextstep.edu.missionutils.Console
+fun createComputerList(): List<Int> {
+    val computerList = mutableListOf<Int>()
+    while (computerList.size < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (randomNumber !in computerList) {
+            computerList.add(randomNumber)
+        }
+    }
+    return computerList
+}
+
+fun checkRestart(): Boolean {
+    println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+    val input = Console.readLine()
+    return when(input){
+        "1"-> true
+        "2"-> false
+        else -> throw IllegalArgumentException("잘못된 입력입니다.")
+    }
 }
 
 fun main() {
-    val computerList = listOf(createRandom(), createRandom(), createRandom())
-    val baseBall = BaseBall(computerList)
-    baseBall.startGame(computerList)
+    while(true){
+        val computerList =  createComputerList()
+        val baseBall = BaseBall(computerList)
+        baseBall.startGame(computerList)
+        if(!checkRestart()){
+            break
+        }
+    }
 }
+
+
 
 
--- src/main/kotlin/baseball/BaseBall.kt
+++ src/main/kotlin/baseball/BaseBall.kt
@@ -1,14 +1,62 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Console
+
+enum class GameStatus{
+    ONGOING,
+    STOP
+}
+
 class BaseBall(computerList: List<Int>) {
-    private val regex = Regex("^([1-9])([1-9])([1-9])\$")
+    private val regex = Regex("^(?!.*(\\d).*\\1)\\d{3}\$")
     private lateinit var userList: List<Int>
+    private var gameStatus = GameStatus.ONGOING
     fun startGame(computerList: List<Int>) {
-        while (true) {
-            val input = readln()
+        println("숫자 야구 게임을 시작합니다.")
+        while (gameStatus == GameStatus.ONGOING) {
+            print("숫자를 입력해주세요 : ")
+            val input = Console.readLine()
             checkInput(input)
             insertUserList(input)
+            gameStatus = printScore(computerList)
         }
+    }
+
+    private fun printScore(computerList: List<Int>):GameStatus {
+        val strike = countStrike(computerList)
+        val ball = countBall(computerList)
+
+        if(strike == 3){
+            println("3스트라이크")
+            println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
+            return GameStatus.STOP
+        }else if(strike ==0 && ball ==0){
+            println("낫싱")
+            return GameStatus.ONGOING
+        } else {
+            println("${ball}볼 ${strike}스트라이크")
+            return GameStatus.ONGOING
+        }
+    }
+
+    private fun countBall(computerList: List<Int>): Int {
+        var ball =0
+        computerList.forEach { item->
+            if(item in userList){
+                ball +=1
+            }
+        }
+        return ball
+    }
+
+    private fun countStrike(computerList: List<Int>): Int {
+        var strike = 0
+        computerList.forEachIndexed { index, item ->
+            if(item==userList[index]){
+                strike+=1
+            }
+        }
+        return strike
     }
 
 
