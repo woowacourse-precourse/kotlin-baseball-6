d51b1fe49df16c6186e9b0460f40c7a0f8a884d1
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2023-10-21 22:40:59 +0900

    refactor: 게임 지속 여부 입력 기능 분리

--- src/main/kotlin/baseball/GameManager.kt
+++ src/main/kotlin/baseball/GameManager.kt
@@ -1,7 +1,8 @@
 package baseball
 
-import baseball.util.GameState
+import baseball.util.GameState.GAME_RESTART_MESSAGE
 import baseball.util.GameState.GAME_START_MESSAGE
+import baseball.util.GameState.MATCHING_ALL_MESSAGE
 import baseball.util.GameValue.GAME_COMPLETE_STRIKE
 import baseball.util.GameValue.RESTART_COMMAND
 import baseball.util.GameValue.STOP_COMMAND
@@ -25,11 +26,19 @@ class GameManager : Game() {
         do {
             playerNumber = Player.inputNumber()
             result(playerNumber, computerNumber)
-        } while (strike != GAME_COMPLETE_STRIKE)
+        } while (isResult())
+    }
+
+    private fun isResult(): Boolean {
+        if (strike != GAME_COMPLETE_STRIKE) {
+            return true
+        }
+        println(MATCHING_ALL_MESSAGE)
+        return false
     }
 
     private fun restartQuestion(): Boolean {
-        println(GameState.GAME_RESTART_MESSAGE)
+        println(GAME_RESTART_MESSAGE)
         val playerCommend = Console.readLine()
         if (playerCommend != RESTART_COMMAND && playerCommend != STOP_COMMAND) {
             throw IllegalArgumentException()
