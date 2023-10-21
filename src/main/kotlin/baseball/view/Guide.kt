3b3290a8c4a98ce17759d41bab8c08b0d0489ac5
윤동현 <donghyunyoon81@gmail.com>
2023-10-21 14:49:56 +0900

    feat: 숫자 입력을 안내하는 문자를 출력하는 기능 추가

--- src/main/kotlin/baseball/view/Guide.kt
+++ src/main/kotlin/baseball/view/Guide.kt
@@ -6,7 +6,11 @@ class Guide {
     fun start() {
         System.out.println(GAME_START)
     }
+    fun inputUserNumber(){
+        System.out.println(INPUT_NUMBER)
+    }
     companion object {
         private const val GAME_START = "숫자 야구 게임을 시작합니다."
+        private const val INPUT_NUMBER = "숫자를 입력해주세요 : "
     }
 }
