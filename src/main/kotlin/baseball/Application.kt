20c55b6fcb93accac04c7b7ed9e65fc7969705b8
j2j2 <fineimagination@gmail.com>
2023-10-21 18:17:35 +0900

    사용자의 입력조건에 따른 예외처리 기능 구현

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -7,7 +7,15 @@ import kotlin.math.absoluteValue
 
 fun main() {
     val collectNumber = creatNumbers()
-    val userAnswer = userInput()
+    var userAnswer: List<Int>? = null
+    try {
+        userAnswer = userInput()
+    } catch (e: IllegalArgumentException) {
+        println(e.message)
+        return
+    }
+    println(collectNumber)
+    println(userAnswer)
 }
 
 fun creatNumbers(): List<Int> {
@@ -25,9 +33,27 @@ fun creatNumbers(): List<Int> {
 fun userInput(): List<Int> {
     print("서로 다른 3자리의 수를 입력해주세요 : ")
     val input = Console.readLine()
+    validateInput(input)
     val user = mutableListOf<Int>()
-    input.forEach{
+    input.forEach {
         user.add(it.toString().toInt())
     }
     return user
+}
+
+fun validateInput(input: String) {
+    // 숫자만 있는지 확인
+    if (!input.all { it.isDigit() }) {
+        throw IllegalArgumentException("입력은 숫자만 가능합니다.")
+    }
+
+    // 입력 길이 확인
+    if (input.length != 3) {
+        throw IllegalArgumentException("정확히 세 자리 숫자를 입력해주세요.")
+    }
+
+    // 중복된 숫자가 있는지 확인
+    if (input.toSet().size != input.length) {
+        throw IllegalArgumentException("숫자는 중복되지 않아야 합니다.")
+    }
 }
