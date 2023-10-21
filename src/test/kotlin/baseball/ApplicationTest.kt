94d0e994f1d3c9b9cba8da188407c67c6813b2ff
minuxx <minux8905@gmail.com>
2023-10-22 00:57:49 +0900

    test(ApplicationTest): 컴퓨터가 생성한 수가 유효한 지 예외 테스트 코드 추가
    - 의존 중인 Randoms가 의도하지 않게 동작했을 때를 대비해 예외를 발생시키지 않는지에 대한 테스트

--- src/test/kotlin/baseball/ApplicationTest.kt
+++ src/test/kotlin/baseball/ApplicationTest.kt
@@ -5,6 +5,7 @@ import camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest
 import camp.nextstep.edu.missionutils.test.NsTest
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
 
 class ApplicationTest : NsTest() {
@@ -24,6 +25,14 @@ class ApplicationTest : NsTest() {
     fun `예외 테스트`() {
         assertSimpleTest {
             assertThrows<IllegalArgumentException> { runException("1234") }
+        }
+    }
+
+    @Test
+    fun `컴퓨터가 생성한 수 유효성 검사 예외 테스트`() {
+        val computer = Computer()
+        assertSimpleTest {
+            assertDoesNotThrow { computer.generateNumbers() }
         }
     }
 
