e909679981a0e34cd471b3e58d92ba5f75c98228
윤동현 <donghyunyoon81@gmail.com>
2023-10-21 22:09:32 +0900

    refactor: Guide -> NoticeView로 변경

--- src/main/kotlin/baseball/view/NoticeView.kt
+++ src/main/kotlin/baseball/view/NoticeView.kt
@@ -1,24 +1,32 @@
 package baseball.view
 
 
-
-class Guide {
+class NoticeView {
     fun start() {
-        System.out.println(GAME_START)
+        println(GAME_START)
     }
-    fun inputUserNumber(){
-        System.out.println(INPUT_NUMBER)
+
+    fun inputUserNumber() {
+        println(INPUT_NUMBER)
     }
-    fun succeed(){
-        System.out.println(SUCCEED)
+
+    fun succeed() {
+        println(SUCCEED)
     }
-    fun restart(){
-        System.out.println(RESTART)
+
+    fun restart() {
+        println(RESTART)
     }
+
+    fun end() {
+        println(END)
+    }
+
     companion object {
         private const val GAME_START = "숫자 야구 게임을 시작합니다."
         private const val INPUT_NUMBER = "숫자를 입력해주세요 : "
         private const val SUCCEED = "3개의 숫자를 모두 맞히셨습니다! 게임 종료"
         private const val RESTART = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
+        private const val END = "게임 종료"
     }
 }
