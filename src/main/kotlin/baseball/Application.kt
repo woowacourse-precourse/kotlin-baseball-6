92e2e73e833a6c535bcc03379b448cb387f89d5f
tgyuu-An <atg0614@gmail.com>
2023-10-22 08:51:38 +0900

    [ADD] Player Class 생성

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -4,5 +4,4 @@ fun main() {
     println("숫자 야구 게임을 시작합니다.")
     val randomTargetGenerator = RandomTargetGenerator()
     val target = randomTargetGenerator.generateRandomTarget()
-    
 }
---
+++ src/test/kotlin/baseball/PlayerTest.kt
@@ -0,0 +1,5 @@
+package baseball
+
+import org.junit.jupiter.api.Assertions.*
+
+class PlayerTest
