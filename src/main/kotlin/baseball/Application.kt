60bbadd280515ae46a6e2f62d744f8732a392322
w36495 <w36495@naver.com>
2023-10-21 12:55:28 +0900

    feat : 컴퓨터로부터 1~9까지의 임의의 숫자를 입력받는다.

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,9 +1,25 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
-    TODO("프로그램 구현")
+    printStartGame()
+    val numberOfComputer = getNumberFromComputer()
 }
 
 fun printStartGame() {
     println("숫자 야구 게임을 시작합니다.")
+}
+
+fun getNumberFromComputer(): List<String> {
+    val randomNumbers = arrayListOf<String>()
+
+    while (randomNumbers.size < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9).toString()
+        if (!randomNumbers.contains(randomNumber)) {
+            randomNumbers.add(randomNumber)
+        }
+    }
+
+    return randomNumbers
 }
