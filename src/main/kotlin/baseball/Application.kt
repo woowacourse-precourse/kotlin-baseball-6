03d939c2fc0e55b4c2412544f558b15d2388b17c
Jun <ayz1070@gmail.com>
2023-10-21 22:25:01 +0900

    2. 랜덤한 3자리 정수 입력 받기

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -6,17 +6,25 @@ import camp.nextstep.edu.missionutils.Console
 
 fun main() {
     println("숫자 야구 게임을 시작합니다.")
-    print("숫자를 입력해주세요 : ")
+
     var numString:String
     var nums:MutableList<Int>
     var strike:Int = 0
     var ball:Int = 0
 
+    var answers:MutableList<Int> = mutableListOf()
+    while (answers.size < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (!answers.contains(randomNumber)) {
+            answers.add(randomNumber)
+        }
+    }
+    print("숫자를 입력해주세요 : ")
     numString = Console.readLine()
     nums = numString.map{it.toString().toInt()}.toMutableList()
-    print("$nums")
 
 
+    print(answers)
 
 
 }
