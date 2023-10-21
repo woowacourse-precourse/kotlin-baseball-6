b987fa8c0d6ab775a499cbb0f5099f22cdeef6d5
Jeongmin Park <60566546+jeongmin-0113@users.noreply.github.com>
2023-10-21 13:29:16 +0900

    게임 종료 로직 작성
    - 1, 2 이외 입력 시 예외 처리
    - 1 입력 시 게임 재시작
    - 2 입력 시 게임 종료

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,32 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
-    TODO("프로그램 구현")
+    println("숫자 야구 게임을 시작합니다.")
+    baseballGame()
+}
+
+fun baseballGame(): Unit {
+    var restartGame = true
+    while (restartGame) {
+
+        // 게임 종료
+        restartGame = inputRestart()
+    }
+}
+
+fun inputRestart(): Boolean {
+    print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요. ")
+    val inputAfterGameover = Console.readLine()
+    if (inputAfterGameover != "1" && inputAfterGameover != "2") {
+        throw IllegalArgumentException("1, 2 이외의 다른 입력 발생")
+    }
+
+    val gameRestartOption = inputAfterGameover.toInt()
+    if (gameRestartOption == 1) {
+        return true
+    }
+    return false
 }
