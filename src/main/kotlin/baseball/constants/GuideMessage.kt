b8eea16f6790ad22c7b81a98672c90e3e42d4711
murjune <sis9221302@gmail.com>
2023-10-21 21:42:47 +0900

    refactor(GameInstruction) : GameMessage -> Game 설명서로 네이밍 변경

--- src/main/kotlin/baseball/constants/GuideMessage.kt
+++
@@ -1,7 +0,0 @@
-package baseball.constants
-
-enum class GuideMessage(val msg: String) {
-    GAME_START("숫자 야구 게임을 시작합니다."),
-    ENTER_NUMBER("숫자를 입력해주세요 : "),
-    RESTART_OR_FINISH("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."),
-}
