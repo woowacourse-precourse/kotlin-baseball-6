d840926879152eee7c403a9bce5f8182f5983dd6
murjune <sis9221302@gmail.com>
2023-10-21 20:07:32 +0900

    style(GameResult) : 코틀린 가이드에 맞게 줄 바꿈

--- src/main/kotlin/baseball/constants/GameResult.kt
+++ src/main/kotlin/baseball/constants/GameResult.kt
@@ -1,5 +1,8 @@
 package baseball.constants
 
 enum class GameResult(val msg: String) {
-    STRIKE("스트라이크"), BALL("볼"), NOTHING("낫싱"), WIN("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
+    STRIKE("스트라이크"),
+    BALL("볼"),
+    NOTHING("낫싱"),
+    WIN("3개의 숫자를 모두 맞히셨습니다! 게임 종료"),
 }
