7a844a0036b28f0d52112d658622de036ba379b2
윤동현 <donghyunyoon81@gmail.com>
2023-10-21 15:03:42 +0900

    feat: 재시도 안내를 출력하는 기능 추가

--- src/main/kotlin/baseball/view/Guide.kt
+++ src/main/kotlin/baseball/view/Guide.kt
@@ -12,9 +12,13 @@ class Guide {
     fun succeed(){
         System.out.println(SUCCEED)
     }
+    fun restart(){
+        System.out.println(RESTART)
+    }
     companion object {
         private const val GAME_START = "숫자 야구 게임을 시작합니다."
         private const val INPUT_NUMBER = "숫자를 입력해주세요 : "
         private const val SUCCEED = "3개의 숫자를 모두 맞히셨습니다! 게임 종료"
+        private const val RESTART = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
     }
 }
