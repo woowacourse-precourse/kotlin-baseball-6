167cfbed6dc3a73524f31ba906044ccf27ca0111
getupminaaa <getupmina@gmail.com>
2023-10-21 19:03:34 +0900

    refactor: create Computer, System classes

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -14,8 +14,6 @@ fun main() {
 
         val userInput = getUserInput()
 
-        val regex = "[1-9]{3}".toRegex() //1-9사이의 숫자로 구성된 세 자릿수 정규표현식
-
         //regex와 일치하면서 set을 이용해 중복된 수가 있는지 확인
         checkUserInputValid(userInput)
 
