b2f05beab7fcedd75be696e55d4f6aea9750db8e
seonghyun <skyladder8895@gmail.com>
2023-10-21 09:45:51 +0900

    start

--- src/main/kotlin/baseball/BaseballGame.kt
+++ src/main/kotlin/baseball/BaseballGame.kt
@@ -1,3 +1,4 @@
+package baseball
 class BaseballGame {
     private val computerNum = mutableListOf<Int>()
 
@@ -14,4 +15,9 @@ class BaseballGame {
         }
     }
 
+    fun start {
+        println("숫자 야구 게임을 시작합니다.")
+
+        println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+    }
 }
