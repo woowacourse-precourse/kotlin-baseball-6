e498e83c3310b3298eb61a06d24db7cd89ee9e75
Hogu59 <dbstjdguswkd@naver.com>
2023-10-21 16:32:36 +0900

    기타기능 - 랜덤숫자 구현 세자리 랜덤숫자/ 각자리 '1'~'9'/ 중복숫자 없음

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,15 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
-    TODO("프로그램 구현")
+    val computer = mutableListOf<Int>()
+    while (computer.size < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (!computer.contains(randomNumber)) {
+            computer.add(randomNumber)
+        }
+    }
+    computer.forEach { print(it) }
 }
