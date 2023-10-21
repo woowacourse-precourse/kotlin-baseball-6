d2f6b2c9727b55a9c1d087184fef5c71fb77ab36
tgyuu-An <atg0614@gmail.com>
2023-10-21 19:29:08 +0900

    [CHORE] RandomTargetGenerator main 함수에 로직 추가

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -3,5 +3,6 @@ package baseball
 fun main() {
     println("숫자 야구 게임을 시작합니다.")
     val randomTargetGenerator = RandomTargetGenerator()
-    println(randomTargetGenerator.generateRandomTarget().toString())
+    val target = randomTargetGenerator.generateRandomTarget()
+    
 }
