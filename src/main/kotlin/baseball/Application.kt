25aa6fd649abcc3db31a885c0ffc3095ddaa6529
juseong <wjdwntjd55@naver.com>
2023-10-21 23:34:28 +0900

    Feat : 스트라이크인지 볼인지 구분하는 기능구현

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -3,3 +3,20 @@ package baseball
 fun main() {
     TODO("프로그램 구현")
 }
+
+data class GuessResult(val strikes: Int, val balls: Int)
+
+fun evaluateGuess(userNumbers: List<Int>, computerNumbers: List<Int>): GuessResult {
+    var strikes = 0
+    var balls = 0
+
+    for (i in userNumbers.indices) {
+        if (userNumbers[i] == computerNumbers[i]) {
+            strikes++
+        } else if (userNumbers[i] in computerNumbers) {
+            balls++
+        }
+    }
+
+    return GuessResult(strikes, balls)
+}
