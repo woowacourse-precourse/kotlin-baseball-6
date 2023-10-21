3cc065db5e1a19fbfbede0c60bde78eadb044d0a
윤동현 <donghyunyoon81@gmail.com>
2023-10-21 15:19:49 +0900

    feat: 게임 실행 기능 추가(미구현)

---
+++ src/main/kotlin/baseball/controller/BaseBallGame.kt
@@ -0,0 +1,17 @@
+package baseball.controller
+
+import baseball.model.StrikeNumber
+import baseball.model.UserNumber
+import camp.nextstep.edu.missionutils.Randoms
+
+class BaseBallGame {
+
+    lateinit var strikeNumber: StrikeNumber
+    lateinit var userNumber :UserNumber
+
+    fun play(){
+
+    }
+    fun getStrikeNumber(){
+    }
+}
