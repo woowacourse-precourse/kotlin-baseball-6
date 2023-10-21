be2c44372bcf189cf68337773bf757f49d47bb21
EunseoPark <88071251+arty0928@users.noreply.github.com>
2023-10-21 23:10:29 +0900

    input distinct 3 numbers

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,24 @@
 package baseball
 
 fun main() {
-    TODO("프로그램 구현")
+    print("숫자 야구 게임을 시작합니다.\n")
+    print("서로 다른 3자리 숫자를 입력해주세요 : ")
+
+    val userInput = readLine()
+    val userInputList = mutableListOf<Int>()
+
+    if (!userInput.isNullOrBlank()) {
+        for (char in userInput) {
+            val digit = Character.getNumericValue(char)
+            if (digit in 0..9) {
+                userInputList.add(digit)
+            }
+        }
+    }
+
+    if (userInputList.size != 3 || userInputList.distinct().size != 3) {
+        throw IllegalArgumentException("서로 다른 3자리 숫자를 입력해야 합니다.")
+    }
+
+    println("입력한 숫자 리스트: $userInputList")
 }
