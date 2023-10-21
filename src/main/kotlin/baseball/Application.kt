8948848d7333d1e5b25d64e2693f2bd7248f40d3
gretea5 <gretea4489@gmail.com>
2023-10-21 12:17:32 +0900

    chore: 게임 재시작 부분 문자열 오타 수정

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -21,7 +21,7 @@ fun gamePlay() {
         val result = calCount(computer, input)
         println(result)
         if(result.equals("3스트라이크")) {
-            println("3개의 숫자를 모두 맞히셨습니다! 게임종료")
+            println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
             println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
             val reInput = Console.readLine()
             if(reInput[0] == '1') {
