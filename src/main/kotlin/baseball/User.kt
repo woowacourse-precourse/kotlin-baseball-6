7e4ae743f720b7526f29660b10028e3825f0aad0
jun <hky08067@naver.com>
2023-10-21 17:29:09 +0900

    feat: 사용자 3개의 숫자 입력 및 유효성 검사

---
+++ src/main/kotlin/baseball/User.kt
@@ -0,0 +1,18 @@
+package baseball
+
+import camp.nextstep.edu.missionutils.Console
+
+object User {
+
+    fun enterUserNumbers(): String = Console.readLine()
+
+    fun String.userNumberValidation() {
+        require(length == 3)
+
+        map {
+            require(it in '1'..'9')
+        }
+
+        require(toSet().size == 3)
+    }
+}
