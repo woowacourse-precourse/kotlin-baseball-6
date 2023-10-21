6ff6c76b4d8c8e785f413d46fcdcd15d3ae155bf
yeji0214 <1225yj50@naver.com>
2023-10-22 01:49:35 +0900

    숫자 랜덤으로 3개 생성

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,15 @@
 package baseball
+import camp.nextstep.edu.missionutils.Randoms
 
 fun main() {
-    TODO("프로그램 구현")
+    val SIZE = 3 // 리스트의 길이
+    val computer = mutableListOf<String>()
+
+    while (computer.size < SIZE) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9).toString() // 랜덤 수 생성
+        if (!computer.contains(randomNumber)) {
+            computer.add(randomNumber)
+        }
+    }
+    println(computer)
 }
