56040c3a266a31eb9b86a5d855c5e35cb69f41b3
EunseoPark <88071251+arty0928@users.noreply.github.com>
2023-10-22 00:35:36 +0900

    2. check ball and strike count

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,8 +1,18 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
     print("숫자 야구 게임을 시작합니다.\n")
     print("서로 다른 3자리 숫자를 입력해주세요 : ")
+
+    val computer = mutableListOf<Int>()
+    while (computer.size < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (!computer.contains(randomNumber)) {
+            computer.add(randomNumber)
+        }
+    }
 
     val userInput = readLine()
     val userInputList = mutableListOf<Int>()
@@ -18,6 +28,36 @@ fun main() {
 
     if (userInputList.size != 3 || userInputList.distinct().size != 3) {
         throw IllegalArgumentException("서로 다른 3자리 숫자를 입력해야 합니다.")
+    }
+
+    var ball = 0
+    var strike = 0
+
+    for (num in userInputList){
+        if (num in computer){
+            val computerNum = computer.indexOf(num)
+            val myNum = userInputList.indexOf(num)
+            if (myNum == computerNum){
+                strike +=1
+            }
+            else{
+                ball +=1
+            }
+        }
+    }
+
+    if(ball==0){
+        if(strike==0){
+            print("낫싱\n")
+        }else{
+            print("$strike"+"스트라이크\n")
+        }
+    }else{
+        if(strike==0){
+            print("$ball"+"볼\n")
+        }else{
+            print("$ball"+"볼 "+"$strike"+"스트라이크\n")
+        }
     }
 
     println("입력한 숫자 리스트: $userInputList")
