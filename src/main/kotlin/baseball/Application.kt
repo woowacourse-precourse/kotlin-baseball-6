cdf74777923fcc1e4aee1edb097e80cad3b85bc9
yein <inwon9892@naver.com>
2023-10-22 00:07:19 +0900

    [feat] Make computer's random number

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,21 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
-    TODO("프로그램 구현")
+    // TODO("프로그램 구현")
+    println("숫자 야구 게임을 시작합니다.")
+    val ComNumber = ComRandomNum()
+}
+
+// 컴퓨터가 세자리 수의 랜덤값 생성
+fun ComRandomNum(): MutableList<Int> {
+    val computerNum = mutableListOf<Int>()
+    while (computerNum.size < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (!computerNum.contains(randomNumber)) {
+            computerNum.add(randomNumber)
+        }
+    }
+    return computerNum
 }
