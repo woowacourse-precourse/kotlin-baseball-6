8d4e6cd92f2d52ad75098817075419793705dc4d
murjune <sis9221302@gmail.com>
2023-10-21 21:43:05 +0900

    refactor(GameResult) : msg -> message

--- src/main/kotlin/baseball/constants/GameResult.kt
+++ src/main/kotlin/baseball/constants/GameResult.kt
@@ -1,8 +1,12 @@
 package baseball.constants
 
-enum class GameResult(val msg: String) {
+enum class GameResult(val message: String) {
     STRIKE("스트라이크"),
     BALL("볼"),
     NOTHING("낫싱"),
-    WIN("3개의 숫자를 모두 맞히셨습니다! 게임 종료"),
+    WIN("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+
+    override fun toString(): String {
+        return this.message
+    }
 }
