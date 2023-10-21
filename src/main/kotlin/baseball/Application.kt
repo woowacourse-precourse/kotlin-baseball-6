22f1887d4d79c6dd5ecef00b0201d45bc0200442
yeji0214 <1225yj50@naver.com>
2023-10-22 01:57:16 +0900

    사용자에게 숫자 입력 받아 리스트에 저장

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -4,6 +4,7 @@ import camp.nextstep.edu.missionutils.Randoms
 fun main() {
     val SIZE = 3 // 리스트의 길이
     val computer = mutableListOf<String>()
+    val user = mutableListOf<String>()
 
     while (computer.size < SIZE) {
         val randomNumber = Randoms.pickNumberInRange(1, 9).toString() // 랜덤 수 생성
@@ -11,5 +12,12 @@ fun main() {
             computer.add(randomNumber)
         }
     }
+
+    print("숫자를 입력해주세요 : ")
+    val userNumber = readLine() ?: throw IllegalArgumentException("입력값이 NULL")// 사용자에게 입력받기
+    for (i in 0 until SIZE) {
+        user.add(userNumber[i].toString())
+    }
     println(computer)
+    println(user)
 }
