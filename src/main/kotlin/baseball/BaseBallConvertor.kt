28ad33f2b652cdf3af94e5e860be03711ec609ad
murjune <sis9221302@gmail.com>
2023-10-21 21:44:04 +0900

    feat(BaseBallConvertor) : input값을 baseBalls로 바꾸는 클래스

---
+++ src/main/kotlin/baseball/BaseBallConvertor.kt
@@ -0,0 +1,15 @@
+package baseball
+
+class BaseBallConvertor(private val ballsValidator: BaseBallsValidator = BaseBallsValidator()) {
+    fun convert(numbers: String): List<Int> {
+        val baseBalls = try {
+            numbers.trim().map { it.toString().toInt() }
+        } catch (e: NumberFormatException) {
+            throw IllegalArgumentException()
+        }
+
+        ballsValidator.validate(baseBalls)
+
+        return baseBalls
+    }
+}
