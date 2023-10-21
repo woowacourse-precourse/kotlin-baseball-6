9b0272d57eaf85855e753b534cfbc32d599208bb
Jeongmin Park <60566546+jeongmin-0113@users.noreply.github.com>
2023-10-21 23:29:10 +0900

    랜덤 숫자 뽑기 로직 작성
    - 랜덤 숫자 뽑기 로직 작성
    - 종료 로직 변수 이름 수정

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -11,20 +11,26 @@ fun main() {
 fun baseballGame(): Unit {
     var restartGame = true
     while (restartGame) {
+        // 매 게임의 타겟 숫자 3개 선택
+        var randomNumbers = mutableListOf<Int>()
+        for (i in 0..2) {
+            randomNumbers.add(Randoms.pickNumberInRange(1, 9))
+        }
 
         // 게임 종료
         restartGame = inputRestart()
     }
 }
 
+
 fun inputRestart(): Boolean {
     print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요. ")
-    val inputAfterGameover = Console.readLine()
-    if (inputAfterGameover != "1" && inputAfterGameover != "2") {
+    val inputAfterGameOver = Console.readLine()
+    if (inputAfterGameOver != "1" && inputAfterGameOver != "2") {
         throw IllegalArgumentException("1, 2 이외의 다른 입력 발생")
     }
 
-    val gameRestartOption = inputAfterGameover.toInt()
+    val gameRestartOption = inputAfterGameOver.toInt()
     if (gameRestartOption == 1) {
         return true
     }
