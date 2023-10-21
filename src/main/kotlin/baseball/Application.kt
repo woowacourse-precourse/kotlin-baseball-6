0a10fbe51801b80aeb39983c4479b04d910be59a
junyong <junyong008@gmail.com>
2023-10-21 22:08:34 +0900

    feat: Add number comparison feature

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -16,15 +16,39 @@ fun main() {
         }
     }
 
-    // 사용자 숫자 입력받기
-    print("숫자를 입력해주세요 : ")
-    val userInput = readLine()?.map { it.toString().toIntOrNull() }
+    while (true) {
+        // 사용자 숫자 입력받기
+        print("숫자를 입력해주세요 : ")
+        val userInput = readLine()?.map { it.toString().toIntOrNull() }
 
-    if (userInput == null ||
-        userInput.size != 3 ||
-        userInput.toSet().size != 3 ||
-        userInput.none { it in 1..9 }
-    ) {
-        throw IllegalArgumentException()
+        if (userInput == null ||
+            userInput.size != 3 ||
+            userInput.toSet().size != 3 ||
+            userInput.none { it in 1..9 }
+        ) {
+            throw IllegalArgumentException()
+        }
+
+        // 입력받은 숫자와 컴퓨터의 숫자를 비교하여 힌트 혹은 정답 여부 출력
+        var strikeCount = 0
+        var ballCount = 0
+
+        for (i in 0 until 3) {
+            if (userInput[i] == computer[i]) {
+                strikeCount++
+            } else if (userInput[i] in computer) {
+                ballCount++
+            }
+        }
+
+        val hint = when {
+            strikeCount == 3 -> "3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료"
+            strikeCount > 0 && ballCount > 0 -> "${ballCount}볼 ${strikeCount}스트라이크"
+            strikeCount > 0 -> "${strikeCount}스트라이크"
+            ballCount > 0 -> "${ballCount}볼"
+            else -> "낫싱"
+        }
+        println(hint)
+        if (strikeCount == 3) break
     }
 }
