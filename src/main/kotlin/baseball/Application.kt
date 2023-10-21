088d4ac87b3185f400af08c3e58db7845a8bec90
yeji0214 <1225yj50@naver.com>
2023-10-22 02:11:12 +0900

    사용자가 맞힐 때까지 반복처리

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -17,36 +17,40 @@ fun main() {
     }
     println(computer)
 
-    print("숫자를 입력해주세요 : ")
-    val userNumber = readLine() ?: throw IllegalArgumentException("입력값이 NULL")// 사용자에게 입력받기
-    for (i in 0 until SIZE) {
-        user.add(userNumber[i].toString())
-    }
+    while (s != 3) {
+        s = 0
+        b = 0
+        user.clear()
+        print("숫자를 입력해주세요 : ")
+        val userNumber = readLine() ?: throw IllegalArgumentException("입력값이 NULL")// 사용자에게 입력받기
+        for (i in 0 until SIZE) {
+            user.add(userNumber[i].toString())
+        }
 
-    for (i in 0 until SIZE) {
-        for (j in 0 until SIZE) {
-            if (computer[i] == user[j]) {
-                if (i == j) {
-                    s++
-                } else {
-                    b++
+        for (i in 0 until SIZE) {
+            for (j in 0 until SIZE) {
+                if (computer[i] == user[j]) {
+                    if (i == j) {
+                        s++
+                    } else {
+                        b++
+                    }
                 }
             }
         }
-    }
-    if (b == 0 && s == 0) {
-        println("낫싱")
-    } else if (b > 0 && s > 0) {
-        println("${b}볼 ${s}스트라이크")
-    } else if (s > 0) {
-        println("${s}스트라이크")
-        if (s == 3) {
-            println("${SIZE}개의 숫자를 모두 맞히셨습니다! 게임 종료")
+        if (b == 0 && s == 0) {
+            println("낫싱")
+        } else if (b > 0 && s > 0) {
+            println("${b}볼 ${s}스트라이크")
+        } else if (s > 0) {
+            println("${s}스트라이크")
+            if (s == 3) {
+                println("${SIZE}개의 숫자를 모두 맞히셨습니다! 게임 종료")
+            }
+        } else if (b > 0) {
+            println("${b}볼")
         }
-    } else if (b > 0) {
-        println("${b}볼")
+
     }
-
-
     println(user)
 }
