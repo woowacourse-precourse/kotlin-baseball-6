f0ca995d3c384fddecd94e45c06ad4a41b5e7148
minuxx <minux8905@gmail.com>
2023-10-22 00:39:00 +0900

    refactor(Computer, Validator): 생성한 수에 대한 유효성 검사 객체 추가
    - 유효성 검사 테스트를 담당하는 Validator 싱글톤 객체 추가
    - Computer generateNumbers 메서드 수정

--- src/main/kotlin/baseball/Computer.kt
+++ src/main/kotlin/baseball/Computer.kt
@@ -2,39 +2,25 @@ package baseball
 
 import camp.nextstep.edu.missionutils.Randoms
 
-const val START_NUMBER = 1
-const val END_NUMBER = 9
-const val NUMBER_COUNT = 3
-
 class Computer : Player<Unit> {
     private lateinit var numbers: List<Int>
 
     override fun generateNumbers() {
-        numbers = Randoms.pickUniqueNumbersInRange(START_NUMBER, END_NUMBER, NUMBER_COUNT)
+        val generatedNumbers = Randoms.pickUniqueNumbersInRange(START_NUMBER, END_NUMBER, NUMBER_COUNT)
+        println(generatedNumbers)
 
-        if (areInvalidCountNumbers(numbers)) {
+        if (Validator.areInvalidCountNumbers(generatedNumbers)) {
             throw IllegalArgumentException("수의 개수가 올바르지 않습니다.")
         }
 
-        if (areOutOfRangeNumbers(numbers)) {
+        if (Validator.areOutOfRangeNumbers(generatedNumbers)) {
             throw IllegalArgumentException("수의 범위가 잘못되었습니다.")
         }
 
-        if (areDuplicateNumbers(numbers)) {
+        if (Validator.areDuplicateNumbers(generatedNumbers)) {
             throw IllegalArgumentException("중복된 수가 있습니다.")
         }
-    }
 
-    private fun areInvalidCountNumbers(numbers: List<Int>): Boolean {
-        return numbers.size == NUMBER_COUNT
-    }
-
-    private fun areOutOfRangeNumbers(numbers: List<Int>): Boolean {
-        return numbers.all { it in START_NUMBER..END_NUMBER }
-    }
-
-    private fun areDuplicateNumbers(numbers: List<Int>): Boolean {
-        val distinctNumbers = numbers.toSet()
-        return numbers.size == distinctNumbers.size
+        numbers = generatedNumbers
     }
 }
---
+++ src/main/kotlin/baseball/Validator.kt
@@ -0,0 +1,20 @@
+package baseball
+
+const val START_NUMBER = 1
+const val END_NUMBER = 9
+const val NUMBER_COUNT = 3
+
+object Validator {
+    fun areInvalidCountNumbers(numbers: List<Int>): Boolean {
+        return numbers.size != NUMBER_COUNT
+    }
+
+    fun areOutOfRangeNumbers(numbers: List<Int>): Boolean {
+        return numbers.any { it < START_NUMBER || it > END_NUMBER }
+    }
+
+    fun areDuplicateNumbers(numbers: List<Int>): Boolean {
+        val distinctNumbers = numbers.toSet()
+        return numbers.size != distinctNumbers.size
+    }
+}
