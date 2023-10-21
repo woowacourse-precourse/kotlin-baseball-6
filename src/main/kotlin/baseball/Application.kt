ac8d0ff3152e88603c25aa9f7e2b5c075c291446
j2j2 <fineimagination@gmail.com>
2023-10-21 17:45:22 +0900

    사용자의 입력을 받는 기능 구현

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,15 +1,18 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Console
 import camp.nextstep.edu.missionutils.Randoms
 import java.lang.IllegalArgumentException
+import kotlin.math.absoluteValue
 
 fun main() {
-    val answer = creatNumbers()
+    val collectNumber = creatNumbers()
+    val userAnswer = userInput()
 }
 
 fun creatNumbers(): List<Int> {
     val computer = mutableListOf<Int>()
-    while (computer.size < 3){
+    while (computer.size < 3) {
         val randomNumber = Randoms.pickNumberInRange(1, 9)
         if (!computer.contains(randomNumber)) {
             computer.add(randomNumber)
@@ -17,4 +20,14 @@ fun creatNumbers(): List<Int> {
     }
     println("숫자 야구 게임을 시작합니다.")
     return computer
+}
+
+fun userInput(): List<Int> {
+    print("서로 다른 3자리의 수를 입력해주세요 : ")
+    val input = Console.readLine()
+    val user = mutableListOf<Int>()
+    input.forEach{
+        user.add(it.toString().toInt())
+    }
+    return user
 }
