57e8f4fda70754d670a488d224dea889aab6f4c2
j2j2 <fineimagination@gmail.com>
2023-10-21 16:59:07 +0900

    3자리 수 생성 기능 구현

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,19 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+import java.lang.IllegalArgumentException
+
 fun main() {
-    TODO("프로그램 구현")
+    val answer = creatNumbers()
+}
+
+fun creatNumbers(): List<Int> {
+    val computer = mutableListOf<Int>()
+    while (computer.size < 3){
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (!computer.contains(randomNumber)) {
+            computer.add(randomNumber)
+        }
+    }
+    return computer
 }
