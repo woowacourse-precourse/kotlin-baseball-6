6eda278063f1e0cef218ce361c9ef6114ec0062f
getupminaaa <getupmina@gmail.com>
2023-10-21 19:01:31 +0900

    refactor: change if/else of ''checkUserInputValid' function into if  and remove empty body

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -100,6 +100,5 @@ fun getUserInput(): String {
 fun checkUserInputValid(userInput: String) {
     val regex = "[1-9]{3}".toRegex() //1-9사이의 숫자로 구성된 세 자릿수 정규표현식
     //regex와 일치하면서 set을 이용해 중복된 수가 있는지 확인
-    if ((regex.matches(userInput) && (userInput.length == userInput.toSet().size))) {
-    } else throw IllegalArgumentException()
+    if (!regex.matches(userInput) || userInput.length != userInput.toSet().size) throw IllegalArgumentException()
 }
