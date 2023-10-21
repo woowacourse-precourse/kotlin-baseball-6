36f26ced2d4dd0d6113bd13590ab6ea9c0b9f4a4
juseong <wjdwntjd55@naver.com>
2023-10-21 23:40:53 +0900

    Feat : 결과 출력하는 기능 구현

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,9 +1,51 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Console
 import camp.nextstep.edu.missionutils.Randoms
+import kotlin.system.exitProcess
 
 fun main() {
-    TODO("프로그램 구현")
+    while (true) {
+        val computerNumbers = generateComputerNumbers()
+        println("숫자 야구 게임을 시작합니다.")
+
+        var attempts = 0
+
+        while (true) {
+            print("숫자를 입력해주세요 : ")
+            val userInput = Console.readLine()
+
+            try {
+                val userNumbers = userInput.split("").mapNotNull { it.toIntOrNull() }
+
+                if (userNumbers.size != 3 || userNumbers.any { it < 1 || it > 9 }) {
+                    throw IllegalArgumentException()
+                }
+
+                val result = evaluateGuess(userNumbers, computerNumbers)
+                printResult(result)
+
+                attempts++
+
+                if (result.strikes == 3) {
+                    println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
+                    break
+                }
+
+            } catch (e: IllegalArgumentException) {
+                exitProcess(0)
+            }
+        }
+
+        println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+        val choice = Console.readLine()
+
+        if (choice == "2") {
+            break
+        }
+    }
+
+    println("게임을 종료합니다.")
 }
 
 data class GuessResult(val strikes: Int, val balls: Int)
@@ -32,4 +74,17 @@ fun generateComputerNumbers(): List<Int> {
         }
     }
     return computer
+}
+
+fun printResult(result: GuessResult) {
+    if (result.balls > 0) {
+        print("${result.balls}볼 ")
+    }
+    if (result.strikes > 0) {
+        print("${result.strikes}스트라이크 ")
+    }
+    if (result.strikes == 0 && result.balls == 0) {
+        print("낫싱 ")
+    }
+    println()
 }
