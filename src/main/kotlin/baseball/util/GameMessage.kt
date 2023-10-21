835645e505df252e6b18a1129bc1448830c19551
jaehan4707 <jahan4707@naver.com>
2023-10-21 17:28:56 +0900

    fix : 게임 종료 메시지 수정

--- src/main/kotlin/baseball/util/GameMessage.kt
+++ src/main/kotlin/baseball/util/GameMessage.kt
@@ -5,7 +5,7 @@ object GameMessage {
     const val GAME_INPUT_NUMBER_MESSAGE = "숫자를 입력해주세요 : "
     const val GAME_SUCCESS_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! "
     const val GAME_END_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
-    const val GAME_OVER_MESSAGE = "게임종료"
+    const val GAME_OVER_MESSAGE = "게임 종료"
     const val GAME_ERROR_INPUT_LENGTH = "입력은 3글자입니다."
     const val GAME_ERROR_INPUT_NUMBER = "입력은 숫자로 구성되어야 합니다."
     const val GAME_ERROR_INPUT_DUPLICATE = "입력은 서로 다른 숫자로 구성되어야 합니다."
