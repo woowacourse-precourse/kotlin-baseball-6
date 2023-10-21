d5b6e19ba810dec3312be35085da3829b62d6a03
윤동현 <donghyunyoon81@gmail.com>
2023-10-21 22:20:12 +0900

    rename: 이름 변경

--- src/main/kotlin/baseball/view/UserInputView.kt
+++ src/main/kotlin/baseball/view/UserInputView.kt
@@ -2,7 +2,7 @@ package baseball.view
 
 import camp.nextstep.edu.missionutils.Console
 
-class UserInput {
+class UserInputView {
     fun getBaseBallNum():String = Console.readLine()
 
     fun getRetryNum():String = Console.readLine()
