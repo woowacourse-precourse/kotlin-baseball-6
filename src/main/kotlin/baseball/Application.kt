18e1d4e713f080ec5f38419a9725c4e0818ea540
park-yina <111878820+park-yina@users.noreply.github.com>
2023-10-21 19:30:36 +0900

    error:낫띵이 0볼 0스트라이크로 처리되는 에러 발생

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,43 +1,84 @@
 package baseball
 
 import camp.nextstep.edu.missionutils.Randoms
+import java.util.Stack
 
 class NumberBaseball {
-    val computerNumber= mutableListOf<Int>()
-    val userNumber= mutableListOf<String>()
-    private fun inputException(inputNumber:MutableList<String>) {
-        if(inputNumber.size!=3)
-            throw IllegalArgumentException("입력된 숫자가 3개가 아닙니다.")
-        for(index in 0 .. inputNumber.size){
-            if(!(inputNumber[index].toInt() in 49..57))
-                //아스키 코드표상 49가 숫자 1 57이 9이므로 이 사이에 없다면 숫자가 아닌 것
-                throw IllegalArgumentException("입력은 숫자만 가능합니다.")
-            if(inputNumber[index]==inputNumber[index+1])
-                throw IllegalArgumentException("숫자의 중복 여부를 확인해주세요")
-        }
-    }
-    fun createComputerNumber(){
-        while(computerNumber.size<3) {
-            val randomNumber = Randoms.pickNumberInRange(1, 9)
-            if (!computerNumber.contains(randomNumber)) {
-                computerNumber.add(randomNumber)
+    var computerNumber = mutableListOf<String>()
+    var userNumber = mutableListOf<String>()
+    var startMent: String = "숫자를 입력해주세요:"
+
+    private fun inputException(inputNumber: MutableList<String>) {
+        for (index in 0 until inputNumber.size) {
+            if (inputNumber[index].length != 3)
+                throw IllegalArgumentException("입력된 숫자가 3개가 아닙니다.")
+            if (!inputNumber[index].matches(Regex("\\d+"))) {
+                throw IllegalArgumentException("숫자가 아닌 문자가 포함되어 있습니다.")
             }
         }
+        if (inputNumber.toSet().size != inputNumber.size) {
+            throw IllegalArgumentException("중복된 숫자가 존재합니다")
+        }
     }
-    fun startUser(){
-        println("숫자를 입력해주세요 : ")
 
+    fun createComputerNumber(): MutableList<String> {
+        val newComputerNumber = mutableListOf<String>()
+        while (newComputerNumber.size < 3) {
+            val randomNumber = Randoms.pickNumberInRange(1, 9).toString()
+            if (!newComputerNumber.contains(randomNumber))
+                newComputerNumber.add(randomNumber)
+        }
+        return newComputerNumber
     }
-    fun readInputNumber(){
-        startUser()
-        var userInput= readLine().toString()
+
+    fun nothing(userNumber: List<String>, computerNumber: List<String>): Boolean {
+        for (index in 0 until userNumber.size) {
+            if (!computerNumber.contains(userNumber[index])) {
+                println("낫띵")
+                return true
+            }
+        }
+        return false
+    }
+
+    fun ballOrstrike(userNumber: List<String>, computerNumber: List<String>): Boolean {
+        var strikeCount: Int = 0
+        var ballCount: Int = 0
+        for (index in 0 until userNumber.size) {
+            if (computerNumber[index] == userNumber[index])
+                strikeCount++
+            else if (computerNumber.contains(userNumber[index]))
+                ballCount++
+        }
+        println("${ballCount}볼 ${strikeCount}스트라이크")
+        return strikeCount == 3
+    }
+
+    fun readInputNumber(): MutableList<String> {
+        val userInput = readLine().toString()
         userNumber.add(userInput)
         inputException(userNumber)
+        return userNumber
     }
-    fun gameStart(){
+
+    fun gameStart() {
         println("숫자 야구 게임을 시작합니다.")
     }
+
     fun main() {
-       gameStart()
+        while (true) {
+            userNumber.clear()
+            computerNumber = createComputerNumber()
+            gameStart();
+            readInputNumber()
+            if (ballOrstrike(userNumber, computerNumber)) {
+                println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
+            }
+            println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+            var userInput = readLine()!!.toInt()
+            if (userInput == 2) {
+                break
+            }
+        }
     }
 }
