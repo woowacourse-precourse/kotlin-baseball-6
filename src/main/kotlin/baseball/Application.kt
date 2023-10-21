e6ad3fbfead64b79214be480e882b70e235d9a75
yeji0214 <1225yj50@naver.com>
2023-10-22 02:06:13 +0900

    안내메시지 출력

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -8,12 +8,14 @@ fun main() {
     var s = 0
     var b = 0
 
+    println("숫자 야구 게임을 시작합니다.")
     while (computer.size < SIZE) {
         val randomNumber = Randoms.pickNumberInRange(1, 9).toString() // 랜덤 수 생성
         if (!computer.contains(randomNumber)) {
             computer.add(randomNumber)
         }
     }
+    println(computer)
 
     print("숫자를 입력해주세요 : ")
     val userNumber = readLine() ?: throw IllegalArgumentException("입력값이 NULL")// 사용자에게 입력받기
@@ -38,10 +40,13 @@ fun main() {
         println("${b}볼 ${s}스트라이크")
     } else if (s > 0) {
         println("${s}스트라이크")
+        if (s == 3) {
+            println("${SIZE}개의 숫자를 모두 맞히셨습니다! 게임 종료")
+        }
     } else if (b > 0) {
         println("${b}볼")
     }
 
-    println(computer)
+
     println(user)
 }
