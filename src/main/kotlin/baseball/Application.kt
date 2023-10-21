c80150059e2b8f20cebbfdd2c5cf3441e74b692b
zoodung <zoodung@naver.com>
2023-10-22 03:32:59 +0900

    fix: 라운드 조건 검사 결과 3스트라이크 미출력 오류 수정

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -9,7 +9,7 @@ fun main() {
 /* 플레이어의 라운드 기록 구조체 Round */
 data class Round(var strike: Int = 0, var ball: Int = 0)
 
-/* 숫자 야구 시작 선언 function : printStart */
+/* 숫자 야구 시작 메세지 function : printStart */
 fun printStart(){
     println("숫자 야구 게임을 시작합니다.")
 }
@@ -58,7 +58,6 @@ fun getPlayerNumber(): List<Int> {
 
 /* 플레이어가 잘못된 값을 입력한 경우 function : ChckNumber */
 fun checkNumber(testNumber: String?): String{
-
     if (testNumber.isNullOrBlank()) {
         throw IllegalArgumentException("잘못된 입력입니다, 공백입니다.")
     }
@@ -100,20 +99,17 @@ fun compareList(computer: List<Int>, player: List<Int>): Round{
 
 /* 라운드 조건 검사 결과 출력 function : printHint*/
 fun printHint(roundResult: Round){
-    when {
-        roundResult.strike == 3 -> println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
-        roundResult.strike == 0 && roundResult.ball == 0 -> println("낫싱")
-        else -> println("${roundResult.ball}볼 ${roundResult.strike}스트라이크")
+    val hint = when {
+        roundResult.ball > 0 && roundResult.strike == 0 -> "${roundResult.ball}볼"
+        roundResult.strike > 0 && roundResult.ball == 0 -> "${roundResult.strike}스트라이크"
+        roundResult.ball > 0 && roundResult.strike > 0 -> "${roundResult.ball}볼 ${roundResult.strike}스트라이크"
+        else -> "낫싱"
     }
+    println(hint)
+    if(roundResult.strike == 3) println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
 }
+
 /*
-fun askRetry(): Boolean{
+fun askRetry(): Boolean {
 
 }*/
-
-
-
-
-
-
-
