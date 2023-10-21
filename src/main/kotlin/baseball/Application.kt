45ec15d2ddd9a331890dc5d274c349e33577fef4
EunseoPark <88071251+arty0928@users.noreply.github.com>
2023-10-22 00:46:27 +0900

    4. refactoring

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -2,19 +2,40 @@ package baseball
 
 import camp.nextstep.edu.missionutils.Randoms
 
+fun makeRandomNumber(): MutableList<Int> {
+    val computer = mutableListOf<Int>()
+    while (computer.size < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (!computer.contains(randomNumber)) {
+            computer.add(randomNumber)
+        }
+    }
+    return computer
+}
+
+fun printResult(ball: Int, strike : Int){
+    if(ball==0){
+        if(strike==0){
+            print("낫싱\n")
+        }else{
+            print("$strike"+"스트라이크\n")
+        }
+    }else{
+        if(strike==0){
+            print("$ball"+"볼\n")
+        }else{
+            print("$ball"+"볼 "+"$strike"+"스트라이크\n")
+        }
+    }
+}
+
 fun main() {
 
     var play = true
     print("숫자 야구 게임을 시작합니다.\n")
 
     while(play){
-        val computer = mutableListOf<Int>()
-        while (computer.size < 3) {
-            val randomNumber = Randoms.pickNumberInRange(1, 9)
-            if (!computer.contains(randomNumber)) {
-                computer.add(randomNumber)
-            }
-        }
+        val computer = makeRandomNumber()
 
         while(true){
             print("숫자를 입력해주세요 : ")
@@ -50,19 +71,7 @@ fun main() {
                 }
             }
 
-            if(ball==0){
-                if(strike==0){
-                    print("낫싱\n")
-                }else{
-                    print("$strike"+"스트라이크\n")
-                }
-            }else{
-                if(strike==0){
-                    print("$ball"+"볼\n")
-                }else{
-                    print("$ball"+"볼 "+"$strike"+"스트라이크\n")
-                }
-            }
+            printResult(ball,strike)
 
             if(strike==3){
                 print("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n")
