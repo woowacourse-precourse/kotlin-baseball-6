18239844ee20b1b641ccf74d0e8eb1a0d721ee90
stopkite <stopkite98@gmail.com>
2023-10-21 16:26:41 +0900

    feat: [BaseBallGame] 정답 여부를 판별해주는 루프문 생성

--- src/main/kotlin/baseball/BaseBallGame.kt
+++ src/main/kotlin/baseball/BaseBallGame.kt
@@ -8,6 +8,18 @@ class BaseBallGame {
 
     private fun startGame() {
         val computerNumbers = Computer().createComputerBallNumbers()
+        runGameLoopUntilCorrectAnswer(computerNumbers)
+    }
+
+    private fun runGameLoopUntilCorrectAnswer(targetNumbers: List<Int>) {
+        var isAnswerCorrect = false
+        var playerNumbers: List<Int>
+
+        while (!isAnswerCorrect) {
+            print("숫자를 입력해주세요 : ")
+            playerNumbers = Player().createComputerBallNumbers()
+            isAnswerCorrect = targetNumbers == playerNumbers
+        }
     }
 
 }
