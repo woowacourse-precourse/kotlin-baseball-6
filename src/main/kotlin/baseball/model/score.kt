7d6f67ba0a83aaace29fd5124ac944f5b9c8a05e
jaehan4707 <jahan4707@naver.com>
2023-10-21 15:41:30 +0900

    feat : score,OutputView printScore 추가

---
+++ src/main/kotlin/baseball/model/score.kt
@@ -0,0 +1,4 @@
+package baseball.model
+
+data class score(val ball : Int, val strike : Int, val out : Boolean)
+
--- src/main/kotlin/baseball/view/OutputView.kt
+++ src/main/kotlin/baseball/view/OutputView.kt
@@ -1,6 +1,8 @@
 package baseball.view
 
+import baseball.model.score
 import baseball.util.GameMessage
+import java.lang.StringBuilder
 
 class OutputView {
 
@@ -8,5 +10,14 @@ class OutputView {
     fun printGameInputMessage() = println(GameMessage.GAME_INPUT_NUMBER_MESSAGE)
     fun printGameSuccessMessage() = println(GameMessage.GAME_SUCCESS_MESSAGE+" "+GameMessage.GAME_OVER_MESSAGE)
     fun printGameEndMessage() = println(GameMessage.GAME_END_MESSAGE)
-    //TODO 숫자 비교 결과 메시지 출력 N볼 N스트라이크 or 낫싱
+    fun printGameScoreMessage(score: score){
+        val str = StringBuilder()
+        if(score.out)
+            str.append("낫싱")
+        if(score.ball!=0)
+            str.append("${score.ball}볼 ")
+        if(score.strike!=0)
+            str.append("${score.strike}스트라이크")
+        println(str)
+    }
 }
