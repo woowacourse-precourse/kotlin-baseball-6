93f47cd7b879d8c6f4362a7992d4056be808ac02
getupminaaa <getupmina@gmail.com>
2023-10-21 19:46:29 +0900

    refactor: 필요없는 변수(strike, ball) 제거

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -2,28 +2,22 @@ package baseball
 
 import baseball.System.Companion.isFinished
 
-//리팩토링-> 작은 덩어리로 쪼개기
 fun main() {
     val system = System()
     val computer = Computer()
 
-    //난수 생성
     while (!isFinished) {
         val userInput = system.getUserInput()
-        //regex와 일치하면서 set을 이용해 중복된 수가 있는지 확인
         system.checkUserInputValid(userInput)
 
         val userInputList: List<String> = userInput.chunked(1)
 
         //난수와 사용자 입력 비교 분리
-        val result = computer.getGameResult(userInputList) //result = Triple(strike, ball, nothing))
+        val result = computer.getGameResult(userInputList) //result = Pair(strike, ball))
 
-        val strike = result.first
-        val ball = result.second
+        system.printGameResult(result.first, result.second)
 
-        system.printGameResult(strike, ball)
-
-        if (strike == 3) {
+        if (result.first == 3) {
             println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
             println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
 
