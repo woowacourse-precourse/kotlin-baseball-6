3dbf7da461f21bdb8183745bd13314b1c47a4928
Jun <ayz1070@gmail.com>
2023-10-21 22:58:06 +0900

    테스트 통과

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -30,8 +30,12 @@ fun main() {
             ball=0
 
             print("숫자를 입력해주세요 : ")
+
             numString = Console.readLine()
             nums = numString.map{it.toString().toInt()}.toMutableList()
+            if(nums.size != 3){
+                throw IllegalArgumentException()
+            }
 
             for(i in 0..2){
                 if(nums[i] in answers && nums[i]==answers[i]){
@@ -52,14 +56,7 @@ fun main() {
         }
         println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
         println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
-        try{
-            again = Console.readLine().toInt()
-            if(again == 2){
-                println("게임 종료")
-            }
-        }catch (e:IllegalArgumentException){
-            throw e
-        }
+        again = Console.readLine().toInt()
     }
 
 
