9c4e18e3f682cd4d611aafc0a6912f8418cafcf6
Jun <ayz1070@gmail.com>
2023-10-21 22:19:54 +0900

    1. 사용자 입력 받기

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,22 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+import camp.nextstep.edu.missionutils.Console
+
+
 fun main() {
-    TODO("프로그램 구현")
+    println("숫자 야구 게임을 시작합니다.")
+    print("숫자를 입력해주세요 : ")
+    var numString:String
+    var nums:MutableList<Int>
+    var strike:Int = 0
+    var ball:Int = 0
+
+    numString = Console.readLine()
+    nums = numString.map{it.toString().toInt()}.toMutableList()
+    print("$nums")
+
+
+
+
 }
