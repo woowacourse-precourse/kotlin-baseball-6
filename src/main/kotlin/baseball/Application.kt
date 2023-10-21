389a9649d0d861fb6d8c88fd446922f122d41417
Hogu59 <dbstjdguswkd@naver.com>
2023-10-21 17:36:49 +0900

    입력 - 게임 입력값 구현 (오류 조건 추가 - 입력 내 동일 값 존재)

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -2,6 +2,7 @@ package baseball
 
 import camp.nextstep.edu.missionutils.Randoms
 import camp.nextstep.edu.missionutils.Console
+import java.lang.IllegalArgumentException
 
 fun main() {
 
@@ -17,7 +18,18 @@ fun main() {
 
 
         println("숫자 야구 게임을 시작합니다.")
-력
+
+        print("숫자를 입력해주세요 : ")
+        val input = Console.readLine().trim()
+        checkValidInput(input)
     }
 
+}
+
+fun checkValidInput(input : String) {
+    if(input.length != 3) throw IllegalArgumentException()
+    for(i in input.indices){
+        if(input[i] !in '1' .. '9') throw IllegalArgumentException()
+        if(input[i] == input[(i+1)%3] || input[i] == input[(i+2)%3]) throw IllegalArgumentException()
+    }
 }
