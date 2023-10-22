965af27583a24ee905f9269c8d330e672952f31d
SeongWonYoon <dbswjdqhd12@gmail.com>
2023-10-23 01:55:56 +0900

    [FEATURE] 게임 로직 구현 3-2
    3-2 반환 받은 결과 값이 정답과 일치한다면 게임을 종료하고 재시작 문구 출력하기

--- src/main/kotlin/baseball/GameManager.kt
+++ src/main/kotlin/baseball/GameManager.kt
@@ -9,9 +9,18 @@ object GameManager {
         val answer = RandomNumberGenerator.generateRandomNumber()
         game = BaseBallGame(answer)
 
+        askRetry()
     }
 
+    fun askRetry(){
+        println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
 
+        when (Console.readLine()){
+            "1" -> startGame()
+            "2" -> return
+            else -> throw IllegalArgumentException("")
+        }
+    }
 
 
 }
