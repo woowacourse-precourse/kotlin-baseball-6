e1ac5abf2e9b5a60d1a64bc066b37827f63b9400
murjune <sis9221302@gmail.com>
2023-10-22 21:06:50 +0900

    refactor(Application) : 절차지향적이였던 코드 객체지향적으로 리팩토링
    - Player : 게임을 하는 객체
    - Referee : 게임 결과를 결정하는 객체
    - BaseBallCreator : 야구공 리스트를 만드는 객체
    - GameGuide : 게임 진행상황을 보여주는 객체

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,67 +1,32 @@
 package baseball
 
-import camp.nextstep.edu.missionutils.Console
-import camp.nextstep.edu.missionutils.Randoms
+import baseball.constants.GameInstruction
+import baseball.constants.GameRestartOption
+import baseball.constants.GameResult
 
 fun main() {
-    val resultMap = mutableMapOf<String, Int>()
-    val BALL = "볼"
-    val STRIKE = "스트라이크"
-    var finishSignal = "1"
-    println("숫자 야구 게임을 시작합니다.")
+    var restartOption = GameRestartOption.INIT
+    val gameGuide = GameGuide()
     val baseBallCreator = BaseBallCreator()
-    while (true) {
+    val player = Player()
+    val referee = Referee()
+
+    gameGuide.show(GameInstruction.GAME_START)
+    // baseballGame class 로 리팩?? baseballGame.play()
+    while (restartOption != GameRestartOption.EXIT) {
         val answerBaseBalls = baseBallCreator.createAnswerBalls()
-
+        // round class 로 리팩?? round.start()
         do {
-            print("숫자를 입력해주세요 : ")
+            gameGuide.show(GameInstruction.ENTER_NUMBER)
+            val numbers = player.selectNumbers()
+            val baseBalls = BaseBallConvertor().convert(numbers)
+            val gameResult = referee.determineGameResult(answerBaseBalls, baseBalls)
+            gameGuide.show(gameResult)
+        } while (answerBaseBalls != baseBalls)
 
-            val input = runCatching {
-                Console.readLine().trim().map { it.toString().toInt() }
-            }.getOrNull()
-                ?: run {
-                    throw IllegalArgumentException()
-                }
+        gameGuide.show(GameResult.WIN)
+        gameGuide.show(GameInstruction.RESTART_OR_FINISH)
 
-            // valid 검증
-            if (BaseBallsValidator().validate(input).not()) throw IllegalArgumentException()
-
-            // 이제 숫자 비교
-            input.indices.forEach { idx ->
-                val num = input[idx]
-                if (answerBaseBalls[idx] == num) {
-                    resultMap += STRIKE to resultMap.getOrDefault(STRIKE, 0) + 1
-                    return@forEach
-                }
-                if (num in answerBaseBalls) {
-                    resultMap += BALL to resultMap.getOrDefault(BALL, 0) + 1
-                }
-            }
-            if (resultMap.getOrDefault(STRIKE, 0) + resultMap.getOrDefault(BALL, 0) == 0) {
-                println("낫싱")
-                continue
-            }
-            val res = run {
-                var str = ""
-                val strikeCnt = resultMap.getOrDefault(STRIKE, 0)
-                val ballCnt = resultMap.getOrDefault(BALL, 0)
-                if (ballCnt > 0) str += "${ballCnt}볼 "
-                if (strikeCnt > 0) str += "${strikeCnt}스트라이크"
-                str
-            }
-            println(res)
-            resultMap.clear()
-        } while (answerBaseBalls != input)
-        println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
-        println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
-        finishSignal = runCatching {
-            Console.readLine().trim()
-        }.getOrNull()
-            ?: run {
-                throw IllegalArgumentException()
-            }
-        if (finishSignal == "1") continue
-        if (finishSignal == "2") return
-        throw IllegalArgumentException()
+        restartOption = player.determineRestart()
     }
 }
