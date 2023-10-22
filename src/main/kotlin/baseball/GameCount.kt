71c8e391232e7ba5a0185c95c8660031d600717f
MinhoiKoo <minlove2013@gmail.com>
2023-10-22 16:11:46 +0900

    feat:GameCount 객체 추가

---
+++ src/main/kotlin/baseball/GameCount.kt
@@ -0,0 +1,21 @@
+package baseball
+
+object GameCount {
+    private var ballCount = 0
+    private var strikeCount = 0
+
+    fun plusBallCount() {
+        ballCount++
+    }
+
+    fun plusStrikeCount() {
+        strikeCount++
+    }
+
+    fun getBallCount() : Int {
+        return ballCount
+    }
+    fun getStrikeCount() : Int{
+        return strikeCount
+    }
+}
