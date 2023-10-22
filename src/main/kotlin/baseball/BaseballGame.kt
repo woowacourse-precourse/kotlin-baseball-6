4a957c188075ada4f6cebeba3526bb6ded71aa05
stopstone <wltjr6432@naver.com>
2023-10-23 02:11:05 +0900

    feat: validateInputUserNumber 중복된 숫자 예외처리

--- src/main/kotlin/baseball/BaseballGame.kt
+++ src/main/kotlin/baseball/BaseballGame.kt
@@ -52,51 +52,55 @@ class BaseballGame {
     private fun validateInputUserNumber(userNumber: String) {
         require(userNumber.length == 3) { "3자리가 아닌 입력값입니다." }
         require(userNumber.all { it.isDigit() } && !userNumber.contains("0")) { "숫자가 아닌 입력값이 포함되었습니다." }
-    }
-
-    private fun compareWithNumber(answer: String, userNumber: String): Pair<Int, Int> {
-        var strike = 0
-        var ball = 0
-
-        for (idx in userNumber.indices) {
-            if (userNumber[idx] == answer[idx]) {
-                strike++
-                continue
-            }
-            if (userNumber.contains(answer[idx])) {
-                ball++
-            }
+        for (x in userNumber) {
+            require(userNumber.count { it == x } <= 1) { "중복된 숫자가 있습니다." }
         }
-
-        return Pair(strike, ball)
-    }
-
-    private fun printHint(strike: Int, ball: Int) {
-        val hint = buildHint(strike, ball)
-        println(hint)
-    }
-
-    private fun buildHint(strike: Int, ball: Int): String {
-        return when {
-            ball != 0 && strike != 0 -> "${ball}볼 ${strike}스트라이크"
-            ball != 0 -> "${ball}볼"
-            strike != 0 -> "${strike}스트라이크"
-            else -> "낫싱"
-        }
-    }
-
-    private fun gameOver() {
-        println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
-        println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
-    }
-
-    private fun chooseRestartAndExit(): Boolean {
-        val flag: Int = Console.readLine().toInt()
-        validateRestartAndExit(flag)
-        return flag == 1
-    }
-
-    private fun validateRestartAndExit(flag: Int) {
-        require(flag in setOf(1, 2)) { "1이나 2가 아닌 다른 입력 값입니다." }
     }
 }
+
+private fun compareWithNumber(answer: String, userNumber: String): Pair<Int, Int> {
+    var strike = 0
+    var ball = 0
+
+    for (idx in userNumber.indices) {
+        if (userNumber[idx] == answer[idx]) {
+            strike++
+            continue
+        }
+        if (userNumber.contains(answer[idx])) {
+            ball++
+        }
+    }
+
+    return Pair(strike, ball)
+}
+
+private fun printHint(strike: Int, ball: Int) {
+    val hint = buildHint(strike, ball)
+    println(hint)
+}
+
+private fun buildHint(strike: Int, ball: Int): String {
+    return when {
+        ball != 0 && strike != 0 -> "${ball}볼 ${strike}스트라이크"
+        ball != 0 -> "${ball}볼"
+        strike != 0 -> "${strike}스트라이크"
+        else -> "낫싱"
+    }
+}
+
+private fun gameOver() {
+    println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
+    println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+}
+
+private fun chooseRestartAndExit(): Boolean {
+    val flag: Int = Console.readLine().toInt()
+    validateRestartAndExit(flag)
+    return flag == 1
+}
+
+private fun validateRestartAndExit(flag: Int) {
+    require(flag in setOf(1, 2)) { "1이나 2가 아닌 다른 입력 값입니다." }
+}
+
