7fa53374bc833e7178f3232ca673e87b309ecfe1
mainfn <_@mainfn.dev>
2023-10-22 17:53:41 +0900

    feat: add DI

--- src/main/kotlin/baseball/controller/GameController.kt
+++ src/main/kotlin/baseball/controller/GameController.kt
@@ -1,23 +1,20 @@
 package baseball.controller
 
-import baseball.domain.AnswerCreator
-import baseball.domain.BallNumbers
-import baseball.domain.BaseballGame
-import baseball.domain.GameStatus
+import baseball.domain.*
 import baseball.view.InputView
 import baseball.view.OutputView
 
 class GameController(
-        private val inputView: InputView,
-        private val outputView: OutputView,
-        private val answerCreator: AnswerCreator,
+    private val inputView: InputView,
+    private val outputView: OutputView,
+    private val answerCreator: AnswerCreator,
 ) {
     fun run() {
         // 1. 게임 생성
-        val game = createGame()
+        val answer = answerCreator.create()
 
         // 2. 게임 실행(정답 맞출 때까지)
-        playRecursive(game)
+        guessRecursive(answer)
 
         // 3.재실행 여부 입력 및 재실행 or 종료 결정
         if (GameStatus.REPLAY == inputView.inputNextGameStatus()) {
@@ -25,24 +22,19 @@ class GameController(
         }
     }
 
-    private fun playRecursive(game: BaseballGame) {
+    private fun guessRecursive(answer: Answer) {
         // 사용자가 추측한 숫자 입력
-        val ballNumbers = BallNumbers(inputView.inputBallNumbers())
+        val ballNumbers = Guess(inputView.inputBallNumbers())
         // 정답과 비교 후 결과 반환
-        val result = game.play(ballNumbers)
+        val result = answer.compareTo(ballNumbers)
 
         // 결과 출력
         outputView.printResult(result)
 
         // 3스트라이크가 아닌 경우 재귀 호출
-        if (result.strikeCount != 3) {
-            playRecursive(game)
+        if (result.strikeCount != BallNumbers.BALL_COUNT) {
+            guessRecursive(answer)
         }
     }
 
-    private fun createGame(): BaseballGame {
-        // 정답으로 사용할 3자리 랜덤 숫자 생성 및 반환
-        val answer = answerCreator.create()
-        return BaseballGame(answer)
-    }
 }
