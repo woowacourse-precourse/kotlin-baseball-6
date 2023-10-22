6cc438ce2063758862918b383b444fb8e118e358
lh99j <grand2181@gmail.com>
2023-10-22 11:49:25 +0900

    refactor: 함수 분리 후 힌트 출력 함수 Game 클래스로 이동

--- src/main/kotlin/baseball/Computer.kt
+++ src/main/kotlin/baseball/Computer.kt
@@ -18,7 +18,7 @@ class Computer {
         return computer
     }
 
-    private fun countStrikeAndBall(computer: MutableList<Int>, user: MutableList<Int>): Pair<Int, Int> {
+    fun countStrikeAndBall(computer: MutableList<Int>, user: MutableList<Int>): Pair<Int, Int> {
         var strike = 0
         var ball = 0
 
@@ -40,17 +40,5 @@ class Computer {
         }
 
         return Pair(strike, ball)
-    }
-
-    fun compareAndPrintHint(computer: MutableList<Int>, user: MutableList<Int>) {
-        val (strike, ball) = countStrikeAndBall(computer, user)
-        val hint = when {
-            strike == 0 && ball == 0 -> "낫싱"
-            strike > 0 && ball == 0 -> "${strike}스트라이크"
-            strike == 0 && ball > 0 -> "${ball}볼"
-            else -> "${ball}볼 ${strike}스트라이크"
-        }
-
-        println(hint)
     }
 }
--- src/main/kotlin/baseball/Game.kt
+++ src/main/kotlin/baseball/Game.kt
@@ -14,7 +14,8 @@ class Game {
 
         while (computer.gameFlag) {
             val userNumber = user.inputUserNumber()
-            computer.compareAndPrintHint(computerNumber, userNumber)
+            val hint = computer.countStrikeAndBall(computerNumber, userNumber)
+            printHint(hint)
 
             if (!computer.gameFlag) {
                 printGameEndMessage()
@@ -25,6 +26,18 @@ class Game {
                 }
             }
         }
+    }
+
+    private fun printHint(hint: Pair<Int, Int>) {
+        val (strike, ball) = hint
+        val resultHint = when {
+            strike == 0 && ball == 0 -> "낫싱"
+            strike > 0 && ball == 0 -> "${strike}스트라이크"
+            strike == 0 && ball > 0 -> "${ball}볼"
+            else -> "${ball}볼 ${strike}스트라이크"
+        }
+
+        println(resultHint)
     }
 
     private fun printGameEndMessage() {
