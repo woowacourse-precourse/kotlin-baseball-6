1f9b3bef458f002fc224c4811051c4c14f4f59d2
추민수 <devsoftychoo@gmail.com>
2023-10-23 01:55:09 +0900

    [Refactoring] : isValidInput
    기존 inputList.length를
    inputList.toSet().size로 변경하여
    그냥 3자리가 아닌 중복된 숫자없이 3자리로 수정

--- src/main/kotlin/baseball/util/GameUtils.kt
+++ src/main/kotlin/baseball/util/GameUtils.kt
@@ -1,6 +1,6 @@
 package baseball.util
 
-import baseball.presentation.model.GameModel
+import baseball.ui.model.GameModel
 import camp.nextstep.edu.missionutils.Console
 import camp.nextstep.edu.missionutils.Randoms
 
@@ -12,9 +12,9 @@ object GameUtils {
 
     /**
      * getRandomList()
-     * 게임에서 사용할 1에서 9까지 서로 다른 임의의 3개 숫자를 생성
+     * 게임에서 사용할 1에서 9까지 서로 다른 임의의 3개 숫자를 생성한다.
      *
-     * @return 1에서 9까지 서로 다른 3개의 숫자로 이루어진 리스트 반환
+     * @return 1에서 9까지 서로 다른 3개의 숫자로 이루어진 리스트
      */
     fun getRandomList(): List<Int> {
         val computerList = mutableListOf<Int>()
@@ -31,7 +31,7 @@ object GameUtils {
 
     /**
      * inputNum()
-     * 사용자가 입력한 연속된 3자리의 문자열을 받아 한 글자씩 분리하여 리스트 형태로 반환
+     * 사용자가 입력한 연속된 3자리의 문자열을 받아 한 글자씩 분리하여 리스트 형태로 반환한다.
      *
      * @return 사용자가 입력한 3자리 숫자를 나타내는 리스트
      * @throws IllegalArgumentException 입력이 유효하지 않을 경우 예외처리
@@ -47,19 +47,19 @@ object GameUtils {
 
     /**
      * isValidInput()
-     * 유효한 입력(길이:3, 입력형태:Int)인지 확인 후 유효할 경우에만 true return
+     * 유효한 입력(길이:3, 입력형태:Int, 중복:X)인지 확인 후 유효할 경우 true, 그렇지 않을 경우 false를 반환한다.
      *
      * @param inputList 확인할 입력 문자열
-     * @return 입력이 3자리이고, 숫자로만 구성된 경우 true를 반환
+     * @return 입력이 서로다른 3자리이고, 숫자로만 구성된 경우 true를 반환
      */
     private fun isValidInput(inputList: String): Boolean {
-        return inputList.length == 3 && inputList.all { it.isDigit() }
+        return inputList.toSet().size == 3 && inputList.all { it.isDigit() }
     }
 
 
     /**
      * compareResult()
-     * 사용자가 입력한 숫자와 컴퓨터가 선택한 숫자를 비교하여 결과를 계산
+     * 사용자가 입력한 숫자와 컴퓨터가 선택한 숫자를 비교하여 결과를 계산한다.
      *
      * @param userList 사용자가 입력한 숫자 리스트
      * @param computerList 컴퓨터가 선택한 숫자 리스트
@@ -97,7 +97,7 @@ object GameUtils {
 
     /**
      * restartGame()
-     * 사용자로부터 게임 재시작 또는 종료를 입력받는 함수
+     * 사용자로부터 게임 재시작 또는 종료를 입력받는 함수이다.
      *
      * @return 게임을 재시작하면 true, 게임을 종료하면 false를 반환
      */
