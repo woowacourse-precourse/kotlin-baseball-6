1bbc9acfee7366c7ffb231cb5fd7219ff81a144e
pingu244 <pingu244@naver.com>
2023-10-23 00:46:56 +0900

    [feat] 컴퓨터는 1-9 사이의 임의의 수 3개를 선택하는 기능 구현

---
+++ src/main/kotlin/baseball/NumberPicker.kt
@@ -0,0 +1,16 @@
+package baseball
+
+import camp.nextstep.edu.missionutils.Randoms
+
+class NumberPicker {
+    fun pickRandomNumber(): List<Int> {
+        val computer = mutableListOf<Int>()
+        while (computer.size < 3) {
+            val randomNumber = Randoms.pickNumberInRange(1, 9)
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber)
+            }
+        }
+        return computer.toList()
+    }
+}
