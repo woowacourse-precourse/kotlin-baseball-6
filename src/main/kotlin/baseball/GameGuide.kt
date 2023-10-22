43422f93509a75225725f340d00fc2970a567131
murjune <sis9221302@gmail.com>
2023-10-22 21:02:46 +0900

    delete(GameGuide) : show 로직 오버로딩  함수 추가

--- src/main/kotlin/baseball/GameGuide.kt
+++ src/main/kotlin/baseball/GameGuide.kt
@@ -4,11 +4,9 @@ import baseball.constants.GameInstruction
 import baseball.constants.GameResult
 
 class GameGuide {
-    fun show(instruction: GameInstruction) {
-        println(instruction.message)
-    }
+    fun show(instruction: GameInstruction) = println(instruction.message)
 
-    fun show(result: GameResult) {
-        println(result.message)
-    }
+    fun show(result: GameResult) = println(result.message)
+
+    fun show(result: String) = println(result)
 }
