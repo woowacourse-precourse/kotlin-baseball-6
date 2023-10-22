a8f160a577c20a317b8d3866bc78cc56a78167f2
LEE <tpqms775@naver.com>
2023-10-22 22:54:07 +0900

    게임안내 메세지 ,computer 클래스, player 클래스 구현 , 숫자 생성 및 숫자 입력 메소드 구현

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,33 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
-    TODO("프로그램 구현")
+    println("숫자 야구 게임을 시작합니다.")
+    print("숫자를 입력해주세요 : ")
+
+}
+
+class Computer{
+    //숫자 생성
+    fun createRandomNum(){
+        val computer : MutableList<Int> = mutableListOf()
+        while (computer.size < 3) {
+            val randomNumber = Randoms.pickNumberInRange(1, 9)
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber)
+            }
+        }
+    }
+    //숫자비교 및 출력
+    
+
+}
+class Player{
+    //숫자입력 - 예외처리
+    fun enterNum()= Console.readLine()
+
+    //게임 종료 , 재시작여부
+
 }
