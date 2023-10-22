74343d0799d593a4d0f34b7f9ca77e00c47ae5e2
Jtility <leejewon97@gmail.com>
2023-10-23 06:50:36 +0900

    feat(main): print out success and game end

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -13,6 +13,7 @@ fun main() {
             val inputNumbers = Console.readLine()
             val fail = printResult(answerNumbers, inputNumbers)
         } while (fail)
+        println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
         println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
         startOrExit = Console.readLine()
     }
