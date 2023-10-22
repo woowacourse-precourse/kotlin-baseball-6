33802c74619e3f29155725a76a22a8ac766d65b6
hevton <hevton22@gmail.com>
2023-10-22 17:04:26 +0900

    feat: 컴퓨터의 랜덤한 3자리 수 생성

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,13 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
-    TODO("프로그램 구현")
+    val computerList = mutableListOf<Int>()
+    while (computerList.size < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (randomNumber !in computerList) {
+            computerList.add(randomNumber)
+        }
+    }
 }
