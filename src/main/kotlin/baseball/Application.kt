a8692a169da9587e87a79b0c9b47fc478cbfb992
seoyeon <seoyeonn051@gmail.com>
2023-10-23 00:02:31 +0900

    Feats: 세 자리의 랜덤 난수 생성 함수 추가
    generateRandomNum() 함수 생성
      - 세 자리의 랜덤 난수를 리스트로 생성 후 해시 맵으로 저장

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,31 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
-    TODO("프로그램 구현")
+
+}
+
+fun getUserGuessNum(){
+
+}
+
+
+fun generateRandomNum():HashMap<Int, Int>{
+    var computer: MutableList<Int> = mutableListOf()
+    while (computer.size < 3) {
+        val randomNumber: Int = Randoms.pickNumberInRange(1, 9)
+        if (!computer.contains(randomNumber)) {
+            computer.add(randomNumber)
+        }
+    }
+    var computerHashMap = HashMap<Int, Int>()
+    for (i in 0 until computer.size){
+        computerHashMap.put(computer[i], i)
+    }
+    return computerHashMap
+}
+
+fun compareNums(){
+
 }
