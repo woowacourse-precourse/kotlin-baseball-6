1ceca1e9eb637fa56450212e646a95a6f43b612a
tgyuu-An <atg0614@gmail.com>
2023-10-22 12:38:47 +0900

    [TEST] 스트라이크, 볼 갯수 계산 로직 구현하기 전 테스트 코드 작성

--- src/main/kotlin/baseball/GameManager.kt
+++ src/main/kotlin/baseball/GameManager.kt
@@ -2,7 +2,9 @@ package baseball
 
 class GameManager {
     lateinit var targetNumber: List<Int>
-    val randomTargetGenerator: RandomTargetGenerator by lazy { RandomTargetGenerator() }
+        private set
+
+    private val randomTargetGenerator: RandomTargetGenerator by lazy { RandomTargetGenerator() }
 
     fun gameStart() {
         targetNumber = randomTargetGenerator.generateRandomTarget()
