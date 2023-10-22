1ec8e1681e0e58d98d7955a4ad7911e262b9689a
tgyuu-An <atg0614@gmail.com>
2023-10-22 10:34:16 +0900

    [CHORE] Player main 함수에 로직 추가

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,7 +1,13 @@
 package baseball
 
 fun main() {
+
     println("숫자 야구 게임을 시작합니다.")
     val randomTargetGenerator = RandomTargetGenerator()
-    val target = randomTargetGenerator.generateRandomTarget()
+    val player = Player()
+    val targetNumber = randomTargetGenerator.generateRandomTarget()
+
+    print("숫자를 입력해주세요 : ")
+    val userInput = readLine()
+    player.setGuessNumber(userInput!!)
 }
