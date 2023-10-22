19233c66533f1f94654f6c37cd5ce73798634e09
seongsan <tjdtks030@naver.com>
2023-10-22 23:23:13 +0900

    feat: 게임 시작
    시작 문구를 출력한다.
    printStartMessage() 구현

---
+++ src/main/kotlin/baseball/Baseball.kt
@@ -0,0 +1,12 @@
+package baseball
+
+class Baseball {
+    init {
+        printStartMessage()
+    }
+
+    private fun printStartMessage() {
+        val message = "숫자 야구 게임을 시작합니다"
+        println(message)
+    }
+}
