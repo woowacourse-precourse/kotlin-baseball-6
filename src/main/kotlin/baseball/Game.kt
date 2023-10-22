9a3d7d1e8d2bea1cdf2144a419410ef67a9ec900
bak <bak4017@gmail.com>
2023-10-22 23:34:52 +0900

    feat: 난수 생성

---
+++ src/main/kotlin/baseball/Game.kt
@@ -0,0 +1,18 @@
+package baseball
+
+import camp.nextstep.edu.missionutils.Randoms
+
+private const val START_INCLUSIVE = 1    // 1부터
+private const val END_INCLUSIVE = 9   // 9까지
+private const val COUNT = 3   // 3자리 수를 맞추는 게임
+
+val answer = IntArray(COUNT) // 정답 저장할 배열
+
+fun run(){
+    setGame()
+}
+
+private fun setGame(){
+    // 랜덤한 수를 answer 배열에 저장해 게임의 정답을 셋팅함
+    for(i in 0 until COUNT) { answer[i] = Randoms.pickNumberInRange(START_INCLUSIVE, END_INCLUSIVE) }
+}
