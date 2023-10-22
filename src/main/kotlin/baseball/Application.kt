b6437858b3ae726fc9e209d47f6db76bb5190b97
millinova98 <kcong1@naver.com>
2023-10-23 00:21:06 +0900

    feat: 입력값 유효성 확인 기능

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -12,5 +12,40 @@ fun main() {
         }
     }
 
+    //(게임 플레이어) 서로 다른 3개의 숫자를 입력한다.
+    println("숫자 야구 게임을 시작합니다.")
+    println("숫자를 입력해주세요 : ")
+    val input = readlnOrNull()
+    var player: List<String> = emptyList<String>()
 
+    // 입력 받은 값이 null인지, 자연수인지 확인한다.
+    if (!input.isNullOrEmpty() && isInteger(input) && input.toInt() > 0){
+        player = input.split("")
+    }
+    else {
+        throw IllegalArgumentException("null이거나 자연수가 아닙니다.")
+    }
+
+    // 입력 받은 값이 서로 다른 세 자리 수인지 확인하다.
+    if (player.size == 5){
+        if (player[1] == player[2] || player[1] == player[3] || player[2] == player[3]){
+            throw IllegalArgumentException("서로 다른 숫자가 아닙니다.")
+        }
+    } else {
+        throw IllegalArgumentException("세 자리 숫자가 아닙니다.")
+    }
+
+    println(computer)
+    println(player)
+
+}
+
+
+fun isInteger(s: String): Boolean {
+    return try {
+        s.toInt()
+        true
+    } catch (e: IllegalArgumentException) {
+        false
+    }
 }
