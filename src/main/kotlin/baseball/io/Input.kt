d8b2a38b2d28b609fdf43b1c49fa4b9213e10c62
kimhm0728 <kimhm0728@naver.com>
2023-10-22 22:04:12 +0900

    style: [Input.kt] 게임 종료 여부 입력받는 기능 출력 형식 수정

--- src/main/kotlin/baseball/io/Input.kt
+++ src/main/kotlin/baseball/io/Input.kt
@@ -14,7 +14,7 @@ fun inputBaseBallNumber(): List<Int> {
 }
 
 fun inputExitGame(): String {
-    print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+    println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
     val exitGame = Console.readLine()
     validateInputExit(exitGame)
 
