5ee5a6e685c465f36f0a35c1b05c3c8d2a2f46c9
JJongmen <pjy90123@gmail.com>
2023-10-22 20:26:34 +0900

    비교 결과에 따른 메시지를 반환한다

--- src/main/kotlin/baseball/BallCount.kt
+++ src/main/kotlin/baseball/BallCount.kt
@@ -1,11 +1,20 @@
 package baseball
 
 data class BallCount(val strike: Int, val ball: Int) {
-    fun isNothing(): Boolean {
-        return strike == 0 && ball == 0
+    fun isThreeStrike() = strike == 3
+
+    fun getMessage(): String {
+        return when {
+            isNothing() -> MessageConstants.NOTHING
+            isBallOnly() -> MessageConstants.BALL_ONLY.format(ball)
+            isStrikeOnly() -> MessageConstants.STRIKE_ONLY.format(strike)
+            else -> MessageConstants.BALL_STRIKE.format(ball, strike)
+        }
     }
 
-    fun isThreeStrike(): Boolean {
-        return strike == 3
-    }
+    private fun isNothing() = strike == 0 && ball == 0
+
+    private fun isStrikeOnly() = ball == 0 && strike > 0
+
+    private fun isBallOnly() = strike == 0 && ball > 0
 }
---
+++ src/main/kotlin/baseball/MessageConstants.kt
@@ -0,0 +1,8 @@
+package baseball
+
+object MessageConstants {
+    const val NOTHING = "낫싱"
+    const val BALL_ONLY = "%d볼"
+    const val STRIKE_ONLY = "%d스트라이크"
+    const val BALL_STRIKE = "%d볼 %d스트라이크"
+}
