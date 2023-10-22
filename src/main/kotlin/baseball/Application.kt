1e182e0ed6aa60ca3266a768b63eecfafeb2faf1
jihyeonbaem <jihyeonbae424@gmail.com>
2023-10-22 23:01:46 +0900

    feat(createComputerNumber): 컴퓨터 숫자 생성하기 <br> 프로그램 구현을 위해서 print로 list를 볼수있게 함

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -6,8 +6,24 @@ import camp.nextstep.edu.missionutils.Randoms
 fun main() {
     // 게임을 시작한다.
     startGame()
+
+    // 컴퓨터 숫자를 생성한다.
+    println(createComputerNumber())
 }
 
 fun startGame() {
     println("숫자 야구 게임을 시작합니다.")
+}
+
+fun createComputerNumber(): MutableList<Int> {
+
+    val computer = mutableListOf<Int>()
+    while (computer.size < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (!computer.contains(randomNumber)) {
+            computer.add(randomNumber)
+        }
+    }
+
+    return computer
 }
