74aa279717b4b3573223ddeb8fdf1999d1160cf8
Jtility <leejewon97@gmail.com>
2023-10-23 04:28:19 +0900

    fix(main): change startOrExit type
    if it is int, it can be error when function readline read ""

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -5,15 +5,15 @@ import camp.nextstep.edu.missionutils.Randoms
 
 fun main() {
     println("숫자 야구 게임을 시작합니다.")
-    var startOrExit = 1
-    while (startOrExit == 1) {
+    var startOrExit = "1"
+    while (startOrExit == "1") {
         val answerNumbers = makeRandomNumbers()
         do {
             print("숫자를 입력해주세요 : ")
             val inputNumbers = Console.readLine()
         } while (false)
         println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
-        startOrExit = Console.readLine().toInt()
+        startOrExit = Console.readLine()
     }
 }
 
