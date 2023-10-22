e3fbb277d8a6abf803c300382a33d041a4ab1228
jihyeonbaem <jihyeonbae424@gmail.com>
2023-10-22 22:30:47 +0900

    feat(startGame): 게임 시작 문구 출력

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,6 +1,13 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+
 // 프로그램의 시작점
 fun main() {
+    // 게임을 시작한다.
+    startGame()
+}
 
+fun startGame() {
+    println("숫자 야구 게임을 시작합니다.")
 }
