1b04a792d82a3302a83403194c585735fab4c628
tgyuu-An <atg0614@gmail.com>
2023-10-22 12:02:18 +0900

    [REFACTOR] userInput -> playerInput으로 수정

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -3,10 +3,9 @@ package baseball
 fun main() {
 
     println("숫자 야구 게임을 시작합니다.")
-    val randomTargetGenerator = RandomTargetGenerator()
     val player = Player()
 
     print("숫자를 입력해주세요 : ")
-    val userInput = readLine()
-    player.setGuessNumber(userInput!!)
+    val playerInput = readLine()
+    player.setGuessNumber(playerInput!!)
 }
--- src/main/kotlin/baseball/GameManager.kt
+++ src/main/kotlin/baseball/GameManager.kt
@@ -1,10 +1,10 @@
 package baseball
 
 class GameManager {
-    lateinit var targetNumber : List<Int>
-    val randomTargetGenerator : RandomTargetGenerator by lazy {RandomTargetGenerator()}
+    lateinit var targetNumber: List<Int>
+    val randomTargetGenerator: RandomTargetGenerator by lazy { RandomTargetGenerator() }
 
-    fun gameStart(){
+    fun gameStart() {
         targetNumber = randomTargetGenerator.generateRandomTarget()
     }
 }
