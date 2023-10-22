077804f182d03f86a691d518712c8ca3086e5482
JJongmen <pjy90123@gmail.com>
2023-10-22 22:41:06 +0900

    게임을 시작한다

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,7 @@
 package baseball
 
 fun main() {
-    TODO("프로그램 구현")
+    val game = Game()
+
+    game.start()
 }
---
+++ src/main/kotlin/baseball/Game.kt
@@ -0,0 +1,38 @@
+package baseball
+
+class Game {
+    private val player = Player()
+    private val umpire = Umpire()
+    private val commentator = Commentator()
+    private val computer: Computer = Computer()
+
+    fun start() {
+        commentator.commentate(MessageConstants.GAME_START)
+        do {
+            playGameUntilFinish()
+        } while (wantsToRestart())
+    }
+
+    private fun playGameUntilFinish() {
+        val computerNumbers = computer.getGameNumbers()
+        var ballCount: BallCount
+        do {
+            commentator.commentate(MessageConstants.INPUT_GAME_NUMBERS)
+            val guessGameNumbers = player.guessGameNumbers()
+            ballCount = umpire.judge(guessGameNumbers, computerNumbers)
+            commentator.commentate(ballCount)
+        } while (isContinuous(ballCount))
+        commentator.commentate(MessageConstants.GAME_FINISH)
+    }
+
+    private fun isContinuous(ballCount: BallCount) = !ballCount.isThreeStrike()
+
+    private fun wantsToRestart(): Boolean {
+        commentator.commentate(MessageConstants.RESTART_OR_EXIT)
+        if (player.askReplayOption() == ReplayOption.EXIT) {
+            return false
+        }
+        computer.changeGameNumbers()
+        return true
+    }
+}
