db82180833ac664ac152f60560c276af62cb1dd1
Junhokim0103 <junho01kim@naver.com>
2023-10-22 13:48:06 +0900

    sencond code revision and test code update

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -8,13 +8,16 @@ fun main() {
     var ball = 0
     var randomlist = mutableListOf<Int>()
     var inputlist = mutableListOf<Int>()
+    println("숫자 야구 게임을 시작합니다.")
     while(true) {
         strike = 0
         ball = 0
         randomlist.clear()
         inputlist.clear()
+
+        println("숫자를 입력해주세요")
         var input = Console.readLine()
-        println(input)
+        //println(input)
 
         if(input.length>3)
         {
@@ -26,7 +29,7 @@ fun main() {
             var temp2 = input.toString().toInt()
             if (temp2 != 1)
             {
-                print("end baseball")
+                //print("end baseball")
                 break;
 
             }
@@ -36,22 +39,22 @@ fun main() {
         while(i<3) {
             var temp: Int = input[i].toString().toInt()
             inputlist.add(temp)
-            println("i: "+i+"temp: "+temp)
+           //println("i: "+i+"temp: "+temp)
             i++
         }
-
+/*
         for( item:Int in inputlist)
         {
             println("list item: " + item)
         }
-
+*/
 
         while (randomlist.count() < 3) {
             var randomnum = Randoms.pickNumberInRange(1, 9)
             if (!randomlist.contains(randomnum)) {
                 randomlist.add(randomnum)
             }
-            println(randomnum)
+           // println(randomnum)
         }
         i = 0
         while(i<3) {
@@ -68,37 +71,56 @@ fun main() {
         }
         if((strike == 0)&&(ball == 0))
         {
-            var output = "낫싱"
-            println("결과"+output)
+            //var output = "낫싱"
+            println("낫싱")
         }else if((strike == 0)&&(ball == 1))
         {
-            println("strike: "+strike+" ball: "+ball)
+            println("1볼")
         }else if((strike == 0)&&(ball == 2))
         {
-            println("strike: "+strike+" ball: "+ball)
+            println("2볼")
         }else if((strike == 0)&&(ball == 3))
         {
-            println("strike: "+strike+" ball: "+ball)
+            println("3볼")
         }else if((strike == 1)&&(ball == 0))
         {
-            println("strike: "+strike+" ball: "+ball)
+            println("1스트라이크")
         }else if((strike == 1)&&(ball == 1))
         {
-            println("strike: "+strike+" ball: "+ball)
+            println("1볼 1스트라이크")
         }else if((strike == 1)&&(ball == 2))
         {
-            println("strike: "+strike+" ball: "+ball)
+            println("2볼 1스트라이")
         }else if((strike == 2)&&(ball == 0))
         {
-            println("strike: "+strike+" ball: "+ball)
+            println("2스트라이")
         }else if((strike == 2)&&(ball == 1))
         {
-            println("strike: "+strike+" ball: "+ball)
+            println("1볼 2스트라이크")
         }else if((strike == 3)&&(ball == 0))
         {
-            println("strike: "+strike+" ball: "+ball)
+            println("3스트라이크")
+            println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
+
+            println("숫자를 입력해주세요")
+            var start = Console.readLine()
+            //println(input)
+
+
+
+                var temp2 = start.toString().toInt()
+                if (temp2 != 1)
+                {
+                    //print("end baseball")
+                    break;
+
+                }
+
+
+
+
         }
-        println("strike: "+strike+" ball: "+ball)
+        //println("strike: "+strike+" ball: "+ball)
 
 
 
