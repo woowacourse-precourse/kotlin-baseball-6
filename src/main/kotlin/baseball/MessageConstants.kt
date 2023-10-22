f86c36c06d0f4114e8c1bb42da5601871c2b7de0
JJongmen <pjy90123@gmail.com>
2023-10-22 22:14:07 +0900

    숫자 입력 안내 문구 상수 추가

--- src/main/kotlin/baseball/MessageConstants.kt
+++ src/main/kotlin/baseball/MessageConstants.kt
@@ -6,6 +6,7 @@ object MessageConstants {
     const val STRIKE_ONLY = "%d스트라이크"
     const val BALL_STRIKE = "%d볼 %d스트라이크"
     const val GAME_START = "숫자 야구 게임을 시작합니다."
+    const val INPUT_GAME_NUMBERS = "숫자를 입력해주세요 : "
     const val GAME_FINISH = "3개의 숫자를 모두 맞히셨습니다! 게임 종료"
     const val RESTART_OR_EXIT = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
 }
