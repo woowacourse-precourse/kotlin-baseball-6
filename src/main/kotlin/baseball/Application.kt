f0ab463f86609adf710d8733cf98f844532d196c
mainfn <_@mainfn.dev>
2023-10-22 17:53:05 +0900

    refact: add input validating logic

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -2,11 +2,13 @@ package baseball
 
 import baseball.controller.GameController
 import baseball.domain.AnswerCreator
+import baseball.view.InputValidator
 import baseball.view.InputView
 import baseball.view.OutputView
 
 fun main() {
-    val inputView = InputView()
+    val inputValidator = InputValidator()
+    val inputView = InputView(inputValidator)
     val outputView = OutputView()
     val answerCreator = AnswerCreator()
     val gameController = GameController(inputView, outputView, answerCreator)
