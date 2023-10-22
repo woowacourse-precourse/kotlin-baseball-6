1013cd1301dda10e67a594fd1d8cca6864f2f92b
이용인 <yongin0517@gmail.com>
2023-10-23 01:20:21 +0900

    feat: 게임 재시작/종료 기능 구현

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -5,12 +5,27 @@ import camp.nextstep.edu.missionutils.Console
 fun main() {
     var computer = Computer()
     println("숫자 야구 게임을 시작합니다.")
+
     while (true) {
         print("숫자를 입력해주세요: ")
         var numberBall = Console.readLine()
-        //판정하기
+
         if (isAvaliableInput(numberBall)) {
-            println(computer.judgeNumberBall(numberBall))
+            val result = computer.judgeNumberBall(numberBall)
+            println(result)
+
+            if (computer.isThreeStrike()) {
+                println("3개의 숫자를 모두 맞히셨습니다! 게임 종료 \n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+                var newOrder = Console.readLine()
+                if (newOrder.equals("1")) {
+                    computer.initializeGame()
+                } else if (newOrder.equals("2")) {
+                    break
+                } else {
+                    throw IllegalArgumentException()
+                    break
+                }
+            }
         } else {
             break
         }
