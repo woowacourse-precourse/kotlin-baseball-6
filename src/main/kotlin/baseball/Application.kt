ac3f70a991715610485570d5f4b7677e4512f06b
이빵주 <yungju9982@naver.com>
2023-10-22 19:04:12 +0900

    [feat] 1~9 세 자리 난수 생성하는 함수 구현

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,8 +1,20 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms.pickNumberInRange
+
 private const val START = 1
 private const val END = 9
 private const val LENGTH = 3
 
 fun main() {
+
+}
+
+private fun generateRandomNumberList(): List<Int> {
+    val numberSet = mutableSetOf<Int>()
+
+    while (numberSet.size < LENGTH) {
+        numberSet.add(pickNumberInRange(START, END))
+    }
+    return numberSet.toList()
 }
