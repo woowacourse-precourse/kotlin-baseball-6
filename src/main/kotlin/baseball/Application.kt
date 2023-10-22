6691564e2556b000d80e09e7e4dee027e57e0c3e
kyunghwa <snoopn303@gmail.com>
2023-10-22 11:33:26 +0900

    컴퓨터가 만든 랜덤숫자 생성

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,22 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+
+class BaseballGame {
+
+    private val computerNumbers = mutableListOf<Int>()
+
+    fun generateComputer() {
+        while (computerNumbers.size < 3) {
+            val randomNumber = Randoms.pickNumberInRange(1, 9)
+            if (randomNumber !in computerNumbers) {
+                computerNumbers.add(randomNumber)
+            }
+        }
+    }
+    fun getComputer(): List<Int> = computerNumbers
+}
+
 fun main() {
-    TODO("프로그램 구현")
+
 }
