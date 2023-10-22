74e4c9feb35519ae081df2dc45be29af65e42342
minuxx <minux8905@gmail.com>
2023-10-23 08:11:02 +0900

    refactor(User): 숫자 입력 요청 코드 리팩토링
    - inputNumber 메서드명 requestInputNumbers 변경
    - requestInputNumbers에서 입력값 유효성 검사

--- src/main/kotlin/baseball/User.kt
+++ src/main/kotlin/baseball/User.kt
@@ -3,9 +3,6 @@ package baseball
 import camp.nextstep.edu.missionutils.Console
 
 class User : Player<List<Int>> {
-    fun inputNumber(): String {
-        return Console.readLine()
-    }
 
     fun requestInputGameState(): Int {
         val input = Console.readLine()
@@ -15,13 +12,19 @@ class User : Player<List<Int>> {
         return input.toInt()
     }
 
+    fun requestInputNumbers(): String {
+        val input = Console.readLine()
+        Validator.validateInput(input)
+        Console.close()
+
+        return input
+    }
+
     /**
      * @param input 사용자로부터의 입력 문자열 (선택적)
      * @throws IllegalArgumentException 유효성 검사 실패 시 발생
      */
     override fun generateNumbers(input: String): List<Int> {
-        Validator.validateInput(input)
-
         val numbers = input.map { it.toString().toInt() }
         Validator.validateNumbers(numbers)
 
