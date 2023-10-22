75f59401e6bce06619d3a46138c9c3525f5cc5da
Junyoung-WON <wjyo60@gmail.com>
2023-10-22 23:34:07 +0900

    feat: 3자리 서로 다른 난수 생성 기능 테스트

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,7 +1,21 @@
 package baseball
+// 필요한 라이브러리 import
+import camp.nextstep.edu.missionutils.Randoms
+
 
 fun main() {
-//    TODO("프로그램 구현")
+    // 시작 문구 출력
     println("숫자 야구 게임을 시작합니다.")
+
+    // 컴퓨터가 생성하는 서로 다른 3자리 난수
+    val computer = mutableListOf<Int>()
+    while (computer.size < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (!computer.contains(randomNumber)) {
+            computer.add(randomNumber)
+        }
+    }
+    // 생성된 난수를 출력
+    println(computer)
 
 }
