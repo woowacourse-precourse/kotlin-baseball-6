fb2ea87bd1f8944f1ff34fcf39c616a4e672e6ad
이빵주 <yungju9982@naver.com>
2023-10-22 19:19:19 +0900

    [feat] 재시작 시 게임을 계속 반복하는 로직 구현

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -8,7 +8,12 @@ private const val END = 9
 private const val LENGTH = 3
 
 fun main() {
-
+    println("숫자 야구 게임을 시작합니다.")
+    do {
+        val computer = generateRandomNumberList()
+        startGame(computer)
+    } while (isRestart())
+    return
 }
 
 private fun startGame(computer: List<Int>) {
