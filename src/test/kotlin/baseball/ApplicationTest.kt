6125c0bb2f0117f441a59c98a1f32c8686cdbed3
jaehan4707 <jahan4707@naver.com>
2023-10-22 17:11:52 +0900

    test : 입력 예외 테스트코드 작성,

--- src/test/kotlin/baseball/ApplicationTest.kt
+++ src/test/kotlin/baseball/ApplicationTest.kt
@@ -1,9 +1,11 @@
 package baseball
 
+import baseball.util.GameMessage
 import camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest
 import camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest
 import camp.nextstep.edu.missionutils.test.NsTest
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
@@ -24,6 +26,35 @@ class ApplicationTest : NsTest() {
     fun `예외 테스트`() {
         assertSimpleTest {
             assertThrows<IllegalArgumentException> { runException("1234") }
+        }
+    }
+    @Test
+    fun `길이를 벗어난 입력 예외 테스트`(){
+        assertSimpleTest{
+            val exception = assertThrows<IllegalArgumentException>{runException("23123124")}
+            assertEquals(GameMessage.GAME_ERROR_INPUT_LENGTH,exception.message)
+        }
+    }
+    @Test
+    fun `숫자가 아닌 경우 입력 예외 테스트`() {
+        assertSimpleTest{
+            val exception = assertThrows<IllegalArgumentException> { runException("abc") }
+            assertEquals(GameMessage.GAME_ERROR_INPUT_NUMBER, exception.message)
+        }
+        assertSimpleTest{
+            val exception = assertThrows<IllegalArgumentException> { runException("우테코") }
+            assertEquals(GameMessage.GAME_ERROR_INPUT_NUMBER, exception.message)
+        }
+        assertSimpleTest{
+            val exception = assertThrows<IllegalArgumentException> { runException("!@$") }
+            assertEquals(GameMessage.GAME_ERROR_INPUT_NUMBER, exception.message)
+        }
+    }
+    @Test
+    fun `중복된 숫자 입력 예외 테스트`(){
+        assertSimpleTest {
+            val exception = assertThrows<IllegalArgumentException> { runException("222") }
+            assertEquals(GameMessage.GAME_ERROR_INPUT_DUPLICATE,exception.message)
         }
     }
 
