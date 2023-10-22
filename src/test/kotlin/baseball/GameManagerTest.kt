efc8fdfbd49391b9740ab930ab58e24e3425a0b0
tgyuu-An <atg0614@gmail.com>
2023-10-22 11:50:47 +0900

    [TEST] GameManager 초기 랜덤 타겟 넘버 설정 기능 구현 전 테스트 코드 작성

--- src/test/kotlin/baseball/GameManagerTest.kt
+++ src/test/kotlin/baseball/GameManagerTest.kt
@@ -1,7 +1,23 @@
 package baseball
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.mockito.ArgumentMatchers.anyInt
 
 class GameManagerTest{
-    
+
+    @Test
+    fun `GameManager는 게임이 시작되면 랜덤한 타겟 넘버를 가진다`(){
+        //given
+        val gameManager = GameManager()
+
+        //when
+        gameManager.gameStart()
+
+        //then
+        val actual = gameManager.targetNumber()
+        val expected = listOf(anyInt(),anyInt(),anyInt())
+        assertThat(actual).isEqualTo(expected)
+    }
 }
