5030fdd391a2bd242fef82e04c5a359c4d0de3a7
Jtility <leejewon97@gmail.com>
2023-10-23 02:09:18 +0900

    feat(main): add do-while for input numbers and check them

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -7,7 +7,11 @@ fun main() {
     println("숫자 야구 게임을 시작합니다.")
     var startOrExit = 1
     while (startOrExit == 1) {
-        val answer = makeRandomNumbers()
+        val answerNumbers = makeRandomNumbers()
+        do {
+            print("숫자를 입력해주세요 : ")
+            val inputNumbers = Console.readLine()
+        } while (false)
         println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
         startOrExit = Console.readLine().toInt()
     }
