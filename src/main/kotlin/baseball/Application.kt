1cb533eeaaf8c43b9b8174793562e6b85eb83a61
park-yina <111878820+park-yina@users.noreply.github.com>
2023-10-23 05:11:01 +0900

    리팩토링:가독성 높이기 위해 함수 1개 삭제

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -5,8 +5,8 @@ import camp.nextstep.edu.missionutils.Randoms
 
 
 class NumberBaseball {
-    val input=InputGame()
-    inner class InputGame{
+    val input = InputGame()
+    inner class InputGame {
         fun inputException(userNumber: MutableList<Char>, inputNumber: String) {
             if (inputNumber.length != 3) {
                 throw IllegalArgumentException("입력된 숫자가 3자리가 아닙니다.")
@@ -21,6 +21,7 @@ class NumberBaseball {
                 userNumber.add(inputNumber[index])
             }
         }
+
         fun createComputerNumber(): String {
             val newComputerNumber = mutableListOf<String>()
             while (newComputerNumber.size < 3) {
@@ -32,6 +33,7 @@ class NumberBaseball {
             return newComputerNumber.joinToString("")
             //joinTostring을 사용하면 컬렉션의 요소를 문자열로 결합하기 때문에 각각 따로 사용할 떄 보다 관리가 용이하다고 판단
         }
+
         fun readInputNumber(): String {
             print("숫자를 입력해주세요: ")
             val userInput = Console.readLine().toString()
@@ -40,7 +42,8 @@ class NumberBaseball {
             return userNumber.joinToString("")
         }
     }
-    inner class Gaming(){
+
+    inner class Gaming() {
         fun seekAnswer(newComputerNumber: String, userNumber: String) {
             var strikeCount: Int = 0
             var ballCount: Int = 0
@@ -52,7 +55,6 @@ class NumberBaseball {
                     ballCount++
                 }
             }
-
             if (strikeCount == 0 && ballCount == 0) {
                 println("낫싱")
             } else if (strikeCount == 3) {
@@ -62,10 +64,6 @@ class NumberBaseball {
                 println("${ballCount}볼 ${strikeCount}스트라이크")
             }
         }
-        fun gameStart() {
-            println("숫자 야구 게임을 시작합니다.")
-        }
-
         fun endGame() {
             println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
             println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
@@ -77,31 +75,24 @@ class NumberBaseball {
             while (true) {
                 user = input.readInputNumber()
                 seekAnswer(computer, user)
-                if (computer == user) {
+                if(computer==user)
                     break
-                }
             }
         }
     }
     fun resetGame(): Boolean {
         val inputNumber = Console.readLine()
-        if (inputNumber.toInt()==1) {
+        if (inputNumber.toInt() == 1) {
             return true
         }
         return false
     }
-
     fun main() {
-        Gaming().gameStart()
-        var wantPlaying=true
-        while(wantPlaying){
+        println("숫자 야구 게임을 시작합니다.")
+        var wantPlaying = true
+        while (wantPlaying) {
             Gaming().playing()
-            wantPlaying=resetGame()
+            wantPlaying = resetGame()
         }
     }
-}
-
-fun main() {
-    val numberBaseball = NumberBaseball()
-    numberBaseball.main()
 }
