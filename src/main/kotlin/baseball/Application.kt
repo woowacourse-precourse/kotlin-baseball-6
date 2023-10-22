8bb31c070eba70d62af15ba51986c58fe936553b
SeongWooHong <pluto0303@naver.com>
2023-10-22 15:46:33 +0900

    Game Managing

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,28 +1,116 @@
 package baseball
 
 import camp.nextstep.edu.missionutils.Randoms
+import org.w3c.dom.ranges.Range
 
-class RandomNumber{
-    var RandomNumber = 123
+const val MIN = 123
+const val MAX = 987
 
-    constructor() {
-        RandomNumber = Randoms.pickNumberInRange(123, 987)
+class BaseballOperate {
+    private var inputNumber = ""
+    private var randomNumber = Randoms.pickNumberInRange(MIN, MAX).toString()
+    private var strike = 0
+    private var ball = 0
+    fun RangeCheck() : Boolean {
+        var check = false
+        // code
+        return check
+    }
+    fun InitRandomNumber() {
+        randomNumber = Randoms.pickNumberInRange(MIN, MAX).toString()
+    }
+    fun setInputNumber() {
+        inputNumber = readLine().toString()
+    }
+    fun InitBallStrike(){
+        strike = 0
+        ball = 0
+    }
+    fun BaseballPlay() {
+        while(true){
+            print("숫자를 입력해 주세요 : ")
+            setInputNumber()
+
+            if(RangeCheck()) {
+                println("범위가 잘못되었습니다. 다시 입력해주세요.")
+                return
+            }
+            CheckStrike()
+            CheckBall()
+            CheckNothing()
+
+            PrintStirkeBall()
+            if(CheckAnswer()) {
+                break
+            }
+            InitBallStrike()
+        }
+        InitRandomNumber()
+        println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
+        return
+    }
+    fun CheckStrike() {
+        for( i in 0..2) {
+            if (inputNumber[i] == randomNumber[i]) {
+                strike += 1
+            }
+        }
+    }
+    fun CheckBall() {
+        for(i in 0..2) {
+            for(j in 0..2){
+                if(i == j) continue
+                if(inputNumber[i] == randomNumber[j]) {
+                    ball += 1
+                }
+            }
+        }
     }
 
-}
-class UserNumber {
-    var InputNumber = 123
+    fun CheckNothing() :Boolean {
+        var checkNothing = false
 
-    constructor(Input: String){
-        InputNumber = Input.toInt() // Need Refactor
+        if(strike == 0 && ball == 0) {
+            println("낫싱")
+            checkNothing = true
+        }
+        return checkNothing
+    }
+    fun PrintStirkeBall() {
+        if (strike == 0 && ball == 0) return
+        else if(strike == 0)
+        {
+            println("${ball}볼")
+        }
+        else if(ball == 0) {
+            println("${strike}스트라이크 ")
+        }
+        else{
+            println("${strike}스트라이크 ${ball}볼")
+        }
+
     }
 
+    fun CheckAnswer(): Boolean {
+        if (strike == 3) {
+            return true
+        }
+        else {
+            return false
+        }
+    }
 }
 
 fun main() {
-    var InputNum = "123" // Need Refactor
+    println("숫자 야구 게임을 시작합니다.")
+    var gameCoin = 1
+    while(gameCoin == 1) {
+        val baseballGame = BaseballOperate()
+        baseballGame.BaseballPlay()
 
-    InputNum = readLine().toString() // Need Refactor
+        print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+        gameCoin = readLine()!!.toInt()
+    }
 
 }
 
