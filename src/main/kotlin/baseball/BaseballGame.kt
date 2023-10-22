9fa1e6676424537b8e959c04d72fb2969570e544
sangil <fhemflrh60@gmail.com>
2023-10-22 23:05:21 +0900

    4. 입력받은 숫자와 정답 숫자의 같은 위치의 개수만큼 "스트라이크" 다른 위치의 개수만큼 "볼", 없다면 "낫싱"을 출력 5. 입력받은 숫자가 정답 숫자와 같다면 게임 종료 메시지 출력

--- src/main/kotlin/baseball/BaseballGame.kt
+++ src/main/kotlin/baseball/BaseballGame.kt
@@ -24,6 +24,8 @@ fun baseBallGame() {
         print(Messages.inputMessage)
         // 입력 및 규칙 검사
         val inputList = checkNumber(Console.readLine())
+        // 정답 여부 확인 및 메시지 출력
+        isAnswer = outputNumber(inputList, answer)
     }
 }
 
@@ -46,4 +48,28 @@ fun checkNumber(input: String): List<Int> {
         }
     }
     return input.map { it.toString().toInt() }
+}
+
+// 입력 숫자 결과 함수
+fun outputNumber(input: List<Int>, answer: List<Int>): Boolean {
+    var strike = 0
+    var ball = 0
+    var message = ""
+    input.forEachIndexed { index, inputNumber ->
+        if (inputNumber == answer[index]) {
+            strike++
+        } else if (answer.contains(inputNumber)) {
+            ball++
+        }
+    }
+
+    if (ball > 0) message += "${ball}${Messages.ball}"
+    if (ball > 0 && strike > 0) message += " "
+    if (strike > 0) message += "${strike}${Messages.strike}"
+    if (ball + strike == 0) message = Messages.out
+    println(message)
+
+    if (strike == 3) print(Messages.correct)
+
+    return strike == 3
 }
