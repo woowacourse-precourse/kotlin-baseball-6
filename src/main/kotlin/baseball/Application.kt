b53a04b0c5af042c00419aeb056cacde4c4693af
Sheep1sik <bohg6799@naver.com>
2023-10-22 19:17:12 +0900

    refactor: randomNum-> generateRandomNumber 변경, 카멜 표기법 적용

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,42 +1,45 @@
 package baseball
+
 import camp.nextstep.edu.missionutils.Randoms
 import camp.nextstep.edu.missionutils.Console
 
 class Game {
-
-
     private var computer: String
     private var user: String
     private var check: String
     private var checkNumResult: String
 
     init {
-        computer = randomNum()
+        computer = generateRandomNumber()
         user = ""
         check = ""
         checkNumResult = ""
     }
 
-    fun play(){
+    fun play() {
         println("숫자 야구 게임을 시작합니다.")
-        while (true){
-            user = getUserNum()
-            validateUser(user)
+        while (true) {
+            user = getUserInput()
+            validateUserInput(user)
             try {
-                checkNum(computer,user)
+                checkNumbers(computer, user)
                 println(checkNumResult)
-                if (checkNumResult == "3스트라이크"){
+                if (checkNumResult == "3스트라이크") {
                     println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
-                    check = reStart()
-                    if (check == "2"){break} else {computer = randomNum()}
+                    check = restartGame()
+                    if (check == "2") {
+                        break
+                    } else {
+                        computer = generateRandomNumber()
+                    }
                 }
-            } catch (e:IllegalArgumentException){
+            } catch (e: IllegalArgumentException) {
                 break
             }
         }
     }
 
-    private fun randomNum(): String {
+    private fun generateRandomNumber(): String {
         val number = mutableListOf<Int>()
         while (number.size < 3) {
             val randomNumber = Randoms.pickNumberInRange(1, 9)
@@ -46,37 +49,41 @@ class Game {
         }
         return number.joinToString("")
     }
-    private fun getUserNum(): String {
+
+    private fun getUserInput(): String {
         print("숫자를 입력해주세요 : ")
         return Console.readLine()
     }
-    private fun reStart(): String {
+
+    private fun restartGame(): String {
         println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
         return Console.readLine()
     }
-    private fun checkNum(computerNumber: String, userNumber: String){
+
+    private fun checkNumbers(computerNumber: String, userNumber: String) {
         var strikes = 0
         var balls = 0
-        for (i in computerNumber.indices){
-            if (computerNumber[i] == userNumber[i]) { //computerNumber[i]의 값이랑 userNumber[i]의 값이 같을때
-                strikes++ // strikes up
-            } else if (computerNumber.contains(userNumber[i])) { //computerNumber 리스트 안에 userNumber[i]가 포함되면
-                balls++ //balls up
+        for (i in computerNumber.indices) {
+            if (computerNumber[i] == userNumber[i]) {
+                strikes++
+            } else if (computerNumber.contains(userNumber[i])) {
+                balls++
             }
         }
-        return when {
-            strikes == 3 -> checkNumResult = "3스트라이크"
-            strikes > 0 || balls > 0 -> checkNumResult = "$balls" + "볼 " + "$strikes" + "스트라이크"
-            else ->  checkNumResult = "낫싱"
-        }
-    }
-    private fun validateUser(user: String) {
-        if (user.length != 3 || user.toSet().size != 3 || user.any { it !in '1'..'9' }){
-            throw IllegalArgumentException()
+        checkNumResult = when {
+            strikes == 3 -> "3스트라이크"
+            strikes > 0 || balls > 0 -> "$balls 볼 $strikes 스트라이크"
+            else -> "낫싱"
         }
     }
 
+    private fun validateUserInput(user: String) {
+        if (user.length != 3 || user.toSet().size != 3 || user.any { it !in '1'..'9' }) {
+            throw IllegalArgumentException()
+        }
+    }
 }
+
 fun main() {
     val baseballGame = Game()
     baseballGame.play()
