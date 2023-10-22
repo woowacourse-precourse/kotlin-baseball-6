1306974578269bcbdbfa5c0bb822ebf6e893dc92
yeonghyeon <agilecatch@gmail.com>
2023-10-22 19:46:40 +0900

    feat : 사용자 입력받기
       - 사용자로부터 3자리의 서로 다른 수 입력 받기

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -8,6 +8,10 @@ fun main() {
     // Game Start
     println("숫자 야구 게임을 시작합니다.")
 
+    /*
+    게임세팅
+     */
+
     // 컴퓨터가 생각하는 3자리 수 생성
     val computer = mutableListOf<Int>()
     while (computer.size < 3) {
@@ -17,6 +21,21 @@ fun main() {
             computer.add(randomNumber)
         }
     }
-    
+
+    try {
+        // 사용자 입력 받기
+        print("숫자를 입력해주세요: ")
+        val userInput = Console.readLine()
+
+        // userInput을 숫자로 변환하여 사용
+        val userNumbers = userInput.map { it.toString().toInt() }
+
+        println("입려한 값 ${userNumbers}")
+
+    } catch (e: IllegalArgumentException) {
+        // 잘못된 값 입력 시 예외 처리
+        println("잘못된 입력입니다. 프로그램을 종료합니다.")
+        return
+    }
 
 }
