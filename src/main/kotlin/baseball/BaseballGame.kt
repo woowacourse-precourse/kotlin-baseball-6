6482d1a0bfd06aba018d1e73d382268c3c0f3daa
sangil <fhemflrh60@gmail.com>
2023-10-22 23:02:55 +0900

    3. 문자열을 입력받아 힌트 메시지 출력    3-1. 입력받은 문자열이 1~9 사이의 서로 다른 3가지 숫자가 아니면 에러 출력

--- src/main/kotlin/baseball/BaseballGame.kt
+++ src/main/kotlin/baseball/BaseballGame.kt
@@ -1,5 +1,7 @@
 package baseball
 
+import baseball.resources.Messages
+import camp.nextstep.edu.missionutils.Console
 import camp.nextstep.edu.missionutils.Randoms
 
 // 입력 및 순환 함수
@@ -16,4 +18,32 @@ fun baseBallGame() {
             answer.add(randomNumber)
         }
     }
+
+    // 숫자 입력 및 메시지 출력 순환
+    while (!isAnswer) {
+        print(Messages.inputMessage)
+        // 입력 및 규칙 검사
+        val inputList = checkNumber(Console.readLine())
+    }
+}
+
+// 입력 숫자 예외 규칙 검사 함수
+fun checkNumber(input: String): List<Int> {
+    // 3이 아닌 길이의 문자열이 입력된 경우
+    if (input.length != 3) {
+        throw IllegalArgumentException(Messages.inputLengthError)
+    }
+    // 1~9 이외의 문자가 입력된 경우
+    input.forEach { inputChar ->
+        if (inputChar.toString().toInt() < 1 || inputChar.toString().toInt() > 9) {
+            throw IllegalArgumentException(Messages.inputRangeError)
+        }
+    }
+    // 같은 숫자가 입력된 경우
+    input.forEach { inputChar ->
+        if (input.filter { it == inputChar }.length > 1) {
+            throw IllegalArgumentException(Messages.inputOverlapError)
+        }
+    }
+    return input.map { it.toString().toInt() }
 }
