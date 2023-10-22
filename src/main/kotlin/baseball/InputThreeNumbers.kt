f2fc06dc60f9e38951a192d76784be11b60447e5
Junyoung-WON <wjyo60@gmail.com>
2023-10-23 03:05:22 +0900

    feat: 사용자로부터 숫자를 입력받아 정수형 리스트로 반환하는 함수 선언

--- src/main/kotlin/baseball/InputThreeNumbers.kt
+++ src/main/kotlin/baseball/InputThreeNumbers.kt
@@ -1,2 +1,16 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Console
+
+fun inputThreeNumbers(): MutableList<Int>{
+    var inputList = mutableListOf<Int>()
+
+    print("숫자를 입력해주세요 : ")
+    var inputString : String = Console.readLine()
+
+    for (char in inputString){
+        inputList.add(char.code)
+    }
+
+    return inputList
+}
