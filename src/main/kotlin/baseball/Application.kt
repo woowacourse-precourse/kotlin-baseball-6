27001545c396612573fa50f92e7f17427944552a
kyunghwa <snoopn303@gmail.com>
2023-10-22 11:57:00 +0900

    유저가 1~9까지 서로 다른 임의 수 3개를 입력

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,6 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Console
 import camp.nextstep.edu.missionutils.Randoms
 
 class BaseballGame {
@@ -14,9 +15,38 @@ class BaseballGame {
             }
         }
     }
+
     fun getComputer(): List<Int> = computerNumbers
+
+    fun getUserInput(): List<Int> {
+        while (true) {
+            try {
+                print("숫자를 입력해 주세요 : ")
+                val input = Console.readLine()
+                return checkUserInput(input)
+            } catch (e: IllegalArgumentException) {
+                println(e.message)
+            }
+        }
+    }
+
+    private fun checkUserInput(input: String): List<Int> {
+        if (!isValid(input)) {
+            throw IllegalArgumentException("잘못된 입력입니다. 1에서 9까지 서로 다른 수 3개를 입력해 주세요.")
+        }
+        return input.map { it.toString().toInt() }
+    }
+
+    private fun isValid(input: String): Boolean {
+        return input.length == 3 &&
+                input.all { it in '1'..'9' } &&
+                input.toSet().size == 3
+    }
 }
 
 fun main() {
+    val baseballGame = BaseballGame()
 
+    println("숫자 야구 게임을 시작합니다.")
+    val userInput = baseballGame.getUserInput()
 }
