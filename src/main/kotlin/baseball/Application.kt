1ea8c69cedbca27021a167f789bf813fa163643a
mainfn <_@mainfn.dev>
2023-10-22 12:57:53 +0900

    feat(Application): DI and run

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,15 @@
 package baseball
 
+import baseball.controller.GameController
+import baseball.domain.AnswerCreator
+import baseball.view.InputView
+import baseball.view.OutputView
+
 fun main() {
-    TODO("프로그램 구현")
+    val inputView = InputView()
+    val outputView = OutputView()
+    val answerCreator = AnswerCreator()
+    val gameController = GameController(inputView, outputView, answerCreator)
+
+    gameController.run()
 }
