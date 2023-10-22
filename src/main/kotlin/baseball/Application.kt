80e420423afdb4e12c324d3914785065e717e4fb
SeongWooHong <pluto0303@naver.com>
2023-10-22 09:38:01 +0900

    initial commit

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,33 @@
 package baseball
 
-fun main() {
-    TODO("프로그램 구현")
+import camp.nextstep.edu.missionutils.Randoms
+
+class RandomNumber{
+    var RandomNumber = 123
+
+    constructor() {
+        RandomNumber = Randoms.pickNumberInRange(123, 987)
+    }
+
 }
+class UserNumber {
+    var InputNumber = 123
+
+    constructor(Input: String){
+        InputNumber = Input.toInt() // Need Refactor
+    }
+
+}
+
+fun main() {
+    var InputNum = "123" // Need Refactor
+
+    InputNum = readLine().toString() // Need Refactor
+
+}
+
+/*
+* 추가할 부분
+* 1. 같은 숫자 (ex. 111, 998, 997 등등) 에 대해서 예외 처리
+*
+* */
