0cba7384b16aed8b2872647b7a85243839a55fc6
book3039 <book3039@knou.ac.kr>
2023-10-23 01:32:04 +0900

    fix: 기존 코드 오류 수정

--- src/main/kotlin/baseball/NumberBaseball.kt
+++ src/main/kotlin/baseball/NumberBaseball.kt
@@ -7,42 +7,40 @@ object NumberBaseball {
     private const val NUM_LENGTH = 3
 
     private val numList: ArrayList<Int> = arrayListOf()
-    private var userInputList: ArrayList<Int> = arrayListOf()
-
-    fun test() {
-        println(numList)
-    }
+    private var userNumList: ArrayList<Int> = arrayListOf()
 
     fun initRandomNumbers(): NumberBaseball {
         while (numList.size < NUM_LENGTH) {
             val randomNumber = Randoms.pickNumberInRange(1, 9)
             if (!numList.contains(randomNumber)) numList.add(randomNumber)
         }
+
         return this
     }
 
     fun getUserInput(): NumberBaseball {
         try {
-            userInputList = Console.readLine()
-                .convertInputToDigit()
-            println(userInputList)
+            userNumList = Console.readLine()
+                .convertInputToNumbers()
         } catch (e: IllegalArgumentException) {
-            println(e.message)
+            println("[ERROR] 잘못된 입력 값입니다.")
         }
+
         return this
     }
 
-    private fun String.convertInputToDigit(): ArrayList<Int> {
+
+    // 사용자의 입력을 Int로 바꿔 ArrayList에 담는다.
+    private fun String.convertInputToNumbers(): ArrayList<Int> {
         val tempList = arrayListOf<Int>()
         this.forEach {
-            when (it) {
-                in '1'..'9' -> tempList.add(it.digitToInt())
-                ' ', '\t' -> {}
-                else -> throw IllegalArgumentException("[ERROR] 잘못된 입력 값입니다.")
+            if ((it in '1'..'9') && !tempList.contains(it.digitToInt())) { // 중복된 숫자를 허용하지 않는다.
+                tempList.add(it.digitToInt())
+                return@forEach
             }
+            if (it != ' ' && it != '\t') throw IllegalArgumentException() // 공백과 탭 문자는 무시한다.
         }
-        tempList.distinct()
-        if (tempList.size != NUM_LENGTH) throw IllegalArgumentException("[ERROR] 숫자는 세 자리여야 합니다.")
+        if (tempList.size != NUM_LENGTH) throw IllegalArgumentException() // 리스트에 담긴 숫자가 NUM_LENGTH가 아니면 예외처리한다.
 
         return tempList
     }
