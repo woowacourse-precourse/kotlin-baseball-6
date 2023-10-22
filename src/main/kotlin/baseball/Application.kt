83b1f7c748fcfef7599b2f7c175b9a7a23edc5a5
yeonghyeon <agilecatch@gmail.com>
2023-10-22 19:56:03 +0900

    feat : 입력 값 검증하기
    - 사용자 입력값이 null 또는 3자리 숫자가 아닌 경우 예외 처리 :  IllegalArgumentException` 발생 및 애플리케이션 종료
    - 입력한 값중 중복된 숫자를 포함하는지 확인후 IllegalArgumentException으로 예외처리

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -5,37 +5,33 @@ import camp.nextstep.edu.missionutils.Randoms
 import camp.nextstep.edu.missionutils.Console
 
 fun main() {
-    // Game Start
+    // GameStart
     println("숫자 야구 게임을 시작합니다.")
 
-    /*
-    게임세팅
-     */
-
-    // 컴퓨터가 생각하는 3자리 수 생성
     val computer = mutableListOf<Int>()
     while (computer.size < 3) {
         val randomNumber = Randoms.pickNumberInRange(1, 9)
-        //중복된 숫자가 없도록 에외처리
-        if (!computer.contains(randomNumber)) {//컴퓨터리스트에 중복된번호가 포함되어있지 않을때!
+        if (!computer.contains(randomNumber)) {
             computer.add(randomNumber)
         }
     }
 
-    try {
-        // 사용자 입력 받기
-        print("숫자를 입력해주세요: ")
+    while (true) {
+        println("숫자를 입력해주세요 :")
         val userInput = Console.readLine()
 
-        // userInput을 숫자로 변환하여 사용
+        // 사용자 입력값이 null 또는 3자리 숫자가 아닌 경우 예외 처리
+        if (userInput == null || userInput.length != 3) {
+            throw IllegalArgumentException("올바른 입력이 아닙니다.")
+        }
+
+        // 사용자 입력값을 정수 리스트로 변환
         val userNumbers = userInput.map { it.toString().toInt() }
 
-        println("입려한 값 ${userNumbers}")
-
-    } catch (e: IllegalArgumentException) {
-        // 잘못된 값 입력 시 예외 처리
-        println("잘못된 입력입니다. 프로그램을 종료합니다.")
-        return
+        // 사용자 입력값이 중복된 숫자를 포함하고 있는지 확인
+        if (userNumbers.toSet().size != userNumbers.size) {
+            throw IllegalArgumentException("중복된 숫자가 포함되어 있습니다.")
+        }
     }
 
 }
