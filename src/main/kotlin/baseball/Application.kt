6287ffcbf9dfa91cefc5ea09e6331378d78a1afd
millinova98 <kcong1@naver.com>
2023-10-22 21:20:29 +0900

    docs: 기능 명세서 수정(기능 목록 추가)

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -2,6 +2,8 @@ package baseball
 import camp.nextstep.edu.missionutils.*
 
 fun main() {
+
+    // (컴퓨터) 1~9까지 서로 다른 임의의 수 3개를 선택한다.
     val computer: MutableList<Int> = mutableListOf<Int>()
     while (computer.size < 3) {
         val randomNumber = Randoms.pickNumberInRange(1, 9)
@@ -9,5 +11,5 @@ fun main() {
             computer.add(randomNumber)
         }
     }
-    println(computer)
+
 }
