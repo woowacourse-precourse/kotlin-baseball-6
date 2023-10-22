d7b3a25f5dd3b81069ee0d22fa6a52cf4821a1a7
추민수 <devsoftychoo@gmail.com>
2023-10-22 19:54:40 +0900

    [Feat] : 컴퓨터 Random List 할당
    getRandomList :컴퓨터는 1에서 9까지 랜덤으로 서로 다른 임의의 수 3개를 할당 받은 후 리스트의 형태로 return

--- src/main/kotlin/baseball/util/Utils.kt
+++ src/main/kotlin/baseball/util/Utils.kt
@@ -1,4 +1,6 @@
 package baseball.util
+import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
 
 /**
  * 작성자 : 추민수
@@ -7,16 +9,35 @@ package baseball.util
 object Utils {
 
     /**
+     * compareList()
+     * 컴퓨터는 1에서 9까지 랜덤으로 서로 다른 임의의 수 3개를 할당 받은 후 리스트의 형태로 return
+     */
+    fun getRandomList(): List<Int> {
+
+        val computerList = mutableListOf<Int>()
+
+        while (computerList.size < 3) {
+            val randomNumber = Randoms.pickNumberInRange(1, 9)
+
+            if (!computerList.contains(randomNumber)) {
+                computerList.add(randomNumber)
+            }
+        }
+        println("컴퓨터가 3개의 숫자를 할당 받았습니다.")
+        return computerList
+    }
+
+    /**
      * inputNum()
      * 사용자가 입력한 연속된 3자리의 문자열을 받아 한글자씩 나눠 리스트의 형태로 return
      */
     fun inputNum(): List<Int> {
         println("연속된 3자리의 숫자를 입력하세요:")
 
-        var inputString = readLine()
+        var inputString = Console.readLine()
         while (inputString == null || !isValidInput(inputString)) {
             println("유효하지 않은 입력입니다. 연속된 3자리의 숫자를 입력하세요:")
-            inputString = readLine()
+            inputString = Console.readLine()
         }
         // 문자열을 한글자씩 나눠 리스트에 나눠 전송
         return inputString.chunked(1).map { it.toInt() }
