1afc3a9d623b19688e6f6b83f1fb3cbc44dc24c3
tgyuu-An <atg0614@gmail.com>
2023-10-22 12:46:42 +0900

    [FEATURE] GameState EnumClass 추가

--- src/main/kotlin/baseball/GameManager.kt
+++ src/main/kotlin/baseball/GameManager.kt
@@ -1,12 +1,25 @@
 package baseball
 
 class GameManager {
+
+    enum class GameState {
+        WAITING, INPROGRESS, ENDED
+    }
+
     lateinit var targetNumber: List<Int>
+        private set
+
+    var gameState: GameState = GameState.WAITING
         private set
 
     private val randomTargetGenerator: RandomTargetGenerator by lazy { RandomTargetGenerator() }
 
     fun gameStart() {
+        setGameState(GameState.INPROGRESS)
         targetNumber = randomTargetGenerator.generateRandomTarget()
+    }
+
+    private fun setGameState(gameState : GameState){
+        this.gameState = gameState
     }
 }
