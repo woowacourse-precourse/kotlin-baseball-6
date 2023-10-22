94c3bdae6edb4c8ac895c25c24ce95b9d2ea3cbc
jun <hky08067@naver.com>
2023-10-22 15:57:09 +0900

    style: 상수 정의

---
+++ src/main/kotlin/baseball/Constants.kt
@@ -0,0 +1,25 @@
+package baseball
+
+object Comment {
+    const val STARTING_COMMENT = "숫자 야구 게임을 시작합니다."
+    const val END_COMMENT = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
+    const val GAME_STOP_COMMENT = "3개의 숫자를 모두 맞히셨습니다! 게임 종료"
+    const val ENTER_NUMBER_COMMENT = "숫자를 입력해주세요 : "
+    const val SPACE = " "
+}
+
+object BaseBallComment {
+    const val BALL_COMMENT = "볼"
+    const val STRIKE_COMMENT = "스트라이크"
+    const val NOTHING_COMMENT = "낫싱"
+}
+
+object UserChoice {
+    const val NONE_VALUE = "0"
+    const val RESTART_VALUE = "1"
+    const val EXIT_VALUE = "2"
+}
+
+object Number {
+    const val ZERO = 0
+}
