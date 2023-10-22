85fa05539f54e8422c675ded2242d1ed15091f4c
Junyoung-WON <wjyo60@gmail.com>
2023-10-23 03:27:33 +0900

    feat: 입력받은 문자열이 유효한지 검사

--- src/main/kotlin/baseball/InputThreeNumbers.kt
+++ src/main/kotlin/baseball/InputThreeNumbers.kt
@@ -1,32 +1,43 @@
 package baseball
 
 import camp.nextstep.edu.missionutils.Console
+import kotlin.concurrent.thread
 
-fun inputThreeNumbers(): MutableList<Int>{
+fun inputThreeNumbers(): MutableList<Int> {
     var inputList = mutableListOf<Int>()
 
     print("숫자를 입력해주세요 : ")
-    var inputString : String = Console.readLine()
+    var inputString: String = Console.readLine()
 
-    for (char in inputString){
-        inputList.add(char.code)
+    if (validateString(inputString)) {
+        for (char in inputString) {
+            inputList.add(char.code)
+        }
+        return inputList
+    } else {
+        throw IllegalArgumentException("input string is not validate")
     }
-
-    return inputList
 }
 
-fun isLengthThree(inputString: String): Boolean{
-    return if(inputString.length == 3){
+fun isLengthThree(inputString: String): Boolean {
+    return if (inputString.length == 3) {
         true
-    } else{
+    } else {
         false
     }
 }
 
 fun isNumber(inputString: String): Boolean {
-    return when(inputString.toIntOrNull())
-    {
+    return when (inputString.toIntOrNull()) {
         null -> false
         else -> true
+    }
+}
+
+fun validateString(inputString: String): Boolean {
+    return if (isNumber(inputString) && isLengthThree(inputString)) {
+        true
+    } else {
+        false
     }
 }
