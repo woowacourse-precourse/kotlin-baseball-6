6e7142049e0bc7b254f2ac4195f0c276deea77fc
이용인 <yongin0517@gmail.com>
2023-10-22 15:40:03 +0900

    feat: 숫자 야구공 입력 구현

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,14 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
-    TODO("프로그램 구현")
+    var computer = Computer()
+    println("숫자 야구 게임을 시작합니다.")
+    while (true) {
+        print("숫자를 입력해주세요: ")
+        var numberBall = Console.readLine()
+
+    }
 }
+
