c4ffd408ce62b49049c92717a1e63e2e0fbe9bb4
stopstone <wltjr6432@naver.com>
2023-10-22 14:08:48 +0900

    feat: 임의의 수 3개를 중복없이 뽑아, 문자열로 반환하는 함수

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,19 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
-    TODO("프로그램 구현")
+}
+
+
+fun pickComputerNumber(): String {
+    val computer:MutableList<Int> = mutableListOf<Int>()
+    while (computer.size < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (!computer.contains(randomNumber)) {
+            computer.add(randomNumber)
+        }
+    }
+    return computer.joinToString("")
 }
