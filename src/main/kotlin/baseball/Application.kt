61ef14e0e7fd9368a0c6181347a39b4e1b548441
Jtility <leejewon97@gmail.com>
2023-10-22 22:07:09 +0900

    feat(main): add while and variable that depending on start or exit
    add while that depending on start of exit
    print out put in start or exit
    put String to Int in startOrExit

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,12 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
     println("숫자 야구 게임을 시작합니다.")
+    var startOrExit = 1
+    while (startOrExit == 1) {
+        println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+        startOrExit = Console.readLine().toInt()
+    }
 }
