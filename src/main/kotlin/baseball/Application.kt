ef813ed5ff8fe79ff1ff0de55e88e09e5c3b1b5a
이빵주 <yungju9982@naver.com>
2023-10-22 19:12:27 +0900

    [feat] 입력된 값의 유효성을 판단하는 함수 구현

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -18,3 +18,29 @@ private fun generateRandomNumberList(): List<Int> {
     }
     return numberSet.toList()
 }
+
+private fun checkUserInput(userInput: String): List<Int> {
+    try {
+        validateUserLength(userInput)
+        validateUserInRange(userInput)
+        return userInput.map { it.toString().toInt() }
+    } catch (e: NumberFormatException) {
+        throw IllegalArgumentException("숫자가 아닙니다.")
+    } catch (e: IllegalArgumentException) {
+        throw e
+    }
+}
+
+private fun validateUserInRange(userInput: String) {
+    userInput.forEach { number ->
+        if (number.toString().toInt() < START || number.toString().toInt() > END) {
+            throw IllegalArgumentException("범위에 속하지 않은 숫자입니다.")
+        }
+    }
+}
+
+private fun validateUserLength(userInput: String) {
+    if (userInput.length != LENGTH) {
+        throw IllegalArgumentException("세 자리가 아닙니다.")
+    }
+}
