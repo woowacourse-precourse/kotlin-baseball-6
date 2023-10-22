6f31a002a78dfe0c473757cb03b85a2ee9762cb9
stopstone <wltjr6432@naver.com>
2023-10-22 16:03:34 +0900

    fix: 재시작 시 답 초기화가 안되는 버그 수정

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -4,20 +4,25 @@ import camp.nextstep.edu.missionutils.Console
 import camp.nextstep.edu.missionutils.Randoms
 
 fun main() {
-    gameRun()
+    printStartGameMessage()
+    do {
+        gameRun()
+    }while (chooseRestartAndExit())
+}
+
+fun printStartGameMessage() {
+    println("숫자 야구 게임을 시작합니다.")
 }
 
 fun gameRun() {
-    var FLAG = 1
     val answer = pickComputerNumber()
-    while(FLAG == 1) {
+    while(true) {
         val userNumber = inputUserNumber()
         val score = compareWithNumber(answer, userNumber)
         if(score.first == 3){
-            FLAG = gameOver()
+            gameOver()
         }
     }
-    print("게임 종료")
 }
 
 fun pickComputerNumber(): String {
@@ -70,13 +75,15 @@ fun printHint(strike:Int, ball:Int) {
     println(hint)
 }
 
-fun gameOver(): Int{
+fun gameOver(){
     println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
     println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
-    return chooseRestartAndExit()
 }
 
-fun chooseRestartAndExit(): Int {
+fun chooseRestartAndExit(): Boolean {
     val flag: Int = Console.readLine().toInt()
-    return flag
+    if(flag==1){
+        return true
+    }
+    return false
 }
