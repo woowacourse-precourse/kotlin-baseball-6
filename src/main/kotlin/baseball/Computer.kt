6725ffc0c88f6acc5b96fe689cf01ba5cced10d3
MinhoiKoo <minlove2013@gmail.com>
2023-10-22 20:02:43 +0900

    feat:컴퓨터가 설정한 수 반환하는 함수 생성

--- src/main/kotlin/baseball/Computer.kt
+++ src/main/kotlin/baseball/Computer.kt
@@ -6,9 +6,14 @@ object Computer {
     private val computerNumbers = mutableSetOf<Int>()
 
     fun setNumber() {
+        computerNumbers.clear()
         while(computerNumbers.size < 3) {
             val selectNumber = Randoms.pickNumberInRange(1,9)
             computerNumbers.add(selectNumber)
         }
+    }
+
+    fun getNumber() : List<Int> {
+        return computerNumbers.toList()
     }
 }
