cd6e65968fe930f0dc29f5e76f2e3057422f8cdc
yeonghyeon <agilecatch@gmail.com>
2023-10-22 19:21:37 +0900

    feat : 컴퓨터가 랜덤한 3자리 다른 수 생성
    - 컴퓨터가 1-9까지 서로 다른수로 이루어진 3자리 수를 랜덤으로 선택할수있도록 구현
    - 랜덤으로 증복된 수 가 들어가지 않도록 예외처리

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,6 +1,22 @@
 package baseball
 
-fun main() {
+import camp.nextstep.edu.missionutils.Randoms
 
+import camp.nextstep.edu.missionutils.Console
+
+fun main() {
+    // Game Start
     println("숫자 야구 게임을 시작합니다.")
+
+    // 컴퓨터가 생각하는 3자리 수 생성
+    val computer = mutableListOf<Int>()
+    while (computer.size < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        //중복된 숫자가 없도록 에외처리
+        if (!computer.contains(randomNumber)) {//컴퓨터리스트에 중복된번호가 포함되어있지 않을때!
+            computer.add(randomNumber)
+        }
+    }
+    
+
 }
