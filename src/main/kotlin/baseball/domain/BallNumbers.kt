59c30fa1819ca98c3db757c042de15bc3cd4027d
mainfn <_@mainfn.dev>
2023-10-22 15:04:52 +0900

    refact: replace magic numbers with static constants

--- src/main/kotlin/baseball/domain/BallNumbers.kt
+++ src/main/kotlin/baseball/domain/BallNumbers.kt
@@ -1,5 +1,6 @@
 package baseball.domain
 
+
 class BallNumbers(private val numbers: List<Int>) {
 
     init {
@@ -15,15 +16,20 @@ class BallNumbers(private val numbers: List<Int>) {
                     numbers.any { it == other.numbers[index] }
 
     companion object {
+
+        private const val BALL_COUNT = 3
+        private const val MIN_BALL_NUMBER = 1
+        private const val MAX_BALL_NUMBER = 9
+
         private fun validateNumbersLength(numbers: List<Int>) {
-            if (numbers.size != 3) {
+            if (numbers.size != BALL_COUNT) {
                 throw IllegalArgumentException("BallNumbers는 3자리로 이루어진 숫자여야 합니다.")
             }
         }
 
         private fun validateNumbers(numbers: List<Int>) {
             for (number in numbers) {
-                if (number < 1 || number > 9) {
+                if (number < MIN_BALL_NUMBER || number > MAX_BALL_NUMBER) {
                     throw IllegalArgumentException("BallNumber는 1~9 사이의 3자리 수여야 합니다.")
                 }
             }
