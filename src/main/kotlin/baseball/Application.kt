116993d86ac5f53652086d654fffafbe71a812db
eunji <nagosoo@kakao.com>
2023-10-22 16:28:04 +0900

    feat: 게임 진행 - 랜덤 숫자 생성

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,7 +1,28 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+
 const val START_MESSAGE = "숫자 야구 게임을 시작합니다."
+const val NUMBER_LENGTH = 3
+const val MIN_NUMBER = 1
+const val MAX_NUMBER = 9
+
+var playingGame = true
 
 fun main() {
     println(START_MESSAGE)
+    while (playingGame) {
+        val randomNum = createRandomNumber()
+    }
+}
+
+private fun createRandomNumber(): List<Int> {
+    val computer = mutableListOf<Int>()
+    while (computer.size < NUMBER_LENGTH) {
+        val randomNumber = Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER)
+        if (!computer.contains(randomNumber)) {
+            computer.add(randomNumber)
+        }
+    }
+    return computer
 }
