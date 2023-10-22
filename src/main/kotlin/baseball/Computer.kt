08a02b76f0732b92636e0e61d062d89a47727d81
MinhoiKoo <minlove2013@gmail.com>
2023-10-22 16:29:47 +0900

    fix:컴퓨터 세자리 수 중복값 나오는 오류 수정

--- src/main/kotlin/baseball/Computer.kt
+++ src/main/kotlin/baseball/Computer.kt
@@ -3,10 +3,10 @@ package baseball
 import camp.nextstep.edu.missionutils.Randoms
 
 object Computer {
-    private val computerNumbers = mutableListOf<Int>()
+    private val computerNumbers = mutableSetOf<Int>()
 
     fun setNumber() {
-        repeat(3) {
+        while(computerNumbers.size < 3) {
             val selectNumber = Randoms.pickNumberInRange(1,9)
             computerNumbers.add(selectNumber)
         }
