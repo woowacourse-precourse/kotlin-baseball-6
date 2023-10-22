94f896e1b54fd4d513f7d7859ee0da7664431f27
Jtility <leejewon97@gmail.com>
2023-10-23 05:28:00 +0900

    feat(strikeCount): add function strikeCount and apply in main

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -11,10 +11,21 @@ fun main() {
         do {
             print("숫자를 입력해주세요 : ")
             val inputNumbers = Console.readLine()
+            val strike = strikeCount(answerNumbers, inputNumbers)
         } while (false)
         println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
         startOrExit = Console.readLine()
     }
+}
+
+fun strikeCount(answerNumbers: String, inputNumbers: String): Int {
+    var numberOfStrike = 0
+    answerNumbers.forEachIndexed { i, answerNumber ->
+        if (answerNumber == inputNumbers[i]) {
+            numberOfStrike++
+        }
+    }
+    return numberOfStrike
 }
 
 fun makeRandomNumbers(): String {
