4a5788b657cb2928951b927ee29eb779fe8efbcc
Wooyoung Myung <mwy3055@gmail.com>
2023-10-23 17:17:44 +0900

    feature: 게임 진행 구조 완성

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,9 +1,32 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Console
 import camp.nextstep.edu.missionutils.Randoms
 
 fun main() {
-    TODO("프로그램 구현")
+    while (true) {
+        playGame()
+        if (!isRestart()) {
+            break
+        }
+    }
+}
+
+fun playGame() {
+    println("숫자 야구 게임을 시작합니다.")
+    while (true) {
+        val input = getUserAnswerInput()
+        println(input)
+    }
+}
+
+
+fun isRestart(): Boolean {
+    return false
+}
+
+fun wrongInput() {
+    throw IllegalArgumentException("잘못된 입력")
 }
 
 fun generateAnswer(): String {
