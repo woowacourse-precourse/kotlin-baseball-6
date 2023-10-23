c601b086482f870d18607d151337846436cb5fc3
hxeyexn <kck85075@kumoh.ac.kr>
2023-10-23 15:41:10 +0900

    Feat: 플레이어가 서로 다른 3자리의 수를 입력하는 기능 수정

--- src/main/kotlin/baseball/Player.kt
+++ src/main/kotlin/baseball/Player.kt
@@ -3,7 +3,18 @@ package baseball
 import camp.nextstep.edu.missionutils.Console
 
 class Player {
-    fun inputNumber(): String {
-        return Console.readLine()
+    fun inputNumber(): List<Int> {
+        var inputNumber = Console.readLine().toInt()
+        val inputNumberList = mutableListOf<Int>()
+        var eachNumber: Int
+
+        while (inputNumber > 0) {
+            eachNumber = inputNumber % 10
+            inputNumberList.add(eachNumber)
+            inputNumber /= 10
+        }
+        inputNumberList.reverse()
+
+        return inputNumberList
     }
 }
