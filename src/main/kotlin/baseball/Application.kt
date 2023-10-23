de48bf313db63513c850b93b9530918c85d9b6bb
LeeChoongHwan <choo901@naver.com>
2023-10-23 23:00:12 +0900

    fix : 랜덤 함수 출력 function 수정(같은 숫자 포함 경우 제외)

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -8,17 +8,21 @@ fun main() {
 
     //실행 중 여부 판단을 위한 변수
     var isRunning = true
+    val randomNum = createRandomNum()
 
     while (isRunning) {
-
+        print("숫자를 입력해주세요 : ")
     }
 }
 
+//서로 다른 세 수를 뽑는 Function
 fun createRandomNum() : List<Int> {
     val numList = ArrayList<Int>()
     for(index in 1..3) {
         val num = Randoms.pickNumberInRange(1,9)
-        numList.add(num)
+        if (!numList.contains(num)) {
+            numList.add(num)
+        }
     }
     return numList
 }
