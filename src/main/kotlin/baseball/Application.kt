31a17db06a68d6f01e0705a76215fa1add027622
jiwon2724 <stopone3119@naver.com>
2023-10-23 17:38:55 +0900

    [FEATURE] 입력을 검증하는 함수 구현 사용자의 입력을 검증하는 함수 구현 했습니다.

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,31 @@
 package baseball
 
-fun main() {
+import camp.nextstep.edu.missionutils.Console.*
 
+private const val INPUT_MIN_VALUE = 111
+private const val INPUT_MAX_VALUE = 999
+
+fun main() {
+    println("숫자 야구 게임을 시작합니다.")
+    startBaseballGame()
+}
+
+private fun startBaseballGame() {
+    print("숫자를 입력해주세요 : ")
+
+    try {
+        val inputBaseballNumbers = validateInput(readLine().toInt())
+    } catch (exception: Exception) {
+        when(exception) {
+            is IllegalStateException -> println("잘못된 값을 입력하셨습니다.")
+            else -> println("잘못된 값을 입력하셨습니다.")
+        }
+        return
+    }
+}
+
+private fun validateInput(input: Int) {
+    if (input !in INPUT_MIN_VALUE..INPUT_MAX_VALUE) {
+        throw IllegalStateException()
+    }
 }
