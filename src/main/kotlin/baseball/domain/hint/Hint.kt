f3ca3703da8ef9ef92a089a24211a6a65fc0959c
chaehyun <80222352+chaehyuns@users.noreply.github.com>
2023-10-23 16:57:26 +0900

    feat: player domain 수정 ⩤ #5

--- src/main/kotlin/baseball/domain/hint/Hint.kt
+++ src/main/kotlin/baseball/domain/hint/Hint.kt
@@ -1,8 +1,14 @@
 package baseball.domain.hint
 
+import baseball.domain.game.Turn
+
 class Hint(private val strikes: Int, private val balls: Int) {
     override fun toString(): String {
         return when {
+            this.strikes == 3 -> {
+                Turn(false)
+                "3개의 숫자를 모두 맞히셨습니다! 게임 종료"
+            }
             this.strikes == 0 && this.balls == 0 -> "낫싱"
             this.strikes != 0 && this.balls == 0 -> "${this.strikes}스트라이크"
             this.strikes == 0 && this.balls != 0 -> "${this.balls}볼"
