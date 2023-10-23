493fa8166a7e90f3710892840f10a8bbe44d1fbd
crAy <craycoding@gmail.com>
2023-10-24 03:13:25 +0900

    4. 게임 재시작/종료

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -17,43 +17,48 @@ fun getResult(strike : Int, ball : Int) : String {
 fun main() {
     println("숫자 야구 게임을 시작합니다.")
 
-    val computer = mutableListOf<Int>()
-    while (computer.size < 3) {
-        val randomNumber = Randoms.pickNumberInRange(1, 9)
-        if (!computer.contains(randomNumber)) {
-            computer.add(randomNumber)
-        }
-    }
-
     do {
-        var strike : Int
-        strike = 0
-        var ball : Int
-        ball = 0
-        print("숫자를 입력해주세요 : ")
-        val input = readLine()
-        val splitInput = input!!.toCharArray()
-        val me = mutableListOf<Int>()
-        for (char in splitInput) {
-            val numberOfInput : Int = Character.getNumericValue(char)
-            me.add(numberOfInput)
-        }
-
-        for (i in 0..<3) {
-            for (j in 0..<3) {
-                if (me[i] == computer[j]) {
-                    if (i == j) {
-                        strike++
-                    }
-                    else {
-                        ball++
-                    }
-                }
+        val computer = mutableListOf<Int>()
+        while (computer.size < 3) {
+            val randomNumber = Randoms.pickNumberInRange(1, 9)
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber)
             }
         }
 
-        println(getResult(strike, ball))
-    } while (strike < 3)
+        do {
+            var strike : Int
+            strike = 0
+            var ball : Int
+            ball = 0
+            print("숫자를 입력해주세요 : ")
+            val input = readLine()
+            val splitInput = input!!.toCharArray()
+            val me = mutableListOf<Int>()
+            for (char in splitInput) {
+                val numberOfInput : Int = Character.getNumericValue(char)
+                me.add(numberOfInput)
+            }
 
-    println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
+            for (i in 0..<3) {
+                for (j in 0..<3) {
+                    if (me[i] == computer[j]) {
+                        if (i == j) {
+                            strike++
+                        }
+                        else {
+                            ball++
+                        }
+                    }
+                }
+            }
+
+            println(getResult(strike, ball))
+        } while (strike < 3)
+
+        println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
+        println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+        val flag = readLine()!!.toInt()
+    } while (flag == 1)
+
 }
