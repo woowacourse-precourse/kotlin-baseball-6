34e725c7709a8ef144fe5997515a3c4d10956b24
YuBeen-Park <park_ubin@naver.com>
2023-10-23 18:12:38 +0900

    [feat] game - 게임 시작부터 유저가 입력한 값을 판정하는 부분까지 구현

---
+++ src/main/kotlin/baseball/BaseBallGame.kt
@@ -0,0 +1,54 @@
+package baseball
+
+class BaseBallGame {
+    private lateinit var computerNumber: List<Int>
+    private lateinit var userInput: List<Int>
+    private val computer = Computer()
+    private val user = User()
+    private var result = Result()
+
+    fun playGame() {
+        println("숫자 야구 게임을 시작합니다.")
+        computerNumber = computer.createRandomNumber()
+        println(computerNumber)
+        while (result.strike != 3) {
+            userInput = user.getNumberInput()
+            result = judgeResult()
+            println(result.toString())
+        }
+    }
+
+    private fun judgeResult(): Result {
+        val result = Result()
+        for (number in userInput) {
+            for (answer in computerNumber) {
+                if (number == answer) { // 같은 수인 경우
+                    // 같은 위치인지 확인
+                    if (userInput.indexOf(number) == computerNumber.indexOf(answer))
+                        result.strike++
+                    else
+                        result.ball++
+                    break
+                }
+            }
+        }
+        return result
+    }
+}
+
+data class Result(
+    var ball: Int = 0,
+    var strike: Int = 0,
+) {
+    override fun toString(): String {
+        var resultString = ""
+        if (ball > 0)
+            resultString += "${ball}볼 "
+        if (strike > 0)
+            resultString += "${strike}스트라이크"
+        return when {
+            resultString.isBlank() -> "낫싱"
+            else -> resultString
+        }
+    }
+}
