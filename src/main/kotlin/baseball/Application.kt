4e32f38411ec3c759212e544dfa5383f5461f483
Taehwani-Storage <wiz_th1024@naver.com>
2023-10-23 23:10:46 +0900

    feat: baseball-game v3 - random numbers(all different)

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -3,35 +3,6 @@ package baseball
 import camp.nextstep.edu.missionutils.Randoms
 import camp.nextstep.edu.missionutils.Console
 
-fun main() {
-    println("숫자 야구 게임을 시작합니다.")
-    var start = true // 게임 시작
-    while (start) {
-        // 숫자 무작위로 1부터 9까지 선택 (컴퓨터 상대)
-        val comNums: List<Int> = listOf(7, 1, 3)
-
-        // 사용자 입력 부분
-        var Input = false // 입력이 안된 경우, 거짓(null)
-        while (!Input) { // Input이 참이면 계속 반복
-            print("숫자를 입력해주세요 : ")
-            val userInput = Console.readLine()
-            // 사용자가 입력한 문자열이 3인지 확인
-            // toIntOrNull()을 이용해서 입력한 문자열이 정수 변환이 되는지(숫자로만 이루어져 있는지) 확인
-            if (userInput.length == 3 && userInput.toIntOrNull() != null) {
-                Input = checkNums(comNums, userInput.map { it.toString().toInt() })
-            } else {
-                println("올바른 숫자 3개를 입력하세요.")
-            }
-        }
-        // 프로그램 종료 부분
-        println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
-        when (Console.readLine()) {
-            "1" -> start = true // 1 입력하면 게임 새로 시작
-            "2" -> start = false // 2 입력하면 게임 종료
-        }
-
-    }
-}
 // 스트라이크와 볼 판정 함수
 fun checkNums(comNums: List<Int>, userNums: List<Int>): Boolean {
     var sVal = 0; // 스트라이크 개수 초기화
@@ -45,13 +16,13 @@ fun checkNums(comNums: List<Int>, userNums: List<Int>): Boolean {
         }
     }
     // 볼과 스트라이크 되는 경우
-    if (bVal > 0 && sVal > 0) {  // 같은 숫자가 존재하고 같은 인덱스에 있는 경우
+    if (bVal > 0 && sVal > 0)  // 같은 숫자가 존재하고 같은 인덱스에 있는 경우
         println("$bVal 볼 $sVal 스트라이크")
-    } else if (sVal > 0) { // 같은 숫자가 같은 인덱스에 있는 경우
+    else if (sVal > 0) // 같은 숫자가 같은 인덱스에 있는 경우
         println("$sVal 스트라이크")
-    } else if (bVal > 0) { // 인덱스는 다르지만 같은 숫자만 있는 경우
+    else if (bVal > 0) // 인덱스는 다르지만 같은 숫자만 있는 경우
         println("$bVal 볼")
-    } else // 모두 다른 숫자인 경우
+    else // 모두 다른 숫자인 경우
         println("낫싱")
     // 스트라이크 3개면 게임 종료
     if (sVal == 3) {
@@ -59,5 +30,42 @@ fun checkNums(comNums: List<Int>, userNums: List<Int>): Boolean {
         return true
     }
     return false;
+}
+
+fun main() {
+    println("숫자 야구 게임을 시작합니다.")
+    var start = true // 게임 시작
+
+    var computerInput = mutableListOf<Int>() // 컴퓨터 입력 변수
+    val nums = mutableListOf<Int>() // 무작위 숫자 저장할 변수 선언
+    while (nums.size < 3) {
+        // 숫자 무작위로 1부터 9까지 선택
+        val randomNum = Randoms.pickNumberInRange(0, 9) + 1
+        if (!nums.contains(randomNum)) { // 숫자가 nums 리스트에 없다면
+            nums.add(randomNum) // 해당 숫자를 nums 리스트에 추가 -> 중복 X
+        }
+    }
+    computerInput = nums; // 저장된 nums 리스트를 computerInput 리스트에 저장
+
+    while (start) {
+        // 사용자 입력 부분
+        var Input = false // 입력이 안된 경우, 거짓(null)
+        while (!Input) { // Input이 참이면 계속 반복
+            print("숫자를 입력해주세요 : ")
+            val userInput = Console.readLine()
+            // 사용자가 입력한 문자열이 3인지 확인
+            // toIntOrNull()을 이용해서
+            // 입력한 문자열이 정수 변환이 되는지와 숫자로만 이루어져 있는지 확인
+            if (userInput.length == 3 && userInput.toIntOrNull() != null)
+                Input = checkNums(computerInput, userInput.map { it.toString().toInt() })
+        }
+        // 프로그램 종료 부분
+        println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+        when (Console.readLine()) {
+            "1" -> start = true // 1 입력하면 게임 새로 시작
+            "2" -> start = false // 2 입력하면 게임 종료
+        }
+
+    }
 }
 
