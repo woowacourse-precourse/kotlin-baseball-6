4d7639b49ee668fa487edf7a4400d9c907aff710
JoYehyun <jo990714@naver.com>
2023-10-23 18:53:09 +0900

    feat: 플레이어 숫자 입력 기능

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,9 +1,13 @@
 package baseball
 import camp.nextstep.edu.missionutils.Randoms
+import camp.nextstep.edu.missionutils.Console
 
 fun main() {
     println("숫자 야구 게임을 시작합니다.")
     val computerNum = getRandomNum()
+    print("숫자를 입력해주세요 : ")
+    val input = Console.readLine().toInt()
+    val inputNum = setUserNum(input)
 }
 
 fun getRandomNum(): List<Int> {
@@ -15,4 +19,14 @@ fun getRandomNum(): List<Int> {
         }
     }
     return numList.toList()
+}
+
+fun setUserNum(inputNum: Int): List<Int> {
+    val numList: MutableList<Int> = mutableListOf()
+    var num = inputNum
+    while (num > 0) {
+        numList.add(num % 10)
+        num /= 10
+    }
+    return numList.reversed().toList()
 }
