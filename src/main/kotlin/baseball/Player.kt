fb9e25b6f46185fe5d2cae53f8f88afd9c44e764
stopkite <stopkite98@gmail.com>
2023-10-24 02:24:16 +0900

    feat: [Player] 주석 추가, 예외 처리 구현 예정

--- src/main/kotlin/baseball/Player.kt
+++ src/main/kotlin/baseball/Player.kt
@@ -4,21 +4,30 @@ package baseball
 class Player {
 
     fun createPlayerBallNumbers(): List<Int> {
-        return readPlayerInputNumbers().map { it.toString().toInt() }
+        val numbersInput = readln()
+        checkBallNumbersValidity(numbersInput)
+        return numbersInput.map { it.toString().toInt() }
     }
 
     fun getGameEndDecision(): String {
-        return readPlayerGameEndDecisionInput()
+        val userInput = readln()
+        checkPlayerDecisionValidity(userInput)
+        return userInput
     }
 
-    // TODO 예외처리
-    private fun readPlayerInputNumbers(): String {
-        return readln()
+    // TODO 로또 번호 유효성 체크
+    private fun checkBallNumbersValidity(numbersInput: String) {
+        // (1) 3자리수인가
+        // (2) 서로 다른 3자리인가
+        // (3) 숫자 외에 다른 값이 들어왔나
+        // (4) 1~9 이외의 다른값이 들어왔나
     }
 
-    // TODO 예외처리
-    private fun readPlayerGameEndDecisionInput(): String {
-        return readln()
+    // TODO 종료 입력 유효성 체크
+    private fun checkPlayerDecisionValidity(userInput: String) {
+        // (1) 1자리수인가
+        // (2) 숫자 외에 다른 값이 들어왔나
+        // (3) 1,2 이외의 다른 값이 들어왔나
     }
 
 }
