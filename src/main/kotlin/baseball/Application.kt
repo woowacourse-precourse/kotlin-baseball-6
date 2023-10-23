e3ab0e2aa65105a7cf7a66a9439014ad37147b90
jihyeonbaem <jihyeonbae424@gmail.com>
2023-10-23 21:12:08 +0900

    feat(): 돌아가는 쓰레기 완성
    예외 입력이 발생하면 그 즉시 프로그램이 종료되어야하는데, 반복문이 false로 끝나게 구현했어서 전면 수정을 했다.
    main에서 try{}catch{}를 없애니깐 왜 테스트가 통과하는지 모르겠다. try{}catch{}를 없애니깐 2번째 케이스가 통과됐다.

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -3,47 +3,71 @@ package baseball
 import camp.nextstep.edu.missionutils.Randoms
 import camp.nextstep.edu.missionutils.Console.readLine
 
-
 // 프로그램의 시작점
 fun main() {
+
     // 게임을 시작한다.
     startGame()
 
-    // 컴퓨터 숫자를 생성한다.
-    val computer = createComputerNumber()
-    println(computer)
+//    try {
+        // 게임 재시작 반복문
+        while (true) {
+            // 컴퓨터 숫자를 생성해 computer에 담는다.
+            val computer = createComputerNumber()
+//          println(computer) // 구현용 임시 출력
 
-    var continueGame = true
-
-    while (continueGame) {
-        // 플레이어 숫자를 입력 받는다.
-        val getPlayer = getPlayerNumber()
-        println(getPlayer)
-
-        // 플레이어 숫자를 검사하고, 예외를 처리한다.
-        try {
-            // 플레이어 숫자를 검사한다.
-            val player = validatePlayerNumber(getPlayer)
-
-            // 예외가 없으면, 플레이어의 숫자와 컴퓨터의 숫자를 비교한다.
-            val calculatePlayerScore = comparePlayerAndComputer(computer, player, playerScore = PlayerScore())
-
-            // 플레이어의 점수를 출력한다.
-            printPlayerScore(calculatePlayerScore)
-
-            // 3스트라이크일 때 루프문을 탈출해 게임 종료
-            if (calculatePlayerScore.strike == 3) {
-                continueGame = false
+            // 게임 라운드 반복문
+            while (true) {
+                // 플레이어가 숫자를 입력해 정답에 시도한다.
+                if (!playerTryAnswer(computer)) break
+                // 예외 발생 시 -> false + restartGame = false
+                // 3스트라이크 -> false
+                // 나머지 -> true
             }
 
-        } catch (e: IllegalArgumentException) {
-            // 예외가 있으면
-            println(e.message)
-            // 루프문 탈출
-            continueGame = false
-        }
-    }
+            // println("여긴 루프밖이야") // 구현용 임시 출력
 
+            // 게임 재시작 또는 프로그램 종료
+            if (!restartGameOrEndProgram()) break
+
+        }
+
+//    } catch (e : IllegalArgumentException) {
+////        println(e.message)
+//    }
+
+//    println("프로그램 종료") // 구현용 임시 출력
+}
+
+fun playerTryAnswer(computer: MutableList<Int>) : Boolean {
+    // 플레이어 숫자를 입력 받는다.
+    val getPlayer = getPlayerNumber()
+//    println(getPlayer) // 구현용 임시 출력
+
+    // 플레이어 숫자를 검사하고, 예외를 처리한다.
+//    try {
+        // 플레이어 숫자를 검사한다.
+        val player = validatePlayerNumber(getPlayer)
+
+        // 예외가 없으면, 플레이어의 숫자와 컴퓨터의 숫자를 비교한다.
+        val calculatePlayerScore = comparePlayerAndComputer(computer, player, playerScore = PlayerScore())
+
+        // 플레이어의 점수를 출력한다.
+        printPlayerScore(calculatePlayerScore)
+
+        // 3스트라이크일 때 루프문을 탈출해 게임 종료
+        if (calculatePlayerScore.strike == 3) {
+            return false
+        }
+
+//    } catch (e: IllegalArgumentException) {
+//        // 예외가 있으면
+////        println(e.message) // 구현용 임시 출력
+//        // 루프문 탈출
+//        return false
+//    }
+
+    return true
 }
 
 fun startGame() {
@@ -143,7 +167,9 @@ fun printPlayerScore(calculatePlayerScore: PlayerScore) {
     // 3개의 숫자를 모두 맞힐 경우
     if (calculatePlayerScore.strike == 3) {
         result = "${calculatePlayerScore.strike}스트라이크"
-        return println(result)
+        println(result)
+        endGame()
+        return
     }
 
     // 입력한 수에 대한 볼 개수 출력
@@ -157,4 +183,27 @@ fun printPlayerScore(calculatePlayerScore: PlayerScore) {
     }
 
     return println(result.trim())
+}
+
+fun endGame() {
+    println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
+}
+
+fun restartGameOrEndProgram() : Boolean {
+
+    println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+    val coin = readLine()
+
+    val coinInt = coin.toIntOrNull()
+
+    // 숫자 검증
+    if (coinInt != 1 && coinInt != 2) {
+        throw IllegalArgumentException("1또는 2만 입력해 주세요")
+    }
+
+    if (coinInt == 1) {
+        return true
+    }
+
+    return false
 }
