34b58d641d5e0ae467ea0653fea3019a3473e94d
yein <inwon9892@naver.com>
2023-10-23 23:33:43 +0900

    [feat] Check and receive inputs

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,6 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Console
 import camp.nextstep.edu.missionutils.Randoms
 
 fun main() {
@@ -9,13 +10,31 @@ fun main() {
 }
 
 // 컴퓨터가 세자리 수의 랜덤값 생성
-fun ComRandomNum(): MutableList<Int> {
-    val computerNum = mutableListOf<Int>()
+fun ComRandomNum(): MutableList<Char> {
+    val computerNum = mutableListOf<Char>()
     while (computerNum.size < 3) {
-        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        val randomNumber = Randoms.pickNumberInRange(1, 9).toChar()
         if (!computerNum.contains(randomNumber)) {
             computerNum.add(randomNumber)
         }
     }
     return computerNum
+}
+
+// 잘못된 입력값 점검
+fun CheckNum(numbers: MutableList<Char>){
+    if (numbers.size != 3) throw IllegalArgumentException("세자리의 수가 아닙니다.")
+
+    for (i in numbers.indices){
+        val number = Integer.parseInt(numbers[i].toString())
+        if(number < 1 || number > 9) throw IllegalArgumentException("1과 9부터의 수가 아닙니다.")
+        if (numbers.count { it == numbers[i] } > 1) throw IllegalArgumentException("세 개의 숫자가 서로 달라야 합니다")
+    }
+}
+
+// 사용자로부터 입력값 받기
+fun inputNum(): MutableList<Char> {
+    val input = Console.readLine()
+    CheckNum(input.toMutableList())
+    return input.toMutableList()
 }
