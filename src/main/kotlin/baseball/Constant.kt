2afcc5692cacbf9a0c3dcd3cce127ef041eafe77
stopkite <stopkite98@gmail.com>
2023-10-24 03:11:53 +0900

    refactor: [Constant] string으로 타입 통일 - 기능이 추가되고 변수들이 늘어나면 프로그램 관리가 힘들어 질 것 같다고 생각해서 타입을 통일했습니다.(char -> string)

--- src/main/kotlin/baseball/Constant.kt
+++ src/main/kotlin/baseball/Constant.kt
@@ -4,7 +4,7 @@ class Constant {
     companion object {
         const val RESTART = "1"
         const val END = "2"
-        const val MIN_BALL_NUMBER = '1'
-        const val MAX_BALL_NUMBER = '9'
+        const val MIN_BALL_NUMBER = "1"
+        const val MAX_BALL_NUMBER = "9"
     }
 }
