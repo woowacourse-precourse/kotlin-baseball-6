06242438fa6013109f0ab34b923c0b1d7263f17c
HYUNJIN LEE <dlguswls456@naver.com>
2023-10-23 21:49:52 +0900

    feat: 랜덤 정답 생성

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,20 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
-    TODO("프로그램 구현")
+
+    println("숫자 야구 게임을 시작합니다.")
+
+    val computer = mutableListOf<Int>()
+    generateRandomNumber(computer)
+}
+
+private fun generateRandomNumber(computer: MutableList<Int>) {
+    while (computer.size < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (!computer.contains(randomNumber)) {
+            computer.add(randomNumber)
+        }
+    }
 }
