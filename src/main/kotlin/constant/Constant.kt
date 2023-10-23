91ed0ba1be84794999811c23f24c81b03c99ec7f
mk43313@gmail.com <mk43313@gmail.com>
2023-10-23 23:53:33 +0900

    complete Constant

--- src/main/kotlin/constant/Constant.kt
+++ src/main/kotlin/constant/Constant.kt
@@ -1,8 +1,22 @@
 package constant
 
 public class Constant{
-    var USER_SIZE = 3
-    var START_NUMBER = 1
-    var END_NUMBER = 9
+    companion object {
+        var CORRECT_USER_SIZE = 3
+        var START_NUMBER = 1
+        var END_NUMBER = 9
 
+        //결과 출력
+        var INPUT_STRING = "숫자를 입력해주세요: "
+        var SUCCESS = 3
+        var NOTHING = "낫싱"
+        var STRIKE = "스트라이크"
+        var BALL = "볼 "
+        var SUCCESS_STRING = "3개의 숫자를 모두 맞히셨습니다! 게임 종료"
+
+        // 재시작
+        var RESTART_STRING = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요: "
+        var RESTART_INPUT = "1"
+        var TERMINATE_INPUT = "2"
+    }
 }
