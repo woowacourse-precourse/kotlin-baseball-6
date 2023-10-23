4c712f14c326c6982bfbe3e2a834f69f3e915393
seoyeon <seoyeonn051@gmail.com>
2023-10-23 10:21:51 +0900

    Feat: 사용자 숫자 입력 및 예외처리 기능 생성 사용자 입력 값이 서로 다른 세 자리 숫자가 아니면 IllegalargumentException 발생

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,17 +1,35 @@
 package baseball
 
 import camp.nextstep.edu.missionutils.Randoms
+import camp.nextstep.edu.missionutils.Console
 
 fun main() {
+    var restartGame = true
+    do {
+        var computerNum = generateRandomNum()
+
+
+
+    } while (restartGame)
+}
+
+fun getUserGuessNum() {
+    var userNum = Console.readLine()
+    require(checkCorrectNum(userNum)){
+        "서로 다른 세 자리 숫자를 입력해주세요."
+    }
+
 
 }
 
-fun getUserGuessNum(){
-
+fun checkCorrectNum(num:String):Boolean{
+    if (num.all { it.isDigit() } && num.length == 3 && num.toSet().size == 3) {
+        return true
+    }
+    return false
 }
 
-
-fun generateRandomNum():HashMap<Int, Int>{
+fun generateRandomNum(): HashMap<Int, Int> {
     var computer: MutableList<Int> = mutableListOf()
     while (computer.size < 3) {
         val randomNumber: Int = Randoms.pickNumberInRange(1, 9)
@@ -20,12 +38,12 @@ fun generateRandomNum():HashMap<Int, Int>{
         }
     }
     var computerHashMap = HashMap<Int, Int>()
-    for (i in 0 until computer.size){
+    for (i in 0 until computer.size) {
         computerHashMap.put(computer[i], i)
     }
     return computerHashMap
 }
 
-fun compareNums(){
+fun compareNums() {
 
 }
