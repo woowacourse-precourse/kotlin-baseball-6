067302bbcce269ee2d430b1a20dc3d859e294c9b
kimdahee7 <kikidahee7@duksung.ac.kr>
2023-10-24 01:38:19 +0900

    [Feat] 랜덤 숫자 생성 함수 구현

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,18 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
-    TODO("프로그램 구현")
+}
+
+//1~9 범위의 서로 다른 3자리의 수 생성
+private fun generateRandomNumbers(): List<Int> {
+    val computer = mutableListOf<Int>()
+    while (computer.size < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (!computer.contains(randomNumber)) {
+            computer.add(randomNumber)
+        }
+    }
+    return computer
 }
