bf3551731d46a4ff83b82bf71299d78a127327e6
parkjiwon <ji11won@naver.com>
2023-10-23 22:07:41 +0900

    Feat : 기능 요구사항 2 완
        - 입력 값 받기
        - validation -> IllegalArgumentException

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,6 +1,7 @@
 package baseball
 
 import camp.nextstep.edu.missionutils.Randoms
+import camp.nextstep.edu.missionutils.Console
 
 fun main() {
     println("숫자 야구 게임을 시작합니다.")
@@ -11,4 +12,26 @@ fun main() {
             computer.add(randomNumber)
         }
     }
+
+    print("숫자를 입력해주세요 : ")
+    val you = inputAndValidate()
+
+}
+
+fun inputAndValidate(): List<Int> {
+    val input = Console.readLine()
+    return validate(input)
+}
+
+fun validate(input: String): List<Int> {
+    if(input.length > 3) {
+        IllegalArgumentException("3자리 이상을 입력하였습니다.")
+    }
+    val numbers = input.toList().map { Character.getNumericValue(it) }
+    for (number in numbers) {
+        if (number !in (1..9)) {
+            IllegalArgumentException("수를 잘못 입력하였습니다.")
+        }
+    }
+    return numbers
 }
