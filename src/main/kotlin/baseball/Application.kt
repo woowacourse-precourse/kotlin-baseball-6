9d6491fc0d9fa4257b1b01987c51c2cf1ed343b6
HYUNJIN LEE <dlguswls456@naver.com>
2023-10-23 22:00:52 +0900

    feat: 사용자 입력 받기 및 오류처리

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -8,6 +8,38 @@ fun main() {
 
     val computer = mutableListOf<Int>()
     generateRandomNumber(computer)
+
+    val player = mutableListOf<Int>()
+    getPlayerNumber(player)
+}
+
+private fun getPlayerNumber(player: MutableList<Int>) {
+    print("숫자를 입력해주세요 : ")
+    val input = readlnOrNull()
+    if (input.isNullOrBlank()) {
+        throw IllegalArgumentException()
+    }
+    if (input.length != 3) {
+        throw IllegalArgumentException()
+    }
+    var intInput = try {
+        input.toInt()
+    } catch (e: NumberFormatException) {
+        throw IllegalArgumentException()
+    }
+
+    while (player.size < 3) {
+        val currentNum = intInput % 10
+        if (currentNum == 0) {
+            throw IllegalArgumentException()
+        }
+        if (player.contains(currentNum)) {
+            throw IllegalArgumentException()
+        }
+        player.add(currentNum)
+        intInput /= 10
+    }
+    player.reverse()
 }
 
 private fun generateRandomNumber(computer: MutableList<Int>) {
