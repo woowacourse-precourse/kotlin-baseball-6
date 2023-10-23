0d8ef36163537b83266e2f949e4a9d983e5053c0
kimdahee7 <kikidahee7@duksung.ac.kr>
2023-10-24 02:55:04 +0900

    [Feat] 게임 시작 문구 출력

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -3,16 +3,30 @@ package baseball
 import camp.nextstep.edu.missionutils.Randoms
 
 fun main() {
+    NumberBaseballGame().start()
 }
 
-//1~9 범위의 서로 다른 3자리의 수 생성
-private fun generateRandomNumbers(): List<Int> {
-    val computer = mutableListOf<Int>()
-    while (computer.size < 3) {
-        val randomNumber = Randoms.pickNumberInRange(1, 9)
-        if (!computer.contains(randomNumber)) {
-            computer.add(randomNumber)
-        }
+class NumberBaseballGame {
+    private val output = OutputWriter()
+    fun start() {
+        output.printGameStart()
+        output.printReceiveNumberInput()
     }
-    return computer
+
+    //1~9 범위의 서로 다른 3자리의 수 생성
+    private fun generateRandomNumbers(): List<Int> {
+        val computer = mutableListOf<Int>()
+        while (computer.size < 3) {
+            val randomNumber = Randoms.pickNumberInRange(1, 9)
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber)
+            }
+        }
+        return computer
+    }
+}
+
+class OutputWriter {
+    fun printGameStart() = println("숫자 야구 게임을 시작합니다.")
+    fun printReceiveNumberInput() = print("숫자를 입력해주세요 : ")
 }
