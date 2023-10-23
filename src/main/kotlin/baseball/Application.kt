0ace1202704b00ff8f47dd2b57e2a5c8afbeedca
Wooyoung Myung <mwy3055@gmail.com>
2023-10-23 17:16:54 +0900

    feature: 각 자릿수가 서로 다른 3자리 숫자 생성

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,12 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
     TODO("프로그램 구현")
+}
+
+fun generateAnswer(): String {
+    val digits = Randoms.pickUniqueNumbersInRange(1, 9, 3)
+    return digits.joinToString(separator = "")
 }
