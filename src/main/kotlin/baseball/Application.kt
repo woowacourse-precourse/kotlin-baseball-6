2e918d303ee6c7a8c279dbcb1b54c1d84f3c902e
april <aprilgom@gmail.com>
2023-10-24 02:06:51 +0900

    3자리의 랜덤 숫자를 만들어 정답으로 설정하기.

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,4 +1,16 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+import camp.nextstep.edu.missionutils.Console
+
+fun makeSecretNumber(): String {
+    val stringBuilder = StringBuilder()
+    repeat(3) {
+        val randomNumber = Randoms.pickNumberInRange(0,9)
+        stringBuilder.append(randomNumber)
+    }
+    return stringBuilder.toString()
+}
 fun main() {
+    var secretNumber = makeSecretNumber()
 }
