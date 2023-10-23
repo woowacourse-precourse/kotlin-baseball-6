1a081f6640bf6dc21d8d1465d57644d9fc9d68e3
book3039 <book3039@knou.ac.kr>
2023-10-23 15:20:51 +0900

    feat: 게임 재시작 및 종료 기능 구현

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -2,9 +2,12 @@ package baseball
 
 fun main() {
     try {
-        NumberBaseball
-            .initRandomNumbers()
-            .playBaseball()
+        println("숫자 야구 게임을 시작합니다.")
+        while (NumberBaseball.isPlaying) {
+            NumberBaseball.playBaseball()
+                .askQuitOrRestart()
+        }
+        println("숫자 야구 게임을 종료합니다.")
     } catch (e: IllegalArgumentException) {
         println("[ERROR] 잘못된 입력 값입니다.")
     }
--- src/main/kotlin/baseball/NumberBaseball.kt
+++ src/main/kotlin/baseball/NumberBaseball.kt
@@ -6,30 +6,38 @@ import camp.nextstep.edu.missionutils.Randoms
 object NumberBaseball {
     private const val NUM_LENGTH = 3
 
-    private val comNumList: ArrayList<Int> = arrayListOf()
+    private var comNumList: ArrayList<Int> = arrayListOf()
     private var userNumList: ArrayList<Int> = arrayListOf()
 
     private var strikeCount = 0
     private var ballCount = 0
 
-    fun initRandomNumbers(): NumberBaseball {
+    var isPlaying = true
+
+    private fun initValues(): NumberBaseball {
         while (comNumList.size < NUM_LENGTH) {
             val randomNumber = Randoms.pickNumberInRange(1, 9)
             if (!comNumList.contains(randomNumber)) comNumList.add(randomNumber)
         }
         println(comNumList)
-        println("숫자 야구 게임을 시작합니다.")
-
         return this
     }
 
+    private fun setDefault() {
+        strikeCount = 0
+        ballCount = 0
+        comNumList.clear()
+        userNumList.clear()
+
+    }
+
     fun playBaseball(): NumberBaseball {
+        initValues()
         while (strikeCount < NUM_LENGTH) {
             getUserInput()
             calculateBallCounts()
         }
         println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
-
         return this
     }
 
@@ -37,6 +45,7 @@ object NumberBaseball {
         print("숫자를 입력해 주세요 : ")
         userNumList = Console.readLine()
             .convertInputToNumbers()
+        if (userNumList.size != NUM_LENGTH) throw IllegalArgumentException()
     }
 
     // 사용자의 입력을 Int로 바꿔 ArrayList에 담는다.
@@ -49,7 +58,6 @@ object NumberBaseball {
             }
             if (it != ' ' && it != '\t') throw IllegalArgumentException() // 공백과 탭 문자는 무시한다.
         }
-        if (tempList.size != NUM_LENGTH) throw IllegalArgumentException() // 리스트에 담긴 숫자가 NUM_LENGTH가 아니면 예외처리한다.
 
         return tempList
     }
@@ -99,4 +107,15 @@ object NumberBaseball {
 
         println(strCall)
     }
+
+    fun askQuitOrRestart() {
+        println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+        val temp = Console.readLine()
+
+        if (temp.length != 1) throw IllegalArgumentException()
+        if (temp != "1" && temp != "2") throw IllegalArgumentException()
+        if (temp == "1") setDefault()
+        if (temp == "2") isPlaying = false
+    }
+
 }
