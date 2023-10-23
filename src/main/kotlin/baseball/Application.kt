d9e99a93a0fb14e294672697a9369f9f65bdaf4c
jooyyoo <aliceyun9@naver.com>
2023-10-24 02:14:35 +0900

    [과제1] - 숫자 야구 게임

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,90 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+import kotlin.system.exitProcess
+
+val computer = mutableListOf<Int>()
+val inputArray = mutableListOf<Int>()
 fun main() {
-    TODO("프로그램 구현")
+    println("숫자 야구 게임을 시작합니다.")
+    printResult()
+}
+
+private fun makeRandomNumber() {
+    while (computer.size < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (!computer.contains(randomNumber)) {
+            computer.add(randomNumber)
+        }
+    }
+    //println(computer)
+}
+
+private fun guessNumber() {
+    print("숫자를 입력해주세요 : ")
+
+    val input = readln()
+    try {
+        if (input.length == 3) {
+            val digit1 = input[0].toString().toInt()
+            val digit2 = input[1].toString().toInt()
+            val digit3 = input[2].toString().toInt()
+
+            if (digit1 != digit2 && digit1 != digit3 && digit2 != digit3) {
+                inputArray.add(digit1)
+                inputArray.add(digit2)
+                inputArray.add(digit3)
+            }else{
+                throw IllegalArgumentException("게임 종료")
+            }
+            //println(inputArray)
+        } else {
+            throw IllegalArgumentException("게임 종료")
+        }
+    } catch (_: IllegalArgumentException) {
+        exitProcess(0)
+    }
+}
+
+private fun printResult() {
+    var strike: Int
+    var ball = 0
+    makeRandomNumber()
+    while (ball != 3) {
+        guessNumber()
+        strike = 0
+        ball = 0
+
+        for (i in inputArray.indices) {
+            if (computer[i] == inputArray[i]) {
+                ball += 1
+            } else if (computer.contains(inputArray[i])) {
+                strike += 1
+            }
+        }
+
+        if (ball > 0 && strike > 0) {
+            println("$ball" + "볼" + " $strike" + "스트라이크")
+        } else if (ball == 0 && strike != 0) {
+            println("$strike" + "스트라이크")
+        } else if (strike == 0 && ball != 0) {
+            println("$ball" + "볼")
+        } else {
+            println("낫싱")
+        }
+        inputArray.clear()
+    }
+
+    computer.clear()
+
+    println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
+    println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+
+    val gameOver: Int = readln().toInt()
+
+    if (gameOver == 1) {
+        main()
+    } else if (gameOver == 2) {
+        exitProcess(0)
+    }
 }
