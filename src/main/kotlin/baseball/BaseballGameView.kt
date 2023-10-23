58d8060a5d8d9df9895790cb8588339e63c84127
stopstone <wltjr6432@naver.com>
2023-10-23 23:56:54 +0900

    style: 공백 수정

--- src/main/kotlin/baseball/BaseballGameView.kt
+++ src/main/kotlin/baseball/BaseballGameView.kt
@@ -17,7 +17,6 @@ class BaseballGameView(private val validate: BaseballGameValidate) {
         return user
     }
 
-
     fun gameOver() {
         println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
         println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
@@ -42,6 +41,4 @@ class BaseballGameView(private val validate: BaseballGameValidate) {
         validate.validateRestartAndExit(flag)
         return flag == 1
     }
-
-
 }
--- src/main/kotlin/baseball/BaseballGameViewModel.kt
+++ src/main/kotlin/baseball/BaseballGameViewModel.kt
@@ -57,5 +57,4 @@ class BaseballGameViewModel(private val model: BaseballGameModel, private val vi
 
         return Pair(strike, ball)
     }
-
 }
