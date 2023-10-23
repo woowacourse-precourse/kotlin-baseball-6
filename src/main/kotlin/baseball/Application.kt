dc9b5aaf240182e8833053f4a27830ce5ed422e1
JoYehyun <jo990714@naver.com>
2023-10-23 18:31:14 +0900

    feat: 상대방(컴퓨터) 랜덤 숫자 생성

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,18 @@
 package baseball
+import camp.nextstep.edu.missionutils.Randoms
 
 fun main() {
-    TODO("프로그램 구현")
+    println("숫자 야구 게임을 시작합니다.")
+    val computerNum = getRandomNum()
+}
+
+fun getRandomNum(): List<Int> {
+    val numList: MutableList<Int> = mutableListOf()
+    while (numList.size < 3) {
+        val randomNum = Randoms.pickNumberInRange(1,9)
+        if (!numList.contains(randomNum)) {
+            numList.add(randomNum)
+        }
+    }
+    return numList.toList()
 }
