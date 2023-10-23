d7731566519b5168d0019a7248748db55be4c14d
LeeChoongHwan <choo901@naver.com>
2023-10-23 22:54:28 +0900

    feature : 랜덤 숫자 출력 함수 구현

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,8 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
     println("숫자 야구 게임을 시작합니다.")
 
@@ -9,4 +12,13 @@ fun main() {
     while (isRunning) {
 
     }
+}
+
+fun createRandomNum() : List<Int> {
+    val numList = ArrayList<Int>()
+    for(index in 1..3) {
+        val num = Randoms.pickNumberInRange(1,9)
+        numList.add(num)
+    }
+    return numList
 }
