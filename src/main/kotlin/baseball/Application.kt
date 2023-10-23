a1b61663d7a6e02cf792a4ce86c1453455b33602
Oh-JunTaek <143782929+Oh-JunTaek@users.noreply.github.com>
2023-10-23 20:27:40 +0900

    base ball game

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -4,42 +4,43 @@ import camp.nextstep.edu.missionutils.Console
 import camp.nextstep.edu.missionutils.Randoms
 
 fun main() {
-    runGame()
+    runGame()//게임을 실행 하는 메인 함수
 }
 
 fun runGame() {
-    var continueGame = true
+    var continueGame = true //게임 지속 여부
 
-    while (continueGame) {
+    println("숫자 야구 게임을 시작합니다.")
+
+    while (continueGame) {//종료전까지 반복 실행
         val target = generateTarget()
         var guess: String
         var result: Pair<Int, Int> = Pair(0, 0)
 
         do {
-            println("숫자 야구 게임을 시작합니다.")
             println("숫자를 입력해주세요: ")
             guess = Console.readLine()
-            if (!isValidInput(guess)) {
+            if (!isValidInput(guess)) {//예외설정
                 throw IllegalArgumentException("Invalid input. Please enter a 3-digit number with distinct digits.")
             }
 
-            result = checkGuess(guess, target)
+            result = checkGuess(guess, target)//입력값과 목표값 비교
 
             when {
                 result.first == 0 && result.second == 0 -> println("낫싱")
                 else -> println("${result.second}볼 ${result.first}스트라이크")
             }
-        } while (result.first < 3)
+        } while (result.first < 3)//스트라이크가 3개 미만이 될 때 까지 반복
 
-        println("축하합니다! 숫자를 맞추셨습니다.")
+        println("축하합니다! 3개의 숫자를 모두 맞추셨습니다.")
 
-        println("게임을 다시 시작하려면 '1'을, 종료하려면 '2'를 입력하세요: ")
+        println("게임을 새로 시작하려면 '1'을, 종료하려면 '2'를 입력하세요: ")
 
         when(Console.readLine()) {
             "1" -> continueGame = true
             "2" -> {
                 continueGame = false
-                println("게임 종료") // 게임 종료 메시지 추가
+                println("게임 종료")
             }
             else -> {
                 throw IllegalArgumentException("잘못된 선택입니다. 게임을 종료합니다.")
@@ -49,11 +50,11 @@ fun runGame() {
     }
 }
 
-fun generateTarget(): String {
-    val numbers = mutableListOf<Int>()
+fun generateTarget(): String {//목표 숫자를 생성
+    val numbers = mutableListOf<Int>()//생성된 숫자를 저장할 리스트
 
-    while (numbers.size < 3) {
-        val randomNumber = Randoms.pickNumberInRange(1, 9)
+    while (numbers.size < 3) {//3개의 숫자를 생성한다.
+        val randomNumber = Randoms.pickNumberInRange(1, 9)//1~9랜덤값 생성
         if (!numbers.contains(randomNumber)) {
             numbers.add(randomNumber)
         }
@@ -62,13 +63,13 @@ fun generateTarget(): String {
     return numbers.joinToString(separator="")
 }
 
-fun isValidInput(input: String): Boolean {
-    if (input.length != 3 || input.toSet().size != input.length) {
+fun isValidInput(input: String): Boolean {//입력받은 값에 대한 유효성 확인
+    if (input.length != 3 || input.toSet().size != input.length) {//길이가 3인지 겹치는지 확인
         return false
     }
 
-    for (char in input) {
-        val digit = char.toString().toIntOrNull()
+    for (char in input) {// 각 자리수 확인
+        val digit = char.toString().toIntOrNull()//정수값이며 1~9사이에 있어야 한다.
         if (digit == null || digit < 1 || digit > 9) {
             return false
         }
@@ -77,17 +78,17 @@ fun isValidInput(input: String): Boolean {
     return true
 }
 
-fun checkGuess(guess: String, target: String): Pair<Int, Int> {
+fun checkGuess(guess: String, target: String): Pair<Int, Int> {//추측값과 변수를 비교하여 볼과 스트라이크 개수 계산
     var strikes = 0
     var balls = 0
 
-    for (i in guess.indices) {
-        if (guess[i] == target[i]) {
+    for (i in guess.indices) {//각 자리 수에 스트라이크와 볼을 체크하는 반복문 설정
+        if (guess[i] == target[i]) {//같은 위치에 같은 숫자가 있으면 스트라이크
             strikes++
-        } else if (target.contains(guess[i])) {
+        } else if (target.contains(guess[i])) {//다른 위치에 같은 숫자가 있으면 볼
             balls++
         }
     }
 
-    return Pair(strikes, balls)
+    return Pair(strikes, balls) // 계산된 스트라이크와 볼 의 개수를 반환
 }
