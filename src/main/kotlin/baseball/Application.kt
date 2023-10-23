ac05217c7f04d6d82cd4646a82f6044cda7faff3
sungsik-git <kss950708@gamil.com>
2023-10-24 01:52:57 +0900

    add compare both computer and user number

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -10,8 +10,9 @@ fun main() {
     println("숫자 야구 게임을 시작합니다.")
 
     while (restart == false){
-        print("숫자를 입력해주세요 : ")
-        var userNumver = checkUserNumber()
+        var userNumber = checkUserNumber()
+        var result = compareBothComputerAndUser(computer, userNumber)
+        
     }
 }
 
@@ -30,9 +31,45 @@ fun checkUserNumber() : Int {
     var userString = readLine()
     var userNumber = userString?.toInt()
 
+    print("숫자를 입력해주세요 : ")
     if (userNumber in 100..999 && userNumber != null){
         return userNumber
     }else{
         throw IllegalArgumentException()
     }
+}
+
+fun compareBothComputerAndUser(computer: MutableList<Int>, userNumber: Int) : Int{
+    val userNumberMut = mutableListOf<Int>()
+    var tempNumber = userNumber
+    var strike = 0
+    var ball = 0
+    while (tempNumber > 0 ) {
+        userNumberMut.add(0, tempNumber % 10)
+        tempNumber /= 10
+    }
+
+    for (i in 0..userNumberMut.size-1){
+        for (j in 0..computer.size-1){
+            if (userNumberMut[i] == computer[j]){
+                if (i == j){
+                    strike ++
+                }else if (i != j){
+                    ball++
+                }
+            }
+        }
+    }
+
+    if(strike == 0 && ball == 0){
+        println("낫싱")
+    }else if(strike == 0 && ball != 0){
+        println("${ball}볼")
+    }else if(strike != 0 && ball == 0){
+        println("${strike}스트라이크")
+    }else{
+        println("${ball}볼 ${strike}스트라이크")
+    }
+
+    return strike
 }
