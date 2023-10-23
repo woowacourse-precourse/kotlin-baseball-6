3328e12f7c5eaf34ec48a57a1f9a28d81bec81fd
april <aprilgom@gmail.com>
2023-10-24 03:05:57 +0900

    입력과 정답 비교후 결과 get

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -3,6 +3,11 @@ package baseball
 import camp.nextstep.edu.missionutils.Randoms
 import camp.nextstep.edu.missionutils.Console
 
+data class GuessResult(
+    val ballNumber: Int,
+    val strikeNumber: Int
+)
+
 fun makeSecretNumber(): String {
     val stringBuilder = StringBuilder()
     repeat(3) {
@@ -18,10 +23,39 @@ fun isValidInput(input: String): Boolean {
 
 fun isNumber(input: Char) = input in '0'..'9'
 
+fun getGuessResult(secretNumber: String, guessNumber: String): GuessResult {
+    val ball = countBall(secretNumber, guessNumber)
+    val strike = countStrike(secretNumber, guessNumber)
+    return GuessResult(ball, strike)
+}
+
+fun countBall(secretNumber: String, guessNumber: String): Int {
+    var ball = 0
+    for (secretIt in secretNumber) {
+        for (guessIt in guessNumber) {
+            if (secretIt == guessIt) {
+                ball++
+            }
+        }
+    }
+    return ball
+}
+
+fun countStrike(secretNumber: String, guessNumber: String): Int {
+    var strike = 0
+    for (it in 0 until 3) {
+        if (secretNumber[it] == guessNumber[it]) {
+            strike++
+        }
+    }
+    return strike
+}
+
 fun main() {
-    var secretNumber = makeSecretNumber()
-    var guessNumber = Console.readLine()
+    val secretNumber = makeSecretNumber()
+    val guessNumber = Console.readLine()
     if (!isValidInput(guessNumber)) {
         throw IllegalArgumentException("Wrong Input")
     }
+
 }
