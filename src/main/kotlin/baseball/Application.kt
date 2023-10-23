d157e292f0596fb8249668644971614c9a295182
eunyeong <feijoaplz@gmail.com>
2023-10-24 02:09:11 +0900

    feat: 사용자입력값과 컴퓨터값 비교하여 ball 과 strike 수 세기

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -48,10 +48,30 @@ fun game(computer: List<Int>, userNumber: List<Int>): List<Int>{
         if (computer[i] == userNumber[i]) {
             strikeCount++
         }
-        else{ // if (computer.contains(userNumber))
+        else{ // if (computer.contains(userNumber[i]))
             ballCount++
         }
     }
 
     return listOf(ballCount,strikeCount)
+}
+
+fun compareNumber(game: List<Int>): String {
+    val ball = game[0]
+    val strike = game[1]
+
+    var str = ""
+
+    if(ball == 0 && strike == 0) {
+        println("낫싱")
+    }
+    if(ball == 0 && strike > 0) {
+        println("${strike}스트라이크")
+    }
+    if(strike == 0 && ball > 0) {
+        println("${ball}볼")
+    }
+    else {
+        println("${ball}볼 ${strike}스트라이크")
+    }
 }
