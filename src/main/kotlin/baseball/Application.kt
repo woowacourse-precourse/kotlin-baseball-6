403b2d61855489ba6007fde62ba8e30009752f10
seoyeon <seoyeonn051@gmail.com>
2023-10-23 15:15:11 +0900

    Feat: compareNum 기능 생성, printBaseBallGameResult 생성
    compareNum 기능 생성
    - strike, ball, nothing 구문 후 카운트
    printBaseBallGameResult 생성
    - strike, ball, nothing 값에 따라 결과
    compareNum, printBaseBallGameResult 함수 적용

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -5,33 +5,39 @@ import camp.nextstep.edu.missionutils.Console
 
 fun main() {
     var count = 0
+    print(Dialogues.startGameMessage)
     do {
-        print(Dialogues.startGameMessage)
-
         var computerNum = generateRandomNum()
         try{
-            solveBaseBallGame()
+            solveBaseBallGame(computerNum)
         } catch (e:java.lang.IllegalArgumentException){
-            println(e.message)
             return throw IllegalArgumentException()
         }
-        count +=1
-
-
+        println(Dialogues.restartMessage)
     } while (count !=1)
 }
 
-fun solveBaseBallGame(){
+fun solveBaseBallGame(computerNum:HashMap<Int, Int>){
     while(true) {
+
+        var userNumHashMap = HashMap<Int, Int>()
         print(Dialogues.requestEnterNumMessage)
         var enterUserNum = Console.readLine()
         println(enterUserNum)
         if (checkCorrectNum(enterUserNum)) {
-            val userNumHashMap = changeToHashMap(enterUserNum.map { it.toString().toInt() }.toMutableList())
-            println(userNumHashMap)
+            userNumHashMap = changeToHashMap(enterUserNum.map { it.toString().toInt() }.toMutableList())
         } else {
             throw IllegalArgumentException("서로 다른 세 자리 숫자를 입력해주세요.")
         }
+        var str = compareNum(computerNum, userNumHashMap)
+        var result = printBaseBallGameResult(str)
+        /// 받아온 문자열로 결과를 확인하는 게 맞을까?
+        println(result)
+        if(result == "3스트라이크"){
+            println(Dialogues.gameOverMessage)
+            break
+        }
+
     }
 }
 
@@ -62,6 +68,43 @@ fun generateRandomNum(): HashMap<Int, Int> {
     return changeToHashMap(computerNum)
 }
 
-fun compareNums(computerNum: HashMap<Int, Int>, userNum: HashMap<Int, Int>) {
+fun compareNum(computerNum: HashMap<Int, Int>, userNum: HashMap<Int, Int>): HashMap<String, Int> {
+    var resultHashMap:HashMap<String, Int> = hashMapOf(
+        "strike" to 0, "ball" to 0, "nothing" to 0
+        )
+    for ((key, value) in userNum) {
+        val hasKey = computerNum.containsKey(key)
+        if (hasKey) {
+            if (computerNum[key] == value) {
+                resultHashMap["strike"] = resultHashMap["strike"]!! + 1
+            } else {
+                resultHashMap["ball"] = resultHashMap["ball"]!! + 1
+            }
+        } else {
+            resultHashMap["nothing"] = resultHashMap["nothing"]!! + 1
+        }
+    }
+    return resultHashMap
+}
 
+fun printBaseBallGameResult(numArray:HashMap<String, Int>):String{
+    var strike = numArray["strike"]
+    var ball = numArray["ball"]
+    var nothing = numArray["nothing"]
+    return if(nothing==3){
+        Dialogues.nothingMessage
+    }else if(strike==3){
+        "3${Dialogues.strikeMessage}"
+    }else{
+        val result = mutableListOf<String>()
+
+        if (ball!! > 0) {
+            result.add("${ball}${Dialogues.ballMessage}")
+        }
+
+        if (strike!! > 0) {
+            result.add("${strike}${Dialogues.strikeMessage}")
+        }
+        result.joinToString(" ")
+    }
 }
