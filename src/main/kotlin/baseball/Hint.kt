30044caec8f1144d4ad3c391700784b4c731579b
MinhoiKoo <minlove2013@gmail.com>
2023-10-23 16:03:48 +0900

    refactor:힌트 계산하는 로직 수정
    -불필요한 함수 제거

--- src/main/kotlin/baseball/Hint.kt
+++ src/main/kotlin/baseball/Hint.kt
@@ -1,34 +1,13 @@
 package baseball
 
 object Hint {
-    fun compareNumber(computer: List<Int>, player: List<Int>): Boolean {
-
-        val nothingFlag = isNothing(computer, player)
-        return when (nothingFlag) {
-            true -> {
-                false
-            }
-
-            else -> {
-                isStrike(computer, player)
-                isBall(computer, player)
-                true
-            }
-        }
-    }
-
-    private fun isNothing(computer: List<Int>, player: List<Int>): Boolean {
-        for(i in 0 until 3) {
-            if (player.contains(computer[i])) {
-                return false
-            }
-            // 포함이 전혀 안된 상태(낫싱)
-        }
-        return true
+    fun compareNumber(computer: List<Int>, player: List<Int>) {
+        isStrike(computer, player)
+        isBall(computer, player)
     }
 
     private fun isStrike(computer: List<Int>, player: List<Int>) {
-        for(i in 0 until 3) {
+        for (i in 0 until 3) {
             if (computer[i] == player[i]) {
                 GameCount.plusStrikeCount()
             }
@@ -37,7 +16,7 @@ object Hint {
 
     private fun isBall(computer: List<Int>, player: List<Int>) {
         for (i in 0 until 3) {
-            for(j in 0 until 3) {
+            for (j in 0 until 3) {
                 if (i != j && computer[i] == player[j]) {
                     GameCount.plusBallCount()
                 }
