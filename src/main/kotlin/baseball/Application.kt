e4d6c38dd2b422a02b2049348c50dca234b7ecda
Taehwani-Storage <wiz_th1024@naver.com>
2023-10-23 13:38:59 +0900

    first-commit

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,10 @@
 package baseball
 
+import java.util.Random // Random 사용을 위한 메소드 import
+
 fun main() {
-    TODO("프로그램 구현")
+    // 게임 시작
+    println("숫자 야구 게임을 시작합니다.")
+    var baseball_com = Random().nextInt(500) + 1
+    println(baseball_com)
 }
