919393d5212a2b4e39b69141df06f9e746255ff4
mediumorange <ujeorange@gmail.com>
2023-10-23 21:43:06 +0900

    Feat: 플레이어 숫자 입력 받기
    잘못된 값 입력시 예외 발생시키기

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,10 +1,12 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Console
 import camp.nextstep.edu.missionutils.Randoms
 
 fun main() {
     println("숫자 야구 게임을 시작합니다.")
-    val answerNumberList = createAnswerNumber()
+    val answerNumberList: List<Int> = createAnswerNumber()
+    val playerNumberList: List<Int> = getPlayerNumber()
 
 }
 
@@ -17,4 +19,24 @@ fun createAnswerNumber(): List<Int> {
         }
     }
     return numbers.toList()
+}
+
+fun getPlayerNumber(): List<Int> {
+    val receiveValue = Console.readLine().trim()
+
+    if(receiveValue.length != 3) {
+        throw IllegalArgumentException("3자리 숫자를 입력해 주세요")
+    }
+    try {
+        receiveValue.toInt()
+    } catch (e: NumberFormatException) {
+        throw IllegalArgumentException("숫자만 입력해 주세요")
+    }
+    if(receiveValue.contains("0")) {
+        throw IllegalArgumentException("1~9까지의 숫자만 입력해 주세요")
+    }
+    if(receiveValue.toList().toSet().size != 3) {
+        throw IllegalArgumentException("중복된 숫자 입력 불가")
+    }
+    return receiveValue.toList().map { Character.getNumericValue(it) }
 }
