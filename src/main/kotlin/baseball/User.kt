5b56b61e0f63baa8bcb5461b505eb49e1ec18938
lh99j <grand2181@gmail.com>
2023-10-23 09:45:02 +0900

    refactor: User 클래스 불변값 상수화

--- src/main/kotlin/baseball/User.kt
+++ src/main/kotlin/baseball/User.kt
@@ -4,6 +4,16 @@ import camp.nextstep.edu.missionutils.Console
 import java.lang.IllegalArgumentException
 
 class User {
+    companion object {
+        private const val RESTART = "1"
+        private const val QUIT = "2"
+        private const val SIZE = 3
+        private const val NOT_VALIDATE_LENGTH = "3자리의 입력이 아닙니다."
+        private const val NOT_VALIDATE_NUMBER = "사용자의 입력이 1부터 9까지의 숫자가 아닙니다."
+        private const val NOT_UNIQUE_NUMBER = "사용자의 입력 중 중복된 숫자가 존재합니다."
+        private const val NOT_VALIDATE_RESTART_NUMBER = "입력이 1 또는 2가 아닙니다."
+    }
+
     fun inputUserNumber(): MutableList<Int> {
         print("숫자를 입력해주세요 : ")
         val userNumber = Console.readLine()
@@ -16,7 +26,7 @@ class User {
         val userInput = Console.readLine()
         checkValidRestartNumber(userInput)
 
-        return userInput == "1"
+        return userInput == RESTART
     }
 
     private fun checkValidUserNumber(number: String) {
@@ -26,8 +36,8 @@ class User {
     }
 
     private fun validateLength(number: String) {
-        if (number.length != 3) {
-            throw IllegalArgumentException("3자리의 입력이 아닙니다.")
+        if (number.length != SIZE) {
+            throw IllegalArgumentException(NOT_VALIDATE_LENGTH)
         }
     }
 
@@ -35,21 +45,21 @@ class User {
         val regex = Regex("[1-9]{3}")
 
         if (!regex.matches(number)) {
-            throw IllegalArgumentException("사용자의 입력이 1부터 9까지의 숫자가 아닙니다.")
+            throw IllegalArgumentException(NOT_VALIDATE_NUMBER)
         }
     }
 
     private fun validateUnique(number: String) {
-        if (number.toList().distinct().size != 3) {
-            throw IllegalArgumentException("사용자의 입력 중 중복된 숫자가 존재합니다.")
+        if (number.toList().distinct().size != SIZE) {
+            throw IllegalArgumentException(NOT_UNIQUE_NUMBER)
         }
     }
 
     private fun checkValidRestartNumber(input: String) {
-        if (input == "1" || input == "2") {
+        if (input == RESTART || input == QUIT) {
             return
         }
 
-        throw IllegalArgumentException("입력이 1 또는 2가 아닙니다.")
+        throw IllegalArgumentException(NOT_VALIDATE_RESTART_NUMBER)
     }
 }
