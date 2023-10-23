8258b65a59d695736b3dfcc84bf6ea5a7487014e
GreenRain <47217640+greenrain78@users.noreply.github.com>
2023-10-24 04:26:31 +0900

    Balls 객체를 다시 numList를 받아서 생성하도록 변경하고 Score 객체의 strike와 ball을 외부에서 볼 수 있되 수정 불가능하도록 수정

--- src/main/kotlin/baseball/model/Balls.kt
+++ src/main/kotlin/baseball/model/Balls.kt
@@ -1,6 +1,6 @@
 package baseball.model
 
-class Balls(num: Int) {
+class Balls(numList: List<Int>) {
     companion object {
         const val MAX_BALLS: Int = 3
     }
@@ -9,17 +9,21 @@ class Balls(num: Int) {
 
 
     init {
-        val numList = num.toString().map { it.toString().toInt() }
-        checkNumList(numList = numList)
+        require(numList.size == MAX_BALLS) { "Ball 갯수가 $MAX_BALLS 이 아닌 ${numList.size} 입니다." }
+        require(numList.size == numList.distinct().size) { "중복된 값이 존재하면 안됩니다." }
         this.balls = numList.map { Ball(num = it) }
     }
 
-    private fun checkNumList(numList: List<Int>) {
-        require(numList.size == MAX_BALLS) { "Ball 갯수가 $MAX_BALLS 이 아닌 ${numList.size} 입니다." }
-        require(numList.size == numList.distinct().size) { "중복된 값이 존재하면 안됩니다." }
+    fun contains(ball: Ball): Boolean {
+        return this.balls.contains(ball)
+    }
+
+    operator fun get(idx: Int): Ball {
+        return this.balls[idx]
+
     }
 
     override fun toString(): String {
-        return "Balls: ${balls.toString()}"
+        return "Balls: $balls"
     }
 }
--- src/main/kotlin/baseball/model/Score.kt
+++ src/main/kotlin/baseball/model/Score.kt
@@ -1,8 +1,8 @@
 package baseball.model
 
 class Score(ball: Int, strike: Int) {
-    private var strike: Int
-    private var ball: Int
+    val strike: Int
+    val ball: Int
 
     init {
         require(strike >= 0) { "strike $strike 가 양수가 아닙니다." }
