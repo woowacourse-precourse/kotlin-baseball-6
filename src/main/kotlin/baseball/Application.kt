b1dcebcb6c385007f099097b66e58774e2d35d6b
seoyeon <seoyeonn051@gmail.com>
2023-10-23 13:24:57 +0900

    Fix: solveBaseBallGame 예외처리, changeToHashMap 모듈 분리
    solveBaseBallGame 수정
    - 예외처리 적용 수정
    - enterUserNum 변수 Char to MutableList
    - main에 예외처리 적용
    changeToHashMap 매개 변수 수정
    - ComputerNum, UserNum 둘 다 사용 가능

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -4,37 +4,45 @@ import camp.nextstep.edu.missionutils.Randoms
 import camp.nextstep.edu.missionutils.Console
 
 fun main() {
-    var restartGame = false
+    var count = 0
     do {
         print(Dialogues.startGameMessage)
 
         var computerNum = generateRandomNum()
+        try{
+            solveBaseBallGame()
+        } catch (e:java.lang.IllegalArgumentException){
+            println(e.message)
+            return throw IllegalArgumentException()
+        }
+        count +=1
 
 
-
-    } while (restartGame)
-
+    } while (count !=1)
 }
 
-fun solveBaseBallGame():Boolean{
+fun solveBaseBallGame(){
     while(true) {
         print(Dialogues.requestEnterNumMessage)
         var enterUserNum = Console.readLine()
-        print(enterUserNum)
-        require(checkCorrectNum(enterUserNum)) {
-            "서로 다른 세 자리 숫자를 입력해주세요."
+        println(enterUserNum)
+        if (checkCorrectNum(enterUserNum)) {
+            val userNumHashMap = changeToHashMap(enterUserNum.map { it.toString().toInt() }.toMutableList())
+            println(userNumHashMap)
+        } else {
+            throw IllegalArgumentException("서로 다른 세 자리 숫자를 입력해주세요.")
         }
     }
-    return true
 }
 
-fun changeToHashMap(num: String):HashMap<Int, Int>{
+fun changeToHashMap(num: MutableList<Int>):HashMap<Int, Int>{
     var hashMap = HashMap<Int, Int>()
     num.forEachIndexed{ index, char ->
-        hashMap[char.code] = index
+        hashMap[char] = index
     }
     return hashMap
 }
+
 fun checkCorrectNum(num:String):Boolean{
     if (num.all { it.isDigit() } && num.length == 3 && num.toSet().size == 3) {
         return true
@@ -50,13 +58,8 @@ fun generateRandomNum(): HashMap<Int, Int> {
             computerNum.add(randomNumber)
         }
     }
-    var computerNumHashMap = HashMap<Int, Int>()
 
-    computerNum.forEachIndexed{ index, char ->
-        computerNumHashMap[char] = index
-    }
-
-    return computerNumHashMap
+    return changeToHashMap(computerNum)
 }
 
 fun compareNums(computerNum: HashMap<Int, Int>, userNum: HashMap<Int, Int>) {
