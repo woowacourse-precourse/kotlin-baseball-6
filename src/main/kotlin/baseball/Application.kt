1155e528480586acfbb92391548d9fa98d5e8312
april <aprilgom@gmail.com>
2023-10-24 04:59:24 +0900

    출력 수정

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -69,17 +69,16 @@ fun printGuessResult(result: GuessResult) {
 
 
 fun main() {
+    println("숫자 야구 게임을 시작합니다.")
     while(true){
         val secretNumber = makeSecretNumber()
         while(true) {
             println(secretNumber)
             val guessNumber = Console.readLine()
             if (!isValidGuessNumber(guessNumber)) {
-                throw IllegalArgumentException("Wrong guessNumber")
+                throw IllegalArgumentException("Wrong guessNumber $guessNumber")
             }
-            println(guessNumber)
             val guessResult = getGuessResult(secretNumber, guessNumber)
-            printGuessResult(guessResult)
             if (guessResult.strikeNumber == 3) {
                 println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
                 break
@@ -88,7 +87,7 @@ fun main() {
         println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
         val continueNumber = Console.readLine()
         if (!isValidContinueNumber(continueNumber)) {
-            throw IllegalArgumentException("Wrong continueNumber")
+            throw IllegalArgumentException("Wrong continueNumber $continueNumber")
         }
         if (continueNumber[0] == '2') {
             break
