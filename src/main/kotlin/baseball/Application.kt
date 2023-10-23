94ed955f169f2577237b8a4a263b54698056dd96
april <aprilgom@gmail.com>
2023-10-24 02:36:57 +0900

    사용자로부터 3자리 숫자를 입력받고, 검증

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -6,11 +6,22 @@ import camp.nextstep.edu.missionutils.Console
 fun makeSecretNumber(): String {
     val stringBuilder = StringBuilder()
     repeat(3) {
-        val randomNumber = Randoms.pickNumberInRange(0,9)
+        val randomNumber = Randoms.pickNumberInRange(0, 9)
         stringBuilder.append(randomNumber)
     }
     return stringBuilder.toString()
 }
+
+fun isValidInput(input: String): Boolean {
+    return input.all { isNumber(it) } && input.length == 3
+}
+
+fun isNumber(input: Char) = input in '0'..'9'
+
 fun main() {
     var secretNumber = makeSecretNumber()
+    var guessNumber = Console.readLine()
+    if (!isValidInput(guessNumber)) {
+        throw IllegalArgumentException("Wrong Input")
+    }
 }
