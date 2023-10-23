484b6d3ddd242986698f48fe51f536bb8adaa0d5
crAy <craycoding@gmail.com>
2023-10-23 16:51:51 +0900

    2. 컴퓨터 생성

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,14 @@
 package baseball
+import camp.nextstep.edu.missionutils.Randoms
 
 fun main() {
     println("숫자 야구 게임을 시작합니다.")
+
+    val computer = mutableListOf<Int>()
+    while (computer.size < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (!computer.contains(randomNumber)) {
+            computer.add(randomNumber)
+        }
+    }
 }
