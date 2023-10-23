19e09f4531e5186016af047e14b0849ba608174e
hjw0623 <117283878+hjw0623@users.noreply.github.com>
2023-10-23 18:01:51 +0900

    Add: 사용자의 입력 값이 유효한지 검사하는 기능 추가

--- src/main/kotlin/baseball/Player.kt
+++ src/main/kotlin/baseball/Player.kt
@@ -6,4 +6,19 @@ class Player {
         println("숫자를 입력해주세요 : ")
         return readLine() ?: ""
     }
+    //사용자가 입력한 값이 유효한지 검사
+    fun isInputValid(userInput: String): Boolean {
+        val inputLength = 3
+        //입력값의 길이 검사
+        if (userInput.length != inputLength) {
+            return false
+        }
+        //입력값에 공백이나 0이 포함되어 있는지 검사
+        if (userInput.contains(" ") || userInput.contains("0")) {
+            return false
+        }
+        //입력값의 중복 검사
+        val distinctChars = userInput.toSet()
+        return distinctChars.size == inputLength
+    }
 }
