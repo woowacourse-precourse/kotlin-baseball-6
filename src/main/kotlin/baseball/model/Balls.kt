34852011adc3b267c62a1a2c983223d283682bee
GreenRain <47217640+greenrain78@users.noreply.github.com>
2023-10-24 02:48:03 +0900

    Balls 객체를 생성할 때 List가 아닌 Int를 받아서 처리하도록 변경

--- src/main/kotlin/baseball/model/Balls.kt
+++ src/main/kotlin/baseball/model/Balls.kt
@@ -1,6 +1,6 @@
 package baseball.model
 
-class Balls(numList: List<Int>) {
+class Balls(num: Int) {
     companion object {
         const val MAX_BALLS: Int = 3
     }
@@ -9,9 +9,14 @@ class Balls(numList: List<Int>) {
 
 
     init {
-        require(numList.size == MAX_BALLS) { "Ball 갯수가 $MAX_BALLS 이 아닌 ${numList.size} 입니다." }
-        require(numList.size == numList.distinct().size) { "중복된 값이 존재하면 안됩니다." } // 별도로 분리? override
+        val numList = num.toString().map { it.toString().toInt() }
+        checkNumList(numList = numList)
         this.balls = numList.map { Ball(num = it) }
+    }
+
+    private fun checkNumList(numList: List<Int>) {
+        require(numList.size == MAX_BALLS) { "Ball 갯수가 $MAX_BALLS 이 아닌 ${numList.size} 입니다." }
+        require(numList.size == numList.distinct().size) { "중복된 값이 존재하면 안됩니다." }
     }
 
     override fun toString(): String {
