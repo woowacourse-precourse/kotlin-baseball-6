2ae14c115be42c63cf37121a75a87cb6a5b96481
murjune <sis9221302@gmail.com>
2023-10-24 23:42:09 +0900

    refactor(Referee) : 함수네이밍 수정, 게임 결과 결정하는 로직 수정
    - Map을 활용하여 볼카운트를 세는 로직을 삭제하고, 지역변수 활용하여 볼카운트를 세도록 수정
    - increaseBallCount -> calculateStrikeAndBallCount로 수정 
    - mergeResult라는 추상적인 네이밍보다는 generateGameResult라는 구체적인 네이밍으로 수정

--- src/main/kotlin/baseball/Referee.kt
+++ src/main/kotlin/baseball/Referee.kt
@@ -3,59 +3,44 @@ package baseball
 import baseball.constants.GameResult
 
 class Referee {
-    private val resultMap = mutableMapOf<GameResult, Int>()
-    private val ballCount get() = resultMap.getOrDefault(GameResult.BALL, 0)
-    private val strikeCount get() = resultMap.getOrDefault(GameResult.STRIKE, 0)
-
     fun determineGameResult(answerBaseBalls: List<Int>, baseBalls: List<Int>): String {
-        if (isWin(answerBaseBalls, baseBalls)) return THREE_STRIKE
-        increaseBallCount(answerBaseBalls, baseBalls)
-        if (isNothing()) return NOTHING
-        val result = mergeResult(ballCount, strikeCount)
-        resultMap.clear()
-        return result
+        val (ballCount, strikeCount) = calculateBallAndStrikeCount(answerBaseBalls, baseBalls)
+        return generateGameResult(ballCount, strikeCount)
     }
 
-    private fun increaseBallCount(answerBaseBalls: List<Int>, baseBalls: List<Int>) {
+    private fun calculateBallAndStrikeCount(answerBaseBalls: List<Int>, baseBalls: List<Int>): Pair<Int, Int> {
+        var ballCount = 0
+        var strikeCount = 0
         for (idx in baseBalls.indices) {
-            val num = baseBalls[idx]
-            if (isStrike(answerBaseBalls[idx], num)) {
-                increaseCount(GameResult.STRIKE)
+            val userBall = baseBalls[idx]
+            if (isStrike(answerBaseBalls[idx], userBall)) {
+                strikeCount++
                 continue
             }
-            if (isBall(answerBaseBalls, num)) {
-                increaseCount(GameResult.BALL)
+            if (isBall(answerBaseBalls, userBall)) {
+                ballCount++
             }
         }
+        return ballCount to strikeCount
     }
 
-    private fun mergeResult(ballCnt: Int, strikeCnt: Int): String {
+    private fun generateGameResult(ballCount: Int, strikeCount: Int): String {
+        if (isNothing(ballCount + strikeCount)) return GameResult.NOTHING.message
         return StringBuilder().apply {
-            if (ballCnt > 0) {
-                append("$ballCnt")
-                append(GameResult.BALL.message)
-                append(EMPTY_SPACE)
+            if (ballCount > 0) {
+                append("$ballCount${GameResult.BALL.message}$EMPTY_SPACE")
             }
-            if (strikeCnt > 0) {
-                append("$strikeCnt")
-                append(GameResult.STRIKE.message)
+            if (strikeCount > 0) {
+                append("$strikeCount${GameResult.STRIKE.message}")
             }
         }.toString()
     }
 
-    private fun isWin(answerBalls: List<Int>, userBalls: List<Int>) = answerBalls == userBalls
+    private fun isNothing(totalCount: Int) = totalCount == 0
     private fun isStrike(answerBall: Int, userBall: Int) = answerBall == userBall
     private fun isBall(answerBalls: List<Int>, userBall: Int) = userBall in answerBalls
-    private fun isNothing() =
-        ballCount + strikeCount == 0
-
-    private fun increaseCount(result: GameResult) {
-        resultMap += result to resultMap.getOrDefault(result, 0) + 1
-    }
 
     companion object {
         private const val EMPTY_SPACE = " "
-        private const val THREE_STRIKE = "3스트라이크"
-        private const val NOTHING = "낫싱"
     }
 }
