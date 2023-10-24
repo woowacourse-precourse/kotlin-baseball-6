298eb7155a0fd04ab6dba9c7d6dc580e4a5acf71
nhkim <nhkim1529@gmail.com>
2023-10-24 18:03:25 +0900

    Feat: 입력한 수가 3자리고 숫자인지 확인하는 함수 checkLengthAndNumeric, isNumeric 생성하고 확인

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -13,6 +13,25 @@ fun main() {
         print("숫자를 입력해주세요 : ")
         comAnswer = pickNumberInRange(100,999).toString()
         myAnswer = readLine()!!.toString()
+        checkLengthAndNumeric(myAnswer)
     }
 
+}
+
+fun checkLengthAndNumeric(s: String) {
+    if (s.length != 3) {
+        throw IllegalArgumentException("The length of the input must be 3")
+    }
+
+    if (!isNumeric(s)) {
+        throw IllegalArgumentException("The input is not a valid number")
+    }
+}
+fun isNumeric(s: String): Boolean {
+    return try {
+        s.toDouble()
+        true
+    } catch (e: IllegalArgumentException) {
+        throw IllegalArgumentException()
+    }
 }
