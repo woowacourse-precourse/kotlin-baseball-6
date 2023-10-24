0c4f72fcd1bbba12bab6c886d2d7fabf1e260a08
chaehyun <80222352+chaehyuns@users.noreply.github.com>
2023-10-24 10:50:16 +0900

    feat: domain 전면 수정 ⩤ #5

--- src/main/kotlin/baseball/domain/computer/Computer.kt
+++ src/main/kotlin/baseball/domain/computer/Computer.kt
@@ -1,28 +1,20 @@
 package baseball.domain.computer
 
+import baseball.domain.player.PlayerNumber
+import baseball.domain.player.PlayerNumbers
 import camp.nextstep.edu.missionutils.Randoms
 
-class Computer(val numbers: List<Int>) {
-
-    init {
-        require(this.numbers.distinct().size == this.numbers.size) { DUPLICATED_ERROR_MESSAGE }
-        require(this.numbers.all { it in 1..9 }) { OUT_OF_RANGE_ERROR_MESSAGE }
-    }
-
-
+class Computer() {
     companion object {
-        private const val DUPLICATED_ERROR_MESSAGE = "컴퓨터가 선택한 숫자 중에는 중복되는 숫자가 없어야 합니다."
-        private const val OUT_OF_RANGE_ERROR_MESSAGE = "컴퓨터가 선택한 숫자는 1부터 9까지의 범위 안에 있어야 합니다."
-
-        fun setRandomNumbers(): Computer {
-            val numbers = mutableListOf<Int>()
+        fun setRandomNumbers(): PlayerNumbers {
+            val numbers = mutableListOf<PlayerNumber>()
             while (numbers.size < 3) {
-                val randomNumber = Randoms.pickNumberInRange(1, 9)
-                if (!numbers.contains(randomNumber)) {
-                    numbers.add(randomNumber)
+                val computer = Randoms.pickNumberInRange(1, 9)
+                if (numbers.none { it.numbers == computer }) {
+                    numbers.add(PlayerNumber(computer))
                 }
             }
-            return Computer(numbers)
+            return PlayerNumbers(numbers)
         }
     }
 }
--- src/main/kotlin/baseball/domain/player/PlayerNumbers.kt
+++ src/main/kotlin/baseball/domain/player/PlayerNumbers.kt
@@ -1,19 +1,22 @@
 package baseball.domain.player
 
+import baseball.domain.Constants
+import baseball.domain.game.BallStatus
+
 data class PlayerNumbers(private val numbers: List<PlayerNumber>) {
 
+    val size: Int get() = this.numbers.size
+    val indices: IntRange get() = this.numbers.indices
+    operator fun get(index: Int): PlayerNumber = this.numbers[index]
+
     init {
-        require(this.numbers.size == NUMBER_SIZE) { LENGTH_ERROR_MESSAGE }
+        require(this.numbers.size == Constants.NUMBER_SIZE) { Constants.LENGTH_ERROR_MESSAGE }
+        require(this.numbers.distinct().size == Constants.NUMBER_SIZE) { Constants.DUPLICATED_ERROR_MESSAGE }
     }
 
     fun getBallStatus(index: Int, number: PlayerNumber): BallStatus {
         if (this.numbers[index] == number) return BallStatus.STRIKE
         if (this.numbers.contains(number)) return BallStatus.BALL
         return BallStatus.NOTHING
-    }
-
-    companion object {
-        private const val LENGTH_ERROR_MESSAGE = "입력값은 3자리여야 합니다."
-        private const val NUMBER_SIZE = 3
     }
 }
