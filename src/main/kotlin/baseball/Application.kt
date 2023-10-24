d29a6ae0332a506096a811fd6ddac12d353ef29a
MinJun <agvber12@gmail.com>
2023-10-24 17:08:02 +0900

    feat: 임의의 숫자를 생성하는 함수 작성

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,18 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
-    TODO("프로그램 구현")
+    var number = getRandomNumber()
+}
+
+fun getRandomNumber(): List<Int> {
+    val computer = mutableListOf<Int>()
+    while (computer.size < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (!computer.contains(randomNumber)) {
+            computer.add(randomNumber)
+        }
+    }
+    return computer
 }
