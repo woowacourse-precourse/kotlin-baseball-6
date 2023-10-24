7f33fb7eb2eadb003354cd2005ad5a59be3acf4c
lee989898 <wnsrud1352@gmail.com>
2023-10-25 00:13:25 +0900

    [feat] OutputView 모든 숫자 정답 문구 출력 메서드 구현

--- src/main/kotlin/view/OutputView.kt
+++ src/main/kotlin/view/OutputView.kt
@@ -3,6 +3,7 @@ package view
 object OutputView {
     const val START_BASEBALL_GAME = "숫자 야구 게임을 시작합니다."
     const val PLEASES_INPUT_BASEBALL_NUMBER = "숫자를 입력해 주세요 : "
+    const val GUESSED_ALL_BASEBALL_NUMBER_GAME_OVER = "3개의 숫자를 모두 맞히셨습니다! 게임 종료"
     const val STRIKE = "스트라이크"
     const val BALL = "볼"
     const val NOTHING = "낫싱"
@@ -14,6 +15,10 @@ object OutputView {
 
     fun pleaseInputBaseballNumber() {
         println(PLEASES_INPUT_BASEBALL_NUMBER)
+    }
+
+    fun guessedAllBaseballNumber() {
+        println(GUESSED_ALL_BASEBALL_NUMBER_GAME_OVER)
     }
 
     fun intermediateResult(strike: Int, ball: Int) {
