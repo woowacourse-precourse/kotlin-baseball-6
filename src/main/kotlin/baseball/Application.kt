a7b96c580b8d59e0b792a5fca9b9529ef4c7b9fe
2kjin <lkjin1202@gmail.com>
2023-10-25 01:29:37 +0900

    refactor: 게임 시작기능과 재시작 및 종료기능 구분

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -8,18 +8,9 @@ fun main() {
     do {
         val computer = computerNumbers()
         playGame(computer)
-        do {
-            print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요. ")
-            val userInput = Console.readLine()
-            if (userInput == "1" || userInput == "2") {
-                if (userInput == "2") return
-                break
-            } else {
-                throw IllegalArgumentException("잘못된 값을 입력하였습니다.")
-            }
-        } while (true)
-    } while (true)
+    } while (gameStatus())
 }
+
 fun computerNumbers(): List<Int> {
     val computer = mutableListOf<Int>()
     while (computer.size < 3) {
@@ -30,26 +21,36 @@ fun computerNumbers(): List<Int> {
     }
     return computer
 }
+
+fun gameStatus(): Boolean {
+    do {
+        print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요. ")
+        val input = Console.readLine()
+        if (input == "2") return false
+        if (input == "1") return true
+        throw IllegalArgumentException("잘못된 값을 입력하였습니다.")
+    } while (true)
+}
+
 fun playGame(computer: List<Int>) {
     while (true) {
         print("숫자를 입력해주세요 : ")
-        val userInput = Console.readLine().orEmpty()
-
+        val input = Console.readLine().orEmpty()
         // 입력값이 없는 경우
-        if (userInput.isEmpty()) {
+        if (input.isEmpty()) {
             throw IllegalArgumentException("잘못된 값을 입력하였습니다.")
         }
-        val userGuess = try {
-            userInput.map { it.toString().toInt() }
+        val guess = try {
+            input.map { it.toString().toInt() }
         } catch (e: NumberFormatException) {
             // 입력값이 숫자가 아닌 경우
             throw IllegalArgumentException("잘못된 값을 입력하였습니다.")
         }
         // 입력값의 길이, 중복, 1~9 범위 검사
-        if (userGuess.size != 3 || userGuess.toSet().size != 3 || userGuess.any { it < 1 || it > 9 }) {
+        if (guess.size != 3 || guess.toSet().size != 3 || guess.any { it < 1 || it > 9 }) {
             throw IllegalArgumentException("잘못된 값을 입력하였습니다.")
         }
-        val result = ballStatus (computer, userGuess)
+        val result = ballStatus (computer, guess)
         if (result == "3스트라이크") {
             println(result)
             println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
@@ -58,17 +59,19 @@ fun playGame(computer: List<Int>) {
         println(result)
     }
 }
-fun ballStatus (computer: List<Int>, userGuess: List<Int>): String {
+
+fun ballStatus(computer: List<Int>, guess: List<Int>): String {
     var strikes = 0
     var balls = 0
 
-    for (i in userGuess.indices) {
-        if (userGuess[i] == computer[i]) {
+    for (i in guess.indices) {
+        if (guess[i] == computer[i]) {
             strikes++
-        } else if (computer.contains(userGuess[i])) {
+        } else if (computer.contains(guess[i])) {
             balls++
         }
     }
+
     if (strikes == 0 && balls == 0) return "낫싱"
     return "${if (balls > 0) "${balls}볼 " else ""}${if (strikes > 0) "${strikes}스트라이크" else ""}".trim()
 }
