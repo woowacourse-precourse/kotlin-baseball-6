ee93861ba4dc2690a7fd9cf8262eaba58c4b3398
lee989898 <wnsrud1352@gmail.com>
2023-10-24 22:51:51 +0900

    [feat] InputView 게임 새로 시작 혹은 게임 종료 입력 메서드 구현

--- src/main/kotlin/view/InputView.kt
+++ src/main/kotlin/view/InputView.kt
@@ -2,6 +2,7 @@ package view
 
 import camp.nextstep.edu.missionutils.Console
 import validator.baseballnumbervalidator.BaseballNumberValidator
+import validator.gamecontrolvalidator.GameControlValidator
 
 object InputView {
 
@@ -9,5 +10,11 @@ object InputView {
         val baseballNumber = Console.readLine()
         baseballNumberValidator.isValidate(baseballNumber)
         return baseballNumber
+    }
+
+    fun inputReStartOrExit(gameControlValidator: GameControlValidator): String {
+        val zeroOrOne = Console.readLine()
+        gameControlValidator.isValidate(zeroOrOne)
+        return zeroOrOne
     }
 }
