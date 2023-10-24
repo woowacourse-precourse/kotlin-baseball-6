9b1d01e06e9443666e0dd2750094939be0a372d9
Choi Seho <kindseho@naver.com>
2023-10-24 15:28:16 +0900

    feat: 초기 세팅

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,24 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
-    TODO("프로그램 구현")
+    // 1. 게임 시작을 위한 세팅
+    val answer = answerSelect()
+    println("숫자 야구 게임을 시작합니다.")
+    println("answer : ${answer}")
+}
+
+fun answerSelect(): ArrayList<Int> {
+    val computer = arrayListOf<Int>()
+
+    while (computer.size < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (!computer.contains(randomNumber)) {
+            computer.add(randomNumber)
+        }
+    }
+
+    return computer
 }
