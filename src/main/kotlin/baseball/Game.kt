3e6535f8ffb71a398d16bcd0682818ff2a1db359
sseung416 <choiseungyeon416@gmail.com>
2023-10-24 23:24:28 +0900

    feat: 게임 구현

--- src/main/kotlin/baseball/Game.kt
+++ src/main/kotlin/baseball/Game.kt
@@ -7,6 +7,68 @@ import baseball.utils.isRangeOf
 import camp.nextstep.edu.missionutils.Console
 
 class Game {
+    fun run() {
+        println("숫자 야구 게임을 시작합니다.")
+        val computer = RandomNumberGenerator.generate(1, 9, 3)
+        while (true) {
+            startGuessingAnswer(computer)
+
+            if (playNextGame()) {
+                break
+            }
+        }
+    }
+
+    private fun startGuessingAnswer(computer: List<Int>) {
+        while (true) {
+            print("숫자를 입력해주세요 : ")
+            val userInput = Console.readLine()
+            checkAnswerInput(userInput)
+            println(calculateResult(userInput, computer))
+
+            if (userInput.isAllStrike(computer)) {
+                return
+            }
+        }
+    }
+
+    private fun String.isAllStrike(computer: List<Int>): Boolean {
+        computer.forEachIndexed { index, value ->
+            if (value != this[index].digitToInt()) {
+                return false
+            }
+        }
+        return true
+    }
+
+    private fun calculateResult(input: String, answer: List<Int>): String {
+        var strike = 0
+        var ball = 0
+        answer.forEachIndexed { index, value ->
+            val current = input[index].digitToInt()
+            if (current == value) {
+                strike++
+            } else if (current in answer) {
+                ball++
+            }
+        }
+
+        if (strike == 0 && ball == 0) {
+            return "낫싱"
+        }
+
+        return buildString {
+            if (ball > 0) append(ball).append("볼 ")
+            if (strike > 0) append(strike).append("스트라이크")
+        }
+    }
+
+    private fun playNextGame(): Boolean {
+        println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+        val answer = Console.readLine()
+        checkNextGameAnswer(answer)
+        return answer == "1"
+    }
 
     private fun checkAnswerInput(input: String) {
         if (input.length != 3) {
@@ -21,12 +83,12 @@ class Game {
             throwIllegalException()
         }
 
-        if (input.isRangeOf(1, 9)) {
+        if (!input.isRangeOf(1, 9)) {
             throwIllegalException()
         }
     }
 
-    private fun checkNextGameAnswer(input: String){
+    private fun checkNextGameAnswer(input: String) {
         if (input == "1" || input == "2") {
             return
         }
