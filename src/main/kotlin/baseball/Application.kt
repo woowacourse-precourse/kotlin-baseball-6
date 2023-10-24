18e5e26acdf977499836ff31a5526143913ebbb4
pearhyunjin <pearhyunjin@gmail.com>
2023-10-24 13:01:40 +0900

    feat: 컴퓨터 랜덤 수 만들기 구현

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -13,5 +13,23 @@ fun main() {
 
 fun startBaseballPlay() {
 
+    // 컴퓨터 랜덤 값 생성
+    val computerRandomNum = createComputerRandomNum()
+
+
 }
 
+fun createComputerRandomNum(): String {
+
+    val computerNum = mutableListOf<Int>()
+    // list 크기 3 미만, 즉 3글자가 아닐경우 1~9 사이 랜덤값 생성
+    while (computerNum.size < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        // 서로 다른 수만 생성
+        if (!computerNum.contains(randomNumber)) {
+            computerNum.add(randomNumber)
+        }
+    }
+    // list 공백 제거하고 문자열로
+    return computerNum.joinToString("")
+}
