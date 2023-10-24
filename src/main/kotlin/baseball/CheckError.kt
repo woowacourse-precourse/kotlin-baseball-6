ea73ed5d3cb3090e7b5e59f80493e8f79bd625fe
shinseung kim <94797657+kimshinseung@users.noreply.github.com>
2023-10-25 03:55:23 +0900

    오류수정4

--- src/main/kotlin/baseball/CheckError.kt
+++ src/main/kotlin/baseball/CheckError.kt
@@ -12,14 +12,14 @@ class CheckError {
         }
     }
     private fun checkNumber(number: String){
-        for (i in 0..number.length-1) { // 숫자가 아니면 에러
+        for (i in number.indices) { // 숫자가 아니면 에러
             if (number[i] < '1' || number[i] > '9') {
                 throw IllegalArgumentException("숫자가 아닙니다.")
             }
         }
     }
     private fun checkDuplication(number: String){
-        for (i in 0..number.length-1) { //숫자가 중복되면 에러
+        for (i in number.indices) { //숫자가 중복되면 에러
             val curNum = number[i]-'0'
             if (!myNumber.contains(curNum)) {
                 myNumber.add(curNum)
