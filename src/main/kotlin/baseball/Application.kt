3531809e4b80da21a165d66b3e40f39c58b3f655
3uomlkh <cw1205@duksung.ac.kr>
2023-10-24 19:59:05 +0900

    Feat: RandomNumber 얻는 기능 추가

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,30 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+import camp.nextstep.edu.missionutils.Console
+
+var userInput = ""
+var userNumber = 0
+var int_one = 0
+var int_two = 0
+var int_three = 0
+const val NUMBER_SIZE = 3
+
+
 fun main() {
-    TODO("프로그램 구현")
+    println("숫자 야구 게임을 시작합니다.")
+    var computer = getRandomNumbers()
+    
 }
+
+fun getRandomNumbers(): MutableList<Int> {
+    val computer: MutableList<Int> = mutableListOf()
+    while (computer.size < NUMBER_SIZE) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (!computer.contains(randomNumber)) {
+            computer.add(randomNumber)
+        }
+    }
+    return computer
+}
+
