57aea2553365d661e9dba5e6d7f2391ba9e06b09
Jtility <leejewon97@gmail.com>
2023-10-24 15:17:16 +0900

    refactor(main): delete variable startOrExit and change while to do-while

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -5,8 +5,7 @@ import camp.nextstep.edu.missionutils.Randoms
 
 fun main() {
     println("숫자 야구 게임을 시작합니다.")
-    var startOrExit = "1"
-    while (startOrExit == "1") {
+    do {
         val answerNumbers = makeRandomNumbers()
         do {
             print("숫자를 입력해주세요 : ")
@@ -15,8 +14,7 @@ fun main() {
         } while (fail)
         println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
         println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
-        startOrExit = validStartOrExit()
-    }
+    } while (validStartOrExit() == "1")
 }
 
 fun validStartOrExit(): String {
