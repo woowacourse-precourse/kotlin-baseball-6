3e9dbfc7d2886c4eb53509795f03c03e9765ed10
Chanwoo <overelectric@gmail.com>
2023-10-25 05:12:56 +0900

    무작위 수 생성 and 입력 부분 작성

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,36 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+
+
+fun userInput(): List<Int> {
+    var userNumber = mutableListOf<Int>()
+    print("숫자를 입력해주세요 : ")
+    val inputLine = readlnOrNull() ?: throw IllegalArgumentException("숫자가 입력되지 않았거나 올바르지 않은 형식입니다.")
+    if (inputLine.length == 3 && inputLine.all { item -> item.isDigit() }) {
+        userNumber = inputLine.map { it.toString().toInt() }.toMutableList()
+    } else {
+        throw IllegalArgumentException("숫자가 입력되지 않았거나 올바르지 않은 형식입니다.")
+    }
+    return userNumber
+}
+
+fun runBaseBallGame() {
+//    숫자 랜덤 생성
+    val baseballNumbers = mutableListOf<Int>()
+    while (baseballNumbers.size < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (!baseballNumbers.contains(randomNumber)) {
+            baseballNumbers.add(randomNumber)
+        }
+    }
+//    유저 입력 받는 부분
+//    TODO 반복문으로 개조 필요
+    val userNumber = userInput()
+}
+
+
 fun main() {
-    TODO("프로그램 구현")
+    println("숫자 야구 게임을 시작합니다.")
+    runBaseBallGame()
 }
