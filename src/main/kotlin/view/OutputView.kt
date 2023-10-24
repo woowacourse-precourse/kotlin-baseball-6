87601c7b779f49706cc0e82b9b053c6a03ad0479
lee989898 <wnsrud1352@gmail.com>
2023-10-25 00:12:10 +0900

    [feat] OutputView 게임 중간 결과 문구 출력 메서드 구현

--- src/main/kotlin/view/OutputView.kt
+++ src/main/kotlin/view/OutputView.kt
@@ -3,6 +3,10 @@ package view
 object OutputView {
     const val START_BASEBALL_GAME = "숫자 야구 게임을 시작합니다."
     const val PLEASES_INPUT_BASEBALL_NUMBER = "숫자를 입력해 주세요 : "
+    const val STRIKE = "스트라이크"
+    const val BALL = "볼"
+    const val NOTHING = "낫싱"
+    const val ZERO = 0
 
     fun startBaseballGame() {
         println(START_BASEBALL_GAME)
@@ -10,5 +14,14 @@ object OutputView {
 
     fun pleaseInputBaseballNumber() {
         println(PLEASES_INPUT_BASEBALL_NUMBER)
+    }
+
+    fun intermediateResult(strike: Int, ball: Int) {
+        when {
+            strike > ZERO && ball > ZERO -> println("${ball}$BALL ${strike}$STRIKE")
+            strike > ZERO -> println("${strike}$STRIKE")
+            ball > ZERO -> println("${ball}$BALL")
+            strike == ZERO && ball == ZERO -> println(NOTHING)
+        }
     }
 }
