a052f7b4b4cc6bc6d5aba0133f32cbfc8388375a
Junyoung-WON <wjyo60@gmail.com>
2023-10-25 01:54:59 +0900

    feat: 사용자 입력을 담당하는 User 클래스를 동반 객체로 구현

--- src/main/kotlin/baseball/User.kt
+++ src/main/kotlin/baseball/User.kt
@@ -1,4 +1,22 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Console
+
 class User {
+    companion object {
+        private val validator = Validator()
+
+        fun getUserNumbers(): MutableList<Int> {
+            val input = inputNumberString()
+            return validator.toNumberList(input)
+        }
+
+        private fun inputNumberString(): String {
+            print("숫자를 입력해주세요 : ")
+            val input: String = Console.readLine()
+            validator.validateString(input)
+            return input
+        }
+
+    }
 }
