b9ae74972b38f201b544aaef6a84904333c157bd
Choi Seho <kindseho@naver.com>
2023-10-24 19:53:08 +0900

    feat: 사용자 입력 검증 및 오류 발생시키기

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -6,11 +6,24 @@ import camp.nextstep.edu.missionutils.Console
 fun main() {
     // 1. 게임 시작을 위한 세팅
     val answer = answerSelect()
+//    println("answer : ${answer}")
     println("숫자 야구 게임을 시작합니다.")
-    println("answer : ${answer}")
+
+    // 2. 사용자 입력
+    println("숫자를 입력해주세요 : ")
+    var inputData = Console.readLine()
+    if(inputData.length != 3){ // 입력 자리수 검증
+        throw IllegalArgumentException()
+    }
+    // 0~9 외의 숫자 검증
+    else if(!inputData.all{it in CharRange(start = '0', endInclusive = '9')}){
+        throw IllegalArgumentException()
+    }
+    println("inputData : ${inputData}")
 }
 
 fun answerSelect(): ArrayList<Int> {
+    // 1. 게임 시작을 위한 세팅
     val computer = arrayListOf<Int>()
 
     while (computer.size < 3) {
