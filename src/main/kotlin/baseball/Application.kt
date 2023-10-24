8e286c554b8393cf92709e634c4c2504823d83ca
EJ-pro <hsshss2522@naver.com>
2023-10-24 13:15:28 +0900

    랜덤 숫자 생성 기능

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,19 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+
+fun generateRandomNumber(): Int {
+    val computer = mutableListOf<Int>()
+    while (computer.size < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (!computer.contains(randomNumber)) {
+            computer.add(randomNumber)
+        }
+    }
+    return computer.joinToString("").toInt()
+}
+
 fun main() {
     println("숫자 야구 게임을 시작합니다.")
+    var number = generateRandomNumber()
 }
