ad7acd6f526a6389d709dc3866770c800d852b9f
lee989898 <wnsrud1352@gmail.com>
2023-10-24 21:51:01 +0900

    [feat] BaseballNumberValidatorImpl 생성

---
+++ src/main/kotlin/validator/baseballnumbervalidator/BaseballNumberValidatorImpl.kt
@@ -0,0 +1,19 @@
+package validator.baseballnumbervalidator
+
+object BaseballNumberValidatorImpl: BaseballNumberValidator {
+    override fun isValidate(baseballNumber: String): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun isThreeLetters(baseballNumber: String): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun isItANumber(baseballNumber: String): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun allThreeLettersDifferent(baseballNumber: String): Boolean {
+        TODO("Not yet implemented")
+    }
+}
