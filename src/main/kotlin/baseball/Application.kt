cbf12779c740a018204901851205896f7010b4df
vonvolous <hanyoojin39@gmail.com>
2023-10-24 17:20:53 +0900

    게임 시작 문구 출력 + 사용자 값 입력 받기 + 종료 조건 구현

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -12,5 +12,25 @@ fun main() {
             computer.add(randomNumber)
         }
     }
-    println("$computer")
+    while(true){
+        println("숫자 야구 게임을 시작합니다.")
+        print("숫자를 입력해주세요 : ")
+        val user = Console.readLine()
+        var choice:String = ""
+        println("$computer")
+        println(user)
+        println(user[1].digitToInt())
+        if (computer[1]==user[1].digitToInt()){
+            println("맞습니다.")
+        } else {
+            continue
+        }
+        println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+        choice = Console.readLine()
+        if (choice=="2") {
+            break
+        } else if (choice=="1"){
+            continue
+        }
+    }
 }
