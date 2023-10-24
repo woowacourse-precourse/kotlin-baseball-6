5ebec73ce39710ccef4cabc1dcc52257835d9902
EJ-pro <hsshss2522@naver.com>
2023-10-24 13:20:09 +0900

    사용자 입력 예외 처리기능

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,6 +1,7 @@
 package baseball
 
 import camp.nextstep.edu.missionutils.Randoms
+import camp.nextstep.edu.missionutils.Console
 
 fun generateRandomNumber(): Int {
     val computer = mutableListOf<Int>()
@@ -12,8 +13,18 @@ fun generateRandomNumber(): Int {
     }
     return computer.joinToString("").toInt()
 }
-
+fun hasDuplicateDigits(number: Int): Boolean{
+    val digits = number.toString().toCharArray()
+    return digits.toSet().size != digits.size
+}
 fun main() {
     println("숫자 야구 게임을 시작합니다.")
     var number = generateRandomNumber()
+    while(true) {
+        print("숫자를 입력해주세요 : ")
+        val num = Console.readLine()
+        if(num.length != 3 || !num.all { it.isDigit() } || hasDuplicateDigits(num.toInt())){
+            throw IllegalArgumentException("[Error]")
+        }
+    }
 }
