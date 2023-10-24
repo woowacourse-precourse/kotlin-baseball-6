c4a09d9df4033865baac02a4e8f37176dde90499
nhkim <nhkim1529@gmail.com>
2023-10-24 18:01:11 +0900

    Feat: 컴퓨터 3자리 수 랜덤 생성

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -3,5 +3,15 @@ package baseball
 import camp.nextstep.edu.missionutils.Randoms.pickNumberInRange
 
 fun main() {
+    var oneOrTwo: Int = 1
+    var myAnswer: String
+    var comAnswer: String
+
+    println("숫자 야구 게임을 시작합니다.")
+
+    while(oneOrTwo==1){
+        print("숫자를 입력해주세요 : ")
+        comAnswer = pickNumberInRange(100,999).toString()
+    }
 
 }
