b2bdfdf28bcf745db0fbb6a9c54f0d493718abb7
yuhyun <dlugus@naver.com>
2023-10-25 03:48:03 +0900

    feat: 랜덤 숫자 생성

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,18 @@
 package baseball
+import camp.nextstep.edu.missionutils.Randoms
 
 fun main() {
     TODO("프로그램 구현")
+}
+
+//랜덤 숫자 생성
+fun makeRandomNumber(): String { 
+    val computerNumber = mutableListOf<Int>()
+    while (computerNumber.size < 3) {
+        val randomNumber: Int = Randoms.pickNumberInRange(1, 9)
+        if (!computerNumber.contains(randomNumber)) {
+        computerNumber.add(randomNumber)
+        }
+    }
+    return computerNumber.joinToString("")
 }
