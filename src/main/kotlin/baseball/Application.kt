9c537fe65709843b61d4f38e737fb4bb32e53034
eunyeong <feijoaplz@gmail.com>
2023-10-25 04:24:44 +0900

    출력 2번씩 되는것 수정

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -14,9 +14,9 @@ const val RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면
 
 fun main() {
     var computer: MutableList<Int> = mutableListOf()
-    var user: List<Int> = listOf()
-    var areUReady: Boolean = true
-    var gameRestart: Boolean = true
+    var user: List<Int>
+    var areUReady = true
+    var gameRestart = true
 
     println(START_MESSAGE)
     while(areUReady or gameRestart){
@@ -71,6 +71,10 @@ fun game(computer: List<Int>, user: List<Int>):Boolean {
         }
     }
     println(compareNumber(ball,strike))
+    if (strike == 3) {
+        println("${strike}스트라이크\n${THREE_STRIKE_MESSAGE}")
+        return false
+    }
 
     return true
 }
@@ -79,24 +83,17 @@ fun game(computer: List<Int>, user: List<Int>):Boolean {
 fun compareNumber(ball:Int, strike: Int): String {
 
     var str = ""
-
     if (ball == 0 && strike == 0) {
-        str = "낫싱"
+        println("낫싱")
     }
     if (ball == 0 && strike > 0) {
-        str = "${strike}스트라이크"
+        println("${strike}스트라이크")
     }
     if (strike == 0 && ball > 0) {
-        str = "${ball}볼"
-    }
-    else {
-        str = "${ball}볼 ${strike}스트라이크"
-    }
-    if (strike == 3) {
-        "${strike}스트라이크\n${THREE_STRIKE_MESSAGE}"
-        return gameOver(areUDone)
-    }
-        return str
+        println("${ball}볼")
+    }else {
+        println("${ball}볼 ${strike}스트라이크")}
+    return str
 }
 
 
