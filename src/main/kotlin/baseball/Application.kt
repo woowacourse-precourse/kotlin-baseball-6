8e306300f740be1d19eea742a6648e733006879a
wonjongin <wonggamggik@gmail.com>
2023-10-25 04:54:40 +0900

    woowacource 1week practice ver 7 구현 기능 목록 1. 컴퓨터의 랜덤한 숫자를 받아오는 기능 - 완료 2. 플레이어가 숫자를 입력하는 기능 - 완료 3. 컴퓨터의 숫자와 플레이어의 숫자를 비교하여 값을 출력해주는 기능 - 완료 4. 3스트라이크가 아닐 시 다시 값을 물어보기 위한 기능 - 완료

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -16,37 +16,42 @@ fun main() {
         }
     }
 
-    // 컴퓨터의 입력값을 확인하기 위한 임시 코드
     println(computer.toString())
+    // 무한번 질문을 위한 무한 반복문
+    while (true) {
+        var strike = 0  // 위치, 숫자 모두 맞음
+        var ball = 0    // 위치만 맞음
+        var out = 0     // 위치, 숫자 모두 틀림
 
-    var strike = 0  // 위치, 숫자 모두 맞음
-    var ball = 0    // 위치만 맞음
-    var out = 0     // 위치, 숫자 모두 틀림
+        print("숫자를 입력해주세요 : ")
+        val myAnswer = readLine()?.map { e -> e.toString().toInt()}?.toIntArray()?.toMutableList()!!
 
-    print("숫자를 입력해주세요 : ")
-    val myAnswer = readLine()?.map { e -> e.toString().toInt()}?.toIntArray()?.toMutableList()!!
+        for (i in 0..2) {
+            if (myAnswer[i] == computer[i]) { // 위치가 같고 숫자가 존재할 경우
+                strike++
+            } else if (myAnswer[i] in computer) {  // 위치는 다르나 숫자가 존재할 경우
+                ball++
+            } else {
+                out++
+            }
+        }
 
-    for (i in 0..2) {
-        if (myAnswer[i] == computer[i]) { // 위치가 같고 숫자가 존재할 경우
-            strike++
-        } else if (myAnswer[i] in computer) {  // 위치는 다르나 숫자가 존재할 경우
-            ball++
+        // 답 출력
+        if (out == 3) {
+            println("낫싱")
         } else {
-            out++
+            if (ball >= 1) {
+                print("${ball}볼 ")
+            }
+            if (strike >= 1) {
+                println("${strike}스트라이크")
+            } else {    // strike == 0일 경우 \n이 발생하지 않는 상황 제거
+                println()
+            }
         }
-    }
 
-    // 답 출력
-    if (out == 3) {
-        println("낫싱")
-    } else {
-        if (ball >= 1) {
-            print("${ball}볼 ")
-        }
-        if (strike >= 1) {
-            println("${strike}스트라이크")
-        } else {    // strike == 0일 경우 \n이 발생하지 않는 상황 제거
-            println()
+        if (strike == 3) {
+            break
         }
     }
 }
