dc02ffb6a2ef945023e7e84fa736967b03fc489f
seongsan <tjdtks030@naver.com>
2023-10-24 14:39:45 +0900

    feat: 게임 진행 - 정수 입력
    사용자로부터 1 부터 9 까지 3 개의 정수를 입력받는다.
    userEnteredNumbers(), condition 구현

--- src/main/kotlin/baseball/Baseball.kt
+++ src/main/kotlin/baseball/Baseball.kt
@@ -1,16 +1,16 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Console
+import java.lang.IllegalArgumentException
 import kotlin.random.Random
 
 class Baseball {
     private var randomNumbers = listOf<Int>()
+    private var userNumbers = listOf<Int>()
+    private val numberCount = 3
 
     init {
         printStartMessage()
-    }
-
-    fun runGame() {
-        randomNumbers = randomlyGeneratedNumbers()
     }
 
     private fun printStartMessage() {
@@ -18,8 +18,13 @@ class Baseball {
         println(message)
     }
 
+    fun runGame() {
+        randomNumbers = randomlyGeneratedNumbers()
+        userNumbers = userEnteredNumbers()
+    }
+
     private fun randomlyGeneratedNumbers(): List<Int> {
-        val count = 3
+        val count = numberCount
         val numbers = mutableListOf<Int>()
 
         for (i in 0..count) {
@@ -28,5 +33,47 @@ class Baseball {
         }
 
         return numbers
+    }
+
+    private fun userEnteredNumbers(): List<Int> {
+        val numbers = mutableListOf<Int>()
+        val userInput = enteredInput()
+
+        formatChangedNumbers(userInput).map { number -> numbers.add(number) }
+
+        return numbers
+    }
+
+    private fun enteredInput(): String {
+        val message = "숫자를 입력해주세요 : "
+
+        print(message)
+        return Console.readLine()
+    }
+
+    private fun formatChangedNumbers(input: String): List<Int> {
+        checkNumbersCondition(input)
+        return input.map { data -> Character.getNumericValue(data) }
+    }
+
+    private fun checkNumbersCondition(input: String) {
+        checkNumberCountCondition(input)
+        checkNumberFormatCondition(input)
+        checkNumberRangeCondition(input)
+    }
+
+    private fun checkNumberCountCondition(input: String) {
+        if (input.length != numberCount)
+            throw IllegalArgumentException()
+    }
+
+    private fun checkNumberFormatCondition(input: String) {
+        if (input.map { data -> Character.isDigit(data) }.contains(false))
+            throw IllegalArgumentException()
+    }
+
+    private fun checkNumberRangeCondition(input: String) {
+        if (input.map { data -> Character.getNumericValue(data) in 1..9 }.contains(false))
+            throw IllegalArgumentException()
     }
 }
