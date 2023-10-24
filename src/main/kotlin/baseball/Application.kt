c5507b97943282454d8e84d361382455e73d6381
MinJun <agvber12@gmail.com>
2023-10-24 17:19:03 +0900

    feat: 플레이어가 3스트라이크 할 경우 다음 진행 단계 출력

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -10,31 +10,46 @@ enum class Score {
 fun main() {
     var number = getRandomNumber()
 
-    print("숫자를 입력해주세요 : ")
-    val inputInt: List<Int> = readLine()!!.toList().map { it.digitToInt() }
+    while (true) {
+        print("숫자를 입력해주세요 : ")
+        val inputInt: List<Int> = readLine()!!.toList().map { it.digitToInt() }
 
-    if (inputInt.size != number.size) {
-        throw IllegalArgumentException()
-    }
+        if (inputInt.size != number.size) {
+            throw IllegalArgumentException()
+        }
 
-    val scores = mutableListOf<Score>()
+        val scores = mutableListOf<Score>()
 
-    number.forEachIndexed { index, it ->
-        if (it == inputInt[index]) {
-            scores.add(STRIKE)
-        } else if (inputInt.contains(it)) {
-            scores.add(BALL)
+        number.forEachIndexed { index, it ->
+            if (it == inputInt[index]) {
+                scores.add(STRIKE)
+            } else if (inputInt.contains(it)) {
+                scores.add(BALL)
+            }
+        }
+
+        if (scores.isEmpty()) {
+            println("낫싱")
+        } else {
+            println(
+                "${removeCount(scores.count { it == BALL }, "볼")} ${removeCount(scores.count { it == STRIKE }, "스트라이크")}".trim()
+            )
+        }
+
+        if (scores.count { it == STRIKE } == 3) {
+            println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+
+            val input = readlnOrNull()!!.toInt()
+
+            if (input == 1) {
+                number = getRandomNumber()
+            } else if (input == 2) {
+                break
+            } else {
+                throw IllegalArgumentException()
+            }
         }
     }
-
-    if (scores.isEmpty()) {
-        println("낫싱")
-    } else {
-        println(
-            "${removeCount(scores.count { it == BALL }, "볼")} ${removeCount(scores.count { it == STRIKE }, "스트라이크")}".trim()
-        )
-    }
-
 
 }
 
