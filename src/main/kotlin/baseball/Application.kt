4acf8fdf6d9e79bdb8a79995480ec218fb820f9f
april <aprilgom@gmail.com>
2023-10-24 16:44:38 +0900

    컨벤션 수정

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -82,7 +82,7 @@ fun main() {
     println("숫자 야구 게임을 시작합니다.")
     while (true) {
         val secretNumber = makeSecretNumber()
-        while(true) {
+        while (true) {
             print("숫자를 입력해주세요 : ")
             val guessNumber = Console.readLine()
             require(isValidGuessNumber(guessNumber)){
