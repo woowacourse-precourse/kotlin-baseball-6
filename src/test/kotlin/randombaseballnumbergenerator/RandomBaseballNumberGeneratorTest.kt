3bc2fc27602a074bcf3f05dc4157be7c0c7e1145
lee989898 <wnsrud1352@gmail.com>
2023-10-25 01:07:04 +0900

    [test] RandomBaseballNumberGeneratorTest 랜덤 야구 숫자 길이가 3이랑 같은지 테스트 구현

--- src/test/kotlin/randombaseballnumbergenerator/RandomBaseballNumberGeneratorTest.kt
+++ src/test/kotlin/randombaseballnumbergenerator/RandomBaseballNumberGeneratorTest.kt
@@ -1,6 +1,8 @@
 package randombaseballnumbergenerator
 
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.RepeatedTest
 
 class RandomBaseballNumberGeneratorTest {
     private lateinit var randomBaseballNumberGenerator: RandomBaseballNumberGenerator
@@ -8,5 +10,16 @@ class RandomBaseballNumberGeneratorTest {
     @BeforeEach
     fun setUp() {
         randomBaseballNumberGenerator = RandomBaseballNumberGeneratorImpl
+    }
+
+    @RepeatedTest(100)
+    fun `랜덤 야구 숫자 길이가 3이랑 같은지`() {
+        val result = randomBaseballNumberGenerator.generateRandomBaseballNumber()
+
+        assertEquals(result.length, BASEBALL_NUMBER_SIZE)
+    }
+
+    companion object {
+        const val BASEBALL_NUMBER_SIZE = 3
     }
 }
