ad23a52ae7b5d831eec7334ae03d8e020bba2448
lee989898 <wnsrud1352@gmail.com>
2023-10-24 22:31:45 +0900

    [test] GameControlValidatorTest 숫자가 1이나 2일때 성공인지 테스트 구현

--- src/test/kotlin/validator/gamecontrolvalidator/GameControlValidatorTest.kt
+++ src/test/kotlin/validator/gamecontrolvalidator/GameControlValidatorTest.kt
@@ -1,6 +1,9 @@
 package validator.gamecontrolvalidator
 
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 class GameControlValidatorTest {
     private lateinit var gameControlValidator: GameControlValidator
@@ -8,5 +11,14 @@ class GameControlValidatorTest {
     @BeforeEach
     fun setUp() {
         gameControlValidator = GameControlValidatorImpl
+    }
+
+    @ParameterizedTest
+    @ValueSource(
+        strings = ["1", "2"]
+    )
+    fun `숫자가 1이나 2일때 성공인지`(oneOrTwo: String) {
+        val result = gameControlValidator.isOneOrTwo(oneOrTwo)
+        assertTrue(result)
     }
 }
