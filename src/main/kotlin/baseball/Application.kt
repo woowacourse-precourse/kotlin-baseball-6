2ef850ecd81f93fb4ea0c94d31d89d0aa74585c4
kimmandoo <mingyu5675@gmail.com>
2023-10-25 01:26:27 +0900

    fix: 재시작시 문구 수정

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -2,19 +2,7 @@ package baseball
 
 import camp.nextstep.edu.missionutils.Console
 import camp.nextstep.edu.missionutils.Randoms
-
-const val INPUT_RULE = 3
-const val INPUT_MESSAGE = "숫자를 입력해주세요 : "
-const val INPUT_EXCEPTION = "잘못된 입력입니다."
-const val CONTINUE = 0
-const val RESTART = 1
-const val EXIT = 2
-const val GAME_START = "숫자 야구 게임을 시작합니다."
-const val NOTHING = "낫싱"
-const val BALL = "볼 "
-const val STRIKE = "스트라이크 "
-const val CORRECT = "3개의 숫자를 모두 맞히셨습니다! 게임 종료"
-const val CORRECT_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
+import res.*
 
 fun main() {
     gameStart()
@@ -23,6 +11,7 @@ fun main() {
 private fun gameStart() {
     val computer = mutableListOf<Int>()
     generateMission(computer)
+    println(GAME_START)
     while (true) {
         print(INPUT_MESSAGE)
         val input = inputCheck()
@@ -55,7 +44,6 @@ private fun generateMission(computer: MutableList<Int>) {
             computer.add(randomNumber)
         }
     }
-    println(GAME_START)
 }
 
 private fun inputCheck(): String {
