31b8c56f8daddd523a17e5773947a2ae00489a92
Wonhui Roh <rwhui012@gmail.com>
2023-10-24 13:04:21 +0900

    FEAT: 1) '컴퓨터의 숫자' 초기화

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,33 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.*
+
+
 fun main() {
-    TODO("프로그램 구현")
+    println("숫자 야구 게임을 시작합니다.")
+    startBaseballGame()
+    continueOrQuit()
+}
+
+fun startBaseballGame() {
+    val computer = randomComputer()
+
+}
+
+fun continueOrQuit() {
+    TODO("Not yet implemented")
+}
+
+// 1) '컴퓨터의 숫자' 초기화
+fun randomComputer(): List<Int> {
+    val rand = mutableListOf<Int>()
+
+    while (rand.size < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (!rand.contains(randomNumber)) {
+            rand.add(randomNumber)
+        }
+    }
+
+    return rand
 }
