87289c871cfb509a7b16459d401a32f63fb4f0dc
okm1172 <okm1172@naver.com>
2023-10-24 11:32:24 +0900

    게임 시작 후 컴퓨터 숫자 결정

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -7,6 +7,28 @@ fun printStartGameSentence() {
     println("숫자 야구 게임을 시작합니다.")
 }
 
+fun startGame() {
+    startGameUser(getAnswerNumber())
+}
+
+fun startGameUser(answerNumber: String) {
+    //answerNumber를 게임 시작 시 인자로 넘겨줌
+}
+
+fun getAnswerNumber(): String {
+    val computer = mutableListOf<Int>()
+    while (computer.size < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (!computer.contains(randomNumber)) {
+            computer.add(randomNumber)
+        }
+    }
+    //joinToString(separator,prefix,postfix,limit,truncated,transform)
+    return computer.joinToString(separator = "")
+}
+
 fun main() {
     printStartGameSentence()
+    //게임시작
+    startGame()
 }
