8ef94f788963bcc4abff405061dda1c9510de1c6
김진아 <c2010333@naver.com>
2023-10-24 23:05:24 +0900

    [FEAT] 게임 시작 시 선택 할 수 있는 번호가 아닌 다른 번호 선택 시 예외 처리

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,24 +1,33 @@
 package baseball
 
+import java.lang.Exception
+
 fun main() {
-    var run = " 숫자 야구 게임에 오신 것을 환영합니다 :) "
-    var gameInfo: String = ""
+    println("숫자 야구 게임에 오신 것을 환영합니다!")
 
-    while (true) {
-        println("[1] 게임 시작하기 [2] 게임 종료하기")
-        val selectStart = readLine()?.toInt()
+    try {
+        while (true) {
+            println("[1] 게임 시작하기 [2] 게임 종료하기")
+            val selectStart = readLine()?.toInt()
 
-        if (!(selectStart == 1 || selectStart == 2)) {
-            println("잘못된 입력입니다.")
-            return
+            when (selectStart) {
+                1 -> {
+                    println("숫자 야구 게임을 시작합니다.")
+                    break
+                }
+
+                2 -> {
+                    println("숫자 야구 게임을 종료합니다.")
+                    break
+                }
+
+                else -> {
+                    println("잘못된 입력입니다. 다시 입력해주세요.")
+                }
+            }
         }
-
-        when (selectStart) {
-            1 -> gameInfo = "숫자 야구 게임을 시작합니다."
-            2 -> gameInfo = "숫자 야구 게임을 종료합니다."
-        }
-        println(gameInfo)
-        break
+    } catch (e: NumberFormatException) {
+        println("잘못된 입력입니다. 다시 입력해주세요.")
     }
 }
 
