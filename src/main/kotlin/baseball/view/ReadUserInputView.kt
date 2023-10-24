6c5d7ca3f8ffcab23924af4c0c84f2a98b536e57
posite <su84053@gmail.com>
2023-10-24 15:37:37 +0900

    refactor: Input받는 class 하나로 통합 및 입력받을 때 validate하게 변경

--- src/main/kotlin/baseball/view/ReadUserInputView.kt
+++ src/main/kotlin/baseball/view/ReadUserInputView.kt
@@ -1,17 +1,29 @@
-package baseball.domain
+package baseball.view
 
-private const val RESTART_GAME = 1
-private const val EXIT_GAME = 2
-private const val MIN_VALUE = 100
-private const val MAX_VALUE = 999
+import baseball.domain.ChoiceState
+import camp.nextstep.edu.missionutils.Console
 
-class ValidateUserInput {
+
+class ReadUserInputView {
+
+    fun readUserNumberInput(): String {
+        print("숫자를 입력해주세요 : ")
+        val userNumber = Console.readLine()
+        validateNumber(userNumber)
+        return userNumber
+    }
+
+    fun readUserRestartInput(): String {
+        println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+        val userRestart = Console.readLine()
+        validateRestart(userRestart)
+        return userRestart
+    }
 
     fun validateNumber(userInput: String): Int {
         require(isNumberFormat(userInput)) { "입력값은 숫자여야 합니다!" }
         val userNumber = userInput.toInt()
         require(isCorrectRangeNumber(userNumber)) { "세자리 숫자여야 합니다!" }
-        require(isContainsZero(userNumber)) { "0은 사용되지 않는 숫자입니다!" }
         return userNumber
 
     }
@@ -21,11 +33,11 @@ class ValidateUserInput {
         val userChoice = userInput.toInt()
         require(isCorrectRangeChoice(userChoice)) { "1 또는 2를 입력해주세요!" }
         return when (userChoice) {
-            RESTART_GAME -> ChoiceState.Restart
-            EXIT_GAME -> ChoiceState.Exit
+            RESTART_GAME -> ChoiceState.RESTART
+            EXIT_GAME -> ChoiceState.EXIT
             else -> {
                 require(true) { "1 또는 2를 입력해주세요!" }
-                ChoiceState.Exit
+                ChoiceState.EXIT
             }
         }
     }
@@ -49,16 +61,10 @@ class ValidateUserInput {
         }
     }
 
-    private fun isContainsZero(userNumber: Int): Boolean {
-        var currentNumber = userNumber
-        val lastNumber = currentNumber % 10
-        if (lastNumber == 0) return false
-        currentNumber /= 10
-
-        val middleNumber = currentNumber % 10
-        if (middleNumber == 0) return false
-        currentNumber /= 10
-
-        return true
+    companion object {
+        const val RESTART_GAME = 1
+        const val EXIT_GAME = 2
+        const val MIN_VALUE = 100
+        const val MAX_VALUE = 999
     }
 }
