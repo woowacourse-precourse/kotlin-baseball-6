419402e23cd0d98e13dedb99f291e5bda895504f
wonjongin <wonggamggik@gmail.com>
2023-10-25 04:57:25 +0900

    woowacource 1week practice ver 7 구현 기능 목록 1. 컴퓨터의 랜덤한 숫자를 받아오는 기능 - 완료 2. 플레이어가 숫자를 입력하는 기능 - 완료 3. 컴퓨터의 숫자와 플레이어의 숫자를 비교하여 값을 출력해주는 기능 - 완료 4. 3스트라이크가 아닐 시 다시 값을 물어보기 위한 기능 - 완료 5. 게임을 종료한 후 다시 시작할 것인지 종료할 것인지 확인하고 다시 실행하는 기능 - 완료 6. 사용자가 잘못된 값을 입력할 경우 오류를 발생하는 기능 - 컴퓨터의 입력은 서로 다른 3개의 숫자가 입력되는 것이 확정이기에 별도의 오류를 부여하지 않음 - 완료
        6-1. 2번 상황에서, 플레이어가 3개의 숫자를 입력하지 않은 경우 - 완료
    
        6-2. 2번 상황에서, 플레이어가 중복된 숫자를 입력하는 경우 - 완료
    
        6-3. 2번 상황에서, 플레이어가 숫자 0을 입력하는 경우 - 완료
    
        6-4. 4번 상황에서, 플레이어가 숫자 1,2 이외의 값을 입력하는 경우 - 완료

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -17,7 +17,6 @@ fun main() {
             }
         }
 
-        println(computer.toString())
         // 무한번 질문을 위한 무한 반복문
         while (true) {
             var strike = 0  // 위치, 숫자 모두 맞음
@@ -25,7 +24,24 @@ fun main() {
             var out = 0     // 위치, 숫자 모두 틀림
 
             print("숫자를 입력해주세요 : ")
-            val myAnswer = readLine()?.map { e -> e.toString().toInt() }?.toIntArray()?.toMutableList()!!
+            val myAnswer = readLine()?.map { e -> e.toString().toInt()}?.toIntArray()?.toMutableList()
+
+            // 3개의 숫자를 입력하지 않은 경우
+            if (myAnswer?.size != 3) {
+                throw IllegalArgumentException("3개의 숫자를 입력하세요")
+            }
+
+            // 중복된 숫자를 입력한 경우
+            if (myAnswer.toSet().size != 3) {
+                throw IllegalArgumentException("서로 다른 숫자를 입력하세요")
+            }
+
+            // 숫자 0을 입력할 경우
+            for (i in 0..2) {
+                if (myAnswer[i] == 0) {
+                    throw IllegalArgumentException("1~9의 숫자를 입력하세요")
+                }
+            }
 
             for (i in 0..2) {
                 if (myAnswer[i] == computer[i]) { // 위치가 같고 숫자가 존재할 경우
@@ -55,6 +71,7 @@ fun main() {
                 break
             }
         }
+
         println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
         println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
         val restart = readLine()
@@ -62,6 +79,8 @@ fun main() {
             continue
         } else if (restart == "2") {    // 게임 종료
             break
+        } else {    // 숫자 1,2를 입력하지 않은 경우
+            throw IllegalArgumentException("1 또는 2를 입력하세요")
         }
     }
 }
