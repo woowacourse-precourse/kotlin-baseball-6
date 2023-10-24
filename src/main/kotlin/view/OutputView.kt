b4a405a329bf7cf22745c38959f7fbc323e80620
lee989898 <wnsrud1352@gmail.com>
2023-10-25 00:10:28 +0900

    [feat] OutputView 숫자 입력 요구 문구 출력 메서드 구현

--- src/main/kotlin/view/OutputView.kt
+++ src/main/kotlin/view/OutputView.kt
@@ -2,8 +2,13 @@ package view
 
 object OutputView {
     const val START_BASEBALL_GAME = "숫자 야구 게임을 시작합니다."
+    const val PLEASES_INPUT_BASEBALL_NUMBER = "숫자를 입력해 주세요 : "
 
     fun startBaseballGame() {
         println(START_BASEBALL_GAME)
+    }
+
+    fun pleaseInputBaseballNumber() {
+        println(PLEASES_INPUT_BASEBALL_NUMBER)
     }
 }
