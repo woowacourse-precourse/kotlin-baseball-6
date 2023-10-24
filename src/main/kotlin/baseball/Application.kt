6aa28e67328feff31900f9bcef752d5c36c37cda
김진아 <c2010333@naver.com>
2023-10-24 22:30:59 +0900

    [FEAT] 숫자 야구 게임 실행 시 인사말 및 게임 시작 & 종료 선택지 출력

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,24 @@
 package baseball
 
 fun main() {
-    TODO("프로그램 구현")
+    var run = " 숫자 야구 게임에 오신 것을 환영합니다 :) "
+    var gameInfo: String = ""
+
+    while (true) {
+        println("[1] 게임 시작하기 [2] 게임 종료하기")
+        val selectStart = readLine()?.toInt()
+
+        if (!(selectStart == 1 || selectStart == 2)) {
+            println("잘못된 입력입니다.")
+            return
+        }
+
+        when (selectStart) {
+            1 -> gameInfo = "숫자 야구 게임을 시작합니다."
+            2 -> gameInfo = "숫자 야구 게임을 종료합니다."
+        }
+        println(gameInfo)
+        break
+    }
 }
+
