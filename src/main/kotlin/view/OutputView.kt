f87c9cacb44813fe2504b66a627fac8f67b35775
lee989898 <wnsrud1352@gmail.com>
2023-10-25 00:18:02 +0900

    [chore] OutputView 상수 private 접근자 추가

--- src/main/kotlin/view/OutputView.kt
+++ src/main/kotlin/view/OutputView.kt
@@ -1,14 +1,14 @@
 package view
 
 object OutputView {
-    const val START_BASEBALL_GAME = "숫자 야구 게임을 시작합니다."
-    const val PLEASES_INPUT_BASEBALL_NUMBER = "숫자를 입력해 주세요 : "
-    const val GUESSED_ALL_BASEBALL_NUMBER_GAME_OVER = "3개의 숫자를 모두 맞히셨습니다! 게임 종료"
-    const val RESTART_OR_TERMINATE_GAME = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
-    const val STRIKE = "스트라이크"
-    const val BALL = "볼"
-    const val NOTHING = "낫싱"
-    const val ZERO = 0
+    private const val START_BASEBALL_GAME = "숫자 야구 게임을 시작합니다."
+    private const val PLEASES_INPUT_BASEBALL_NUMBER = "숫자를 입력해 주세요 : "
+    private const val GUESSED_ALL_BASEBALL_NUMBER_GAME_OVER = "3개의 숫자를 모두 맞히셨습니다! 게임 종료"
+    private const val RESTART_OR_TERMINATE_GAME = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
+    private const val STRIKE = "스트라이크"
+    private const val BALL = "볼"
+    private const val NOTHING = "낫싱"
+    private const val ZERO = 0
 
     fun startBaseballGame() {
         println(START_BASEBALL_GAME)
