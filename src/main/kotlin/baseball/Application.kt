0f2f44bd3bdb4b657a154b41a64de77e913888be
kimdahee7 <kikidahee7@duksung.ac.kr>
2023-10-24 13:49:35 +0900

    [Feat] 사용자 숫자 입력 함수 구현

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,6 +1,7 @@
 package baseball
 
 import camp.nextstep.edu.missionutils.Randoms
+import camp.nextstep.edu.missionutils.Console
 
 fun main() {
     NumberBaseballGame().start()
@@ -11,6 +12,17 @@ class NumberBaseballGame {
     fun start() {
         output.printGameStart()
         output.printReceiveNumberInput()
+        inputUserNumbers()
+    }
+
+    //서로 다른 3자리의 수 입력
+    private fun inputUserNumbers(): String? {
+        val user = Console.readLine()
+
+        if (user.length != 3 || user.toSet().size != 3) {
+            throw IllegalArgumentException()
+        }
+        return user
     }
 
     //1~9 범위의 서로 다른 3자리의 수 생성
