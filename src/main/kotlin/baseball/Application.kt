7b22041ba3b2d63945d05becf28d820b7ab80489
wonjongin <wonggamggik@gmail.com>
2023-10-25 04:56:03 +0900

    woowacource 1week practice ver 7 구현 기능 목록 1. 컴퓨터의 랜덤한 숫자를 받아오는 기능 - 완료 2. 플레이어가 숫자를 입력하는 기능 - 완료 3. 컴퓨터의 숫자와 플레이어의 숫자를 비교하여 값을 출력해주는 기능 - 완료 4. 3스트라이크가 아닐 시 다시 값을 물어보기 위한 기능 - 완료 5. 게임을 종료한 후 다시 시작할 것인지 종료할 것인지 확인하고 다시 실행하는 기능 - 완료

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -6,51 +6,61 @@ import camp.nextstep.edu.missionutils.Console
 fun main() {
     println("숫자 야구 게임을 시작합니다.")
 
-    val computer = mutableListOf<Int>()
-
-    // computer에 랜덤한 값 입력할 코드
-    while (computer.size < 3) {
-        val randomNumber = Randoms.pickNumberInRange(1, 9)
-        if (!computer.contains(randomNumber)) {
-            computer.add(randomNumber)
-        }
-    }
-
-    println(computer.toString())
-    // 무한번 질문을 위한 무한 반복문
     while (true) {
-        var strike = 0  // 위치, 숫자 모두 맞음
-        var ball = 0    // 위치만 맞음
-        var out = 0     // 위치, 숫자 모두 틀림
+        val computer = mutableListOf<Int>()
 
-        print("숫자를 입력해주세요 : ")
-        val myAnswer = readLine()?.map { e -> e.toString().toInt()}?.toIntArray()?.toMutableList()!!
+        // computer에 랜덤한 값 입력할 코드
+        while (computer.size < 3) {
+            val randomNumber = Randoms.pickNumberInRange(1, 9)
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber)
+            }
+        }
 
-        for (i in 0..2) {
-            if (myAnswer[i] == computer[i]) { // 위치가 같고 숫자가 존재할 경우
-                strike++
-            } else if (myAnswer[i] in computer) {  // 위치는 다르나 숫자가 존재할 경우
-                ball++
+        println(computer.toString())
+        // 무한번 질문을 위한 무한 반복문
+        while (true) {
+            var strike = 0  // 위치, 숫자 모두 맞음
+            var ball = 0    // 위치만 맞음
+            var out = 0     // 위치, 숫자 모두 틀림
+
+            print("숫자를 입력해주세요 : ")
+            val myAnswer = readLine()?.map { e -> e.toString().toInt() }?.toIntArray()?.toMutableList()!!
+
+            for (i in 0..2) {
+                if (myAnswer[i] == computer[i]) { // 위치가 같고 숫자가 존재할 경우
+                    strike++
+                } else if (myAnswer[i] in computer) {  // 위치는 다르나 숫자가 존재할 경우
+                    ball++
+                } else {
+                    out++
+                }
+            }
+
+            // 답 출력
+            if (out == 3) {
+                println("낫싱")
             } else {
-                out++
+                if (ball >= 1) {
+                    print("${ball}볼 ")
+                }
+                if (strike >= 1) {
+                    println("${strike}스트라이크")
+                } else {    // strike == 0일 경우 \n이 발생하지 않는 상황 제거
+                    println()
+                }
+            }
+
+            if (strike == 3) {
+                break
             }
         }
-
-        // 답 출력
-        if (out == 3) {
-            println("낫싱")
-        } else {
-            if (ball >= 1) {
-                print("${ball}볼 ")
-            }
-            if (strike >= 1) {
-                println("${strike}스트라이크")
-            } else {    // strike == 0일 경우 \n이 발생하지 않는 상황 제거
-                println()
-            }
-        }
-
-        if (strike == 3) {
+        println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
+        println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+        val restart = readLine()
+        if (restart == "1") {   // 게임 다시 시작
+            continue
+        } else if (restart == "2") {    // 게임 종료
             break
         }
     }
