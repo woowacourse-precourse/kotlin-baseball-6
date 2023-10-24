37612a769de421b737783717d9a41797ef00b231
pearhyunjin <pearhyunjin@gmail.com>
2023-10-24 12:35:05 +0900

    feat: 게임 시작 문구 출력 및 함수 생성

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,17 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms //Random 값 추출 - pickNumberInRange()
+import camp.nextstep.edu.missionutils.Console //사용자 입력 값 - readLine()
+
 fun main() {
-    TODO("프로그램 구현")
+
+    // 게임 시작
+    println("숫자 야구 게임을 시작합니다.")
+    startBaseballPlay()
+
 }
+
+fun startBaseballPlay() {
+
+}
+
