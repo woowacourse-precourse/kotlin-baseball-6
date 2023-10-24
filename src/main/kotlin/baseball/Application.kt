454964c84136ec813457d8b4f4f8d2757fb8c572
junyeoleee <117695928+junyeoleee@users.noreply.github.com>
2023-10-24 22:05:17 +0900

    임의의 숫자 생성 기능 구현

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,28 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
-    TODO("프로그램 구현")
+    val game = Game()
+    println("숫자 야구 게임을 시작합니다.")
+    game.start()
+}
+class Game {
+    private var computerNumbers = mutableListOf<Int>()
+
+    fun start() {
+        generateRandomNumbers()
+        // 나머지 로직 추가
+    }
+
+    private fun generateRandomNumbers() {
+        computerNumbers.clear()
+        while (computerNumbers.size < 3) {
+            val randomNumber = Randoms.pickNumberInRange(1, 9)
+            if (!computerNumbers.contains(randomNumber)) {
+                computerNumbers.add(randomNumber)
+            }
+        }
+    }
 }
