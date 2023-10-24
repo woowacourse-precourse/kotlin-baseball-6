8894821936afd48c59a0a087a177982ae343e158
posite <su84053@gmail.com>
2023-10-24 15:28:54 +0900

    refactor: PrintResultView else 제거 및 분리

--- src/main/kotlin/baseball/view/PrintResultView.kt
+++ src/main/kotlin/baseball/view/PrintResultView.kt
@@ -8,26 +8,32 @@ private const val NOTHING_COUNT = 0
 class PrintResultView {
 
     fun printGameResult(gameResult: Map<BallCount, Int>) {
-        if (gameResult[BallCount.Strike] == MAX_STRIKE_COUNT) {
+        if (gameResult[BallCount.STRIKE] == MAX_STRIKE_COUNT) {
             println("3스트라이크")
             println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
             return
-        } else {
-            if (gameResult[BallCount.Strike] == NOTHING_COUNT &&
-                gameResult[BallCount.Ball] == NOTHING_COUNT
-            ) {
-                println("낫싱")
-            } else {
-                if (gameResult[BallCount.Ball] != NOTHING_COUNT) {
-                    print("${gameResult[BallCount.Ball]}볼 ")
-                }
-                if (gameResult[BallCount.Strike] != NOTHING_COUNT) {
-                    println("${gameResult[BallCount.Strike]}스트라이크")
-                } else {
-                    println()
-                }
-            }
+        }
+        printBallCount(gameResult)
+    }
+
+    private fun printBallCount(gameResult: Map<BallCount, Int>) {
+        if (gameResult[BallCount.STRIKE] == NOTHING_COUNT &&
+            gameResult[BallCount.BALL] == NOTHING_COUNT
+        ) {
+            println("낫싱")
             return
         }
+        printRemainBallCount(gameResult)
+    }
+
+    private fun printRemainBallCount(gameResult: Map<BallCount, Int>) {
+        if (gameResult[BallCount.BALL] != NOTHING_COUNT) {
+            print("${gameResult[BallCount.BALL]}볼 ")
+        }
+        if (gameResult[BallCount.STRIKE] != NOTHING_COUNT) {
+            println("${gameResult[BallCount.STRIKE]}스트라이크")
+            return
+        }
+        println()
     }
 }
