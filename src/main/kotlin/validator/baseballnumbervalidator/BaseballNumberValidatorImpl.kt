313ec6718f7a5ec99d0f69e9fc12ef97b1f889c3
lee989898 <wnsrud1352@gmail.com>
2023-10-24 22:15:21 +0900

    [feat] BaseballNumberValidatorImpl 1~9 사이 숫자만 있는지 검증하는 메서드 구현

--- src/main/kotlin/validator/baseballnumbervalidator/BaseballNumberValidatorImpl.kt
+++ src/main/kotlin/validator/baseballnumbervalidator/BaseballNumberValidatorImpl.kt
@@ -2,6 +2,8 @@ package validator.baseballnumbervalidator
 
 object BaseballNumberValidatorImpl: BaseballNumberValidator {
     private const val BASEBALL_NUMBER_SIZE = 3
+    private const val BASEBALL_MINIMUM_NUMBER = '1'
+    private const val BASEBALL_MAXIMUM_NUMBER = '9'
 
     override fun isValidate(baseballNumber: String): Boolean {
         TODO("Not yet implemented")
@@ -9,12 +11,14 @@ object BaseballNumberValidatorImpl: BaseballNumberValidator {
 
     override fun isThreeLetters(baseballNumber: String): Boolean {
         require(baseballNumber.length == BASEBALL_NUMBER_SIZE)
-        
+
         return true
     }
 
     override fun isItANumber(baseballNumber: String): Boolean {
-        TODO("Not yet implemented")
+        require(baseballNumber.toCharArray().all { it in BASEBALL_MINIMUM_NUMBER..BASEBALL_MAXIMUM_NUMBER })
+
+        return true
     }
 
     override fun allThreeLettersDifferent(baseballNumber: String): Boolean {
