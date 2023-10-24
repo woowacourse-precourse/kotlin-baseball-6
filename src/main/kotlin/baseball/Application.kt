5a7a621d1415387730869d1af6789bceeef1a114
junyeoleee <117695928+junyeoleee@users.noreply.github.com>
2023-10-24 23:10:33 +0900

    플레이어 숫자 입력 받기 및 예외처리

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -50,26 +50,6 @@ class Game {
         return Console.readLine().map { it.toString().toInt() }
     }
 
-    private fun compareNumbers(userInput: List<Int>): Pair<Int, Int> {
-        var strikes = 0
-        var balls = 0
-        for (i in userInput.indices) {
-            if (userInput[i] == computerNumbers[i]) {
-                strikes++
-            } else if (computerNumbers.contains(userInput[i])) {
-                balls++
-            }
-        }
-        return Pair(strikes, balls)
-    }
-
-    private fun printResult(strikes: Int, balls: Int) {
-        when {
-            strikes == 0 && balls == 0 -> println("낫싱")
-            strikes == 3 -> println("3스트라이크")
-            else -> println("${balls}볼 ${strikes}스트라이크")
-        }
-    }
 
     private fun askForRestart(): Int {
         println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
