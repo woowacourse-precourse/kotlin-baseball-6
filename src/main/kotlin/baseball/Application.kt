eb5063e6b151a7b847b5286d590e012fecc36143
park-yina <111878820+park-yina@users.noreply.github.com>
2023-10-24 20:34:18 +0900

    Update Application.kt
    0을 거르는 부분 삭제

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -11,7 +11,7 @@ class NumberBaseball {
             if (inputNumber.length != 3) {
                 throw IllegalArgumentException("입력된 숫자가 3자리가 아닙니다.")
             }
-            if (!inputNumber.matches(Regex("\\d+"))||inputNumber.matches(Regex(".*0.*"))){
+            if (!inputNumber.matches(Regex("\\d+"))){
                 throw IllegalArgumentException("숫자가 아닌 문자 또는 0이 포함되어있습니다.")
             }
             if (inputNumber.toSet().size != inputNumber.length) {
