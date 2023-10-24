0bc81ecc262a48efb2bcf99d1e1f36deeeec66b5
hxeyexn <kck85075@kumoh.ac.kr>
2023-10-24 20:25:47 +0900

    Feat: 플레이어가 게임 지속 여부를 입력하는 기능 구현

--- src/main/kotlin/baseball/Player.kt
+++ src/main/kotlin/baseball/Player.kt
@@ -10,4 +10,12 @@ class Player {
             throw IllegalArgumentException(ERROR_INPUT_NUMBER_ONLY)
         }
     }
+
+    fun inputContinueGameOrNot(): Int {
+        return try {
+            Console.readLine().toInt()
+        } catch (e: Exception) {
+            throw IllegalArgumentException(ERROR_INPUT_ONE_OR_TWO_ONLY)
+        }
+    }
 }
