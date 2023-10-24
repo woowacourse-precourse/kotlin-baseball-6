3ce76c323cc00f3bf666052e59aee6411012fd87
crAy <craycoding@gmail.com>
2023-10-25 03:05:47 +0900

    refactor exception, print result

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,20 +1,6 @@
 package baseball
 import camp.nextstep.edu.missionutils.Randoms
 import camp.nextstep.edu.missionutils.Console
-
-fun getResult(strike : Int, ball : Int) : String {
-    if (ball > 0 && strike > 0) {
-        return ball.toString() + "볼 " + strike.toString() + "스트라이크"
-    }
-    if (ball > 0) {
-        return ball.toString() + "볼"
-    }
-    if (strike > 0) {
-        return strike.toString() + "스트라이크"
-    }
-    return "낫싱"
-}
-
 fun main() {
     println("숫자 야구 게임을 시작합니다.")
 
@@ -34,16 +20,12 @@ fun main() {
             ball = 0
             print("숫자를 입력해주세요 : ")
             val input = Console.readLine()
-            if (input!!.length != 3) {
-                throw IllegalArgumentException()
-            }
+            require(input.length == 3)
             val splitInput = input.toCharArray()
             val me = mutableListOf<Int>()
             for (char in splitInput) {
                 val numberOfInput : Int = Character.getNumericValue(char)
-                if (numberOfInput !in 1..9) {
-                    throw IllegalArgumentException()
-                }
+                require(numberOfInput in 1..9)
                 me.add(numberOfInput)
             }
 
@@ -60,15 +42,19 @@ fun main() {
                 }
             }
 
-            println(getResult(strike, ball))
+            val result : String = when {
+                strike > 0 && ball > 0 -> ball.toString() + "볼 " + strike.toString() + "스트라이크"
+                ball > 0 -> ball.toString() + "볼"
+                strike > 0 -> strike.toString() + "스트라이크"
+                else -> "낫싱"
+            }
+            println(result)
         } while (strike < 3)
 
         println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
         println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
-        val flag = Console.readLine()
-        if (flag != "1" && flag != "2") {
-            throw IllegalArgumentException()
-        }
-    } while (flag == "1")
+        val flag = Console.readLine().toInt()
+        require(flag == 1 || flag == 2)
+    } while (flag == 1)
 
 }
