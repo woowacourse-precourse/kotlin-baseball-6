3da2e4066113bf23b8129080526fe72b1cca8dbc
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2023-10-24 17:24:49 +0900

    refactor: Computer의 랜덤 최소,최대 값 상수처리

--- src/main/kotlin/baseball/Computer.kt
+++ src/main/kotlin/baseball/Computer.kt
@@ -1,5 +1,7 @@
 package baseball
 
+import baseball.util.GameValue.COMPUTER_MAX_NUMBER
+import baseball.util.GameValue.COMPUTER_MIN_NUMBER
 import baseball.util.GameValue.GAME_NUMBER_LENGTH
 import camp.nextstep.edu.missionutils.Randoms
 
@@ -8,7 +10,7 @@ object Computer {
     fun makeNumber(): List<Int> {
         val computerNumber = mutableSetOf<Int>()
         while (computerNumber.size != GAME_NUMBER_LENGTH) {
-            val randomNumber = Randoms.pickNumberInRange(1, 9)
+            val randomNumber = Randoms.pickNumberInRange(COMPUTER_MIN_NUMBER, COMPUTER_MAX_NUMBER)
             computerNumber.add(randomNumber)
         }
         return computerNumber.toList()
--- src/main/kotlin/baseball/util/GameValue.kt
+++ src/main/kotlin/baseball/util/GameValue.kt
@@ -3,6 +3,8 @@ package baseball.util
 object GameValue {
     const val PLAYER_NIM_NUMBER = '1'
     const val PLAYER_MAX_NUMBER = '9'
+    const val COMPUTER_MIN_NUMBER = 1
+    const val COMPUTER_MAX_NUMBER = 9
     const val GAME_NUMBER_LENGTH = 3
     const val RESTART_COMMAND = "1"
     const val STOP_COMMAND = "2"
