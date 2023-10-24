4ea0ab800aece7d101cb0d7dbd4205d00fdc93f8
lee989898 <wnsrud1352@gmail.com>
2023-10-24 22:28:15 +0900

    [feat] GameControlValidatorImpl 생성

---
+++ src/main/kotlin/validator/gamecontrolvalidator/GameControlValidatorImpl.kt
@@ -0,0 +1,11 @@
+package validator.gamecontrolvalidator
+
+object GameControlValidatorImpl: GameControlValidator {
+    override fun isValidate(oneOrTwo: String): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun isOneOrTwo(oneOrTwo: String): Boolean {
+        TODO("Not yet implemented")
+    }
+}
