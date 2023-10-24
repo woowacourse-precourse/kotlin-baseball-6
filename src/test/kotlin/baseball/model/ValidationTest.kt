96ea6a49f81aaaed8eb27e0477187ddf5e8a955a
sseung416 <choiseungyeon416@gmail.com>
2023-10-24 17:28:14 +0900

    Test: ValidationTest의 assertThrows 대신 assertEqualsThrowsMessage를 사용하도록 리팩토링

--- src/test/kotlin/baseball/model/ValidationTest.kt
+++ src/test/kotlin/baseball/model/ValidationTest.kt
@@ -1,46 +1,40 @@
 package baseball.model
 
-import baseball.model.validation.DuplicateNumberValidation
-import baseball.model.validation.InvalidLengthValidation
-import baseball.model.validation.PositiveNumberValidation
-import baseball.model.validation.OutOfRangeNumberValidation
+import baseball.assertEqualsThrowsMessage
+import baseball.model.validation.*
+import baseball.model.validation.InputError.*
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertDoesNotThrow
-import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 
 class ValidationTest {
     @ParameterizedTest
-    @ValueSource(strings = ["-21", "마라탕", "0"])
-    fun `입력 값이 양수인지`(input: String) {
-        assertThrows<IllegalArgumentException> {
+    @ValueSource(strings = ["-21", "마라탕", "+12"])
+    fun `입력 값이 기호를 포함하지 않는 숫자인지`(input: String) {
+        assertEqualsThrowsMessage<IllegalArgumentException>(NotPositiveNumber.message) {
             PositiveNumberValidation().check(input)
         }
     }
 
     @Test
     fun `입력 값에 중복 값이 존재하는지`() {
-        assertThrows<IllegalArgumentException> {
+        assertEqualsThrowsMessage<IllegalArgumentException>(DuplicateNumber.message) {
             DuplicateNumberValidation().check("111")
         }
     }
 
     @Test
     fun `입력 값이 세 자리인지`() {
-        assertThrows<IllegalArgumentException> {
+        assertEqualsThrowsMessage<IllegalArgumentException>(InvalidLength.message) {
             InvalidLengthValidation().check("1234")
         }
     }
 
-    @Test
-    fun `입력 값의 범위가 1~9까지인지`() {
-        val validation = OutOfRangeNumberValidation()
-        assertThrows<IllegalArgumentException> {
-            validation.check("012")
-        }
-        assertDoesNotThrow {
-            validation.check("123")
+    @ParameterizedTest
+    @ValueSource(strings = ["+12", "-12", "0", "0123", "가나다"])
+    fun `입력 값의 각각의 범위가 1~9까지인지`(input: String) {
+        assertEqualsThrowsMessage<IllegalArgumentException>(OutOfRangeNumber.message) {
+            OutOfRangeNumberValidation().check(input)
         }
     }
 }
