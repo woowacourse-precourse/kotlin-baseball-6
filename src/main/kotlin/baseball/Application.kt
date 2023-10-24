82c3bebb3387f715a6e90abbc1af8bba583e5145
iiolo <alohalo98@naver.com>
2023-10-24 21:53:31 +0900

    feat : 1번 기능(컴퓨터로부터 랜덤한 3개의 숫자 가져오기) 추가

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,18 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
     TODO("프로그램 구현")
+}
+
+fun getComputerNumList(): MutableList<Int> {
+    val computerNumList = mutableListOf<Int>()
+    while (computerNumList.size < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (!computerNumList.contains(randomNumber)) {
+            computerNumList.add(randomNumber)
+        }
+    }
+    return computerNumList
 }
