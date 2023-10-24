3bfd07405894b8774e38ec9e5b84e5c171294603
leehandsub <gustjq7027@naver.com>
2023-10-24 20:30:02 +0900

    feat : 입력이 숫자가 아닐 경우

--- src/main/kotlin/baseball/model/User.kt
+++ src/main/kotlin/baseball/model/User.kt
@@ -6,8 +6,19 @@ class User {
     fun inputUser(): BaseBall {
         val userList = mutableListOf<Int>()
         val input = Console.readLine().toCharArray()
+
+        checkNumber(input)
+
         for (i in input)
             userList.add(i.digitToInt())
         return BaseBall(userList)
+    }
+
+    private fun checkNumber(input: CharArray) {
+        input.forEach {
+            if (it !in '1'..'9') {
+                throw IllegalArgumentException()
+            }
+        }
     }
 }
