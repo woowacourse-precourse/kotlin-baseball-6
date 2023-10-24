ea6fa9b463bcac9b3336e0d13d09658c1a6dcbfe
김진아 <c2010333@naver.com>
2023-10-24 23:50:21 +0900

    [FEAT] USER 게임 진행 및 입력된 값에 따른 Error 메세지 추가

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -5,8 +5,8 @@ import java.lang.Exception
 fun main() {
     println("숫자 야구 게임에 오신 것을 환영합니다!")
 
-    try {
-        while (true) {
+    while (true) {
+        try {
             println("[1] 게임 시작하기 [2] 게임 종료하기")
             val selectStart = readLine()?.toInt()
 
@@ -26,9 +26,9 @@ fun main() {
                     println("잘못된 입력입니다. 다시 입력해주세요.")
                 }
             }
+        } catch (e: NumberFormatException) {
+            println("잘못된 입력입니다. 다시 입력해주세요.")
         }
-    } catch (e: NumberFormatException) {
-        println("잘못된 입력입니다. 다시 입력해주세요.")
     }
 }
 
@@ -49,3 +49,23 @@ class Computer {
     }
 }
 
+class User {
+    fun inputNums(): IntArray {
+        while (true) {
+            println("각 자리가 겹치지 않는 세 자리의 숫자를 입력해주세요. 예시 - 789")
+            val userInput = readLine()
+
+            if (userInput == null) {
+                println("[Error] 입력된 값이 없습니다. 다시 입력해주세요.")
+            } else if (!userInput.all { it.isDigit() }) {
+                println("[Error] 숫자만 입력해주세요.")
+            } else if (userInput.toSet().size != 3) {
+                println("[Error] 숫자가 중복되었습니다.")
+            } else if (userInput.length != 3) {
+                println("[Error] 3자리의 수를 입력해주세요.")
+            } else {
+               println("입력하신 숫자는 ${userInput}입니다.")
+            }
+        }
+    }
+}
