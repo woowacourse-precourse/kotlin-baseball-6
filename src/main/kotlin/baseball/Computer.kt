3ab9d07cd545cd958034be117d00de5bc890501c
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2023-10-24 18:25:48 +0900

    fix: Computer에서 생성한 번호의 중복값 들어가는 점 수정

--- src/main/kotlin/baseball/Computer.kt
+++ src/main/kotlin/baseball/Computer.kt
@@ -8,12 +8,11 @@ import camp.nextstep.edu.missionutils.Randoms
 object Computer {
 
     fun makeNumber(): List<Int> {
-        val computerNumber = mutableListOf<Int>()
+        val computerNumber = mutableSetOf<Int>()
         while (computerNumber.size != GAME_NUMBER_LENGTH) {
             val randomNumber = Randoms.pickNumberInRange(COMPUTER_MIN_NUMBER, COMPUTER_MAX_NUMBER)
             computerNumber.add(randomNumber)
-            computerNumber.distinct()
         }
-        return computerNumber
+        return computerNumber.toList()
     }
 }
