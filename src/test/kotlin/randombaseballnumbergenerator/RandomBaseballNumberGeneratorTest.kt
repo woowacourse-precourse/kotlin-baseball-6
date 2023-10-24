69e9ec48054e2f00049ac08b1c575e0c44e50ec8
lee989898 <wnsrud1352@gmail.com>
2023-10-25 01:08:40 +0900

    [test] RandomBaseballNumberGeneratorTest 랜덤 야구 숫자가 전부 숫자인지 테스트 구현

--- src/test/kotlin/randombaseballnumbergenerator/RandomBaseballNumberGeneratorTest.kt
+++ src/test/kotlin/randombaseballnumbergenerator/RandomBaseballNumberGeneratorTest.kt
@@ -1,6 +1,7 @@
 package randombaseballnumbergenerator
 
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.RepeatedTest
 
@@ -19,7 +20,17 @@ class RandomBaseballNumberGeneratorTest {
         assertEquals(result.length, BASEBALL_NUMBER_SIZE)
     }
 
+    @RepeatedTest(100)
+    fun `랜덤 야구 숫자가 전부 숫자인지`() {
+        val result = randomBaseballNumberGenerator.generateRandomBaseballNumber()
+            .all { it in BASEBALL_NUMBER_MINIMUM_NUMBER..BASEBALL_NUMBER_MAXIMUM_NUMBER }
+
+        assertTrue(result)
+    }
+
     companion object {
         const val BASEBALL_NUMBER_SIZE = 3
+        const val BASEBALL_NUMBER_MINIMUM_NUMBER = '1'
+        const val BASEBALL_NUMBER_MAXIMUM_NUMBER = '9'
     }
 }
