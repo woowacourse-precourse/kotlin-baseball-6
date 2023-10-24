d7ef545cf7479f72cef4af13bb91a2aee59fb26a
sooohi <68193613+sooohi@users.noreply.github.com>
2023-10-24 17:37:55 +0900

    refactor : 라이브러리 활용 후 테트에 맞게 수정

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,26 +1,31 @@
 package baseball
 import kotlin.random.Random
+import camp.nextstep.edu.missionutils.Randoms
+import camp.nextstep.edu.missionutils.Console
 
 fun main() {
-    while(true) {
+    var restart = true;
+    println("숫자 야구 게임을 시작합니다.")
+    while(restart) {
         val computer = mutableListOf<Int>()
         while (computer.size < 3) {
-            val randomNumber = Random.nextInt(1, 10)
+            val randomNumber = Randoms.pickNumberInRange(1, 9)
             if (!computer.contains(randomNumber)) {
                 computer.add(randomNumber)
             }
         }
-
-        println("숫자 야구 게임을 시작합니다.")
+        println(computer);
         var isCorrect = false
 
         while (!isCorrect) {
             print("숫자를 입력해주세요 : ")
-            val inputNum = readLine()
+            val inputNum = Console.readLine()
+            println(inputNum);
             if (inputNum != null && inputNum.length == 3 && inputNum.all { it.isDigit() }) {
                 val myNum = inputNum.map { it.toString().toInt() }
                 val result = checkNum(computer, myNum)
                 if (result == "3스트라이크") {
+                    println("3스트라이크")
                     isCorrect = true
                     println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
                 } else {
@@ -31,10 +36,21 @@ fun main() {
                 throw IllegalArgumentException("잘못된 형식의 숫자가 입력되었습니다.")
             }
         }
-        print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요 : ")
-        if (readLine() == "2") {
-            break
+        println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요. ")
+        val restartInput = Console.readLine()
+        if (restartInput == "1") {
+            restart = true
+        } else if (restartInput == "2") {
+            restart = false
+        } else {
+            throw IllegalArgumentException("1 또는 2를 입력해주세요.")
         }
+        /*val restartInput = Console.readLine()
+        restart = when (restartInput) {
+            "1" -> true
+            "2" -> false
+            else -> throw IllegalArgumentException("1 또는 2를 입력해주세요.")
+        }*/
     }
 }
 
