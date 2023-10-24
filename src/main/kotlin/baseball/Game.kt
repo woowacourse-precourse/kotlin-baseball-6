074ef52741045ed65f2da561b212fdc43ff84172
bak <bak4017@gmail.com>
2023-10-24 12:14:14 +0900

    refactor: getUserNumber함수 에러처리 수정

--- src/main/kotlin/baseball/Game.kt
+++ src/main/kotlin/baseball/Game.kt
@@ -12,13 +12,9 @@ private const val TERMINATION = "2"    // 종료 상수
 val answer = IntArray(COUNT) // 정답 저장할 배열
 
 fun run() {
-    try {
         setGame()
         playGame()
         askRestart()
-    } catch (e: IllegalArgumentException){
-        throw IllegalArgumentException("올바르지 않은 입력값입니다.", e)
-    }
 }
 
 private fun setGame() {
@@ -37,8 +33,10 @@ private fun playGame() {
 
 private fun getUserNumber(): IntArray {
     print("숫자를 입력해주세요 : ")
-    val readLine = Console.readLine().split("").drop(1).dropLast(1)    // split함수의 시작과 끝에 추가되는 빈 문자열 제거
-    return readLine.map { it.toInt() }.toIntArray()    // 사용자 입력값을 정수형 배열로 변환해서 반환함
+    val userInput = Console.readLine().split("").drop(1).dropLast(1)  // split함수의 시작과 끝에 추가되는 빈 문자열 제거
+    if (userInput.any { it.toInt() in 48..57 } || userInput.size != COUNT) {    // 입력값이 아스키코드로 48(숫자0) ~ 57(숫자9)에 포함되지 않거나
+        throw IllegalArgumentException("올바른 수를 입력해주세요.")                       // 길이가 COUNT와 같지 않으면 올바르지 않은 입력값으로 에러발생
+    } else return userInput.map { it.toInt() }.toIntArray()                          // 입력값을 정수배열로 반환
 }
 
 private fun printResult(strike: Int, ball: Int) {
