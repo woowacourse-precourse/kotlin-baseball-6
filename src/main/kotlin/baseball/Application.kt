d911a77dc98617065443cad05ff9926a95094df9
vonvolous <hanyoojin39@gmail.com>
2023-10-24 16:24:11 +0900

    docs/README.md 추가 + 컴퓨터 수 랜덤 생성

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,16 @@
 package baseball
 
+import kotlin.collections.mutableListOf
+import camp.nextstep.edu.missionutils.Randoms
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
-    TODO("프로그램 구현")
+    val computer = mutableListOf<Int>()
+    while (computer.size < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (!computer.contains(randomNumber)) {
+            computer.add(randomNumber)
+        }
+    }
+    println("$computer")
 }
