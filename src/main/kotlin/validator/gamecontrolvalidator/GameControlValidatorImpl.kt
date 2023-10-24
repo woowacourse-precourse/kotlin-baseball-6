efd5076583d70012fa4f6fe6ef8389c09c3d7fc0
lee989898 <wnsrud1352@gmail.com>
2023-10-24 22:34:00 +0900

    [feat] GameControlValidatorImpl 1이나 2인지 검증하는 메서드 구현

--- src/main/kotlin/validator/gamecontrolvalidator/GameControlValidatorImpl.kt
+++ src/main/kotlin/validator/gamecontrolvalidator/GameControlValidatorImpl.kt
@@ -1,11 +1,16 @@
 package validator.gamecontrolvalidator
 
 object GameControlValidatorImpl: GameControlValidator {
+    private const val ONE = "1"
+    private const val TWO = "2"
+
     override fun isValidate(oneOrTwo: String): Boolean {
         TODO("Not yet implemented")
     }
 
     override fun isOneOrTwo(oneOrTwo: String): Boolean {
-        TODO("Not yet implemented")
+        require(oneOrTwo == ONE || oneOrTwo == TWO)
+
+        return true
     }
 }
