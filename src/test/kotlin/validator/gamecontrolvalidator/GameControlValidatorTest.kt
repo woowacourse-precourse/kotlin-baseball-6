bebd5f3cda6ef9795be08be052980e124a1343f3
lee989898 <wnsrud1352@gmail.com>
2023-10-24 22:33:08 +0900

    [test] GameControlValidatorTest 숫자가 1이나 2가 아닐때 오류를 던지는지 테스트 구현

--- src/test/kotlin/validator/gamecontrolvalidator/GameControlValidatorTest.kt
+++ src/test/kotlin/validator/gamecontrolvalidator/GameControlValidatorTest.kt
@@ -1,5 +1,7 @@
 package validator.gamecontrolvalidator
 
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.params.ParameterizedTest
@@ -20,5 +22,13 @@ class GameControlValidatorTest {
     fun `숫자가 1이나 2일때 성공인지`(oneOrTwo: String) {
         val result = gameControlValidator.isOneOrTwo(oneOrTwo)
         assertTrue(result)
+    }
+
+    @ParameterizedTest
+    @ValueSource(
+        strings = ["0", "34", "d", "ㅋ", "안녕", "-1"]
+    )
+    fun `숫자가 1이나 2가 아닐때 오류를 던지는지`(noneOneOrTwo: String) {
+        assertThrows(IllegalArgumentException::class.java) { gameControlValidator.isOneOrTwo(noneOneOrTwo) }
     }
 }
