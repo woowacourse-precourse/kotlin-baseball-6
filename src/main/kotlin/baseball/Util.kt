afd19a1a9eeaf5364bfded895ce9b5d6ade19434
leehandsub <gustjq7027@naver.com>
2023-10-24 20:34:07 +0900

    update : string으로만 쓰지 않아 파일 이름 변경

--- src/main/kotlin/baseball/Util.kt
+++ src/main/kotlin/baseball/Util.kt
@@ -7,3 +7,5 @@ const val BALL_MESSAGE = "볼"
 const val NOTING_MESSAGE = "낫싱"
 const val GAME_OVER_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료"
 const val CHECK_CONTINUE_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
+
+const val BASEBALL_MAX_SIZE = 3
