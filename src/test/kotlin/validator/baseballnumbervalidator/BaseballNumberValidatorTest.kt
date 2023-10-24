84463a2dd1e8d128aa870fd1a76debc1ee44854e
lee989898 <wnsrud1352@gmail.com>
2023-10-24 22:06:07 +0900

    [test] BaseballNumberValidatorTest 세 자리 수 일때 성공인지 테스트 구현

--- src/test/kotlin/validator/baseballnumbervalidator/BaseballNumberValidatorTest.kt
+++ src/test/kotlin/validator/baseballnumbervalidator/BaseballNumberValidatorTest.kt
@@ -1,6 +1,9 @@
 package validator.baseballnumbervalidator
 
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 class BaseballNumberValidatorTest {
     private lateinit var baseballNumberValidator: BaseballNumberValidator
@@ -8,5 +11,14 @@ class BaseballNumberValidatorTest {
     @BeforeEach
     fun setUp() {
         baseballNumberValidator = BaseballNumberValidatorImpl
+    }
+
+    @ParameterizedTest
+    @ValueSource(
+        strings = ["123", "145", "789", "235"]
+    )
+    fun `세 자리 수 일때 성공인지`(baseballRandomNumber: String) {
+        val result = baseballNumberValidator.isThreeLetters(baseballRandomNumber)
+        assertTrue(result)
     }
 }
