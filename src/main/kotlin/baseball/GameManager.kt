5d5d74503da725de29b6e4274f33e2bc212724be
Junyoung-WON <wjyo60@gmail.com>
2023-10-25 05:38:43 +0900

    feat: 최초 실행 시 게임 시작 문구 출력 구현

--- src/main/kotlin/baseball/GameManager.kt
+++ src/main/kotlin/baseball/GameManager.kt
@@ -2,8 +2,14 @@ package baseball
 
 class GameManager {
     companion object{
-        fun baseballGame(){
-            
+        fun start(){
+            printStartMessage()
         }
+
+        // 최초 실행 시 게임 시작 문구 출력
+        private fun printStartMessage(){
+            println("숫자 야구 게임을 시작합니다.")
+        }
+
     }
 }
