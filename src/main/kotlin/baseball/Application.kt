8fd536735a85106b302cc6c87fe0b6cdcfe7b994
Wonhui Roh <rwhui012@gmail.com>
2023-10-24 15:36:48 +0900

    FEAT: 2) '나의 숫자' 입력 - 잘못된 입력에 대한 예외처리

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,6 +1,7 @@
 package baseball
 
 import camp.nextstep.edu.missionutils.*
+import java.lang.Exception
 
 
 fun main() {
@@ -43,6 +44,7 @@ fun guessComputer(): List<Int> {
     val input = Console.readLine()
     val result = mutableListOf<Int>()
 
+    // 잘못된 입력에 대한 예외처리
     checkGuess(input)
 
     for (c in input) {
@@ -52,6 +54,28 @@ fun guessComputer(): List<Int> {
     return result
 }
 
-fun checkGuess(m: String): String {
-    TODO("Not yet implemented")
+// 2) '나의 숫자' 입력 - 잘못된 입력에 대한 예외처리
+fun checkGuess(m: String) {
+    // 숫자가 아닌 값을 입력한 경우
+    if (m.toIntOrNull() == null) {
+        throw IllegalArgumentException("숫자가 아닌 값을 입력하셨습니다.")
+    }
+
+    // 숫자이지만,
+        // 3자리가 아닌 경우
+    if (m.length != 3) {
+        throw IllegalArgumentException("3자리의 수를 입력해주세요.")
+    }
+
+        // 서로 다른 수가 아닌 경우
+    if (m[0] == m[1] || m[0] == m[2] || m[1] == m[2]) {
+        throw IllegalArgumentException("서로 다른 수로 이뤄져야 합니다.")
+    }
+
+    for (c in m) {
+        // 1부터 9까지의 수로 이뤄지지 않은 경우
+        if (Character.getNumericValue(c) in 1..9) {
+            throw IllegalArgumentException("1부터 9까지의 수로 이뤄져야 합니다.")
+        }
+    }
 }
