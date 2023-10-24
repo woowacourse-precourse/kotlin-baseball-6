6d6c8d717101ddf2456fbf68a67ec5bdec71cf13
jooyyoo <aliceyun9@naver.com>
2023-10-24 16:51:15 +0900

    [mod] 함수 추가 및 코드 수정

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,29 +1,41 @@
 package baseball
 
-import camp.nextstep.edu.missionutils.Randoms
-import kotlin.system.exitProcess
+import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms.pickNumberInRange
+import java.lang.NumberFormatException
 
-val computer = mutableListOf<Int>()
-val inputArray = mutableListOf<Int>()
 fun main() {
     println("숫자 야구 게임을 시작합니다.")
-    printResult()
+    do {
+        printResult()
+    }while (printResult() != 2)
+    return
+
 }
 
-private fun makeRandomNumber() {
+private fun makeRandomNumber(): MutableList<Int> {
+    val computer = mutableListOf<Int>()
     while (computer.size < 3) {
-        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        val randomNumber = pickNumberInRange(1, 9)
         if (!computer.contains(randomNumber)) {
             computer.add(randomNumber)
         }
     }
     //println(computer)
+    return computer
 }
 
-private fun guessNumber() {
+private fun guessNumber(): MutableList<Int> {
     print("숫자를 입력해주세요 : ")
 
-    val input = readln()
+    val input = Console.readLine()
+
+    return checkInput(input)
+}
+
+private fun checkInput(input: String): MutableList<Int> {
+    val inputArray = mutableListOf<Int>()
+
     try {
         if (input.length == 3) {
             val digit1 = input[0].toString().toInt()
@@ -34,24 +46,27 @@ private fun guessNumber() {
                 inputArray.add(digit1)
                 inputArray.add(digit2)
                 inputArray.add(digit3)
+
+                return inputArray
+
             } else {
-                throw IllegalArgumentException("게임 종료")
+                throw IllegalArgumentException("숫자가 중복되었습니다")
             }
-            //println(inputArray)
         } else {
-            throw IllegalArgumentException("게임 종료")
+            throw IllegalArgumentException("세자리 수가 아닙니다")
         }
-    } catch (_: IllegalArgumentException) {
-        exitProcess(0)
+    } catch (e: NumberFormatException) {
+        throw IllegalArgumentException("숫자가 아닙니다")
     }
 }
 
-private fun printResult() {
+private fun printResult(): Int {
     var strike = 0
     var ball: Int
-    makeRandomNumber()
+    val computer = makeRandomNumber()
+
     while (strike != 3) {
-        guessNumber()
+        val inputArray = guessNumber()
         strike = 0
         ball = 0
 
@@ -78,13 +93,11 @@ private fun printResult() {
     computer.clear()
 
     println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
+
+    return gameOver()
+}
+
+private fun gameOver(): Int {
     println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
-
-    val gameOver: Int = readln().toInt()
-
-    if (gameOver == 1) {
-        main()
-    } else if (gameOver == 2) {
-        exitProcess(0)
-    }
+    return Console.readLine().toInt()
 }
