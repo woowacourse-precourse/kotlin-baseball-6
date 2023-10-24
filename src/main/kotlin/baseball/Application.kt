e95437d4c4dc2d46d4fbb48b3bcbafe96c0126e6
iiolo <alohalo98@naver.com>
2023-10-25 01:16:10 +0900

    feat: 2번 기능(사용자로부터 숫자 3개를 입력 받는 기능) 추가

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,9 +1,10 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Console
 import camp.nextstep.edu.missionutils.Randoms
 
 fun main() {
-    TODO("프로그램 구현")
+
 }
 
 fun getComputerNumList(): MutableList<Int> {
@@ -15,4 +16,33 @@ fun getComputerNumList(): MutableList<Int> {
         }
     }
     return computerNumList
+}
+
+fun getUserNum(): MutableList<Int> {
+    val userInput = Console.readLine().toString()
+
+    if (!isUserNumValid(userInput)) {
+        throw IllegalArgumentException("잘못된 값을 입력하셨습니다. 3자리의 수로 다시 입력해주세요.")
+    }
+    return userInput.toCharArray()
+        .map { it.digitToInt() }
+        .toMutableList()
+
+}
+
+fun isUserNumValid(userInput: String): Boolean{
+    if (userInput != null) {
+        if (!userInput.trim().matches(Regex("\\d{3}"))) {
+            throw IllegalArgumentException("잘못된 값을 입력하셨습니다. 3자리의 수로 다시 입력해주세요.")
+            return false
+        } else if (userInput.toCharArray().map { it.digitToInt() }.toMutableList().distinct().size != 3) {
+            throw IllegalArgumentException("잘못된 값을 입력하셨습니다. 3자리의 수로 다시 입력해주세요.")
+            return false
+        } else {
+            return true
+        }
+    } else {
+        throw IllegalArgumentException("잘못된 값을 입력하셨습니다. 3자리의 수로 다시 입력해주세요.")
+        return false
+    }
 }
