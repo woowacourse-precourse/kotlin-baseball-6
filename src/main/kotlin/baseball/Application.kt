ac601d690aaea580562a88711d1cb95fd8719ad3
vonvolous <hanyoojin39@gmail.com>
2023-10-24 21:16:53 +0900

    사용자가 입력한 값에 대한 결과 출력

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -16,16 +16,45 @@ fun main() {
             }
         }
 
-        print("숫자를 입력해주세요 : ")
-        val user: String = Console.readLine()
+        while(true) {
+            var cntBall:Int = 0
+            var cntStrike:Int = 0
+
+            print("숫자를 입력해주세요 : ")
+            val user: String = Console.readLine()
 
 
-        if (computer[1]==user[1].digitToInt()){
-            println("맞습니다.")
-        } else {
-            continue
+            for (i in 0..2){
+                if (computer[i]==user[i].digitToInt()){
+                    cntStrike += 1
+                } else {
+                    if (user.contains(computer[i].toString())) {
+                        cntBall += 1
+                    }
+                }
+            }
+
+            if(cntStrike==3){
+                println("3스트라이크")
+                break
+            }
+
+            if (cntBall==0){
+                if (cntStrike==0){
+                    println("낫싱")
+                } else {
+                    println("${cntStrike}스트라이크")
+                }
+            } else {
+                if (cntStrike==0){
+                    println("${cntBall}볼")
+                } else {
+                    println("${cntBall}볼 ${cntStrike}스트라이크")
+                }
+            }
         }
 
+        println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
         println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
         val choice = Console.readLine()
         if (choice=="2") {
