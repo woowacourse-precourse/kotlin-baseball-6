ac5d6291ba2ef9f826fb7b08df75e82d2e44cc8e
iiolo <alohalo98@naver.com>
2023-10-25 03:10:00 +0900

    feat: 4번 기능 구현

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -87,3 +87,8 @@ fun printGameOverMessage(){
     println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
     println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
 }
+
+fun checkExitStatus(): Boolean {
+    val exitStatus = Console.readLine().trim()
+    return exitStatus == "1"
+}
