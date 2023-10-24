31bf564758a5316f3d9866a0359c860085654b57
lee989898 <wnsrud1352@gmail.com>
2023-10-24 22:13:05 +0900

    [feat] BaseballNumberValidatorImpl 세 자리 문자열인지 검증하는 메서드 구현

--- src/main/kotlin/validator/baseballnumbervalidator/BaseballNumberValidatorImpl.kt
+++ src/main/kotlin/validator/baseballnumbervalidator/BaseballNumberValidatorImpl.kt
@@ -1,12 +1,16 @@
 package validator.baseballnumbervalidator
 
 object BaseballNumberValidatorImpl: BaseballNumberValidator {
+    private const val BASEBALL_NUMBER_SIZE = 3
+
     override fun isValidate(baseballNumber: String): Boolean {
         TODO("Not yet implemented")
     }
 
     override fun isThreeLetters(baseballNumber: String): Boolean {
-        TODO("Not yet implemented")
+        require(baseballNumber.length == BASEBALL_NUMBER_SIZE)
+        
+        return true
     }
 
     override fun isItANumber(baseballNumber: String): Boolean {
