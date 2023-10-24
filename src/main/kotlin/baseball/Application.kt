13558917681944d1baee8206dcacbe3f68ecd2f7
EJ-pro <hsshss2522@naver.com>
2023-10-24 13:21:53 +0900

    결과 표시기능

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -17,6 +17,28 @@ fun hasDuplicateDigits(number: Int): Boolean{
     val digits = number.toString().toCharArray()
     return digits.toSet().size != digits.size
 }
+fun compareNumbers(number1: Int, number2: Int): String {
+    val str1 = number1.toString()
+    val str2 = number2.toString()
+    var strike = 0
+    var ball = 0
+    for (i in 0 until minOf(str1.length, str2.length)) {
+        if (str1[i] == str2[i]) {
+            strike++
+        } else if (str1.any { it == str2[i] }) {
+            ball++
+        }
+    }
+    return if(strike == 0 && ball == 0){
+        "낫싱"
+    }else if(strike == 0) {
+        "$ball"+"볼"
+    }else if(ball == 0){
+        "$strike"+"스트라이크"
+    }else{
+        "$ball"+"볼 "+"$strike"+"스트라이크"
+    }
+}
 fun main() {
     println("숫자 야구 게임을 시작합니다.")
     var number = generateRandomNumber()
@@ -26,5 +48,6 @@ fun main() {
         if(num.length != 3 || !num.all { it.isDigit() } || hasDuplicateDigits(num.toInt())){
             throw IllegalArgumentException("[Error]")
         }
+        println(compareNumbers(num.toInt(), number));
     }
 }
