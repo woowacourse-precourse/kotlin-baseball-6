cc675ae89bc4ed65f0dd3542c6c955dbbae4e677
sseung416 <choiseungyeon416@gmail.com>
2023-10-24 20:15:23 +0900

    Fix: toIntList 예외 처리

--- src/main/kotlin/baseball/util/Extensions.kt
+++ src/main/kotlin/baseball/util/Extensions.kt
@@ -2,8 +2,14 @@ package baseball.util
 
 fun String.toIntList(): List<Int> =
     this.map { char ->
-        char.toString().toInt()
+        val current = char.toString()
+        if (!current.isInt()) {
+            throw IllegalArgumentException("Int 타입이 아님")
+        }
+        current.toInt()
     }
+
+fun String.isInt(): Boolean = toIntOrNull() != null
 
 fun String.containsSign(): Boolean =
     listOf('+', '-').contains(this.getOrNull(0)) ?: false
