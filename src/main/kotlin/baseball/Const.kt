386d8bcd67107c2e05299b7c2dcdc38241e030f5
NOGUEN <nhg1113@naver.com>
2023-10-24 14:10:22 +0900

    chore: 상수 추가 업로드

---
+++ src/main/kotlin/baseball/Const.kt
@@ -0,0 +1,7 @@
+const val STR_WELCOME_MESSAGE = "숫자 야구 게임을 시작합니다."
+const val STR_INPUT_MESSAGE = "숫자를 입력해주세요 : "
+const val STR_BALL = "볼"
+const val STR_STRIKE = "스트라이크"
+const val STR_NOTHING = "낫싱"
+const val STR_GAME_END = "3개의 숫자를 모두 맞히셨습니다! 게임 종료"
+const val STR_ASK_RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
