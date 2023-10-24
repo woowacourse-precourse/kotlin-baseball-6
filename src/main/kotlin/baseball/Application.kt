cf58d388cfe56aeefbeeefa6d6fe8324fbd0fcbd
somin <salth0601@gmail.com>
2023-10-24 22:17:51 +0900

    refactor: Constants util 패키지에 분리

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -2,14 +2,13 @@ package baseball
 
 import camp.nextstep.edu.missionutils.Console
 import camp.nextstep.edu.missionutils.Randoms
-
-const val START_MESSAGE = "숫자 야구 게임을 시작합니다."
-const val INPUT_MESSAGE = "숫자를 입력해주세요 : "
-const val RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
-const val SUCCESS_MESSAGE = "3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료"
-const val INVALID_INPUT_MESSAGE = "숫자만 입력하세요"
-const val INVALID_LENGTH_MESSAGE = "3자리 수를 입력하세요"
-const val DUPLICATE_NUMBER_MESSAGE = "각기 다른 3개의 숫자를 입력하세요"
+import util.Constants.DUPLICATE_NUMBER_MESSAGE
+import util.Constants.INPUT_MESSAGE
+import util.Constants.INVALID_INPUT_MESSAGE
+import util.Constants.INVALID_LENGTH_MESSAGE
+import util.Constants.RESTART_MESSAGE
+import util.Constants.START_MESSAGE
+import util.Constants.SUCCESS_MESSAGE
 
 fun main() {
     println(START_MESSAGE)
---
+++ src/main/kotlin/util/Constants.kt
@@ -0,0 +1,11 @@
+package util
+
+object Constants {
+    const val START_MESSAGE = "숫자 야구 게임을 시작합니다."
+    const val INPUT_MESSAGE = "숫자를 입력해주세요 : "
+    const val RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
+    const val SUCCESS_MESSAGE = "3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료"
+    const val INVALID_INPUT_MESSAGE = "숫자만 입력하세요"
+    const val INVALID_LENGTH_MESSAGE = "3자리 수를 입력하세요"
+    const val DUPLICATE_NUMBER_MESSAGE = "각기 다른 3개의 숫자를 입력하세요"
+}
