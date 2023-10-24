c89e4da160dbecf5903ef9231be7e28ca59f49cf
Junyoung-WON <wjyo60@gmail.com>
2023-10-25 06:08:01 +0900

    feat: 테스트코드 삭제 및 게임관리 클래스로 게임 진행

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -4,19 +4,6 @@ import camp.nextstep.edu.missionutils.Randoms
 
 
 fun main() {
-    // 시작 문구 출력(함수 사용)
-    printStartMessage()
-    // println("숫자 야구 게임을 시작합니다.")
-
-    // 컴퓨터가 생성하는 서로 다른 3자리 난수(함수 사용)
-    val computer = Computer.generateRandomNumber()
-
-    // 생성된 난수를 출력
-    println(computer)
-
-    for (i in 0..4) {
-        var user = User.getUserNumbers()
-
-    }
+    GameManager.start()
 }
 
