fe8bec781aef387cae4248f921c47ac9935351e3
bak <bak4017@gmail.com>
2023-10-24 17:12:05 +0900

    refactor: MVC 패턴 적용

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -2,5 +2,10 @@ package baseball
 
 fun main() {
     println("숫자 야구 게임을 시작합니다.")
-    run()
+
+    val model = model.BaseballModel()
+    val view = view.BaseballView()
+    val controller = Controller(model, view)
+
+    controller.run()
 }
