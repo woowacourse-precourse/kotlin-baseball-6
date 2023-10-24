6552049b0dfe0d0aec0db812b161c0243348cd1b
m6z1 <sonmyungj1zz@gmail.com>
2023-10-24 18:48:43 +0900

    feat : 플레이어의 입력 숫자에 대한 예외처리 구현

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -22,5 +22,9 @@ fun main() {
 fun userInputNum(): Int {
     print("숫자를 입력하세요: ")
     val input = Console.readLine()
+
+    if(input.length!=3 || !input.all {it.isDigit()}) {
+        throw IllegalArgumentException()
+    }
     return input.toInt()
 }
