91fb9ee243b3dbc27273961d73df226f7b64bf30
EJ-pro <hsshss2522@naver.com>
2023-10-24 13:41:16 +0900

    Kotlin 코드 컨벤션 가이드

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -13,53 +13,60 @@ fun generateRandomNumber(): Int {
     }
     return computer.joinToString("").toInt()
 }
-fun hasDuplicateDigits(number: Int): Boolean{
+
+fun hasDuplicateDigits(number: Int): Boolean {
     val digits = number.toString().toCharArray()
     return digits.toSet().size != digits.size
 }
+
 fun compareNumbers(number1: Int, number2: Int): String {
     val str1 = number1.toString()
     val str2 = number2.toString()
+
     var strike = 0
     var ball = 0
-    for (i in 0 until minOf(str1.length, str2.length)) {
-        if (str1[i] == str2[i]) {
-            strike++
-        } else if (str1.any { it == str2[i] }) {
-            ball++
-        }
+
+    for (i in str1.indices) {
+        if (str1[i] == str2[i]) strike++
+        else if (str1.any { it == str2[i] }) ball++
     }
-    return if(strike == 0 && ball == 0){
-        "낫싱"
-    }else if(strike == 0) {
-        "$ball"+"볼"
-    }else if(ball == 0){
-        "$strike"+"스트라이크"
-    }else{
-        "$ball"+"볼 "+"$strike"+"스트라이크"
+
+    return when{
+        strike == 0 && ball == 0 -> "낫싱"
+        strike == 0 -> "$ball"+"볼"
+        ball == 0 -> "$strike"+"스트라이크"
+        else -> "$ball"+"볼 "+"$strike"+"스트라이크"
     }
 }
 fun main() {
+
     println("숫자 야구 게임을 시작합니다.")
     var number = generateRandomNumber()
+
     while(true) {
+
         print("숫자를 입력해주세요 : ")
-        val num = Console.readLine()
-        if(num.length != 3 || !num.all { it.isDigit() } || hasDuplicateDigits(num.toInt())){
+        val numString = Console.readLine() ?: throw IllegalArgumentException("[Error]")
+        val numInt= numString.toIntOrNull() ?: throw IllegalArgumentException("[Error]")
+
+        if(numString.length != 3 || !numString.all { it.isDigit() } || hasDuplicateDigits(numInt)){
             throw IllegalArgumentException("[Error]")
         }
-        println(compareNumbers(num.toInt(), number));
-        if(num.toInt().equals(number)){
+
+        println(compareNumbers(numInt, number))
+
+        if(numInt.equals(number)){
             println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
             println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
-            val restart = Console.readLine()
-            if (restart != null) {
-                if(restart.toInt() == 2){
-                    break
-                }else{
-                    number = generateRandomNumber()
+
+            when(Console.readLine()?.toIntOrNull()){
+                null -> throw IllegalArgumentException("[Error]")
+                2 -> break
+                else->{
+                    number=generateRandomNumber()
                 }
             }
+
         }
     }
 }
