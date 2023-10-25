c2fd9016ace5b10aeb7740a169511550fec7c449
K_Gs:D <rootachieve6053@gmail.com>
2023-10-25 18:58:57 +0900

    feat : CheckResult toString override

--- src/main/kotlin/baseball/CheckResult.kt
+++ src/main/kotlin/baseball/CheckResult.kt
@@ -3,4 +3,23 @@ package baseball
 data class CheckResult(
     val strike: Int = 0,
     val ball: Int = 0
-)
+) {
+    override fun toString(): String {
+        val msg = StringBuilder()
+        if (ball != 0) {
+            msg.append("${ball}볼 ")
+        }
+
+        if (strike != 0) {
+            msg.append("${strike}스트라이크 ")
+        }
+
+        if (strike == 0 && ball == 0) {
+            msg.append("낫싱")
+        }
+
+        msg.trim()
+        return msg.toString()
+    }
+
+}
