1a60a8cd2c87fb49c51586f2e6b1a6e350971b1a
leehandsub <gustjq7027@naver.com>
2023-10-25 20:39:51 +0900

    update : 로직에 대한 주석 추가

--- src/main/kotlin/baseball/view/OutputView.kt
+++ src/main/kotlin/baseball/view/OutputView.kt
@@ -4,14 +4,14 @@ class OutputView {
     fun printGameOver() = println(GAME_OVER_MESSAGE)
 
     fun printBaseBallResult(strike: Int, ball: Int) {
-        if (ball == 0 && strike == 0) {
+        if (ball == 0 && strike == 0) {//볼이 없으면 낫싱
             println(NOTING_MESSAGE)
-        } else if (ball != 0 && strike == 0) {
+        } else if (ball != 0 && strike == 0) {//볼이 있으면서 strike가 없을경우 \n이 있어야 위한 조건
             println("$ball$BALL_MESSAGE")
-        } else if (ball != 0) {
+        } else if (ball != 0) { // 볼이 있고 strike 가 있을 경우 먼저 볼을 출력
             print("$ball$BALL_MESSAGE ")
         }
-        if (strike != 0) {
+        if (strike != 0) { // 스트라이크 출력하고 \n 출력
             println("$strike$STRIKE_MESSAGE")
         }
     }
