e4accf5b8f06b2a85642e05945798b291ff823ec
pearhyunjin <pearhyunjin@gmail.com>
2023-10-25 21:37:15 +0900

    feat: 상수 선언 파일 작성

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -4,7 +4,7 @@ import camp.nextstep.edu.missionutils.Randoms
 import camp.nextstep.edu.missionutils.Console
 
 fun main() {
-    println("숫자 야구 게임을 시작합니다.")
+    println(START_PLAY_TEXT)
     do {
         startBaseballPlay()
     } while (restartBaseballPlay())
@@ -12,25 +12,18 @@ fun main() {
 }
 
 fun startBaseballPlay() {
-
     val computerRandomNum = createComputerRandomNum()
-
     do {
         val playerAnswer = inputPlayerNum()
-
         strikeAndBall(computerRandomNum, playerAnswer)
-
-    } while (compareComputerAndPlayer(computerRandomNum, playerAnswer) == 0)
+    } while (!compareComputerAndPlayer(computerRandomNum, playerAnswer))
     endBaseballPlay()
-
-
 }
 
 fun createComputerRandomNum(): String {
-
     val computerNum = mutableListOf<Int>()
-    while (computerNum.size < 3) {
-        val randomNumber = Randoms.pickNumberInRange(1, 9)
+    while (computerNum.size < INPUT_ANSWER_LENGTH) {
+        val randomNumber = Randoms.pickNumberInRange(MIN_INPUT_NUM, MAX_INPUT_NUM)
         if (!computerNum.contains(randomNumber)) {
             computerNum.add(randomNumber)
         }
@@ -39,40 +32,32 @@ fun createComputerRandomNum(): String {
 }
 
 fun inputPlayerNum(): String {
-
-    val playerNum = mutableListOf<Char>()
-
-    print("숫자를 입력해주세요 : ")
+    print(INPUT_ANSWER_TEXT)
     val inputPlayer = Console.readLine()
-
+    val playerNum = mutableListOf<Char>()
     if (!isNum(inputPlayer)) {
-        throw IllegalArgumentException("잘못된 입력 형식")
+        throw IllegalArgumentException(WRONG_NUM_EXCEPTION)
     }
-    if (inputPlayer.length != 3) {
-        throw IllegalArgumentException("잘못된 길이의 입력값 (3자리 수로 입력하세요.)")
+    if (inputPlayer.length != INPUT_ANSWER_LENGTH) {
+        throw IllegalArgumentException(WRONG_NUM_LENGTH_EXCEPTION)
     }
     for (index in inputPlayer.indices) {
         if (playerNum.contains(inputPlayer[index])) {
-            throw IllegalArgumentException("중복된 숫자가 존재")
+            throw IllegalArgumentException(DUPLICATED_NUM_EXCEPTION)
         }
         playerNum.add(inputPlayer[index])
     }
     return playerNum.joinToString("")
 }
 
-fun compareComputerAndPlayer(computer: String, player: String): Int {
-    return if (computer == player) {
-        1
-    } else {
-        0
-    }
+fun compareComputerAndPlayer(computer: String, player: String): Boolean {
+    return computer == player
 }
 
 fun strikeAndBall(computer: String, player: String) {
-    var strike = 0
-    var ball = 0
-
-    for (index in 0..2) {
+    var strike = NO_STRIKE
+    var ball = NO_BALL
+    for (index in MIN_ANSWER_INDEX_NUM..MAX_ANSWER_INDEX_NUM) {
         if (computer[index] == player[index]) {
             strike++
         } else {
@@ -81,38 +66,34 @@ fun strikeAndBall(computer: String, player: String) {
             }
         }
     }
-
     baseballHint(strike, ball)
 }
 
 fun baseballHint(strike: Int, ball: Int) {
-
     when {
-        (strike == 0 && ball == 0) -> println("낫싱")
-        (strike != 0 && ball == 0) -> println("${strike}스트라이크")
-        (strike == 0 && ball != 0) -> println("${ball}볼")
-        (strike != 0 && ball != 0) -> println("${ball}볼 ${strike}스트라이크")
+        (strike == NO_STRIKE && ball == NO_BALL) -> println("낫싱")
+        (strike != NO_STRIKE && ball == NO_BALL) -> println("${strike}스트라이크")
+        (strike == NO_STRIKE && ball != NO_BALL) -> println("${ball}볼")
+        (strike != NO_STRIKE && ball != NO_BALL) -> println("${ball}볼 ${strike}스트라이크")
     }
 }
 
 fun isNum(inputPlayer: String): Boolean {
-
     var isNum = true
-
     inputPlayer.forEach { char ->
         val charConvertToInt = char.digitToIntOrNull()
         val charConvertToCode = char.code
-        if (charConvertToCode > 57 || charConvertToCode < 48) {
+        if (charConvertToCode > MAX_NUM_RANGE_ASCII || charConvertToCode < MIN_NUM_RANGE_ASCII) {
             isNum = false
             return isNum
         }
         if (charConvertToInt == null) {
-            throw IllegalArgumentException("입력값이 존재하지 않습니다.")
+            throw IllegalArgumentException(NO_EXIST_INPUT_NUM_TEXT)
             isNum = false
             return isNum
         }
-        if (charConvertToInt > 9 || charConvertToInt < 1) {
-            throw IllegalArgumentException("입력값 범위가 옳지 않습니다. 1~9범위 사용해주세요")
+        if (charConvertToInt > MAX_INPUT_NUM || charConvertToInt < MIN_INPUT_NUM) {
+            throw IllegalArgumentException(WRONG_INPUT_NUM_RANGE_EXCEPTION)
             isNum = false
             return isNum
         }
@@ -121,15 +102,18 @@ fun isNum(inputPlayer: String): Boolean {
 }
 
 fun endBaseballPlay() {
-    println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
+    println(END_PLAY_TEXT)
 }
 
 fun restartBaseballPlay(): Boolean {
-    println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+    println(RESTART_PLAY_TEXT)
 
     val inputPlayerRestart = Console.readLine()
-    if (inputPlayerRestart.toInt() == 1) {
+    if (inputPlayerRestart.toInt() == RESTART_INPUT_NUM) {
         return true
     }
-    return false
+    if (inputPlayerRestart.toInt() == END_INPUT_NUM) {
+        return false
+    }
+    throw IllegalArgumentException(WRONT_RESTART_NUM_EXCEPTION)
 }
