ce31d3af9fd0249c132446e61d4a05a64778a5f7
april <aprilgom@gmail.com>
2023-10-25 22:07:52 +0900

    GuessResult 별개 파일로 분리

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -7,52 +7,6 @@ import kotlin.collections.mutableListOf
 //숫자야구는 3개 혹은 4개의 숫자로 하는 게임이다. 변경 여지 있음.
 const val MAX_NUMBER = 3
 
-class GuessResult(secretNumber: String, guessNumber: String) {
-    private val ball: Int
-    private val strike: Int
-    private val text: String
-
-    private fun countBall(secretNumber: String, guessNumber: String): Int {
-        var ball = 0
-        for (secretIt in 0 until MAX_NUMBER) {
-            for (guessIt in 0 until MAX_NUMBER) {
-                if (secretIt == guessIt) {
-                    continue
-                }
-                if (secretNumber[secretIt] == guessNumber[guessIt]) {
-                    ball++
-                }
-            }
-        }
-        return ball
-    }
-
-    private fun countStrike(secretNumber: String, guessNumber: String): Int {
-        var strike = 0
-        for (it in 0 until MAX_NUMBER) {
-            if (secretNumber[it] == guessNumber[it]) {
-                strike++
-            }
-        }
-        return strike
-    }
-
-    override fun toString(): String = text
-
-    fun isCorrect() = strike == MAX_NUMBER
-
-    init {
-        this.ball = countBall(secretNumber, guessNumber)
-        this.strike = countStrike(secretNumber, guessNumber)
-        this.text = when {
-            ball == 0 && strike != 0 -> "${strike}스트라이크"
-            ball != 0 && strike == 0 -> "${ball}볼"
-            ball == 0 -> "낫싱"
-            else -> "${ball}볼 ${strike}스트라이크"
-        }
-    }
-}
-
 fun makeSecretNumber(): String {
     val computer: MutableList<Int> = mutableListOf()
     for (it in 0 until MAX_NUMBER) {
