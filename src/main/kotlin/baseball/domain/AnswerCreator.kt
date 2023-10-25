a8a6570607efe72ab865885b5fe41cd2433f3ca5
mainfn <_@mainfn.dev>
2023-10-25 13:07:12 +0900

    refact: inject dependency in constructor

--- src/main/kotlin/baseball/domain/AnswerCreator.kt
+++ src/main/kotlin/baseball/domain/AnswerCreator.kt
@@ -1,21 +1,17 @@
 package baseball.domain
 
-import camp.nextstep.edu.missionutils.Randoms
+import baseball.util.NumberPicker
 
-class AnswerCreator {
+class AnswerCreator(private val numberPicker: NumberPicker) {
     fun create(): Answer {
         // 자동으로 중복 제거해주는 Set
-        val set = mutableSetOf<BallNumber>()
+        val set = mutableSetOf<BallNumber>() as LinkedHashSet
 
         // Set에 숫자 3개가 들어갈 때까지
         while (set.size < BallNumbers.BALL_COUNT) {
-            set.add(BallNumber(pickRandomNumber()))
+            set.add(BallNumber(numberPicker.pick()))
         }
 
         return Answer(set.toList())
     }
-
-    // 1~9 사이의 랜덤 생성 숫자 생성
-    private fun pickRandomNumber() = Randoms.pickNumberInRange(1, 9)
-
 }
