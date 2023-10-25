3ccea4ade23e554c3e5ba9de3f600aa3f87fc6a2
eunyeong <feijoaplz@gmail.com>
2023-10-25 11:10:43 +0900

    compareNumber함수 기능 수정

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -86,10 +86,10 @@ fun compareNumber(ball:Int, strike: Int): String {
     if (ball == 0 && strike == 0) {
         println("낫싱")
     }
-    if (ball == 0 && strike > 0) {
+    else if (ball == 0 && strike > 0) {
         println("${strike}스트라이크")
     }
-    if (strike == 0 && ball > 0) {
+    else if (strike == 0 && ball > 0) {
         println("${ball}볼")
     }else {
         println("${ball}볼 ${strike}스트라이크")}
