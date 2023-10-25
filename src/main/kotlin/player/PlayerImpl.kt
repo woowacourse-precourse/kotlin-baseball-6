0bac83a5286d848bdb985f438e780286feb98d51
lee989898 <wnsrud1352@gmail.com>
2023-10-25 10:36:15 +0900

    [feat] PlayerImpl 생성

---
+++ src/main/kotlin/player/PlayerImpl.kt
@@ -0,0 +1,10 @@
+package player
+
+class PlayerImpl : Player {
+    override val baseballNumber: String
+        get() = TODO("Not yet implemented")
+
+    override fun setBaseballNumber(inputBaseballNumber: String) {
+        TODO("Not yet implemented")
+    }
+}
