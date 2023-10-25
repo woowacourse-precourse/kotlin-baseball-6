0f01be1b974b538201c0cb27ca1d3913d47639d5
iamsseaa <homingsea@gmail.com>
2023-10-25 18:54:32 +0900

    게임 종료 안내 문구 수정

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -48,7 +48,7 @@ fun main() {
 
             program.returnResult(countOfStrike, countOfBall)
         }
-
+        println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
         println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
         try {
             restart = user.inputRestartNumber()
