612c904d3b5bf1b260638fd20879c6010d6bc782
lee989898 <wnsrud1352@gmail.com>
2023-10-25 10:40:20 +0900

    [test] PlayerTest 설정한 번호가 저장되는지 확인 테스트 구현

--- src/test/kotlin/player/PlayerTest.kt
+++ src/test/kotlin/player/PlayerTest.kt
@@ -1,6 +1,9 @@
 package player
 
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 class PlayerTest {
     private lateinit var player: Player
@@ -8,5 +11,16 @@ class PlayerTest {
     @BeforeEach
     fun setUp() {
         player = PlayerImpl()
+    }
+
+    @ParameterizedTest
+    @ValueSource(
+        strings = ["123", "791", "234"]
+    )
+    fun `설정한 번호가 저장되는지 확인`(inputNumber: String) {
+        player.setBaseballNumber(inputNumber)
+        val result = player.baseballNumber
+
+        assertEquals(result, inputNumber)
     }
 }
