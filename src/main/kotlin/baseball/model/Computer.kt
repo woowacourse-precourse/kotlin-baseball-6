13382f90c116ff065a6c1ce308eb52c9076fa0d2
sh1mj1 <7wlgns@gmail.com>
2023-10-25 21:36:09 +0900

    fix: 랜덤 수를 만들어내는 함수 generateAnswer() 의 버그 수정 기존에 `answer: MutableSet<Int> = HashSet()` 를 사용하면 리스트로 변환할 때 오름차순으로 정렬됨. 이에 대해서 `answer = mutableListOf<Int>` 로 변경하여 이 버그 수정함.

--- src/main/kotlin/baseball/model/Computer.kt
+++ src/main/kotlin/baseball/model/Computer.kt
@@ -7,15 +7,17 @@ import camp.nextstep.edu.missionutils.Randoms
 
 class Computer {
     var input: String = ""
-    var answer: List<Int> = emptyList()
+    private var answer: List<Int> = emptyList()
 
     private val numberComparator = NumberComparator()
 
     fun generateAnswer() {
-        val answer: MutableSet<Int> = HashSet()
+        val answer = mutableListOf<Int>()
         while (answer.size < DIGIT_NUMBER) {
             val randomNum = Randoms.pickNumberInRange(START_INCLUSIVE, END_INCLUSIVE)
-            answer.add(randomNum)
+            if (!answer.contains(randomNum)) {
+                answer.add(randomNum)
+            }
         }
         this.answer = answer.toList()
     }
