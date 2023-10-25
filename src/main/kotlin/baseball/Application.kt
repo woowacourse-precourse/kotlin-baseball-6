7e6f68821a18f6ab4a60ac4453f29188fd5759e7
ye <ye6194@naver.com>
2023-10-25 18:01:16 +0900

    ill 전 단계

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,8 +1,9 @@
 package baseball
 
 import camp.nextstep.edu.missionutils.Randoms
+import kotlin.system.exitProcess
 
-fun main(args: Array<String>) {
+fun main() {
     // TODO 제일 큰 while문 만들고 그 안에 중첩 while문에 println("숫자야구시작") 다음 단계부터 넣기
 
     loop@ while (true) {
@@ -11,22 +12,20 @@ fun main(args: Array<String>) {
 
         for (i: Int in 0..2) {  // 정답 배열 저장
             answerArray[i] = Randoms.pickNumberInRange(1, 9)
-            print(answerArray[i])  // 맞게 저장됐는지 출력해보기
+            //print(answerArray[i])  // 맞게 저장됐는지 출력해보기
         }
-        println()
+        //println()
 
         println("숫자 야구를 시작합니다.")  // 시작
 
-        while (true) {
+        loop1@while (true) {
             print("숫자를 입력해주세요 : ")
             val myAnswer = readLine()
-            //println("입력한 숫자 : " + myAnswer)
 
             if (myAnswer.isNullOrBlank()) {  // isNullOrEmpty()랑 다름
-                continue
+                break
             } else if (myAnswer.length > 3 || myAnswer.length <= 2) {
-                println("3자리 숫자를 입력해주세요.")
-                continue
+                break
             } else {  // 입력받은 숫자(문자열) 나눠서 int로 변환하고 myAnswerArray에 저장
                 val myAnswer1 = myAnswer?.substring(0 until 1)
                 myAnswerArray[0] = myAnswer1!!.toInt()
@@ -57,8 +56,10 @@ fun main(args: Array<String>) {
 
             if (ball != 0)
                 print("${ball}볼 ")
-            if (strike !=0)
-                print("${strike}스크라이크")
+            if (strike != 0)
+                print("${strike}스트라이크")
+            if (ball == 0 && strike == 0)
+                print("낫싱")
             println()
 
             if (strike == 3) {  //3스트라이크
@@ -68,22 +69,24 @@ fun main(args: Array<String>) {
                     게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.
                 """.trimIndent()
                 )
-                var number = readLine()!!.toInt()
 
-                if (number == 1)
-                    continue@loop
-                else if (number == 2)
-                    break@loop
-                else {
-                    while (number != 1 && number != 2) {
-                        println("1 또는 2를 입력해주세요.")
-                        number = readLine()!!.toInt()
-                        continue
+                try {
+                    var number = readLine()!!.toInt()
+
+                    if (number == 1)
+                        continue@loop
+                    else if (number == 2)
+                        break@loop
+                    else {
+                        while (number != 1 && number != 2) {
+                            println("1 또는 2를 입력해주세요.")
+                            number = readLine()!!.toInt()
+                            continue
+                        }
                     }
+                } catch (e: NumberFormatException) {
+                    break@loop1
                 }
-            }
-            else {
-                continue
             }
         }
     }
