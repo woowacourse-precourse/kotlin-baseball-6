efe36ca5e3ff7d92466de490f0157ef639b637c3
mcdmaisd <tkim0156@gmail.com>
2023-10-25 22:41:11 +0900

    feat : 게임 상태 메시지 출력 기능 구현

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -8,22 +8,57 @@ fun main() {
     makeRandomNumbers()
 }
 
-fun getUserInput(randomNumbers: MutableList<Int>){
+fun getUserInput(randomNumbers: MutableList<Int>) {
     val userInput = readLine().map { it.digitToInt() }.toMutableList()
     val verifiedInput = validateInput(userInput)
     if (verifiedInput) {
-        
+        val gameResult = validateStrikeAndBall(randomNumbers, userInput)
+        printGameResult(gameResult)
     } else {
         throw IllegalArgumentException("입력값이 유효하지 않습니다. 게임을 종료합니다.")
     }
 }
 
+fun printGameResult(gameResult: MutableList<Int>) {
+    if (gameResult[0] != 0 && gameResult[1] == 0) {
+        println("${gameResult[0]}볼")
+    } else if (gameResult[0] == 0 && gameResult[1] != 0) {
+        println("${gameResult[1]}스트라이크")
+    } else if (gameResult[0] != 0 && gameResult[1] != 0) {
+        println("${gameResult[0]}볼 ${gameResult[1]}스트라이크")
+    } else {
+        println("낫싱")
+    }
+    if (gameResult[1] == 3) {
+        println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n 게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+    }
+}
+
+fun validateStrikeAndBall(randomNumbers: MutableList<Int>, userInput: MutableList<Int>): MutableList<Int> {
+    var strike = 0
+    var ball = 0
+    val result = mutableListOf<Int>()
+
+    for (i in 0..2) {
+        for (j in 0..2) {
+            if (randomNumbers[i] == userInput[j]) {
+                if (i == j) strike += 1
+                else ball += 1
+            }
+        }
+    }
+    println("${ball}, ${strike}")
+    result.add(ball)
+    result.add(strike)
+    return result
+}
+
 fun validateInput(userInput: MutableList<Int>): Boolean {
     val checkedUserInput = userInput.distinct()
-    if (userInput.size != checkedUserInput.size || userInput.size != 3) {
-        return false
+    return if (userInput.size != checkedUserInput.size || userInput.size != 3) {
+        false
     } else {
-        return true
+        true
     }
 }
 
@@ -35,6 +70,7 @@ fun makeRandomNumbers() {
             randomNumbers.add(randomNumber)
         }
     }
+    println(randomNumbers)
     getUserInput(randomNumbers)
 }
 
