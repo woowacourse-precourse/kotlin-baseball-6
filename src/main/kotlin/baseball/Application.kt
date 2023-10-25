bb656027c6f27b25cea5baa838a6a391945b8184
HeewonP825 <7hwon@naver.com>
2023-10-25 20:44:43 +0900

    Feat(Application.kt): add main()

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,22 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
-    TODO("프로그램 구현")
+    var playAgain = true
+
+    println("숫자 야구 게임을 시작합니다.")
+
+    while (playAgain) {
+        playGame()
+        print("게임을 새로 시작하려면 1, 종료하려면 2을 입력하세요: ")
+        val input = Console.readLine()
+        if (input == "2") {
+            playAgain = false
+        }
+    }
+
+    println("게임 종료!")
 }
+
+fun playGame() {
