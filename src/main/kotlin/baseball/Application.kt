d27026dbd171ea2b67565a3536413a12bcb370c4
blueme0 <ksytpdus0304@gmail.com>
2023-10-25 21:29:50 +0900

    [Feat] 볼/스트라이크 판단

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -7,21 +7,23 @@ fun main() {
     println("숫자 야구 게임을 시작합니다.")
 
     while (true) {
-        print("숫자를 입력해주세요 : ")
-        val userNum = Console.readLine()
-        val userArr = mutableListOf<Int>()
-
-        if (userNum?.length != 3) throw IllegalArgumentException()
-        for (i in userNum) {
-            if (!i.isDigit() || userArr.contains(i.digitToInt())) throw IllegalArgumentException()
-            userArr.add(i.digitToInt())
-        }
-
         val comArr = getComputerNum()
+        while (true) {
+            print("숫자를 입력해주세요 : ")
+            val userNum = Console.readLine()
+            val userArr = mutableListOf<Int>()
 
+            if (userNum?.length != 3) throw IllegalArgumentException()
+            for (i in userNum) {
+                if (!i.isDigit() || userArr.contains(i.digitToInt())) throw IllegalArgumentException()
+                userArr.add(i.digitToInt())
+            }
 
-
-
+            val result = playGame(userArr, comArr)
+            println(result)
+            if (result == "3스트라이크") break
+        }
+        println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
         println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
         val again = Console.readLine()
         if (again == "1") continue
@@ -36,7 +38,25 @@ fun getComputerNum(): MutableList<Int> {
     val com = mutableListOf<Int>()
     while (com.size < 3) {
         val rand = Randoms.pickNumberInRange(1, 9)
-        if (com.contains(rand)) com.add(rand)
+        if (!com.contains(rand)) com.add(rand)
+        println(com)
     }
     return com
+}
+
+fun playGame(user: List<Int>, com: List<Int>): String {
+    var ball = 0
+    var strike = 0
+    for (i in 0..2) {
+        if (com.contains(user[i])) {
+            if (com[i] == user[i]) strike++
+            else ball++
+        }
+    }
+    var result = ""
+    if (ball > 0) result += "${ball}볼"
+    if (ball > 0 && strike > 0) result += " "
+    if (strike > 0) result += "${strike}스트라이크"
+    if (ball == 0 && strike == 0) result = "낫싱"
+    return result
 }
