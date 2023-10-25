855eca23144600a6c3201883c1047edcd6b5f9f7
oh in seong <superohinsung@naver.com>
2023-10-25 16:37:15 +0900

    [feat] 랜덤 값 예외 처리

--- src/main/kotlin/baseball/Computer.kt
+++ src/main/kotlin/baseball/Computer.kt
@@ -7,6 +7,14 @@ class Computer {
 
     init {
         this.randomNumbers = Randoms.pickUniqueNumbersInRange(1, 9, 3)
+        // 입력 값이 3자리가 아닐 경우 체크
         require(randomNumbers.size == 3)
+
+        // 입력 값 내에 중복 숫자가 있는 체크
+        val hashSet = hashSetOf<Int>()
+        randomNumbers.forEach { randomNumber ->
+            require(!hashSet.contains(randomNumber))
+            hashSet.add(randomNumber)
+        }
     }
 }
