0ffa4517b13b3285f12f68f54d2a86fc1c11a03e
oh in seong <superohinsung@naver.com>
2023-10-25 16:02:58 +0900

    [fix] 예외검증과 randomNubers private 삭제

--- src/main/kotlin/baseball/Computer.kt
+++ src/main/kotlin/baseball/Computer.kt
@@ -3,9 +3,10 @@ package baseball
 import camp.nextstep.edu.missionutils.Randoms
 
 class Computer {
-    private val randomNumbers: List<Int>
+    val randomNumbers: List<Int>
 
     init {
         this.randomNumbers = Randoms.pickUniqueNumbersInRange(1, 9, 3)
+        require(randomNumbers.size == 3)
     }
 }
