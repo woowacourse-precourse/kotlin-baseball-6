3eec85236b53f8076403a12b51254c52934461d2
yein <inwon9892@naver.com>
2023-10-25 16:24:52 +0900

    [feat] calculating and printing strike, balls

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -5,15 +5,14 @@ import camp.nextstep.edu.missionutils.Randoms
 
 fun main() {
     // TODO("프로그램 구현")
-    println("숫자 야구 게임을 시작합니다.")
-    val ComNumber = ComRandomNum()
+
 }
 
 // 컴퓨터가 세자리 수의 랜덤값 생성
 fun ComRandomNum(): MutableList<Char> {
-    val computerNum = mutableListOf<Char>()
+    var computerNum = mutableListOf<Char>()
     while (computerNum.size < 3) {
-        val randomNumber = Randoms.pickNumberInRange(1, 9).toChar()
+        val randomNumber = Randoms.pickNumberInRange(1, 9).toString()
         if (!computerNum.contains(randomNumber)) {
             computerNum.add(randomNumber)
         }
@@ -27,14 +26,40 @@ fun CheckNum(numbers: MutableList<Char>){
 
     for (i in numbers.indices){
         val number = Integer.parseInt(numbers[i].toString())
-        if(number < 1 || number > 9) throw IllegalArgumentException("1과 9부터의 수가 아닙니다.")
+        if( number < 1 || number > 9) throw IllegalArgumentException("1과 9부터의 수가 아닙니다.")
         if (numbers.count { it == numbers[i] } > 1) throw IllegalArgumentException("세 개의 숫자가 서로 달라야 합니다")
     }
 }
 
 // 사용자로부터 입력값 받기
-fun inputNum(): MutableList<Char> {
+fun InputNum(): MutableList<Char> {
     val input = Console.readLine()
     CheckNum(input.toMutableList())
     return input.toMutableList()
 }
+
+// 스트라이크와 볼 개수 계산
+fun GameNum(userNum: MutableList<Char>, randNum: MutableList<Char>): MutableList<Int> {
+    var ball = 0
+    var strike = 0
+
+    for (i in userNum.indices){
+        if(userNum[i] == randNum[i]){
+            strike += 1
+        } else if (randNum.contains(userNum[i])){
+            ball += 1
+        }
+    }
+    return mutableListOf(ball, strike)
+}
+
+// 스트라이크 볼 개수 출력
+fun GamePrint(gameNum: MutableList<Int>){
+    var ball = gameNum[0]
+    var strike = gameNum[1]
+    if (ball != 0) print("${ball}볼")
+    if (strike != 0) print("${strike}스트라이크")
+    if (ball == 0 && strike == 0) print("낫싱")
+    println()
+}
+
