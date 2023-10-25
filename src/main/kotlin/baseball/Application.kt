c3025f3830801a11d85be8e7171dd1a86f2b7a82
Namyunsuk <yunsuk0328@naver.com>
2023-10-25 18:42:03 +0900

    feat(Application): 세 자리 랜덤 수 생성기능 구현

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,20 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
+
+fun createRandomNumbers(): List<Int>{
+    var randomNumbers = mutableListOf<Int>()
+
+    while(randomNumbers.size < 3){
+        val number = Randoms.pickNumberInRange(1,9)
+        if(!randomNumbers.contains(number)){
+            randomNumbers.add(number)
+        }
+    }
+    return randomNumbers
+}
+
 fun main() {
     println("숫자 야구 게임을 시작합니다.")
 
