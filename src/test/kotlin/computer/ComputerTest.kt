c916d731e0043f81334986cb89f9a0f41acd1fa7
lee989898 <wnsrud1352@gmail.com>
2023-10-25 10:25:52 +0900

    [test] ComputerTest 랜덤 야구 숫자 길이가 3인지 테스트 구현

--- src/test/kotlin/computer/ComputerTest.kt
+++ src/test/kotlin/computer/ComputerTest.kt
@@ -1,6 +1,8 @@
 package computer
 
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
 import randombaseballnumbergenerator.RandomBaseballNumberGenerator
 import randombaseballnumbergenerator.RandomBaseballNumberGeneratorImpl
 
@@ -12,5 +14,17 @@ class ComputerTest {
     fun setUp() {
         randomBaseballNumberGenerator = RandomBaseballNumberGeneratorImpl
         computer = ComputerImpl(randomBaseballNumberGenerator)
+    }
+
+    @Test
+    fun `랜덤 야구 숫자 길이가 3인지`() {
+        computer.setRandomBaseballNumber()
+        val result = computer.randomBaseballNumber.length
+
+        assertEquals(result, BASEBALL_NUMBER_SIZE)
+    }
+
+    companion object {
+        const val BASEBALL_NUMBER_SIZE = 3
     }
 }
