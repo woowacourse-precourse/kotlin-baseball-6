e0ae07fdb93e5a5e7741c8cb7cb0b72d2880a22f
lee989898 <wnsrud1352@gmail.com>
2023-10-25 12:42:42 +0900

    [feat] Application 야구 게임 구동 구현

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,42 @@
 package baseball
 
+import Controller
+import baseballnumbercomparator.BaseballNumberComparator
+import baseballnumbercomparator.BaseballNumberComparatorImpl
+import computer.Computer
+import computer.ComputerImpl
+import player.Player
+import player.PlayerImpl
+import randombaseballnumbergenerator.RandomBaseballNumberGenerator
+import randombaseballnumbergenerator.RandomBaseballNumberGeneratorImpl
+
 fun main() {
-    TODO("프로그램 구현")
+    val baseballNumberComparator: BaseballNumberComparator = BaseballNumberComparatorImpl
+    val randomBaseballNumberGenerator: RandomBaseballNumberGenerator = RandomBaseballNumberGeneratorImpl
+    val computer: Computer = ComputerImpl(randomBaseballNumberGenerator)
+    val player: Player = PlayerImpl()
+    val controller = Controller(
+        baseballNumberComparator,
+        computer,
+        player
+    )
+
+    controller.beginOfBaseballGame()
+    controller.setComputerNumber()
+
+    while (true) {
+        controller.inputBaseballNumber()
+        val baseballResult = controller.compareAnswerAndBaseballNumber()
+
+        if (baseballResult.strike == Controller.THREE_STRIKE) {
+            controller.showIntermediateResult(baseballResult)
+            controller.threeStrike()
+            if (controller.inputRestartOrTerminate() == Controller.TERMINATE) {
+                break
+            }
+            controller.setComputerNumber()
+        } else {
+            controller.showIntermediateResult(baseballResult)
+        }
+    }
 }
