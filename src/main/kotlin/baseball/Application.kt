57f07869375688a0ef5775505d533eaefe9d2eb7
mainfn <_@mainfn.dev>
2023-10-25 13:30:08 +0900

    refact: modify for DI

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -2,6 +2,8 @@ package baseball
 
 import baseball.controller.GameController
 import baseball.domain.AnswerCreator
+import baseball.domain.Computer
+import baseball.util.RandomNumberPicker
 import baseball.view.InputValidator
 import baseball.view.InputView
 import baseball.view.OutputView
@@ -10,8 +12,10 @@ fun main() {
     val inputValidator = InputValidator()
     val inputView = InputView(inputValidator)
     val outputView = OutputView()
-    val answerCreator = AnswerCreator()
-    val gameController = GameController(inputView, outputView, answerCreator)
+    val randomNumberPicker = RandomNumberPicker()
+    val answerCreator = AnswerCreator(randomNumberPicker)
+    val computer = Computer(answerCreator)
+    val gameController = GameController(inputView, outputView, computer)
 
     gameController.run()
 }
--- src/main/kotlin/baseball/controller/GameController.kt
+++ src/main/kotlin/baseball/controller/GameController.kt
@@ -1,20 +1,23 @@
 package baseball.controller
 
-import baseball.domain.*
+import baseball.domain.BallNumbers
+import baseball.domain.Computer
+import baseball.domain.GameStatus
+import baseball.domain.Guess
 import baseball.view.InputView
 import baseball.view.OutputView
 
 class GameController(
     private val inputView: InputView,
     private val outputView: OutputView,
-    private val answerCreator: AnswerCreator,
+    private val computer: Computer,
 ) {
     fun run() {
         // 1. 게임 생성
-        val answer = answerCreator.create()
+        computer.createAnswer()
 
-        // 2. 게임 실행(정답 맞출 때까지)
-        guessRecursive(answer)
+        // 2. 정답 맞출 때까지 사용자의 추측 입력 및 비교
+        askUntilCorrectGuess(computer)
 
         // 3.재실행 여부 입력 및 재실행 or 종료 결정
         if (GameStatus.REPLAY == inputView.inputNextGameStatus()) {
@@ -22,18 +25,18 @@ class GameController(
         }
     }
 
-    private fun guessRecursive(answer: Answer) {
+    private fun askUntilCorrectGuess(computer: Computer) {
         // 사용자가 추측한 숫자 입력
-        val ballNumbers = Guess(inputView.inputBallNumbers())
+        val guess = Guess(inputView.inputBallNumbers())
         // 정답과 비교 후 결과 반환
-        val result = answer.compareTo(ballNumbers)
+        val result = computer.checkGuess(guess)
 
         // 결과 출력
         outputView.printResult(result)
 
         // 3스트라이크가 아닌 경우 재귀 호출
         if (result.strikeCount != BallNumbers.BALL_COUNT) {
-            guessRecursive(answer)
+            askUntilCorrectGuess(computer)
         }
     }
 
