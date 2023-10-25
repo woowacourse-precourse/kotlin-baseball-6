eaaa35d4a09ed0716b17649e341c2e681f33ac72
유성(yuseong) <62369592+yuseong01@users.noreply.github.com>
2023-10-25 22:16:12 +0900

    [ADD] 컴퓨터가 서로 다른 숫자 3개 선택

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,21 @@
 package baseball
+import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
 
 fun main() {
-    TODO("프로그램 구현")
+    println("숫자 야구 게임을 시작합니다.")
+    while (true) {
+        val computer = generateRandomNumber()
+        var attempts = 0
+}
+    
+fun generateRandomNumber(): List<Int> {
+    val computer = mutableListOf<Int>()
+    while (computer.size < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (!computer.contains(randomNumber)) {
+            computer.add(randomNumber)
+        }
+    }
+    return computer
 }
