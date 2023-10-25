b84cdbeff4c5db4cb2d193b20be54fd63e066853
oh in seong <superohinsung@naver.com>
2023-10-25 15:53:59 +0900

    [fix] GamePrint에서 안내문을 부정확하게 프린트하는 문제점 개선

--- src/main/kotlin/baseball/GamePrint.kt
+++ src/main/kotlin/baseball/GamePrint.kt
@@ -1,13 +1,13 @@
 package baseball
 
 class GamePrint {
-    fun startGamePrint() = print("숫자 야구 게임을 시작합니다.\n")
+    fun startGamePrint() = println("숫자 야구 게임을 시작합니다.")
 
-    fun endGamePrint() = print("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n")
+    fun endGamePrint() = println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
 
     fun inputUserPrint() = print("숫자를 입력해주세요 : ")
 
-    fun gameOptionPrint() = print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n")
+    fun gameOptionPrint() = println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
 
     fun resultPrint(result: Result) = println(result.toMessage())
 
