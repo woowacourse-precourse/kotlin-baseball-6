a79ef029de3ad2b11dcdb2d5c5ca14b6c0799105
lee989898 <wnsrud1352@gmail.com>
2023-10-25 10:51:19 +0900

    [test] BaseballNumberComparatorTest 세 자리수 모두 자리, 숫자가 같을 떄 3스트라이크가 나오는지 확인 테스트 구현

--- src/test/kotlin/baseballnumbercomparator/BaseballNumberComparatorTest.kt
+++ src/test/kotlin/baseballnumbercomparator/BaseballNumberComparatorTest.kt
@@ -1,6 +1,8 @@
 package baseballnumbercomparator
 
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
 
 class BaseballNumberComparatorTest {
     private lateinit var baseballNumberComparator: BaseballNumberComparator
@@ -8,5 +10,12 @@ class BaseballNumberComparatorTest {
     @BeforeEach
     fun setUp() {
         baseballNumberComparator = BaseballNumberComparatorImpl()
+    }
+
+    @Test
+    fun `세 자리수 모두 자리, 숫자가 같을 떄 3스트라이크가 나오는지 확인`() {
+        val result = baseballNumberComparator.compareAnswerWithPlayerBaseballNumber("123", "123")
+
+        assertEquals(result, BaseballResult(strike = 3, ball = 0))
     }
 }
