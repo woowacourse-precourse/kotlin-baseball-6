274bd20820a2bd0cc4231ddcc10cea4853be6654
민감자 <mouse0429@naver.com>
2023-10-25 20:53:49 +0900

    feat: 숫자 입력 기능

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -2,4 +2,12 @@ package baseball
 
 fun main() {
     showGameInfo(InfoMsgType.StartGame)
+
+    var isProgramEnded = false
+    while(!isProgramEnded) {
+        val answer : Array<Char> = createGameAnswer()
+        val numberInput : Array<Char> = requestNumberInput()
+
+        isProgramEnded = true
+    }
 }
