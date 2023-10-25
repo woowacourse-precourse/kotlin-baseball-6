8512c49bfa91c2bea585b2eba5756304e184130c
이아름 <souk0712@naver.com>
2023-10-25 16:30:17 +0900

    함수 분리 및 예외 처리

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -7,52 +7,82 @@ fun main() {
     var isPlaying = true
     while (isPlaying) {
         val computer = initComputerNumber()
-        var isPlayRound = true
-        while (isPlayRound) {
-            print("숫자를 입력해주세요 : ")
-
-            val player = mutableListOf<Int>()
-            val input = readlnOrNull()!!
-            if (input.length > 3) {
-                throw IllegalArgumentException()
-            }
-
-            for (c in input) {
-                player.add(c.digitToInt())
-            }
-
+        var isRound = true
+        while (isRound) {
+            val player = inputNumber()
             val result = checkResult(computer, player)
 
-            if (result[1] > 0) {
-                print("${result[1]}볼 ")
-            }
-
-            if (result[0] > 0) {
-                print("${result[0]}스트라이크")
-                if (result[0] == 3) {
-                    print("\n3개의 숫자를 모두 맞히셨습니다! 게임 종료")
-                    isPlayRound = false
-                }
-            }
-
-            if (result[0] == 0 && result[1] == 0) {
-                print("낫싱")
-            }
-
-            println()
+            isRound = printResult(result)
         }
 
-        println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+        isPlaying = inputRestart()
+    }
+}
 
-        val restart = readlnOrNull()!!.toInt()
-        if (restart == 2) {
-            isPlaying = false
-        }else if (restart == 1){
-            continue
-        }else{
+fun inputRestart(): Boolean {
+    println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+
+    val restart = readlnOrNull()!!.toInt()
+    return when (restart) {
+        2 -> {
+            false
+        }
+
+        1 -> {
+            true
+        }
+
+        else -> {
             throw IllegalArgumentException()
         }
     }
+}
+
+fun printResult(result: IntArray): Boolean {
+    if (result[1] > 0) {
+        print("${result[1]}볼 ")
+    }
+
+    if (result[0] > 0) {
+        print("${result[0]}스트라이크")
+        if (result[0] == 3) {
+            println("\n3개의 숫자를 모두 맞히셨습니다! 게임 종료")
+            return false
+        }
+    }
+
+    if (result[0] == 0 && result[1] == 0) {
+        print("낫싱")
+    }
+
+    println()
+
+    return true
+}
+
+fun inputNumber(): MutableList<Int> {
+    print("숫자를 입력해주세요 : ")
+
+    val player = mutableListOf<Int>()
+    val input = readlnOrNull()!!
+    if (input.length != 3) {
+        throw IllegalArgumentException()
+    }
+
+    if (Regex("[1-9]+").matches(input)) {
+        for (c in input) {
+            val i = c.digitToInt()
+            if (player.contains(i)) {
+                throw IllegalArgumentException()
+            } else {
+                player.add(c.digitToInt())
+            }
+        }
+    } else {
+        throw IllegalArgumentException()
+    }
+
+    return player
 }
 
 fun checkResult(computer: MutableList<Int>, player: MutableList<Int>): IntArray {
@@ -80,6 +110,5 @@ fun initComputerNumber(): MutableList<Int> {
         }
     }
 
-//    println(computer.joinToString(" "))
     return computer
 }
