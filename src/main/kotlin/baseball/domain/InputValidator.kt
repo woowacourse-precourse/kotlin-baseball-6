a2d3e27738e487b200d1acb7575d3e88aee358ff
m6z1 <sonmyungj1zz@gmail.com>
2023-10-25 18:56:16 +0900

    feat : 플레이어의 입력값에 따른 예외처리(0이 포함된 경우)

--- src/main/kotlin/baseball/domain/InputValidator.kt
+++ src/main/kotlin/baseball/domain/InputValidator.kt
@@ -2,7 +2,7 @@ package baseball.domain
 
 class InputValidator {
     fun validate(input: String): Int {
-        if (input.length != 3 || !input.all { it.isDigit() } || input.toSet().size != input.length) {
+        if (input.length != 3 || !input.all { it.isDigit() } || input.toSet().size != input.length || input.contains('0')) {
             throw IllegalArgumentException()
         }
         return input.toInt()
