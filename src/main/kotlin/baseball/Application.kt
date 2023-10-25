916c025db091b223d0d9833bb61ea7a0cc353de2
leeugun123 <leeugun123@naver.com>
2023-10-25 15:46:34 +0900

    숫자야구 게임 기능 구현 완료
    1.랜덤 숫자 생성
    
    2. 사용자 입력 받기 및 조건 확인
    -> 다시 시작 or 게임 종료
    
    3. 입력 값 유효 값 검사 및 아웃 판정
    
    4. 스트라이크와 볼 판정 메소드 구현

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,102 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+import camp.nextstep.edu.missionutils.Console
+
+
 fun main() {
-    TODO("프로그램 구현")
+
+    var playAgain = true
+
+    while (playAgain) {
+
+        println("숫자 야구 게임을 시작합니다.")
+
+        val answer = generateRandomNumber()
+        // println(answer)
+        var isGameWon = false
+
+        while (!isGameWon) {
+
+            print("숫자를 입력해주세요 : ")
+            val guess = Console.readLine()
+
+            //guess.all { it.isDigit() } -> 사용자가 입력한 문자가 모두 숫자인지를 확인하는 코틀린의 내장 함수
+
+            if (guess != null && guess.length == 3 && guess.all { it.isDigit() }) {
+
+                val result = getResult(guess, answer)
+                println(result)
+
+                if (result == "3스트라이크") {
+
+                    isGameWon = true
+                    println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
+
+                }
+
+            } else {
+
+                throw IllegalArgumentException("")
+
+            }//예외처리
+
+
+
+        }
+
+        print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요: ")
+        val choice = Console.readLine()
+        playAgain = choice == "1"
+
+
+    }
+
+
 }
+
+
+fun generateRandomNumber(): String {
+
+    val number1 = Randoms.pickNumberInRange(0,9).toString()
+    var number2: String
+    var number3: String
+
+    do {
+        number2 = Randoms.pickNumberInRange(0, 9).toString()
+    } while (number2 == number1)
+
+    do {
+        number3 = Randoms.pickNumberInRange(0, 9).toString()
+    } while (number3 == number1 || number3 == number2)
+
+    return "$number1$number2$number3"
+
+
+} //서로 다른 3자리수 구현
+
+fun getResult(guess: String, answer: String): String {
+
+    var strikes = 0
+    var balls = 0
+
+    for (i in guess.indices) {
+
+        if (guess[i] == answer[i]) {
+            strikes++
+        } else if (guess[i] in answer) {
+            balls++;
+        }
+
+    }
+
+    val strikesText = if (strikes > 0)  "$strikes"+"스트라이크" else ""
+    val ballsText = if (balls > 0) "$balls"+"볼 " else ""
+    return if (strikes == 0 && balls == 0) "낫싱" else "$ballsText$strikesText"
+
+
+}
+
+
+
+
