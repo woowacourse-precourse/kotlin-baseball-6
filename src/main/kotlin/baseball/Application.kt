9c3106d075348b4634ec18b5d3690b49742fe7dc
eunsxx <88652027+eunsxx@users.noreply.github.com>
2023-10-25 11:39:28 +0900

    feat(input) : User Input and Computer Random Number generate

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,24 @@
 package baseball
-
+import camp.nextstep.edu.missionutils.Randoms.pickNumberInRange
 fun main() {
-    TODO("프로그램 구현")
+    println("숫자 야구 게임을 시작합니다.")
+    print("숫자를 입력해주세요 : ")
+    var number:Int = readLine()!!.toInt()
+    println("당신이 입력한 숫자는 ${number}입니다.")
+
+    var com1:String
+    var com2:String
+    var com3:String
+
+    com1 = pickNumberInRange(0,9)!!.toString()
+    com2 = pickNumberInRange(0,9)!!.toString()
+    com3 = pickNumberInRange(0,9)!!.toString()
+    while(com2 == com1) com2 = pickNumberInRange(0,9)!!.toString();
+    while(com3 == com1 || com3 == com2) com3 = pickNumberInRange(0,9)!!.toString();
+
+    var comNumber = com1 + com2 + com3;
+    println("컴퓨터의 숫자는 ${comNumber}입니다.")
+
 }
+
+
