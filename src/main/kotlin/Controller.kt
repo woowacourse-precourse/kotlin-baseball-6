9371ee1f8dc538bc3d2527a0d68ac1291433b685
lee989898 <wnsrud1352@gmail.com>
2023-10-25 12:40:18 +0900

    [feat] Controller 야구 숫자 입력 메서드 구현

--- src/main/kotlin/Controller.kt
+++ src/main/kotlin/Controller.kt
@@ -1,8 +1,12 @@
 import computer.Computer
+import player.Player
+import validator.baseballnumbervalidator.BaseballNumberValidatorImpl
+import view.InputView
 import view.OutputView
 
 class Controller(
     private val computer: Computer,
+    private val player: Player,
 ) {
     fun beginOfBaseballGame() {
         OutputView.startBaseballGame()
@@ -10,6 +14,12 @@ class Controller(
 
     fun setComputerNumber() {
         computer.setRandomBaseballNumber()
+    }
+
+    fun inputBaseballNumber() {
+        OutputView.pleaseInputBaseballNumber()
+        val responseRandomBaseballNumber = InputView.askBaseballNumber(BaseballNumberValidatorImpl)
+        player.setBaseballNumber(responseRandomBaseballNumber)
     }
 }
 
