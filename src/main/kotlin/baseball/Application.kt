e3a31ff11241c658f206673bc7440606e6d8c476
이홍진\iit80 <hong80714@gmail.com>
2023-10-25 20:46:50 +0900

    [fix] 게임 재시작 버그 수정 게임 재시작시 getTargetNumber()의 targetNum가 초기화가 되지 않아 while()문이 돌지 않게되는 버그 발생, gradlew clean test에서 오류가 발생
    getTargetNumer()에 targetNum.clear()로 재시작시 targetNum을 초기화
    
    gradlew clean test BUILD SUCCESSFUL

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -43,7 +43,10 @@ fun gameStart(){
 // pickNumberInRange() 함수를 이용해 1~9까지 숫자를 생성
 // 랜덤 생성된 숫자는 Int, 입력숫자는 string
 // 비교를 위해서 랜덤 숫자를 string으로 바꿈
+
+// 게임 재시작시 targetNum가 초기화가 안되어 targetNum.size == 3이기 때문에 코드가 돌아가지 않는 버그
 fun getTargetNumber(targetNum: MutableSet<String>){
+    targetNum.clear()
     while (targetNum.size < 3) {
         val number = pickNumberInRange(1, 9)
         if (number.toString() !in targetNum) {
