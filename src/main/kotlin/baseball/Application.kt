ba3e81a8830b64acc3d9aaea382b25dee71e80ab
blueme0 <ksytpdus0304@gmail.com>
2023-10-25 21:09:27 +0900

    [Feat] 기본 입력 및 변수 설정

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,27 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
-    TODO("프로그램 구현")
+    println("숫자 야구 게임을 시작합니다.")
+
+    while (true) {
+        print("숫자를 입력해주세요 : ")
+        val userNum = Console.readLine()
+        val userArr = mutableListOf<Int>()
+
+
+        println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+        val again = Console.readLine()
+        if (again == "1") continue
+        else if (again == "2") break
+//        else
+    }
+    Console.close()
+
+}
+
+fun getComputerNum(): MutableList<Int> {
+    val computer = mutableListOf<Int>()
+
 }
