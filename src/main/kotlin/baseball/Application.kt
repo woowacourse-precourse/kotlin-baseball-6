4e462763031edf5f9a916620a2b583b71ee6046c
pengcon <vpdzjs@gmail.com>
2023-10-25 22:01:39 +0900

    fix: camp.nextstep.edu.missionutils.Console의 readLine()을 사용하도록 수정 camp.nextstep.edu.missionutils.Console의 readLine()을 사용하도록 수정

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,6 +1,7 @@
 package baseball
 
 import camp.nextstep.edu.missionutils.Randoms
+import camp.nextstep.edu.missionutils.Console
 
 fun main() {
     println("숫자 야구 게임을 시작합니다.")
@@ -15,7 +16,7 @@ fun startGame() {
     var ball: Int
     while (true) {
         print("숫자를 입력해주세요 : ")
-        val people = (readLine() ?: "").map { it.toString().toInt() }
+        val people = (Console.readLine() ?: "").map { it.toString().toInt() }
         val set = HashSet<Int>(people)
         if (people.size != 3 || set.size < people.size) throw IllegalArgumentException()
         for (num in people) if (num !in 1..9) throw IllegalArgumentException()
@@ -38,7 +39,7 @@ fun startGame() {
 fun restartGame(): Boolean {
     println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
     println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
-    val restart = readLine()!!.toInt()
+    val restart = Console.readLine()!!.toInt()
     if (restart != 1 && restart != 2) {
         throw IllegalArgumentException()
     }
