9e5d4ae2afe5ac7b5569163c366c6aed7c3c1957
최형우 <guddn106901@gmail.com>
2023-10-25 23:13:44 +0900

    throw exception 추가

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,27 +1,28 @@
 package baseball
+
 import camp.nextstep.edu.missionutils.Console
 import camp.nextstep.edu.missionutils.Randoms
 
 
-
 fun main() {
     println("숫자 야구 게임을 시작합니다.")
-    var restart = true
-    while (restart) {
+    var restart : Boolean = true
+    while(restart) {
         val answer = makeAnswer()
-        while (true) {
-            var guess : String?
-            //입력 check
+        var guess: String?
+        //입력 check
+        var gamegoing = true
+        while(gamegoing) {
             try {
                 print("숫자를 입력해주세요 : ")
                 guess = Console.readLine()
                 checkInput(guess)
-            }
-            catch (e: IllegalArgumentException) {
+            } catch (e: IllegalArgumentException) {
+                throw IllegalArgumentException()
                 return
             }
 
-            val result = gameResult(answer,guess)
+            val result = gameResult(answer, guess)
             println(result)
 
             if (result == "3스트라이크") {
@@ -29,23 +30,17 @@ fun main() {
                 break
             }
         }
-
         println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
-        try {
-            val newgame = readLine()
-            if (newgame != "1" && newgame != "2") {
-                throw IllegalArgumentException("1,2 숫자 이외의 문자 입력.")
-            }
-            if(newgame == "1") restart = true
-            else restart = false
-        }
-        catch (e: IllegalArgumentException) {
-            return
+        val newgame = readLine()
+        when (newgame) {
+            "1" -> { continue}
+            "2" -> restart = false
+            else -> throw IllegalArgumentException()
         }
     }
 }
 
-fun makeAnswer(): MutableList<Int>{
+fun makeAnswer(): MutableList<Int> {
     val computer = mutableListOf<Int>()
     while (computer.size < 3) {
         val randomNumber = Randoms.pickNumberInRange(1, 9)
@@ -80,13 +75,20 @@ fun gameResult(answer: List<Int>, guess: String): String {
     }
 }
 
-fun checkInput(check: String) {
-    if (check.length != 3 || !check.all { it.isDigit() }) {
-        throw IllegalArgumentException("입력 오류.")
-    }
-
-    val uniqueNum = check.toSet()
-    if (uniqueNum.size != 3) {
-        throw IllegalArgumentException("서로 다른 수가 아닙니다.")
+fun checkInput(number: String) {
+    // null, 정수, 입력 자리수 체크
+    if (number.isNotEmpty() && number.chars()
+            .allMatch { Character.isDigit(it) } && number.length == 3
+    ) {
+        // 겹치지 않는 숫자 체크와 1-9 사이 체크
+        for (i in 0..<3) {
+            if (number.count { it == number[i] } > 1 || Character.getNumericValue(number[i]) == 0) {
+                throw IllegalArgumentException()
+            }
+        }
+    } else {
+        throw IllegalArgumentException()
     }
 }
+
+
