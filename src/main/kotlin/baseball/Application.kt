17498f882ec290f798a37eaef1700a573e811838
mcdmaisd <tkim0156@gmail.com>
2023-10-25 20:07:44 +0900

    feat : 난수 생성 기능 구현

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,19 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
-    TODO("프로그램 구현")
+    makeRandomNumbers()
 }
+
+fun makeRandomNumbers() {
+    val randomNumbers = mutableListOf<Int>()
+    while (randomNumbers.size < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (!randomNumbers.contains(randomNumber)) {
+            randomNumbers.add(randomNumber)
+        }
+    }
+}
+
