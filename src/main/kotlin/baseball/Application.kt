8d4145712394ea2118cdfcbbc90b6ae31c6b9bda
Namyunsuk <yunsuk0328@naver.com>
2023-10-25 18:58:38 +0900

    feat(Application): 사용자로부터 입력 값 받기 기능 구현

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -15,7 +15,16 @@ fun createRandomNumbers(): List<Int>{
     return randomNumbers
 }
 
+fun userInputNumbers(): List<Int>{
+    var userInputNumberList = mutableListOf<Int>()
+    val userInput = Console.readLine()
+
+    for(i in 0 until userInput.length){
+        userInputNumberList.add(userInput[i].code - '0'.code)
+    }
+    return userInputNumberList
+}
+
 fun main() {
     println("숫자 야구 게임을 시작합니다.")
-
 }
