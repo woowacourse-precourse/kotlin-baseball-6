948f7f07411c85f46756741bfbb9269c1c2e0233
book3039 <book3039@knou.ac.kr>
2023-10-25 11:18:30 +0900

    refactor: throw 문을 require() 함수로 변경 및 변수 이름 변경

--- src/main/kotlin/baseball/NumberBaseball.kt
+++ src/main/kotlin/baseball/NumberBaseball.kt
@@ -44,34 +44,32 @@ object NumberBaseball {
     private fun getUserInput() {
         print("숫자를 입력해 주세요 : ")
         if (userNumbers.size > 0) userNumbers.clear()
-        val userInput = Console.readLine()
-            .removeWhiteSpaces()
+        val userInput = Console.readLine().removeWhiteSpaces()
         checkValidateInput(userInput)
         userNumbers = userInputToNumbers(userInput)
     }
 
     // 사용자 입력에서 공백 문자를 제거하기 위한 확장 함수
     private fun String.removeWhiteSpaces(): String {
-        return this.replace(" ", "")
-            .replace("\t", "")
+        return this.replace(" ", "").replace("\t", "")
     }
 
     private fun checkValidateInput(input: String) {
-        if (input.length != NUM_LENGTH) throw IllegalArgumentException()
+        require(input.length == NUM_LENGTH) { "세 자리 수를 입력하세요." }
         input.forEach {
-            if (it !in '1'..'9') throw IllegalArgumentException()
+            require(it in '1'..'9') { "올바른 입력이 아닙니다." }
         }
     }
 
     // 사용자의 입력을 정수로 변환해 ArrayList<Int>에 담고 ArrayList<Int>를 반환합니다.
     private fun userInputToNumbers(input: String): ArrayList<Int> {
-        val tempNumList = arrayListOf<Int>()
+        val tempNumbers = arrayListOf<Int>()
         input.forEach {
-            if (tempNumList.contains(it.digitToInt())) throw IllegalArgumentException()
+            require(!tempNumbers.contains(it.digitToInt())) { "중복된 숫자는 입력하실 수 없습니다." }
 
-            tempNumList.add(it.digitToInt())
+            tempNumbers.add(it.digitToInt())
         }
-        return tempNumList
+        return tempNumbers
     }
 
     private fun calculateBallCounts() {
@@ -97,9 +95,7 @@ object NumberBaseball {
     private fun printBallCounts(ball: Int, strike: Int) {
         var call = ""
 
-        if (ball == 0 &&
-            strike == 0
-        ) {
+        if (ball == 0 && strike == 0) {
             call += "낫싱"
             println(call)
             return
@@ -119,10 +115,8 @@ object NumberBaseball {
         println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
         val tempStr = Console.readLine().trim()
 
-        if (tempStr.length != 1) throw IllegalArgumentException()
-        if (tempStr != RESTART_GAME &&
-            tempStr != QUIT_GAME
-        ) throw IllegalArgumentException()
+        require(tempStr.length == 1) { "올바른 입력이 아닙니다." }
+        require(tempStr == RESTART_GAME || tempStr == QUIT_GAME) { "올바른 입력이 아닙니다." }
 
         if (tempStr == QUIT_GAME) isPlaying = false
     }
