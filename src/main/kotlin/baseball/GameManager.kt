a2621da351aba24b1de18446ef75c57a01e3df1e
SeungGu Kang <alex9366@naver.com>
2023-10-25 20:47:48 +0900

    refactor: 코틀린 스타일 가이드 적용 및 if문을 when으로 수정

--- src/main/kotlin/baseball/GameManager.kt
+++ src/main/kotlin/baseball/GameManager.kt
@@ -5,16 +5,18 @@ class GameManager: ResultListener {
     private val player: Player = Player(inputManager)
     private val computer: Computer = Computer(inputManager)
 
-    companion object ExitCode {
-        const val ONE: Int = 1
-        const val TWO: Int = 2
-    }
-
     fun startGame() = Referee(computer.generateValidAnswer(), player.inputNum(), this)
 
     override fun restartGame() {
-        val exitCode: Int = inputManager.input("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n", Digit.ONE)
-        if(exitCode==ONE) { startGame() }
-        else if(exitCode==TWO) { return }
+        val exitCode: Int = inputManager.input("게임을 새로 시작하려면 ${ONE}, 종료하려면 ${TWO}를 입력하세요.\n", Digit.ONE)
+        when(exitCode) {
+            ONE -> startGame()
+            TWO -> return
+        }
+    }
+
+    companion object ExitCode {
+        const val ONE: Int = 1
+        const val TWO: Int = 2
     }
 }
