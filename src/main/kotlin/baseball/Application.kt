6edd103d81a51cae1044edf4099791b5c53ba968
hugesilver <xor3160@naver.com>
2023-10-25 14:50:32 +0900

    Fixed Number format exception issue

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -5,7 +5,7 @@ import camp.nextstep.edu.missionutils.Console
 
 fun main() {
     // 기본적으로 3자리로 설정한다.
-    val baseballLength = 3
+    val BASEBALL_LENGTH = 3
     println("숫자 야구 게임을 시작합니다.")
 
     // 게임을 종료할 때까지 무한 반복
@@ -13,7 +13,7 @@ fun main() {
         val computer = mutableListOf<Int>()
 
         // 배열에 겹치지 않는 숫자들을 추가
-        while (computer.size < baseballLength) {
+        while (computer.size < BASEBALL_LENGTH) {
             val randomNumber = Randoms.pickNumberInRange(1, 9)
             if (!computer.contains(randomNumber)) {
                 computer.add(randomNumber)
@@ -27,10 +27,10 @@ fun main() {
 
             // null, 정수, 입력 자리수 체크
             if (number != null && number.chars()
-                    .allMatch { Character.isDigit(it) } && number.length == baseballLength
+                    .allMatch { Character.isDigit(it) } && number.length == BASEBALL_LENGTH
             ) {
                 // 겹치지 않는 숫자 체크와 1-9 사이 체크
-                for (i in 0..<baseballLength) {
+                for (i in 0..<BASEBALL_LENGTH) {
                     if (number.count { it == number[i] } > 1 || Character.getNumericValue(number[i]) == 0) {
                         throw IllegalArgumentException()
                     }
@@ -40,7 +40,7 @@ fun main() {
                 var ball = 0
 
                 // 자리가 같으면 strike, 포함만 되어있다면 ball에 1을 추가한다
-                for (i in 0 until baseballLength) {
+                for (i in 0 until BASEBALL_LENGTH) {
                     if (computer[i] == Character.getNumericValue(number[i])) {
                         strike++
                     } else if (computer.contains(Character.getNumericValue(number[i]))) {
@@ -63,9 +63,9 @@ fun main() {
                     }
                     println(result)
 
-                    // 만약 strike가 baseballLength 값과 같다면 게임을 종료한다.
-                    if (strike == baseballLength) {
-                        println("${baseballLength}개의 숫자를 모두 맞히셨습니다! 게임 종료")
+                    // 만약 strike가 BASEBALL_LENGTH 값과 같다면 게임을 종료한다.
+                    if (strike == BASEBALL_LENGTH) {
+                        println("${BASEBALL_LENGTH}개의 숫자를 모두 맞히셨습니다! 게임 종료")
                         break
                     } else {
                         continue
@@ -80,20 +80,24 @@ fun main() {
         }
 
         println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
-        val regame = Console.readLine()
-        when (regame) {
-            "1" -> {
-                continue
-            }
+        try {
+            val regame = Console.readLine()?.toInt()
+            when (regame) {
+                1 -> {
+                    continue
+                }
 
-            "2" -> {
-                break
-            }
+                2 -> {
+                    break
+                }
 
-            // 1 혹은 2가 아닐 경우 throw
-            else -> {
-                throw IllegalArgumentException()
+                // 1 혹은 2가 아닐 경우 throw
+                else -> {
+                    throw IllegalArgumentException()
+                }
             }
+        } catch (e: NumberFormatException) {
+            throw IllegalArgumentException()
         }
     }
 
