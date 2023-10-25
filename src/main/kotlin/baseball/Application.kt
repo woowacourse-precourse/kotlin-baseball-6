7a4e372cba333cfd5cfa64c793eed4addbc44c12
Gahee Lim <ka614323@naver.com>
2023-10-25 23:07:40 +0900

    모든 기능 구현 및 기능별로 함수로 분리

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,9 +1,15 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Console
 import camp.nextstep.edu.missionutils.Randoms
 
-fun main() {
-    println("숫자 야구 게임을 시작합니다.")
+fun inputCheck(inputNumber: String) {
+    if (inputNumber.length != 3) {
+        throw IllegalArgumentException ()
+    }
+}
+
+fun randomNum(): MutableList<Int> {
     val computer = mutableListOf<Int>()
     while (computer.size < 3) {
         val randomNumber = Randoms.pickNumberInRange(1, 9)
@@ -11,40 +17,74 @@ fun main() {
             computer.add(randomNumber)
         }
     }
-    println(computer)
-    println(computer[1]) // 랜덤 확인 테스트
+    return computer
+}
 
+fun inputNum(): MutableList<Int> {
+    val user = mutableListOf<Int>()
+    print("숫자를 입력해주세요 : ")
+    val inputNumber = Console.readLine()!!
+    inputCheck(inputNumber)
+    for (i in inputNumber) {
+        user.add(i.digitToInt())
+    }
+    return user
+}
 
-        print("숫자를 입력해주세요 : ")
-        val user = readLine()!!
-        val inputNum = mutableListOf<Int>()
-        for (i in user) {
-            inputNum.add(i.digitToInt())
-        }
-
-        var strike = 0
-        var ball = 0
-        var not = 0
-        for (i in inputNum.indices) {
-            if (inputNum[i] == computer[i]) {
+fun hintResult(computerNum: MutableList<Int>, userNum: MutableList<Int>): Int {
+    var strike = 0
+    var ball = 0
+    var not = 0
+        for (i in userNum.indices) {
+            if (userNum[i] == computerNum[i]) {
                 strike += 1
-            } else if (computer.contains(inputNum[i])) {
+            } else if (computerNum.contains(userNum[i])) {
                 ball += 1
             } else {
                 not += 1
             }
         }
-
         if (strike == 3) {
-            println("스")
+            println("${strike}스트라이크")
+            println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
         } else if (not == 3) {
-            println("낫")
+            println("낫싱")
         } else if (strike == 0 && ball == 0) {
-            println("낫")
+            println("낫싱")
         } else if (strike > 0 && ball == 0) {
-            println("스 $strike")
+            println("${strike}스트라이크")
         } else if (strike == 0 && ball > 0) {
-            println("볼 $ball")
-        } else println("스 $strike, 볼 $ball")
-    }
+            println("${ball}볼")
+        } else println("${ball}볼 ${strike}스트라이크 ")
+    return strike
+}
 
+fun restartEnd(): Int {
+    println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+    val choice = Console.readLine()!!.toInt()
+    return choice
+}
+
+fun playGame() {
+    while (true) {
+        val computerNum = randomNum()
+        while (true) {
+            val userNum = inputNum()
+            val strike = hintResult(computerNum, userNum)
+            if (strike == 3) {
+                break
+            }
+        }
+        val userChoice = restartEnd()
+        if (userChoice == 1) {
+            continue
+        } else {
+            break
+        }
+    }
+}
+
+fun main() {
+    println("숫자 야구 게임을 시작합니다.")
+    playGame()
+}
