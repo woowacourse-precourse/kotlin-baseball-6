ed35ef849672bda8bd7b68f20d2d34fd38aca076
이홍진\iit80 <hong80714@gmail.com>
2023-10-25 19:56:22 +0900

    [feat] 게임 재개 선택 기능 구현 게임이 종료됐을때 사용자의 입력으로 종료, 재시작을 결정할 수 있는 기능
    세부사항
     게임 재개선택 함수
     게임이 종료되었을때 사용자로부터 입력을 받음
     1은 게임 새로시작, 2는 게임 종료
     1, 2를 제외한 모든 입력을 받을시 illegalArgumentException 발생 후 어플 종료
     gameStart()에 isPlaying 값을 반환해 게임 재시작과 종료를 결정

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -8,7 +8,10 @@ fun main() {
 }
 
 fun gameStart(){
-
+    var isPlaying = true
+    while(isPlaying){
+        isPlaying = getReGameChoice()
+    }
 }
 
 // 랜덤 숫자 생성 함수
@@ -64,8 +67,19 @@ fun countBall(inputNum: String, targetNum: MutableSet<String>): String {
     }
 }
 
-fun getReGameChoice(){
+// 게임 재개선택 함수
+// 게임이 종료되었을때 사용자로부터 입력을 받음
+// 1은 게임 새로시작, 2는 게임 종료
+// 1, 2를 제외한 모든 입력을 받을시 illegalArgumentException 발생 후 어플 종료
+// gameStart()에 isPlaying 값을 반환해 게임 재시작과 종료를 결정
 
+fun getReGameChoice(): Boolean{
+    println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+    return when (Console.readLine()) {
+        "1" -> true
+        "2" -> false
+        else -> throw IllegalArgumentException("1 또는 2만 입력가능합니다.")
+    }
 }
 
 
