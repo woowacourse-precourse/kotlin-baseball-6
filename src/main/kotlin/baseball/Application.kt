5282e46bdc7e1e7a6d73933be490d7720ca9a2c7
김진아 <c2010333@naver.com>
2023-10-25 10:46:45 +0900

    [FEAT] 게임 진행 구현

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,6 +1,7 @@
 package baseball
 
-import java.lang.Exception
+import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
 
 fun main() {
     println("숫자 야구 게임에 오신 것을 환영합니다!")
@@ -8,13 +9,14 @@ fun main() {
     while (true) {
         try {
             println("[1] 게임 시작하기 [2] 게임 종료하기")
-            val selectStart = readLine()?.toInt()
+            val selectStart = Console.readLine()?.toInt()
 
             when (selectStart) {
                 1 -> {
                     println("숫자 야구 게임을 시작합니다.")
                     val computer = Computer()
                     computer.createNumbers()
+                    playGame(computer)
                 }
 
                 2 -> {
@@ -38,7 +40,7 @@ class Computer {
     fun createNumbers() {
         for (computerNum in randNums.indices) {
             while (true) {
-                val comRandom = (1..9).random()
+                val comRandom = Randoms.pickNumberInRange(1, 9)
                 if (!randNums.contains(comRandom)) {
                     randNums[computerNum] = comRandom
                     break
@@ -57,15 +59,46 @@ class User {
 
             if (userInput == null) {
                 println("[Error] 입력된 값이 없습니다. 다시 입력해주세요.")
+            }  else if (userInput.length != 3) {
+                println("[Error] 3자리의 수를 입력해주세요.")
             } else if (!userInput.all { it.isDigit() }) {
                 println("[Error] 숫자만 입력해주세요.")
             } else if (userInput.toSet().size != 3) {
                 println("[Error] 숫자가 중복되었습니다.")
-            } else if (userInput.length != 3) {
-                println("[Error] 3자리의 수를 입력해주세요.")
             } else {
-               println("입력하신 숫자는 ${userInput}입니다.")
+                return userInput.map { it.toString().toInt() }.toIntArray()
             }
+        }
+    }
+}
+
+fun playGame(computer: Computer) {
+    val user = User()
+    var strike: Int
+    var ball: Int
+
+    while (true) {
+        val userNums = user.inputNums()
+        strike = 0
+        ball = 0
+
+        for (index in userNums.indices) {
+            if (userNums[index] == computer.randNums[index]) {
+                strike++
+            } else if (computer.randNums.contains(userNums[index])) {
+                ball++
+            }
+        }
+
+        when {
+            strike == 3 -> {
+                println("3개의 숫자를 모두 맞히셨습니다!\n게임 종료")
+                return
+            }
+            strike > 0 && ball == 0 -> println("$strike 스트라이크")
+            strike == 0 && ball > 0 -> println("$ball 볼")
+            strike > 0 && ball > 0 -> println("$strike 스트라이크 $ball 볼")
+            strike == 0 && ball == 0 -> println("낫싱")
         }
     }
 }
