86eb3549eaadd056ea0a91afb37d1caa13e25a33
oh in seong <superohinsung@naver.com>
2023-10-25 17:13:04 +0900

    [fix] 잘못 사용된 코드 수정 및 예외처리 수정

--- src/main/kotlin/baseball/Computer.kt
+++ src/main/kotlin/baseball/Computer.kt
@@ -3,18 +3,29 @@ package baseball
 import camp.nextstep.edu.missionutils.Randoms
 
 class Computer {
-    val randomNumbers: List<Int>
+    companion object {
+        fun createRandomNumbers(): MutableList<Int> {
+            val randomNumbers = mutableListOf<Int>()
+            while (randomNumbers.size < 3) {
+                val randomNumber = Randoms.pickNumberInRange(1, 9)
+                if (!randomNumbers.contains(randomNumber)) {
+                    randomNumbers.add(randomNumber)
+                }
+            }
+            throwsUserGameNumberInputError(randomNumbers)
+            return randomNumbers
+        }
 
-    init {
-        this.randomNumbers = Randoms.pickUniqueNumbersInRange(1, 9, 3)
-        // 입력 값이 3자리가 아닐 경우 체크
-        require(randomNumbers.size == 3)
+        private fun throwsUserGameNumberInputError(randNumbers: List<Int>) {
+            // 랜덤 값이 3자리가 아닐 경우 체크
+            require(randNumbers.size == 3)
 
-        // 입력 값 내에 중복 숫자가 있는 체크
-        val hashSet = hashSetOf<Int>()
-        randomNumbers.forEach { randomNumber ->
-            require(!hashSet.contains(randomNumber))
-            hashSet.add(randomNumber)
+            // 랜덤 값 내에 중복 숫자가 있는 체크
+            val hashSet = hashSetOf<Int>()
+            randNumbers.forEach { i ->
+                require(!hashSet.contains(i))
+                hashSet.add(i)
+            }
         }
     }
 }
