a57f98bf2b512442b8f053c7817f4f466bb0ae22
kang-moonsun <kangms8084@naver.com>
2023-10-25 16:17:06 +0900

    Feat: Add output result messages

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -22,6 +22,14 @@ fun main() {
             else { if(value == user[j]) ball++ }
         }
     }
+
+    if (strike == 3) {
+        println("${strike}스트라이크")
+        println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
+    } else if (strike != 0 && ball != 0) println("${ball}볼 ${strike}스트라이크")
+    else if (ball != 0) println("${ball}볼")
+    else if (strike != 0) println("${strike}스트라이크")
+    else print("낫싱")
 }
 
 private fun setComputerNums(computer: MutableList<Int>) {
