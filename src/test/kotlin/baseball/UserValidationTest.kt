e5c92c8741a73835fac5e138cd290da860cefe69
jun <hky08067@naver.com>
2023-10-25 10:18:39 +0900

    test: 테스트 수정

--- src/test/kotlin/baseball/UserValidationTest.kt
+++ src/test/kotlin/baseball/UserValidationTest.kt
@@ -1,7 +1,7 @@
 package baseball
 
-import baseball.domain.User.userChoiceValidation
-import baseball.domain.User.userNumberValidation
+import baseball.domain.userChoiceValidation
+import baseball.domain.userNumberValidation
 import camp.nextstep.edu.missionutils.test.NsTest
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
