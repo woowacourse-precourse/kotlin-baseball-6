a0819e0702aead51dee96154ffaa85ecb253e6be
pingu244 <pingu244@naver.com>
2023-10-25 15:24:41 +0900

    [feat] 서로 다른 3자리 수가 입력된 경우에 대한 예외 처리

--- src/main/kotlin/baseball/model/UserNumber.kt
+++ src/main/kotlin/baseball/model/UserNumber.kt
@@ -1,5 +1,11 @@
 package baseball.model
 
-data class UserNumber(val number: List<Int>){
-    // todo 예외처리
+data class UserNumber(val number: List<Int>) {
+    init {
+        require(number.toSet().size == 3) { throw IllegalArgumentException(USER_NUMBER_ERROR) }
+    }
+
+    companion object {
+        private const val USER_NUMBER_ERROR = "서로 다른 3자리수를 입력해주세요"
+    }
 }
