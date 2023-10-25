8797a0c79dbe34480fb390de62248f67d9bfef76
ls2hn <s2hnsu@gmail.com>
2023-10-25 19:20:27 +0900

    Docs: Add README.md

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,27 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
-    TODO("프로그램 구현")
+    //TODO("프로그램 구현")
+
+
+}
+
+fun playGame() {
+
+
+}
+
+fun genAnswer() : String {
+    val answer = mutableListOf<Int>()
+    while (answer.size < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (!answer.contains(randomNumber)) {
+            answer.add(randomNumber)
+        }
+    }
+    return answer.joinToString("")
+
 }
