b7177a0950fb0ee249de7518423ab5a159795d1a
kang-moonsun <kangms8084@naver.com>
2023-10-25 14:45:38 +0900

    Feat: Add setComputerNums

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,19 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
     println("숫자 야구 게임을 시작합니다.")
+    val computer = mutableListOf<Int>()
+    setComputerNums(computer)
+
+}
+
+private fun setComputerNums(computer: MutableList<Int>) {
+    while (computer.size < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (!computer.contains(randomNumber)) {
+            computer.add(randomNumber)
+        }
+    }
 }
