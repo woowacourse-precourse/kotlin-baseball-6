0908d6d13aae56da67cced311b6a4a7d41aba129
yuhyun <dlugus@naver.com>
2023-10-25 21:05:17 +0900

    feat: 숫자야구 기능 구현

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,75 +1,129 @@
 package baseball
-import camp.nextstep.edu.missionutils.Randoms
+
 import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
 
 fun main() {
-    TODO("프로그램 구현")
+    printFirstStartMessage()
+    do {
+        startBaseballGame()
+    } while (restartOrQuit())
 }
 
-//랜덤 숫자 생성
-fun makeRandomNumber(): String { 
+fun startBaseballGame() {
+    val answer = chooseComputerNumber()
+    while (true) {
+        var userNumber = getUserNumber()
+        var result = compareWithAnswer(answer, userNumber)
+        printHint(result)
+        if (result.first == 3) {
+            printGameOver()
+            return
+        }
+    }
+}
+
+fun chooseComputerNumber(): String {
     val computerNumber = mutableListOf<Int>()
     while (computerNumber.size < 3) {
-        val randomNumber: Int = Randoms.pickNumberInRange(1, 9)
-        if (!computerNumber.contains(randomNumber)) {
-        computerNumber.add(randomNumber)
-        }
+        var randomNumber: Int = Randoms.pickNumberInRange(1, 9)
+        addComputerNumber(randomNumber, computerNumber)
     }
     return computerNumber.joinToString("")
 }
 
- //사용자로부터 숫자를 입력받기
- fun getUserNumber(): String {
-    print("숫자를 입력해주세요: ")
-    val userInputNumber = Console.readline().toString()
+/* 뽑은 랜덤 숫자의 중복 여부를 확인하고 아닐 시 리스트에 추가하는 메소드  */
+fun addComputerNumber(randomNumber: Int, computerNumber: MutableList<Int>) {
+    if (!computerNumber.contains(randomNumber)) {
+        computerNumber.add(randomNumber)
+    }
+}
+
+fun printFirstStartMessage() {
+    println("숫자 야구 게임을 시작합니다.")
+}
+
+fun getUserNumber(): String {
+    print("숫자를 입력해주세요 : ")
+    val userInput = Console.readLine().toString()
     val userNumber = mutableListOf<Char>()
-    
-    if (userInputNumber.length != 3) { //예외처리
-        throw IllegalArgumentException("숫자를 ${'"'}3개${'"'} 입력해야 합니다.")
-    }
-
-    for (char in userInputNumber) { // 입력받은 값을 userNumber에 저장
-        userNumber.add(char)
-    }
-
+    validationUserNumber(userNumber, userInput)
     return userNumber.joinToString("")
 }
 
-// 스트라이크와 볼 갯수를 카운트하고 힌트 출력
-fun countStrikeAndBallAndPrintHint(answer: String, userNumber: String) {
+fun validationUserNumber(userNumber: MutableList<Char>, userInput: String) {
+    // 사용자의 입력이 세 글자가 아닌경우
+    if (userInput.length != 3) {
+        throw IllegalArgumentException("You can enter only a three-digit number")
+    }
+
+    for (index in userInput.indices) {
+        // 사용자의 입력이 숫자가 아닌 경우
+        if (userInput[index] < '1' || userInput[index] > '9') {
+            throw IllegalArgumentException("You can enter only a number")
+        }
+        // 사용자의 입력이 같은 숫자를 포함하는 경우
+        if (userNumber.contains(userInput[index])) {
+            throw IllegalArgumentException("Each digit number must be different")
+        }
+        userNumber.add(userInput[index])
+    }
+}
+
+fun compareWithAnswer(answer: String, userNumber: String): Pair<Int, Int> {
     var strike = 0
     var ball = 0
 
     for (index in userNumber.indices) {
         if (userNumber[index] == answer[index]) {
             strike++
-        } else if (answer.contains(userNumber[index])) {
+            continue
+        }
+        if (answer.contains(userNumber[index])) {
             ball++
         }
     }
+    return Pair(strike, ball)
+}
+
+fun printHint(result: Pair<Int, Int>) {
+    val strike = result.first
+    val ball = result.second
 
     if (strike == 0 && ball == 0) {
         println("낫싱")
-    } else if (strike == 0) {
-        println("${ball}볼")
-    } else if (ball == 0) {
-        println("${strike}스트라이크")
-    } else {
-        println("${ball}볼 ${strike}스트라이크")
+        return
     }
-}
 
-//게임 시작 문구 출력
-fun printGameStart() {
-    println("숫자 야구 게임을 시작합니다.")
-}
+    if (strike == 0) {
+        println("${ball}볼")
+        return
+    }
 
-//게임 종료 문구 출력
-fun printGameOVerMessage() {
+    if (ball == 0) {
+        println("${strike}스트라이크")
+        return
+    }
+
+    println("${ball}볼 ${strike}스트라이크")
+}
+ 
+fun printGameOver() {
     println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
+	    println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
 }
 
-//게임 다시할건지 물어보기
-fun askToRestartGame() {
-    print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+fun restartOrQuit(): Boolean {
+    val inputNumber = Console.readLine().toInt()
+    validationRestartOrQuit(inputNumber)
+    if (inputNumber == 1) {
+        return true
+    }
+    return false
+}
+
+fun validationRestartOrQuit(inputNumber: Int) {
+    if (inputNumber != 1 && inputNumber != 2) {
+        throw IllegalArgumentException("You can only enter 1 or 2")
+    }
 }
