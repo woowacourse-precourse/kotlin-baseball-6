75d4c252af334a5fc84feb849fdcbc3faff8ee89
Taehwani-Storage <wiz_th1024@naver.com>
2023-10-25 22:15:30 +0900

    feat: baseball-game v14 - refactoring Application(add Exception)

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -8,40 +8,62 @@ fun main() {
     var playBall = true // 게임 시작
 
     while (playBall) {
-        // 컴퓨터 입력 변수
-        val computerInput = mutableListOf<Int>()
-        while (computerInput.size < 3) {
-            // 숫자 무작위로 1부터 9까지 선택
-            val randomNum = Randoms.pickNumberInRange(1, 9)
-            if (!computerInput.contains(randomNum)) { // 숫자가 nums 리스트에 없다면
-                computerInput.add(randomNum) // 해당 숫자를 nums 리스트에 추가 -> 중복 X
-            }
+        val computerInput = readComputerInput()
+
+        var isGameDone = false // 게임 종료 변수
+        while (!isGameDone) { // 게임이 진행중이면\
+            // 사용자 입력
+            val userInput = readUserInput()
+            // 컴퓨터의 숫자 3개와 사용자의 숫자 3개 비교
+            isGameDone = checkNums(computerInput, userInput)
         }
-        // 사용자 입력 부분
-        var playDone = false // 사용자가 입력하지 않은 경우 false
-        while (!playDone) {
-            print("숫자를 입력해주세요 : ")
-            val userInput = Console.readLine()!! // Null이 아님
-            // 사용자가 입력한 문자가 3개가 맞는지 확인
-            // 입력한 문자열이 정수 변환이 되는지와 숫자로만 이루어져 있는지 확인(숫자 or 문자)
-            // 1부터 9까지 입력을 했는지 확이(숫자 0은 X)
-            if (userInput.length == 3 && userInput.toIntOrNull() != null
-                && userInput.all { it in '1'..'9' } && userInput.toSet().size == 3
-            ) {
-                playDone = checkNums(computerInput, userInput.map { it.toString().toInt() })
-            } else {
-                throw IllegalArgumentException("올바른 3자리 숫자를 입력해야합니다.")
-            }
+        playBall = gameReplay() // 게임 종료 후 재시작 여부 확인
+    }
+}
+
+// 컴퓨터 입력 부분(1부터 9중 중복되지 않는 숫자 3개 생성)
+fun readComputerInput(): List<Int> {
+    val compuInput = mutableListOf<Int>()
+    while (compuInput.size < 3) {
+        // 숫자 무작위로 1부터 9까지 선택
+        val randomNum = Randoms.pickNumberInRange(1, 9)
+        if (!compuInput.contains(randomNum)) { // 숫자가 nums 리스트에 없다면
+            compuInput.add(randomNum) // 해당 숫자를 nums 리스트에 추가 -> 중복 X
         }
-        // 프로그램 종료 부분
-        println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
-        when (readlnOrNull()) {
-            "1" -> playBall = true // 1 입력하면 게임 새로 시작
-            "2" -> playBall = false // 2 입력하면 게임 종료
-            else -> {
-                println("1 또는 2를 입력해주세요.")
-                playBall = false
-            }
+    }
+    return compuInput
+}
+
+// 사용자 입력 부분
+fun readUserInput(): List<Int> {
+    while (true) {
+        print("숫자를 입력해주세요 : ")
+        val userInput = Console.readLine() ?: "" // 입력이 Null이 아님
+        if (isValidNumber(userInput)) { // 사용자 입력이 유효한 입력이면
+            return userInput.map { it.toString().toInt() } // 정수 리스트 반환
+        } else { // 그렇지 않은 입력이라면 아래 문구 출력
+            throw IllegalArgumentException("올바른 3자리 숫자를 입력해야 합니다.")
+        }
+    }
+}
+
+// 사용자가 입력한 숫자 검토 부분
+fun isValidNumber(input: String): Boolean {
+    return input.length == 3 && // 입력한 문자열이 정수 변환이 되는지와 숫자로만 이루어져 있는지 확인(숫자 or 문자)
+            input.toIntOrNull() != null &&
+            input.all { it in '1'..'9' } && // 1부터 9까지 입력을 했는지 확이(숫자 0은 X)
+            input.toSet().size == 3
+}
+
+// 게임 종료 후 다시 게임 시작 여부 확인 함수 (1: 시작, 2: 종료)
+fun gameReplay(): Boolean {
+    println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+    when (Console.readLine()) {
+        "1" -> return true // 1 입력하면 게임 새로 시작
+        "2" -> return false // 2 입력하면 게임 종료
+        else -> {
+            println("1 또는 2를 입력해주세요.")
+            return false
         }
     }
 }
@@ -58,6 +80,11 @@ fun checkNums(comNums: List<Int>, userNums: List<Int>): Boolean {
             ball++ // 볼 개수 + 1
         }
     }
+    return gameRes(strike, ball)
+}
+
+// 게임 결과 출력 함수
+fun gameRes(strike: Int, ball: Int): Boolean {
     // 볼과 스트라이크 되는 경우
     when {
         // 같은 숫자가 존재하고 같은 인덱스에 있는 경우
@@ -69,6 +96,7 @@ fun checkNums(comNums: List<Int>, userNums: List<Int>): Boolean {
                 return true
             }
         }
+
         ball > 0 -> println("${ball}볼") // 인덱스는 다르지만 같은 숫자만 있는 경우
         else -> println("낫싱") // 모두 다른 숫자인 경우
     }
