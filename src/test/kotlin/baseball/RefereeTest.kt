b52788bc95c1af763436b65a801894eb8ff51666
seongsan <tjdtks030@naver.com>
2023-10-25 16:27:09 +0900

    test: Referee 테스트 코드 리팩토링
    기존 countBall, countStrike, checkNothingOrNot 메서드에 대한 테스트 방식에서
    judge 메서드에 대해 통합으로 테스트 진행하는 방식으로 변경

--- src/test/kotlin/baseball/RefereeTest.kt
+++ src/test/kotlin/baseball/RefereeTest.kt
@@ -7,31 +7,32 @@ class RefereeTest {
 
     @Test
     fun `볼 카운트 판정 테스트`() {
-        val numbers: List<Int> = listOf(1, 2, 3)
-        val userNumbers: List<Int> = listOf(3, 2, 1)
-        val expected = 2
+        val pitcherNumbers: List<Int> = listOf(1, 4, 3)
+        val batterNumbers: List<Int> = listOf(3, 2, 1)
+        val expected = "2볼"
 
-        referee.countBall()
-        assert(referee.getBallCount() == expected)
+        referee.judge(pitcherNumbers, batterNumbers)
+        println(referee.getResultMessage())
+        assert(expected == referee.getResultMessage())
     }
 
     @Test
     fun `스트라이크 카운트 판정 테스트`() {
-        val numbers: List<Int> = listOf(2, 4, 7)
-        val userNumbers: List<Int> = listOf(1, 3, 7)
-        val expected = 1
+        val pitcherNumbers: List<Int> = listOf(2, 4, 7)
+        val batterNumbers: List<Int> = listOf(1, 3, 7)
+        val expected = "1스트라이크"
 
-        referee.countStrike()
-        assert(referee.getStrikeCount() == expected)
+        referee.judge(pitcherNumbers, batterNumbers)
+        assert(expected == referee.getResultMessage())
     }
 
     @Test
     fun `낫싱 판정 테스트`() {
-        val numbers: List<Int> = listOf(1, 2, 3)
-        val userNumbers: List<Int> = listOf(4, 5, 6)
-        val expected = true
+        val pitcherNumbers: List<Int> = listOf(1, 2, 3)
+        val batterNumbers: List<Int> = listOf(4, 5, 6)
+        val expected = "낫싱"
 
-        referee.checkNothing()
-        assert(referee.getNothingOrNot() == expected)
+        referee.judge(pitcherNumbers, batterNumbers)
+        assert(expected == referee.getResultMessage())
     }
 }
