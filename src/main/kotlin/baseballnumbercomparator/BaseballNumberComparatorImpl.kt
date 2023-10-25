9cbd245d5c14c7ff3afdacfc14e5af88e72f3aa3
lee989898 <wnsrud1352@gmail.com>
2023-10-25 12:36:45 +0900

    [refactor] BaseballNumberComparatorImpl contain, for -> count, forEachIndexed로 변경

--- src/main/kotlin/baseballnumbercomparator/BaseballNumberComparatorImpl.kt
+++ src/main/kotlin/baseballnumbercomparator/BaseballNumberComparatorImpl.kt
@@ -9,41 +9,35 @@ object BaseballNumberComparatorImpl : BaseballNumberComparator {
         var strike = ZERO
         var ball = ZERO
 
-        val checkContainingNumbersResult = checkContainingNumbers(answer, baseballNumber, ball)
-        ball = checkContainingNumbersResult.ball
+        val checkContainingNumbersResult = checkContainingNumbers(answer, baseballNumber)
+        ball += checkContainingNumbersResult.ball
 
-        val checkNumbersAreInSamePlaceResult = checkNumbersAreInSamePlace(answer, baseballNumber, strike, ball)
-        strike = checkNumbersAreInSamePlaceResult.strike
-        ball = checkNumbersAreInSamePlaceResult.ball
+        val checkNumbersAreInSamePlaceResult = checkNumbersAreInSamePlace(answer, baseballNumber)
+        strike += checkNumbersAreInSamePlaceResult.strike
+        ball += checkNumbersAreInSamePlaceResult.ball
 
         return BaseballResult(strike = strike, ball)
     }
 
-    private fun checkContainingNumbers(answer: String, baseballNumber: String, currentBall: Int): BaseballResult {
-        var ball = currentBall
-        for (index in 0..2) {
-            if (answer.contains(baseballNumber[index])) {
-                ball += ONE_BALL
-            }
-        }
+    private fun checkContainingNumbers(answer: String, baseballNumber: String): BaseballResult {
+        val ball = baseballNumber.count { answer.contains(it) }
         return BaseballResult(ZERO, ball)
     }
 
     private fun checkNumbersAreInSamePlace(
         answer: String,
-        baseballNumber: String,
-        currentStrike: Int,
-        currentBall: Int
+        baseballNumber: String
     ): BaseballResult {
-        var strike = currentStrike
-        var ball = currentBall
+        var strike = ZERO
+        var ball = ZERO
 
-        for (index in 0..2) {
-            if (answer[index] == baseballNumber[index]) {
+        answer.forEachIndexed { index, answerNumber ->
+            if (answerNumber == baseballNumber[index]) {
                 strike += ONE_STRIKE
                 ball -= ONE_BALL
             }
         }
+
         return BaseballResult(strike, ball)
     }
 }
