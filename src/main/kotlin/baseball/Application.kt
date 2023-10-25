2b6f7f6565816177f286296140cecb29ba050de7
K_Gs:D <rootachieve6053@gmail.com>
2023-10-25 18:52:14 +0900

    feat : 유저 입력 받기

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,10 +1,13 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Console
 import camp.nextstep.edu.missionutils.Randoms
 
 fun main() {
     println("숫자 야구 게임을 시작합니다.")
     val targetList = setNumber()
+
+    val input = getInput()
 }
 
 fun setNumber(): List<Int> {
@@ -17,3 +20,22 @@ fun setNumber(): List<Int> {
     }
     return list
 }
+
+fun getInput(): String {
+    val userInput = Console.readLine()
+
+    val numberChecker = userInput.all {
+        it in '1'..'9'
+    }
+
+    val distinctChecker = userInput.map {
+        it
+    }.distinct().size == 3
+
+    if (numberChecker.not() || distinctChecker.not() || userInput.length != 3) {
+        throw IllegalArgumentException()
+    } else {
+        return userInput
+    }
+}
+
