4a15664780c4edae9b2f41cc0ab2fa85c0a66ce4
lee989898 <wnsrud1352@gmail.com>
2023-10-25 10:27:34 +0900

    [test] ComputerTest 랜덤 야구 숫자가 전부 숫자인지 테스트 구현

--- src/test/kotlin/computer/ComputerTest.kt
+++ src/test/kotlin/computer/ComputerTest.kt
@@ -1,6 +1,7 @@
 package computer
 
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import randombaseballnumbergenerator.RandomBaseballNumberGenerator
@@ -24,7 +25,19 @@ class ComputerTest {
         assertEquals(result, BASEBALL_NUMBER_SIZE)
     }
 
+    @Test
+    fun `랜덤 야구 숫자가 전부 숫자인지`() {
+        computer.setRandomBaseballNumber()
+        val result = computer
+            .randomBaseballNumber
+            .all { it in BASEBALL_NUMBER_MINIMUM_NUMBER..BASEBALL_NUMBER_MAXIMUM_NUMBER }
+
+        assertTrue(result)
+    }
+
     companion object {
         const val BASEBALL_NUMBER_SIZE = 3
+        const val BASEBALL_NUMBER_MINIMUM_NUMBER = '1'
+        const val BASEBALL_NUMBER_MAXIMUM_NUMBER = '9'
     }
 }
