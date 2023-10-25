70c08edc052fcf8d164a85b5164344648ee7654b
K_Gs:D <rootachieve6053@gmail.com>
2023-10-25 18:49:17 +0900

    feat : 숫자 리스트 생성

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,19 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
-    TODO("프로그램 구현")
+    println("숫자 야구 게임을 시작합니다.")
+    val targetList = setNumber()
+}
+
+fun setNumber(): List<Int> {
+    val list = mutableListOf<Int>()
+    while (list.size < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (list.contains(randomNumber).not()) {
+            list.add(randomNumber)
+        }
+    }
+    return list
 }
