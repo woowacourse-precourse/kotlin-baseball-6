73bdd1b6625fc60e9d49dd46683d1b957e1dd697
Bin <ahb0226@gmail.com>
2023-10-25 16:59:12 +0900

    * 2차 기능 구현(main, playNumberBaseballGame, getHint, Hint)
    1. playNumberBaseballGame 함수 내용
    - 힌트 출력
    - 정답 체크
    
    2. Computer 클래스 내용
    - 컴퓨터의 3자리 랜덤 숫자 생성
    
    3. getHint 함수 내용
    - 각 자리수 비교 및 스트라이크, 볼 집계 및 출력
    
    4. Hint 클래스 내용
    - ThreeStrikes 함수 - 스트라이크 3개 일시 참
    - 각각의 경우에 따라 낫싱, 3스트라이크, n볼, n스트라이크 판별 및 출력

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,6 @@
 package baseball
 import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
 
 fun main() {
     println("숫자 야구 게임을 시작합니다.")
@@ -20,6 +21,8 @@ fun main() {
 
 fun playNumberBaseballGame() {
     var GameFinished = false
+    val computer = Computer()
+
 
     while (!GameFinished) {
         println("숫자를 입력해주세요:")
@@ -29,10 +32,56 @@ fun playNumberBaseballGame() {
             throw IllegalArgumentException("잘못된 입력입니다.")
         }
 
+        val hint = computer.getHint(userInput)
+        GameFinished = hint.ThreeStrikes()
+        println(hint)
     }
-
+    println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
 }
 
 fun CorrectInput(input: String?): Boolean {
     return input?.matches(Regex("^[1-9]{3}$")) == true && input.toCharArray().distinct().size == 3
+}
+
+
+class Computer {
+    private val numbers = mutableListOf<Int>()
+
+    init {
+        while (numbers.size < 3) {
+            val randomNumber = Randoms.pickNumberInRange(1, 9)
+            if (!numbers.contains(randomNumber)) {
+                numbers.add(randomNumber)
+            }
+        }
+    }
+
+    fun getHint(userInput: String): Hint {
+        var balls = 0
+        var strikes = 0
+
+        for (i in numbers.indices) {
+            val guessedNumber = Character.getNumericValue(userInput[i])
+            if (guessedNumber == numbers[i]) {
+                strikes++
+            } else if (numbers.contains(guessedNumber)) {
+                balls++
+            }
+        }
+
+        return Hint(balls, strikes)
+    }
+}
+
+class Hint(private val balls: Int, private val strikes: Int) {
+    fun ThreeStrikes(): Boolean = strikes == 3
+
+    override fun toString(): String {
+        if (balls == 0 && strikes == 0) return "낫싱"
+        if (strikes == 3) return "3스트라이크"
+        var result = ""
+        if (balls > 0) result += "${balls}볼 "
+        if (strikes > 0) result += "${strikes}스트라이크"
+        return result.trim()
+    }
 }
