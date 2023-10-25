d580349bb09cd53fda03493d18ead589204aa30a
상은 <71699054+vmkmym@users.noreply.github.com>
2023-10-25 16:02:06 +0900

    feat: gameResults 함수 만들기 게임종료 or 게임시작 여부를 사용자에게 묻고 선택에 따른 결과를 반환하는 함수로 잘못된 입력값은 예외 처리

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,27 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms.pickNumberInRange
+
 fun main() {
-    TODO("프로그램 구현")
+    // 야구게임 함수 호출
+}
+
+/**
+ * 게임 종료 또는 새로운 게임 시작 여부를 사용자에게 물어 사용자 선택에 따라 반환하는 함수.
+ *
+ * @return 게임을 새로 시작할 경우 true, 종료할 경우 false
+ * @throws 은 잘못된 입력값일 경우 예외를 던짐
+ */
+fun gameResults(inputNumber: String, numberSet: MutableSet<String>): String {
+    val inputNumIdx = inputNumber.toList().map { it.toString() }
+    val strike = inputNumIdx.zip(numberSet).count { (inputIdx, targetNumIdx) -> inputIdx == targetNumIdx }
+    val ball = inputNumIdx.count { it in numberSet } - strike
+
+    return when {
+        strike > 0 && ball > 0 -> "${ball}볼 ${strike}스트라이크"
+        strike > 0 -> "${strike}스트라이크"
+        ball > 0 -> "${ball}볼"
+        else -> "낫싱"
+    }
 }
