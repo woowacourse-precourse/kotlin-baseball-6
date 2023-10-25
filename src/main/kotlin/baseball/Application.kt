0adce16c13b00110c08eab4db2f6b7db5cd67cd8
Gahee Lim <ka614323@naver.com>
2023-10-25 14:36:44 +0900

    값 입력받기 기능 수정 및 값 비교, 결과 출력 기능 구현

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -11,13 +11,40 @@ fun main() {
             computer.add(randomNumber)
         }
     }
-    println(computer) // 랜덤 확인 테스트
-
-    print("숫자를 입력해주세요 : ")
-    val inputNum = mutableListOf<Int>()
-    val user = readLine()!!.toInt()
-    inputNum.add(user)
-    println(inputNum) // 입력 확인 테스트
+    println(computer)
+    println(computer[1]) // 랜덤 확인 테스트
 
 
-}
+        print("숫자를 입력해주세요 : ")
+        val user = readLine()!!
+        val inputNum = mutableListOf<Int>()
+        for (i in user) {
+            inputNum.add(i.digitToInt())
+        }
+
+        var strike = 0
+        var ball = 0
+        var not = 0
+        for (i in inputNum.indices) {
+            if (inputNum[i] == computer[i]) {
+                strike += 1
+            } else if (computer.contains(inputNum[i])) {
+                ball += 1
+            } else {
+                not += 1
+            }
+        }
+
+        if (strike == 3) {
+            println("스")
+        } else if (not == 3) {
+            println("낫")
+        } else if (strike == 0 && ball == 0) {
+            println("낫")
+        } else if (strike > 0 && ball == 0) {
+            println("스 $strike")
+        } else if (strike == 0 && ball > 0) {
+            println("볼 $ball")
+        } else println("스 $strike, 볼 $ball")
+    }
+
