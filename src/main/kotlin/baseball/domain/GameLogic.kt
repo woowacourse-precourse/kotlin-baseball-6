42956154f62a1778abdb6cda0227cf6825df0953
m6z1 <sonmyungj1zz@gmail.com>
2023-10-25 19:01:38 +0900

    refactor : 분기에 따른 반복문 수정

--- src/main/kotlin/baseball/domain/GameLogic.kt
+++ src/main/kotlin/baseball/domain/GameLogic.kt
@@ -10,33 +10,29 @@ class GameLogic(private val computerNum: Int, private val userNum: Int) {
         val strikes = countStrikes(computerNum, userNum)
         val balls = countBalls(computerNum, userNum)
 
-        return when (strikes) {
-            3 -> {
-                println(Constants.THREE_STRIKE_MESSAGE)
-                println(Constants.GAME_OVER_MESSAGE)
-                true
-            }
-
-            else -> {
-                resultView.printResultCount(strikes, balls)
-                false
-            }
+        return if (strikes == 3) {
+            println(Constants.THREE_STRIKE_MESSAGE)
+            println(Constants.GAME_OVER_MESSAGE)
+            true
+        } else {
+            resultView.printResultCount(strikes, balls)
+            false
         }
     }
+}
 
-    private fun countStrikes(computer: Int, user: Int): Int {
-        var strikes = 0
-        for (i in computer.toString().indices) {
-            if (computer.toString()[i] == user.toString()[i]) strikes++
-        }
-        return strikes
+private fun countStrikes(computer: Int, user: Int): Int {
+    var strikes = 0
+    for (i in computer.toString().indices) {
+        if (computer.toString()[i] == user.toString()[i]) strikes++
     }
+    return strikes
+}
 
-    private fun countBalls(computer: Int, user: Int): Int {
-        var balls = 0
-        for (digit in user.toString()) {
-            if (digit in computer.toString()) balls++
-        }
-        return balls - countStrikes(computer, user)
+private fun countBalls(computer: Int, user: Int): Int {
+    var balls = 0
+    for (digit in user.toString()) {
+        if (digit in computer.toString()) balls++
     }
+    return balls - countStrikes(computer, user)
 }
