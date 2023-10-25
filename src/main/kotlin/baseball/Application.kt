26af6b9ad79cc68a7373eb52887b6f361aa5e48a
altpfwlzh <altpfwlzh@gmail.com>
2023-10-25 14:23:04 +0900

    feat : createQuestion(pick random 3 nums)

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,6 +1,20 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
     println("숫자 야구 게임을 시작합니다.")
+
+    var ans : MutableList<Int> = createQuestion()
 }
 
+private fun createQuestion(): MutableList<Int> {
+    val questionNums : MutableList<Int> = mutableListOf<Int>()
+    while (questionNums.size < 3) {
+        val curNum : Int = Randoms.pickNumberInRange(1, 9)
+        if(!questionNums.contains(curNum)) {
+            questionNums.add(curNum)
+        }
+    }
+    return questionNums
+}
