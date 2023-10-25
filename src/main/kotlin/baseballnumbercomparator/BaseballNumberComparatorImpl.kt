375dca4587a3a67395c05f583687f63435f848db
lee989898 <wnsrud1352@gmail.com>
2023-10-25 11:42:25 +0900

    [feat] BaseballNumberComparatorImpl 컴퓨터의 랜덤 야구 수에 포함되면 볼 개수를 늘리는 메서드 구현

--- src/main/kotlin/baseballnumbercomparator/BaseballNumberComparatorImpl.kt
+++ src/main/kotlin/baseballnumbercomparator/BaseballNumberComparatorImpl.kt
@@ -5,11 +5,20 @@ class BaseballNumberComparatorImpl : BaseballNumberComparator {
         var strike = ZERO
         var ball = ZERO
 
+        val checkContainingNumbersResult = checkContainingNumbers(answer,baseballNumber, ball)
+        ball = checkContainingNumbersResult.ball
+        
         return BaseballResult(strike = strike, ball)
     }
 
-    private fun checkContainingNumbers() {
-
+    private fun checkContainingNumbers(answer: String, baseballNumber: String, currentBall: Int): BaseballResult {
+        var ball = currentBall
+        for (index in 0..2) {
+            if (answer.contains(baseballNumber[index])) {
+                ball += 1
+            }
+        }
+        return BaseballResult(0, ball)
     }
 
     private fun checkNumbersAreInSamePlace() {
