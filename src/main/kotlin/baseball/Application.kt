125bf48356f6c31e5bafc8e0d4a65913ff4fa6b6
Bin <ahb0226@gmail.com>
2023-10-25 16:40:23 +0900

    1. main 함수 내용 - 숫자 야구 게임 시작 멘트 - 게임 새로 시작 1, 종료 2 숫자 입력 받기
    2. playNumberBaseballGame 함수 내용
    - 게임 종료 여부 판단
    - 3자리 숫자 입력 받기
    - 잘못된 입력일 시 오류 문구 출력
    
    3. CorrectInput 함수 내용
    - 숫자 3자리인지, 1-9 사이의 숫자인지, 중복 없는지 체크

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,38 @@
 package baseball
+import camp.nextstep.edu.missionutils.Console
 
 fun main() {
-    TODO("프로그램 구현")
+    println("숫자 야구 게임을 시작합니다.")
+
+    var continueGame = true
+
+    while (continueGame) {
+        playNumberBaseballGame()
+
+        println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+        val choice = Console.readLine()
+        if (choice != "1") {
+            continueGame = false
+        }
+    }
+}
+
+
+fun playNumberBaseballGame() {
+    var GameFinished = false
+
+    while (!GameFinished) {
+        println("숫자를 입력해주세요:")
+        val userInput = Console.readLine()
+
+        if (!CorrectInput(userInput)) {
+            throw IllegalArgumentException("잘못된 입력입니다.")
+        }
+
+    }
+
+}
+
+fun CorrectInput(input: String?): Boolean {
+    return input?.matches(Regex("^[1-9]{3}$")) == true && input.toCharArray().distinct().size == 3
 }
