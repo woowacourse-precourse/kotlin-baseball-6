9d60493f38fbb1e11e5f8656aeae98e0117599f1
pengcon <vpdzjs@gmail.com>
2023-10-25 19:32:50 +0900

    feat: 컴퓨터 숫자 설정, 숫자 입력 받는 기능 추가 야구게임의 컴퓨터측의 숫자를 설정하는 기능과 사용자에게 숫자를 입력받고 조건에 맞는지 판단하는 기능 추가

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,28 @@
 package baseball
+import camp.nextstep.edu.missionutils.Randoms
 
 fun main() {
-    TODO("프로그램 구현")
+    val computer = setComputerNumber()
+    println("숫자 야구 게임을 시작합니다.")
+    println(computer)
+    print("숫자를 입력해주세요 : ")
+    val people = (readLine() ?: "").map { it.toString().toInt() }
+    val set = HashSet<Int>(people)
+    if (people.size != 3 || set.size < people.size) throw IllegalArgumentException("잘못된 값을 입력하셨습니다.")
+    for (num in people) if (num !in 1..9) throw IllegalArgumentException("잘못된 값을 입력하셨습니다.")
+}
+
+fun setComputerNumber(): MutableList<Int> {
+    val computer = mutableListOf<Int>()
+    while (computer.size < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (!computer.contains(randomNumber)) {
+            computer.add(randomNumber)
+        }
+    }
+    return computer
+
+
+
+
 }
