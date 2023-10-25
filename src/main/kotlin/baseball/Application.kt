a68b49e01a0460070a17509e0b65ef070f14c08c
kang-moonsun <kangms8084@naver.com>
2023-10-25 15:53:07 +0900

    Feat: Add inputUserNums

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,12 +1,17 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Console
 import camp.nextstep.edu.missionutils.Randoms
 
 fun main() {
     println("숫자 야구 게임을 시작합니다.")
     val computer = mutableListOf<Int>()
+    val user = mutableListOf<Int>()
+
     setComputerNums(computer)
 
+    val input = Console.readLine()
+    input.forEach { user.add(it.digitToInt()) }
 }
 
 private fun setComputerNums(computer: MutableList<Int>) {
