d5fb09d5b51e937e01129b1b0cf2503f592f593e
mainfn <_@mainfn.dev>
2023-10-25 13:30:33 +0900

    refact: remove compareTo and reveal counting methods

--- src/main/kotlin/baseball/domain/Answer.kt
+++ src/main/kotlin/baseball/domain/Answer.kt
@@ -2,17 +2,10 @@ package baseball.domain
 
 class Answer(numbers: List<BallNumber>) : BallNumbers(numbers) {
 
-    fun compareTo(guess: Guess): BallResult {
-        return BallResult(
-            countStrikes(guess),
-            countBalls(guess),
-        )
-    }
-
-    private fun countStrikes(guess: Guess) =
+    fun countStrikes(guess: Guess) =
         (0..<BALL_COUNT).count { isStrikeAt(it, guess) }
 
-    private fun countBalls(guess: Guess) =
+    fun countBalls(guess: Guess) =
         (0..<BALL_COUNT).count { isBallAt(it, guess) }
 
     private fun isStrikeAt(index: Int, other: Guess) =
