fd638ba85a98a18a4a73f04683a38ff27a23dc4f
mcdmaisd <tkim0156@gmail.com>
2023-10-25 20:58:48 +0900

    feat : 사용자의 입력을 받아 정수배열로 변환하는 기능 구현

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,19 +1,27 @@
 package baseball
 
-import camp.nextstep.edu.missionutils.Console
-import camp.nextstep.edu.missionutils.Randoms
+import camp.nextstep.edu.missionutils.Console.readLine
+import camp.nextstep.edu.missionutils.Randoms.pickNumberInRange
 
 fun main() {
     makeRandomNumbers()
+    getUserInput()
 }
+
+fun getUserInput() {
+    val userInput = readLine().map { it.digitToInt() }.toMutableList()
+}
+
 
 fun makeRandomNumbers() {
     val randomNumbers = mutableListOf<Int>()
     while (randomNumbers.size < 3) {
-        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        val randomNumber = pickNumberInRange(1, 9)
         if (!randomNumbers.contains(randomNumber)) {
             randomNumbers.add(randomNumber)
         }
     }
 }
+
+
 
