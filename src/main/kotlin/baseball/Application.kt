abb4b2e1f7b4d50135258f425b2234a3ddb9265d
yuhyun <dlugus@naver.com>
2023-10-25 20:34:59 +0900

    feat: 게임 종료 문구, 재시작 문구 출력

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -60,6 +60,16 @@ fun countStrikeAndBallAndPrintHint(answer: String, userNumber: String) {
 }
 
 //게임 시작 문구 출력
-fun printFirstMessage() {
+fun printGameStart() {
     println("숫자 야구 게임을 시작합니다.")
+}
+
+//게임 종료 문구 출력
+fun printGameOVerMessage() {
+    println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
+}
+
+//게임 다시할건지 물어보기
+fun askToRestartGame() {
+    print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
 }
