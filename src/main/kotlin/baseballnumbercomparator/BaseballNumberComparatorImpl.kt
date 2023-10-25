3b0da39ae493f879485956b94eddd04e5547c7bf
lee989898 <wnsrud1352@gmail.com>
2023-10-25 11:41:21 +0900

    [feat] BaseballNumberComparatorImpl 정답과 야구 번호를 비교해서 BaseballResult 반환하는 메서드 구현

--- src/main/kotlin/baseballnumbercomparator/BaseballNumberComparatorImpl.kt
+++ src/main/kotlin/baseballnumbercomparator/BaseballNumberComparatorImpl.kt
@@ -1,7 +1,22 @@
 package baseballnumbercomparator
 
-class BaseballNumberComparatorImpl: BaseballNumberComparator {
+class BaseballNumberComparatorImpl : BaseballNumberComparator {
     override fun compareAnswerWithPlayerBaseballNumber(answer: String, baseballNumber: String): BaseballResult {
-        TODO("Not yet implemented")
+        var strike = ZERO
+        var ball = ZERO
+
+        return BaseballResult(strike = strike, ball)
+    }
+
+    private fun checkContainingNumbers() {
+
+    }
+
+    private fun checkNumbersAreInSamePlace() {
+
+    }
+
+    companion object {
+        private const val ZERO = 0
     }
 }
