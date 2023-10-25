95dec38e45601b554ce97a25ede26436624ea349
pingu244 <pingu244@naver.com>
2023-10-25 15:40:34 +0900

    [feat] 0이 포함된 입력을 하는 경우의 예외 처리

--- src/main/kotlin/baseball/model/UserNumber.kt
+++ src/main/kotlin/baseball/model/UserNumber.kt
@@ -3,9 +3,11 @@ package baseball.model
 data class UserNumber(val number: List<Int>) {
     init {
         require(number.toSet().size == 3) { throw IllegalArgumentException(USER_NUMBER_ERROR) }
+        require(!number.contains(0)) { throw IllegalArgumentException(USER_NUMBER_ZERO_ERROR) }
     }
 
     companion object {
         private const val USER_NUMBER_ERROR = "서로 다른 3자리수를 입력해주세요"
+        private const val USER_NUMBER_ZERO_ERROR = "1-9의 수만 입력 가능합니다"
     }
 }
