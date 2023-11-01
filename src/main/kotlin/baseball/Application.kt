8b96a54372e88fdf0dae11e359621296531b15c1
lh99j <grand2181@gmail.com>
2023-11-01 12:55:46 +0900

    feat: 사용자의 입력을 가져오는 기능 구현

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,9 +1,16 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
     printGameStart()
+    val userInput = getUserInput()
 }
 
 fun printGameStart() {
     println("숫자 야구 게임을 시작합니다.")
+}
+
+fun getUserInput(): String {
+    return Console.readLine()
 }
