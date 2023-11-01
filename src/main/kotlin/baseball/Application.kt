df3561d7d6afe8d1530dfaceaf24e9fee4dd4053
lh99j <grand2181@gmail.com>
2023-11-01 13:09:54 +0900

    feat: 컴퓨터 고유의 숫자 정하는 기능 구현

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -5,9 +5,11 @@ import baseball.Util.Validator.validateLength
 import baseball.Util.Validator.validateRange
 import baseball.Util.Validator.validateUniqueNumber
 import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
 
 fun main() {
     printGameStart()
+    val computerNumber = setComputerNumber()
     val userInput = getUserInput()
     validateLength(userInput)
     validateInteger(userInput)
@@ -21,4 +23,15 @@ fun printGameStart() {
 
 fun getUserInput(): String {
     return Console.readLine()
+}
+
+fun setComputerNumber(): List<Int> {
+    val computer = mutableListOf<Int>()
+    while (computer.size < 3) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (!computer.contains(randomNumber)) {
+            computer.add(randomNumber)
+        }
+    }
+    return computer.toList()
 }
