71bfffabc747f447649f37fc38e3de25aef1efff
lh99j <grand2181@gmail.com>
2023-11-01 13:03:57 +0900

    feat: 사용자의 입력 중 중복된 수가 있는지 검증 기능 구현

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -8,6 +8,7 @@ fun main() {
     val userInput = getUserInput()
     validateLength(userInput)
     validateInteger(userInput)
+    validateUniqueNumber(userInput)
 }
 
 fun printGameStart() {
@@ -24,4 +25,9 @@ fun validateLength(number: String) {
 
 fun validateInteger(number: String) {
     number.toIntOrNull() ?: throw IllegalArgumentException("사용자의 입력이 정수가 아닙니다.")
+}
+
+fun validateUniqueNumber(number: String) {
+    val validation = number.toSet()
+    if (validation.size != 3) throw IllegalArgumentException("사용자의 입력 중 중복된 숫자가 존재합니다.")
 }
