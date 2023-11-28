3ed274c6232019096c55b7342e793d376911db63
sh1mj1 <7wlgns@gmail.com>
2023-11-28 15:48:31 +0900

    refactor(InputView): 잘못된 커맨드를 입력하면 예외를 던진다

--- src/main/kotlin/baseball/view/Command.kt
+++ src/main/kotlin/baseball/view/Command.kt
@@ -3,5 +3,4 @@ package baseball.view
 enum class Command(val commandName: String, val command: Int) {
     RESTART("새로 시작", 1),
     EXIT("종료", 2),
-    NONE("잘못된 입력", -1)
 }
--- src/main/kotlin/baseball/view/InputView.kt
+++ src/main/kotlin/baseball/view/InputView.kt
@@ -11,16 +11,21 @@ class InputView {
 
     fun readCommand(): Command {
         println(COMMAND_PROMPT)
-        val command = readLine().toIntOrNull() ?: -1
-        return Command.entries.firstOrNull { command == it.command } ?: Command.NONE
+        val command = readLine().toIntOrNull()
+        require(command != null) {
+            INVALID_COMMAND
+        }
+        return Command.entries.first { command == it.command }
     }
 
 
     companion object {
         private const val DELIMITER = " : "
         const val INPUT_NUMBERS_PROMPT = "숫자를 입력해주세요$DELIMITER"
+
         private val COMMAND_PROMPT =
             "게임을 ${Command.RESTART.commandName}하려면 ${Command.RESTART.command}, " +
                     "${Command.EXIT.commandName}하려면 ${Command.EXIT.command}를 입력하세요."
+        private val INVALID_COMMAND = "잘못된 입력입니다.\n$COMMAND_PROMPT"
     }
 }
