a3aa77cc0f30c39d9c5ef5c704b125597c11d877
sh1mj1 <7wlgns@gmail.com>
2023-11-28 14:30:21 +0900

    feat(InputView): 게임 종료시의 사용자의 입력을 커맨드로 변환한다

--- src/main/kotlin/baseball/view/InputView.kt
+++ src/main/kotlin/baseball/view/InputView.kt
@@ -1,16 +1,26 @@
 package baseball.view
 
+
 import camp.nextstep.edu.missionutils.Console.readLine
 
 class InputView {
     fun readNumbers(): Int {
         print(INPUT_NUMBERS_PROMPT)
-        return readLine().trim().toIntOrNull() ?: -1
+        return readLine().toIntOrNull() ?: -1
+    }
+
+    fun readCommand(): Command {
+        println(COMMAND_PROMPT)
+        val command = readLine().toIntOrNull() ?: -1
+        return Command.entries.firstOrNull { command == it.command } ?: Command.NONE
     }
 
 
     companion object {
         private const val DELIMITER = " : "
         const val INPUT_NUMBERS_PROMPT = "숫자를 입력해주세요$DELIMITER"
+        private val COMMAND_PROMPT =
+            "게임을 ${Command.RESTART.commandName}하려면 ${Command.RESTART.command}, " +
+                    "${Command.EXIT.commandName}하려면 ${Command.EXIT.command}를 입력하세요."
     }
 }
