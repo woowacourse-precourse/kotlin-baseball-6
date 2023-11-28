7c75a956d32d4d2b1fee4589a4e3da087a32b4a7
sh1mj1 <7wlgns@gmail.com>
2023-11-28 17:06:00 +0900

    refactor(RefereeTest): 입력값과 정답을 비교해서 결과를 알려주는 테스트에서 여러 케이스를 한번에 테스트한다

--- src/test/kotlin/baseball/model/RefereeTest.kt
+++ src/test/kotlin/baseball/model/RefereeTest.kt
@@ -1,31 +1,44 @@
 package baseball.model
 
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
 
 class RefereeTest {
-    @Test
-    fun `입력과 정답을 비교해서 결과를 알려준다`() {
+
+    @ParameterizedTest
+    @CsvSource(
+        "1,2,3 , 4,5,6 , 0,0 ",
+        "1,2,3 , 1,2,3 , 3,0",
+        "1,2,3 , 3,1,2 , 0,3",
+        "1,2,3 , 1,3,2 , 1,2",
+        "4,6,2 , 2,7,9 , 0,1",
+    )
+    fun `입력과 정답을 비교해서 결과를 알려준다`(
+        inputFirst: Int, inputSecond: Int, inputThird: Int,
+        answerFirst: Int, answerSecond: Int, answerThird: Int,
+        strikeCount: Int, ballCount: Int
+    ) {
         // given
         val inputNumbers = BaseballNumbers(
             listOf(
-                BaseballNumber(3),
-                BaseballNumber(6),
-                BaseballNumber(2)
+                BaseballNumber(inputFirst),
+                BaseballNumber(inputSecond),
+                BaseballNumber(inputThird)
             )
         )
         val answer = BaseballNumbers(
             listOf(
-                BaseballNumber(2),
-                BaseballNumber(6),
-                BaseballNumber(9)
+                BaseballNumber(answerFirst),
+                BaseballNumber(answerSecond),
+                BaseballNumber(answerThird)
             )
         )
 
         // when
         val result = Referee().compare(inputNumbers, answer)
-        val expected = BallAndStrike(strikeCount = 1, ballCount = 1)
+        val expected = BallAndStrike(strikeCount, ballCount)
 
         // then
         assertThat(result).isEqualTo(expected)
