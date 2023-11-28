f096adcc98482266638e07511736c36427f8f8bc
sh1mj1 <7wlgns@gmail.com>
2023-11-28 13:12:29 +0900

    feat(BaseBallNumber): 사용자가 입력한 각 숫자의 유효성을 검증한다

--- src/main/kotlin/baseball/model/BaseballNumber.kt
+++ src/main/kotlin/baseball/model/BaseballNumber.kt
@@ -1,3 +1,15 @@
 package baseball.model
 
-data class BaseballNumber(val number: Int)
+data class BaseballNumber(val number: Int) {
+    init {
+        require(number in START_NUMBER..END_NUMBER) {
+            INVALID_NUMBER_RANGE
+        }
+    }
+
+    companion object {
+        const val START_NUMBER = 1
+        const val END_NUMBER = 9
+        const val INVALID_NUMBER_RANGE = "숫자는 $START_NUMBER 와 $END_NUMBER 사이의 수여야 합니다."
+    }
+}
