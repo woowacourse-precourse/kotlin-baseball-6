cd16128704aab642c767336f4a1e30cd1fa15719
sh1mj1 <7wlgns@gmail.com>
2023-11-28 17:54:30 +0900

    style(Referee): 코드의 가독성을 개선한다

--- src/main/kotlin/baseball/model/Referee.kt
+++ src/main/kotlin/baseball/model/Referee.kt
@@ -1,22 +1,21 @@
 package baseball.model
 
 class Referee {
-
     fun compare(inputNumbers: BaseballNumbers, answer: BaseballNumbers): BallAndStrike {
         var strikeCount = 0
         var ballCount = 0
         for (i in 0 until inputNumbers.numbers.size) {
-            if (inputNumbers.numbers[i] == answer.numbers[i]) {
+            val inputNumber = inputNumbers.numbers[i]
+            val answerNumber = answer.numbers[i]
+
+            if (inputNumber == answerNumber) {
                 strikeCount++
-                continue
-            }
-            if (answer.numbers.contains(BaseballNumber(inputNumbers.numbers[i].number))) {
+            } else if (answer.numbers.any { it == inputNumber }) {
                 ballCount++
             }
         }
 
         return BallAndStrike(strikeCount, ballCount)
     }
-
 
 }
