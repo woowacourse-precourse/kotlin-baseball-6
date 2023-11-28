905a9bacb097337111f54effe05ff87512801da8
sh1mj1 <7wlgns@gmail.com>
2023-11-28 17:17:40 +0900

    refactor(BaseballNumbersTest): 게임에서 사용되는 숫자 셋을 테스트할 때 여러 케이스를 한번에 테스트한다

--- src/test/kotlin/baseball/model/BaseballNumbersTest.kt
+++ src/test/kotlin/baseball/model/BaseballNumbersTest.kt
@@ -3,44 +3,61 @@ package baseball.model
 import baseball.model.BaseballNumbers.Companion.INVALID_NUMBERS_SIZE
 import baseball.model.BaseballNumbers.Companion.NUMBERS_DUPLICATED
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
 
 class BaseballNumbersTest {
-    @Test
-    fun `게임에 사용되는 숫자 셋은 세자리임을 검증한다`() {
+    @ParameterizedTest
+    @MethodSource("provideInvalidCountBaseballNumbers")
+    fun `게임에 사용되는 숫자 셋은 세자리가 아니면 예외를 던진다`(baseballNumbers: List<BaseballNumber>) {
         val exception = assertThrows<IllegalArgumentException> {
-            BaseballNumbers(
-                listOf(
-                    BaseballNumber(1), BaseballNumber(2)
-                )
-            )
+            BaseballNumbers(baseballNumbers)
         }
         assertThat(exception.message).isEqualTo(INVALID_NUMBERS_SIZE)
     }
 
-    @Test
-    fun `게임에 사용되는 숫자 셋에는 중복 없음을 검증한다`() {
+    @ParameterizedTest
+    @MethodSource("provideDuplicatedBaseballNumbers")
+    fun `게임에 사용되는 숫자 셋에는 중복이 있으면 예외를 던진다`(baseballNumbers: List<BaseballNumber>) {
         val exception = assertThrows<IllegalArgumentException> {
-            BaseballNumbers(
-                listOf(
-                    BaseballNumber(1), BaseballNumber(3), BaseballNumber(1)
-                )
-            )
+            BaseballNumbers(baseballNumbers)
         }
         assertThat(exception.message).isEqualTo(NUMBERS_DUPLICATED)
     }
 
-    @Test
-    fun `세자리 중복이 없는 숫자 셋의 경우 정상이다`() {
+    @ParameterizedTest
+    @MethodSource("provideNormalBaseballNumbers")
+    fun `세자리 중복이 없는 숫자 셋의 경우 정상이다`(baseballNumbers: List<BaseballNumber>) {
         assertDoesNotThrow {
-            BaseballNumbers(
-                listOf(
-                    BaseballNumber(1), BaseballNumber(2), BaseballNumber(3),
-                )
-            )
+            BaseballNumbers(baseballNumbers)
         }
+    }
+
+    companion object {
+        @JvmStatic
+        fun provideInvalidCountBaseballNumbers(): Stream<List<BaseballNumber>> = Stream.of(
+            listOf(),
+            listOf(BaseballNumber(1)),
+            listOf(BaseballNumber(1), BaseballNumber(2)),
+            listOf(BaseballNumber(1), BaseballNumber(2), BaseballNumber(3), BaseballNumber(4)),
+        )
+
+        @JvmStatic
+        fun provideDuplicatedBaseballNumbers(): Stream<List<BaseballNumber>> = Stream.of(
+            listOf(BaseballNumber(1), BaseballNumber(1), BaseballNumber(1)),
+            listOf(BaseballNumber(1), BaseballNumber(3), BaseballNumber(3)),
+            listOf(BaseballNumber(9), BaseballNumber(3), BaseballNumber(9)),
+        )
+
+        @JvmStatic
+        fun provideNormalBaseballNumbers(): Stream<List<BaseballNumber>> = Stream.of(
+            listOf(BaseballNumber(1), BaseballNumber(2), BaseballNumber(3)),
+            listOf(BaseballNumber(1), BaseballNumber(3), BaseballNumber(7)),
+            listOf(BaseballNumber(9), BaseballNumber(3), BaseballNumber(1)),
+        )
     }
 
 }
