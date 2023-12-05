412f81fa20c4a35a7f8b919eb5b29aada1474b35
junpakPark <112045553+junpakPark@users.noreply.github.com>
2023-12-05 23:52:05 +0900

    feat: 게임을 종료한 후 게임을 다시 시작하거나 완전히 종료할 수 있다.

--- src/main/kotlin/baseball/controller/Controller.kt
+++ src/main/kotlin/baseball/controller/Controller.kt
@@ -6,15 +6,25 @@ import baseball.model.Score
 import baseball.view.InputView
 import baseball.view.OutputView
 
+private const val START_CODE = 1
+private const val END_CODE = 2
+
 class Controller(
         private val inputView: InputView,
         private val outputView: OutputView
 ) {
 
     fun process() {
+        do {
+            playingInning()
+        } while (shouldContinueGame())
+    }
+
+    private fun playingInning() {
         outputView.printStartMessage()
         val computerNumber = PlayingNumber.pitchBall()
         val matchUp = MatchUp(computerNumber)
+
         while (true) {
             val userNumber = inputView.readNumbers()
             val score = matchUp.play(userNumber)
@@ -35,4 +45,13 @@ class Controller(
         outputView.printResult(ball = score.balls, strike = score.strikes)
     }
 
+    private fun shouldContinueGame(): Boolean {
+        val restartCode = inputView.readRestart()
+
+        return when (restartCode) {
+            START_CODE -> true
+            END_CODE -> false
+            else -> throw IllegalArgumentException("$START_CODE 또는 ${END_CODE}만 입력해주세요")
+        }
+    }
 }
