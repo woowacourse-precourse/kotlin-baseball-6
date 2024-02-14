a0ee3f0b742b61f674924202db21ed26920c388a
KJBig <kjgi73k@gmail.com>
2024-02-14 21:11:21 +0900

    feat : 게임 시작 및 랜덤 숫자 생성
    - 게임 시작 메시지 출력.
    - 지정된 길이의 랜덤 숫자를 생성.

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -1,5 +1,18 @@
 package baseball
 
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
-    TODO("프로그램 구현")
+
+    println("숫자 야구 게임을 시작합니다.")
+    val numberSize = 3
+
+    val computer = mutableListOf<Int>()
+    while (computer.size < numberSize) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (!computer.contains(randomNumber)) {
+            computer.add(randomNumber)
+        }
+    }
+
 }
