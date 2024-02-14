e8ea538e4178698fe49831ceb9326881dfc33351
KJBig <kjgi73k@gmail.com>
2024-02-14 21:24:09 +0900

    refactor : 메소드 1차 분리
    - 메인 메소드 안에 있는 기능들을 1차 분리

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -4,66 +4,85 @@ import camp.nextstep.edu.missionutils.Console
 import camp.nextstep.edu.missionutils.Randoms
 
 fun main() {
+    val numberSize = 3
     while (true) {
-        println("숫자 야구 게임을 시작합니다.")
-        val numberSize = 3
-        val computer = mutableListOf<Int>()
-        while (computer.size < numberSize) {
-            val randomNumber = Randoms.pickNumberInRange(1, 9)
-            if (!computer.contains(randomNumber)) {
-                computer.add(randomNumber)
+        printGameStartMessage()
+
+        val computer = getComputerNumbers(numberSize)
+
+        startGameProcess(numberSize, computer)
+
+        printGameEndMessage(numberSize)
+
+        if (!isReStart()) break
+    }
+}
+
+private fun printGameStartMessage() {
+    println("숫자 야구 게임을 시작합니다.")
+}
+
+private fun printGameEndMessage(numberSize: Int) {
+    println("${numberSize}개의 숫자를 모두 맞히셨습니다! 게임 종료")
+    println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+}
+
+private fun isReStart(): Boolean {
+    return Console.readLine().toInt() == 1
+}
+
+private fun startGameProcess(numberSize: Int, computer: MutableList<Int>) {
+    while (true) {
+        var ballCount = 0
+        var strikeCount = 0
+        print("숫자를 입력해주세요 : ")
+        val inputNumbers = Console.readLine()
+
+        if (inputNumbers.length != numberSize) {
+            throw IllegalArgumentException("숫자는 ${numberSize}자리를 입력해주세요")
+        }
+
+        for (i in 0 until numberSize) {
+            if (computer[i] == inputNumbers[i].toString().toInt()) {
+                strikeCount++
             }
         }
 
-        while (true) {
-            var ballCount = 0
-            var strikeCount = 0
-            print("숫자를 입력해주세요 : ")
-            val inputNumbers = Console.readLine()
-
-            if (inputNumbers.length != numberSize) {
-                throw IllegalArgumentException("숫자는 ${numberSize}자리를 입력해주세요")
-            }
-
-            for (i in 0 until numberSize) {
-                if (computer[i] == inputNumbers[i].toString().toInt()) {
-                    strikeCount++
+        for (i in 0 until numberSize) {
+            if (computer.contains(inputNumbers[i].toString().toInt())) {
+                if (computer[i] != inputNumbers[i].toString().toInt()) {
+                    ballCount++
                 }
-            }
-
-            for (i in 0 until numberSize) {
-                if (computer.contains(inputNumbers[i].toString().toInt())) {
-                    if (computer[i] != inputNumbers[i].toString().toInt()) {
-                        ballCount++
-                    }
-                }
-            }
-
-            if (ballCount > 0) {
-                print("${ballCount}볼 ")
-            }
-
-            if (strikeCount > 0) {
-                print("${strikeCount}스트라이크 ")
-            }
-
-            if (ballCount == 0 && strikeCount == 0) {
-                print("낫싱")
-            }
-
-            println()
-
-            if (strikeCount == numberSize) {
-                break
             }
         }
 
-        println("${numberSize}개의 숫자를 모두 맞히셨습니다! 게임 종료")
-        println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
-        val restartValue = Console.readLine().toInt()
+        if (ballCount > 0) {
+            print("${ballCount}볼 ")
+        }
 
-        if (restartValue == 2) {
+        if (strikeCount > 0) {
+            print("${strikeCount}스트라이크 ")
+        }
+
+        if (ballCount == 0 && strikeCount == 0) {
+            print("낫싱")
+        }
+
+        println()
+
+        if (strikeCount == numberSize) {
             break
         }
     }
+}
+
+private fun getComputerNumbers(numberSize: Int): MutableList<Int> {
+    val computer = mutableListOf<Int>()
+    while (computer.size < numberSize) {
+        val randomNumber = Randoms.pickNumberInRange(1, 9)
+        if (!computer.contains(randomNumber)) {
+            computer.add(randomNumber)
+        }
+    }
+    return computer
 }
