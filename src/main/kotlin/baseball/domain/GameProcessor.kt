2f99fbd9d236351905c3e604bcaba211d85aa61f
KJBig <kjgi73k@gmail.com>
2024-02-18 20:47:34 +0900

    fix : Game의 결과를 종합적으로 판단하는 권한을 GameProcessor에게 위임.
    - judgeStrike(), judgeBall(), isGameOver()를 public으로 변경
    - judgeGameResult()를 Judge -> GameProcessor로 위임.

--- src/main/kotlin/baseball/domain/GameProcessor.kt
+++ src/main/kotlin/baseball/domain/GameProcessor.kt
@@ -1,19 +1,27 @@
 package baseball.domain
 
 import baseball.view.InputView
+import baseball.view.OutputView
 
 class GameProcessor {
 
     fun startGameProcess(numberSize: Int): Boolean {
         val computer = Computer(numberSize)
-        val judge = Judge()
 
         while (true) {
             val userNumbers = InputView.inputUserNumbers(numberSize)
-            val gameResult = judge.judgeGameResult(numberSize, computer.numbers, userNumbers)
+            val gameResult = judgeGameResult(numberSize, computer.numbers, userNumbers)
             if (gameResult) break
         }
 
         return InputView.isReStart()
+    }
+
+    private fun judgeGameResult(numberSize: Int, computerNumbers: MutableList<Int>, userNumbers: String): Boolean {
+        val judge = Judge()
+        val strikeCount = judge.judgeStrike(numberSize, computerNumbers, userNumbers)
+        val ballCount = judge.judgeBall(numberSize, computerNumbers, userNumbers)
+        OutputView.printJudgeResult(ballCount, strikeCount)
+        return judge.isGameOver(strikeCount, numberSize)
     }
 }
--- src/main/kotlin/baseball/domain/Judge.kt
+++ src/main/kotlin/baseball/domain/Judge.kt
@@ -1,17 +1,7 @@
 package baseball.domain
 
-import baseball.view.OutputView
-
 class Judge {
-
-    fun judgeGameResult(numberSize: Int, computerNumbers: MutableList<Int>, userNumbers: String): Boolean {
-        val strikeCount = judgeStrike(numberSize, computerNumbers, userNumbers)
-        val ballCount = judgeBall(numberSize, computerNumbers, userNumbers)
-        OutputView.printJudgeResult(ballCount, strikeCount)
-        return isGameOver(strikeCount, numberSize)
-    }
-
-    private fun judgeBall(numberSize: Int, computerNumbers: MutableList<Int>, userNumbers: String): Int {
+    fun judgeBall(numberSize: Int, computerNumbers: MutableList<Int>, userNumbers: String): Int {
         var ballCount = 0
         for (i in 0 until numberSize) {
             if (computerNumbers.contains(userNumbers[i].toString().toInt())) {
@@ -23,7 +13,7 @@ class Judge {
         return ballCount
     }
 
-    private fun judgeStrike(numberSize: Int, computerNumbers: MutableList<Int>, userNumbers: String): Int {
+    fun judgeStrike(numberSize: Int, computerNumbers: MutableList<Int>, userNumbers: String): Int {
         var strikeCount = 0
         for (i in 0 until numberSize) {
             if (computerNumbers[i] == userNumbers[i].toString().toInt()) {
@@ -33,8 +23,7 @@ class Judge {
         return strikeCount
     }
 
-    private fun isGameOver(strikeCount: Int, numberSize: Int): Boolean {
+    fun isGameOver(strikeCount: Int, numberSize: Int): Boolean {
         return strikeCount == numberSize
     }
-
 }
