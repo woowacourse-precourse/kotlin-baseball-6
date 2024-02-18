1a843b2163089160da172d166a84a7c3feab4fe7
KJBig <kjgi73k@gmail.com>
2024-02-18 20:27:15 +0900

    feat : 숫자가 아닌 값 입력 시 예외처리 테스트
    - InputValidator.validateNumberFormat() 에 숫자 이외의 String 값이 제공되면 IllegalArgumentException이 발생하는지 테스트

---
+++ src/test/kotlin/baseball/InputValidatorTest.kt
@@ -0,0 +1,24 @@
+package baseball
+
+import baseball.validate.InputValidator
+import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.*
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import java.lang.IllegalArgumentException
+
+class InputValidatorTest {
+
+    @Test
+    @DisplayName("숫자 이외의 값을 입력하면 예외가 발생한다.")
+    fun inputNotNumberTest() {
+        // when
+        assertThat(
+            assertThrows<IllegalArgumentException>
+            { InputValidator.validateNumberFormat("1e3") }
+        ).isInstanceOf(IllegalArgumentException::class.java)
+
+    }
+
+}
